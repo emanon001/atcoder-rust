20192ba89357811646946494f21e6aed3ff77b48
emanon001 <emanon001@gmail.com>
2020-03-12 20:18:39 +0900

    solve abc158/c

--- abc158/src/bin/c.rs
+++ abc158/src/bin/c.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: u32,
+    b: u32
   };
+
+  for n in 1..=1000 {
+    if n * 8 / 100 == a && n * 10 / 100 == b {
+      println!("{}", n);
+      std::process::exit(0);
+    }
+  }
+  println!("-1");
 }
