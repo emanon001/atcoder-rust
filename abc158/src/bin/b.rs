109614e0909cb43d94a178c000169ef5615b7457
emanon001 <emanon001@gmail.com>
2020-03-12 19:28:14 +0900

    solve abc158/b

--- abc158/src/bin/b.rs
+++ abc158/src/bin/b.rs
@@ -2,5 +2,13 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: u64,
+    a: u64,
+    b: u64
   };
+  let ab = a + b;
+  let c = n / ab;
+  let rest = n % ab;
+  let ans = c * a + std::cmp::min(rest, a);
+  println!("{}", ans);
 }
