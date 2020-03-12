3fc96e04ff796bf9f80b28c17105889fcc268c9b
emanon001 <emanon001@gmail.com>
2020-03-12 20:18:43 +0900

    solve abc158/d

--- abc158/src/bin/d.rs
+++ abc158/src/bin/d.rs
@@ -1,6 +1,42 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars,
+    q: i32
   };
+
+  let mut deque = s.into_iter().collect::<std::collections::VecDeque<char>>();
+  let mut reversed = false;
+  for _ in 0..q {
+    input! { t: i32 }
+    if t == 1 {
+      reversed = !reversed;
+    } else {
+      input! {
+        f: i32,
+        c: char
+      };
+      if f == 1 {
+        if !reversed {
+          deque.push_front(c);
+        } else {
+          deque.push_back(c);
+        }
+      } else {
+        if !reversed {
+          deque.push_back(c);
+        } else {
+          deque.push_front(c);
+        }
+      }
+    };
+  }
+  let ans = if reversed {
+    deque.into_iter().rev().collect::<String>()
+  } else {
+    deque.into_iter().collect::<String>()
+  };
+  println!("{}", ans);
 }
