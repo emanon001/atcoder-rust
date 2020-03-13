6a11a3285a85b83a0af7eac6a44aa4afb1edcb8b
emanon001 <emanon001@gmail.com>
2020-03-13 17:02:31 +0900

    solve abc064/a

--- abc064/src/bin/a.rs
+++ abc064/src/bin/a.rs
@@ -2,5 +2,10 @@ use proconio::input;
 
 fn main() {
   input! {
+    r: usize, g: usize, b: usize
   };
+
+  let n = r * 100 + g * 10 + b;
+  let ans = if n % 4 == 0 { "YES" } else { "NO" };
+  println!("{}", ans);
 }
