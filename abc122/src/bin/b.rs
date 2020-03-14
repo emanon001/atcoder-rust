c9a8059e4d23a3ad9fc33d69c3a031333a49409d
emanon001 <emanon001@gmail.com>
2020-03-15 03:18:43 +0900

    solve abc122/b

--- abc122/src/bin/b.rs
+++ abc122/src/bin/b.rs
@@ -1,6 +1,23 @@
 use proconio::input;
 
+fn is_acgt(s: &str) -> bool {
+  let acgt = vec!['A', 'C', 'G', 'T'];
+  s.chars().all(|x| acgt.contains(&x))
+}
+
 fn main() {
   input! {
+    s: String
   };
+
+  let mut res = 0;
+  for i in 0..s.len() {
+    for j in i..s.len() {
+      let len = j - i + 1;
+      if is_acgt(&s[i..=j]) && len > res {
+        res += 1;
+      }
+    }
+  }
+  println!("{}", res);
 }
