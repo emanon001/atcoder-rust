74b62dee788a43732a63553cea42e5ef440d8156
emanon001 <emanon001@gmail.com>
2020-03-15 05:15:24 +0900

    solve abc095/c

--- abc095/src/bin/c.rs
+++ abc095/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: i64, b: i64, c: i64, x: i64, y: i64
   };
+
+  let mut res = std::i64::MAX;
+  for cc in 0..=(std::cmp::max(x, y) * 2) {
+    let ac = std::cmp::max(x - cc / 2, 0);
+    let bc = std::cmp::max(y - cc / 2, 0);
+    let sum = a * ac + b * bc + c * cc;
+    if sum < res {
+      res = sum;
+    }
+  }
+  println!("{}", res);
 }
