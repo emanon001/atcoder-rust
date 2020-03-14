7aab6de4e89674127d82df4e5f1b3c48f4da9536
emanon001 <emanon001@gmail.com>
2020-03-15 03:36:16 +0900

    solve pakencamp-2019-day3/c

--- pakencamp-2019-day3/src/bin/c.rs
+++ pakencamp-2019-day3/src/bin/c.rs
@@ -2,5 +2,21 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize, m: usize,
+    scores: [[u64; m]; n]
   };
+
+  let mut res = 0_u64;
+  for t1 in 0..(m - 1) {
+    for t2 in (t1 + 1)..m {
+      let mut sum = 0_u64;
+      for s in 0..n {
+        sum += std::cmp::max(scores[s][t1], scores[s][t2]);
+      }
+      if sum > res {
+        res = sum;
+      }
+    }
+  }
+  println!("{}", res);
 }
