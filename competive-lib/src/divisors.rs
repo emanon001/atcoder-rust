8c037f09e7df97fd9070b9ac5a440d6dbbeceb79
emanon001 <emanon001@gmail.com>
2020-03-15 04:58:29 +0900

    add divisors

---
+++ competive-lib/src/divisors.rs
@@ -0,0 +1,34 @@
+pub fn divisors(n: u64) -> Vec<u64> {
+  let mut res = Vec::new();
+  let mut x = 1;
+  while x * x <= n {
+    if n % x == 0 {
+      res.push(x);
+      let y = n / x;
+      if y != x {
+        res.push(y);
+      }
+    }
+    x += 1;
+  }
+  res
+}
+
+#[cfg(test)]
+mod tests {
+  use super::divisors;
+
+  #[test]
+  fn test_devisors() {
+    assert!(divisors(0).is_empty());
+    assert_eq!(vec![1], sort(divisors(1)));
+    assert_eq!(vec![1, 2], sort(divisors(2)));
+    assert_eq!(vec![1, 2, 4], sort(divisors(4)));
+    assert_eq!(vec![1, 3, 5, 15], sort(divisors(15)));
+  }
+
+  fn sort(mut v: Vec<u64>) -> Vec<u64> {
+    v.sort();
+    v
+  }
+}
--- competive-lib/src/lib.rs
+++ competive-lib/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod divisors;
+
 #[cfg(test)]
 mod tests {
     #[test]
