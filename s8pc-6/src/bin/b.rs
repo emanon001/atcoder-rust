b8b3cdbefbb53b260194a945abafbfb16eeceb62
emanon001 <emanon001@gmail.com>
2020-03-15 14:44:40 +0900

    solve s8pc-6/b

--- s8pc-6/src/bin/b.rs
+++ s8pc-6/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    ab_list: [(i64, i64); n]
   };
+
+  let mut a_list: Vec<i64> = ab_list.iter().copied().map(|(a, _)| a).collect();
+  let mut b_list: Vec<i64> = ab_list.iter().copied().map(|(_, b)| b).collect();
+  a_list.sort();
+  b_list.sort();
+  let start = a_list[n / 2];
+  let goal = b_list[n / 2];
+  let mut res = 0_i64;
+  for (a, b) in ab_list {
+    res += (start - a).abs() + (a - b).abs() + (b - goal).abs();
+  }
+  println!("{}", res);
 }
