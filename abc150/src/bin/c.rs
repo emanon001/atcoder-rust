a60dc3cf098f62af60f5fc6c826124e0828a999a
emanon001 <emanon001@gmail.com>
2020-03-17 05:08:26 +0900

    solve abc150/c

--- abc150/src/bin/c.rs
+++ abc150/src/bin/c.rs
@@ -1,6 +1,17 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    ps: [usize; n],
+    qs: [usize; n]
   };
+
+  let mut vec = (1..=n).permutations(n).collect::<Vec<Vec<usize>>>();
+  vec.sort();
+  let pi = vec.iter().position(|v| v == &ps).unwrap();
+  let qi = vec.iter().position(|v| v == &qs).unwrap();
+  let res = (pi as isize - qi as isize).abs();
+  println!("{}", res);
 }
