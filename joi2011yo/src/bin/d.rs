88a6ce8591be7b2072ed8f59af2d52f8dc79db84
emanon001 <emanon001@gmail.com>
2020-03-20 03:09:37 +0900

    solve joi2011yo/d

--- joi2011yo/src/bin/d.rs
+++ joi2011yo/src/bin/d.rs
@@ -2,5 +2,22 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    ns: [usize; n]
   };
+
+  let mut dp = vec![vec![0_u64; 21]; n];
+  dp[0][0] = 1;
+  for i in 0..(n - 1) {
+    let n = ns[i];
+    for j in 0..=20 {
+      if j + n <= 20 {
+        dp[i + 1][j + n] += dp[i][j];
+      }
+      if i > 0 && j >= n {
+        dp[i + 1][j - n] += dp[i][j];
+      }
+    }
+  }
+  println!("{}", dp[n - 1][ns[n - 1]]);
 }
