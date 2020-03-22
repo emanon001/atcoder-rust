d01f6d249f5b5fd0361a88a7e77909ddb8e9bc1d
emanon001 <emanon001@gmail.com>
2020-03-23 03:46:54 +0900

    solve dwacon5th-prelims/a

--- dwacon5th-prelims/src/bin/a.rs
+++ dwacon5th-prelims/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    al: [f32; n]
   };
+
+  let sum = al.iter().fold(0_f32, |acc, a| acc + a);
+  let avg = sum / (n as f32);
+  let mut absl = al
+    .iter()
+    .enumerate()
+    .map(|(i, &a)| ((a - avg).abs(), i))
+    .collect::<Vec<_>>();
+  absl.sort_by(|a, b| a.partial_cmp(b).unwrap());
+  println!("{}", absl[0].1);
 }
