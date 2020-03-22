2966233d325b39bffa80c57c8973b0c9acbbec9b
emanon001 <emanon001@gmail.com>
2020-03-23 03:53:19 +0900

    solve tenka1-2018-beginner/a

--- tenka1-2018-beginner/src/bin/a.rs
+++ tenka1-2018-beginner/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::input;
 
 fn main() {
   input! {
+    s: String
   };
+
+  if s.len() == 2 {
+    println!("{}", s);
+  } else {
+    println!("{}", s.chars().rev().collect::<String>());
+  }
 }
