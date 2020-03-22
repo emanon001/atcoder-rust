40dd252d83ad6485ef07603e8f74dac66f1c1e09
emanon001 <emanon001@gmail.com>
2020-03-23 04:00:10 +0900

    solve tenka1-2018-beginner/b

--- tenka1-2018-beginner/src/bin/b.rs
+++ tenka1-2018-beginner/src/bin/b.rs
@@ -2,5 +2,27 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: u64, b: u64, k: usize
   };
+
+  let mut a = a;
+  let mut b = b;
+  let mut t = true;
+  for _ in 0..k {
+    if t {
+      if a % 2 == 1 {
+        a -= 1;
+      }
+      a /= 2;
+      b += a;
+    } else {
+      if b % 2 == 1 {
+        b -= 1;
+      }
+      b /= 2;
+      a += b;
+    }
+    t = !t;
+  }
+  println!("{} {}", a, b);
 }
