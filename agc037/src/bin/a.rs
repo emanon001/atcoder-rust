276cc9d46d4dc01bf3a75dc12daf02cf92c4e7e7
emanon001 <emanon001@gmail.com>
2020-03-23 03:18:30 +0900

    solve agc037/a

--- agc037/src/bin/a.rs
+++ agc037/src/bin/a.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
   input! {
+    s: String
   };
+
+  let mut res = 0;
+  let mut prev = Vec::new();
+  let mut cur = Vec::new();
+  for ch in s.chars() {
+    cur.push(ch);
+    if cur != prev {
+      res += 1;
+      prev = cur;
+      cur = Vec::new();
+    }
+  }
+  println!("{}", res);
 }
