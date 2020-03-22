78df6611a7a0aab056d7731eaa2b34ebeefe3810
emanon001 <emanon001@gmail.com>
2020-03-23 03:22:54 +0900

    solve diverta2019-2/a

--- diverta2019-2/src/bin/a.rs
+++ diverta2019-2/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize, k: usize
   };
+  if k == 1 {
+    println!("0");
+    std::process::exit(0);
+  }
+  let res = n - k;
+  println!("{}", res);
 }
