96174185bdc6c45b7968b19075941a9cec1d7e1a
emanon001 <emanon001@gmail.com>
2020-03-23 06:40:03 +0900

    solve agc004/a

--- agc004/src/bin/a.rs
+++ agc004/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: u64, b: u64, c: u64
   };
+
+  let res = vec![a * c * (b % 2), b * c * (a % 2), a * b * (c % 2)]
+    .into_iter()
+    .min()
+    .unwrap();
+  println!("{}", res);
 }
