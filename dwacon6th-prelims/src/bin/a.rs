b9069269c0c1545699a4ee906f50bf7d9b71ef57
emanon001 <emanon001@gmail.com>
2020-03-23 02:28:03 +0900

    solve dwacon6th-prelims/a

--- dwacon6th-prelims/src/bin/a.rs
+++ dwacon6th-prelims/src/bin/a.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    stl: [(String, usize); n],
+    x: String
   };
+
+  let mut res = 0;
+  let mut sleeping = false;
+  for (s, t) in stl {
+    if sleeping {
+      res += t;
+    } else if s == x {
+      sleeping = true;
+    }
+  }
+  println!("{}", res);
 }
