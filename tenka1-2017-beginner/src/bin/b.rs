669c604c4a62bd3b03da1d74d666c7e12f2a0b84
emanon001 <emanon001@gmail.com>
2020-03-23 06:08:08 +0900

    solve tenka1-2017-beginner/b

--- tenka1-2017-beginner/src/bin/b.rs
+++ tenka1-2017-beginner/src/bin/b.rs
@@ -2,5 +2,13 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    abl: [(usize, usize); n]
   };
+
+  let mut abl = abl;
+  abl.sort_by_key(|&(a, _)| a);
+  let (a, b) = abl.last().unwrap();
+  let res = a + b;
+  println!("{}", res);
 }
