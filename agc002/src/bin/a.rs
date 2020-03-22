1a78e3b0df0e9e6294458e153d3832084785dd79
emanon001 <emanon001@gmail.com>
2020-03-23 07:35:48 +0900

    solve agc002/a

--- agc002/src/bin/a.rs
+++ agc002/src/bin/a.rs
@@ -2,5 +2,18 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: i64, b: i64
   };
+  if a <= 0 && b >= 0 {
+    println!("Zero");
+  } else if a > 0 {
+    println!("Positive");
+  } else {
+    let minus_count = (a - b) + 1;
+    if minus_count % 2 == 0 {
+      println!("Positive");
+    } else {
+      println!("Negative");
+    }
+  }
 }
