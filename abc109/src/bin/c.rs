3ae502a893dc508c76f984b16e8d7612e10e4a4e
emanon001 <emanon001@gmail.com>
2020-03-22 12:15:48 +0900

    solve abc109/c

--- abc109/src/bin/c.rs
+++ abc109/src/bin/c.rs
@@ -1,6 +1,14 @@
+use num::*;
 use proconio::input;
 
 fn main() {
   input! {
+    n: usize, x: i32,
+    xs: [i32; n]
   };
+
+  let distances = xs.into_iter().map(|a| abs(a - x)).collect::<Vec<_>>();
+  let first = distances[0];
+  let res = distances.into_iter().fold(first, |acc, x| acc.gcd(&x));
+  println!("{}", res);
 }
