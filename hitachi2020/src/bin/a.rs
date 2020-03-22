cd743061e21165a65b0ee52f7e0b11d126eeccc8
emanon001 <emanon001@gmail.com>
2020-03-23 02:15:04 +0900

    solve hitachi2020/a

--- hitachi2020/src/bin/a.rs
+++ hitachi2020/src/bin/a.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
   input! {
+    s: String
   };
+
+  if s.len() % 2 == 1 {
+    println!("No");
+    std::process::exit(0);
+  }
+
+  for i in 0..(s.len() / 2) {
+    if &s[(i * 2)..=(i * 2 + 1)] != "hi" {
+      println!("No");
+      std::process::exit(0);
+    }
+  }
+  println!("Yes");
 }
