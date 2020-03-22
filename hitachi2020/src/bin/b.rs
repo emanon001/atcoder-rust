425f7fafa77467a0c7f9bf6aa626d389727760f9
emanon001 <emanon001@gmail.com>
2020-03-23 02:22:07 +0900

    solve hitachi2020/b

--- hitachi2020/src/bin/b.rs
+++ hitachi2020/src/bin/b.rs
@@ -1,6 +1,25 @@
 use proconio::input;
+use proconio::marker::Usize1;
 
 fn main() {
   input! {
+    a: usize, b: usize, m: usize,
+    al: [usize; a],
+    bl: [usize; b],
+    xycl: [(Usize1, Usize1, usize); m]
   };
+
+  let mut sorted_al = al.clone();
+  sorted_al.sort();
+  let mut sorted_bl = bl.clone();
+  sorted_bl.sort();
+
+  let mut res = sorted_al[0] + sorted_bl[0];
+  for (x, y, c) in xycl {
+    let total = al[x] + bl[y] - c;
+    if total < res {
+      res = total;
+    }
+  }
+  println!("{}", res);
 }
