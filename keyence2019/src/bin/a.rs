6fec85fe88e882660f16ac660f9571d7c432fe8f
emanon001 <emanon001@gmail.com>
2020-03-23 03:27:28 +0900

    solve keyence2019/a

--- keyence2019/src/bin/a.rs
+++ keyence2019/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    n1: usize, n2: usize, n3: usize, n4: usize
   };
+  let mut set = std::collections::HashSet::new();
+  set.insert(n1);
+  set.insert(n2);
+  set.insert(n3);
+  set.insert(n4);
+  let res = if set.contains(&1) && set.contains(&9) && set.contains(&7) && set.contains(&4) {
+    "YES"
+  } else {
+    "NO"
+  };
+  println!("{}", res);
 }
