fae167134464115a1b30683cd390b6bfebf557ca
emanon001 <emanon001@gmail.com>
2020-03-23 06:27:45 +0900

    solve code-festival-2016-quala/b

--- code-festival-2016-quala/src/bin/b.rs
+++ code-festival-2016-quala/src/bin/b.rs
@@ -1,6 +1,17 @@
 use proconio::input;
+use proconio::marker::Usize1;
 
 fn main() {
   input! {
+    n: usize,
+    al: [Usize1; n]
   };
+
+  let mut res = 0;
+  for i in 0..n {
+    if i == al[al[i]] {
+      res += 1;
+    }
+  }
+  println!("{}", res / 2);
 }
