b8bb05acd930b527912c2dfb93c129602ef1b35f
emanon001 <emanon001@gmail.com>
2020-03-23 04:14:23 +0900

    solve soundhound2018-summer-qual/b

--- soundhound2018-summer-qual/src/bin/b.rs
+++ soundhound2018-summer-qual/src/bin/b.rs
@@ -1,6 +1,18 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars,
+    w: usize
   };
+
+  let mut res = Vec::new();
+  let mut i = 0;
+  let len = s.len();
+  while i < len {
+    res.push(s[i]);
+    i += w;
+  }
+  println!("{}", res.into_iter().collect::<String>());
 }
