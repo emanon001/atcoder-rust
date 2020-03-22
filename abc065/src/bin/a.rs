6f03ceb2b78b5f03ea4ba8cd54516e49b3b5d4b5
emanon001 <emanon001@gmail.com>
2020-03-23 06:20:19 +0900

    solve abc065/a

--- abc065/src/bin/a.rs
+++ abc065/src/bin/a.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
   input! {
+    x: i64, a: i64, b: i64
   };
+
+  let res = if a >= b {
+    "delicious"
+  } else if b - a <= x {
+    "safe"
+  } else {
+    "dangerous"
+  };
+  println!("{}", res);
 }
