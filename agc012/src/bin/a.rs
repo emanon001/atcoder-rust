b6d8dea273ef8e42c59361ce3c6af3c9ff2d6e89
emanon001 <emanon001@gmail.com>
2020-03-23 07:01:01 +0900

    solve agc012/a

--- agc012/src/bin/a.rs
+++ agc012/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    al: [u64; 3 * n]
   };
+
+  let mut al = al;
+  al.sort_by_key(|&a| -(a as i64));
+  let res = al.into_iter().skip(1).step_by(2).take(n).sum::<u64>();
+  println!("{}", res);
 }
