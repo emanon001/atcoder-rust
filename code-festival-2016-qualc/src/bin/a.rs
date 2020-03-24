58a8fa44db2851fd751ca56ecb950b3e1e8324e9
emanon001 <emanon001@gmail.com>
2020-03-24 10:05:23 +0900

    solve code-festival-2016-qualc/a

--- code-festival-2016-qualc/src/bin/a.rs
+++ code-festival-2016-qualc/src/bin/a.rs
@@ -2,5 +2,14 @@ use proconio::input;
 
 fn main() {
   input! {
+    s: String
   };
+
+  let i = s.find('C').unwrap_or(s.len());
+  let j = s.rfind('F').unwrap_or(0);
+  if i < j {
+    println!("Yes");
+  } else {
+    println!("No");
+  }
 }
