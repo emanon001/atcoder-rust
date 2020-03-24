dc6daa02210588f7fdf27990792d0c4bf2053b93
emanon001 <emanon001@gmail.com>
2020-03-24 13:08:30 +0900

    solve agc016/a

--- agc016/src/bin/a.rs
+++ agc016/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::input;
 
 fn main() {
   input! {
+    s: String
   };
+
+  let mut res = 1 << 30;
+  for ch in "abcdefghijklmnopqrstuvwxyz".chars() {
+    if let Some(max) = s.split(ch).map(|w| w.len()).max() {
+      if max < res {
+        res = max;
+      }
+    }
+  }
+  println!("{}", res);
 }
