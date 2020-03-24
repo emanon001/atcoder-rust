8d6b4ff5e3dd03954b59cf2fa6a1e8d7777893b8
emanon001 <emanon001@gmail.com>
2020-03-24 09:53:24 +0900

    solve code-festival-2016-qualb/b

--- code-festival-2016-qualb/src/bin/b.rs
+++ code-festival-2016-qualb/src/bin/b.rs
@@ -1,6 +1,37 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    _: usize, a: usize, b: usize,
+    s: Chars
   };
+
+  let mut b_count = 0;
+  let mut q_count = 0;
+  for ch in s {
+    match ch {
+      'a' => {
+        if q_count < a + b {
+          q_count += 1;
+          println!("Yes");
+        } else {
+          println!("No");
+        }
+      }
+      'b' => {
+        if q_count < a + b && b_count < b {
+          b_count += 1;
+          q_count += 1;
+          println!("Yes");
+        } else {
+          println!("No");
+        }
+      }
+      'c' => {
+        println!("No");
+      }
+      _ => {}
+    }
+  }
 }
