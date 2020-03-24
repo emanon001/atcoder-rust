2101a886e6b9782a0d98342d0e99ec8dc1f10b36
emanon001 <emanon001@gmail.com>
2020-03-24 11:01:58 +0900

    solve agc015/a

--- agc015/src/bin/a.rs
+++ agc015/src/bin/a.rs
@@ -2,5 +2,28 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: u64, a: u64, b: u64
   };
+
+  if a > b {
+    println!("0");
+    std::process::exit(0);
+  }
+  if a == b {
+    println!("1");
+    std::process::exit(0);
+  }
+  if n < 2 {
+    println!("0");
+    std::process::exit(0);
+  }
+  if n == 2 {
+    println!("1");
+    std::process::exit(0);
+  }
+
+  let m = n - 2;
+  let c = b - a;
+  let res = m * c + 1;
+  println!("{}", res);
 }
