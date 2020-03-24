b72da06f4dfc9d67728f8476fd735b03a00a0f96
emanon001 <emanon001@gmail.com>
2020-03-24 12:27:18 +0900

    solve agc006/a

--- agc006/src/bin/a.rs
+++ agc006/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    s: String,
+    t: String
   };
+
+  let mut c = 0;
+  for i in 1..=n {
+    if s[(n - i)..] == t[..i] {
+      c = i;
+    }
+  }
+  let res = n * 2 - c;
+  println!("{}", res);
 }
