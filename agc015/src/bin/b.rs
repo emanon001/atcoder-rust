3921424ec619d4f4b95e4c4000808ed973dad1e9
emanon001 <emanon001@gmail.com>
2020-03-24 13:19:08 +0900

    solve agc015/b

--- agc015/src/bin/b.rs
+++ agc015/src/bin/b.rs
@@ -1,6 +1,22 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars
   };
+
+  let n = s.len() as u64;
+  let mut res = 0_u64;
+  for (i, ch) in s.into_iter().enumerate() {
+    let i = i as u64;
+    if ch == 'U' {
+      res += i * 2;
+      res += n - i - 1;
+    } else {
+      res += i;
+      res += (n - i - 1) * 2;
+    }
+  }
+  println!("{}", res);
 }
