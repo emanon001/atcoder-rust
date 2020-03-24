d6bf919047d4910d641107f775b85b7f9a1dfdf6
emanon001 <emanon001@gmail.com>
2020-03-24 13:34:32 +0900

    solve cf17-final/b

--- cf17-final/src/bin/b.rs
+++ cf17-final/src/bin/b.rs
@@ -1,6 +1,24 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars
   };
+
+  let n = s.len();
+  let mut table = std::collections::HashMap::new();
+  for ch in s {
+    let x = table.entry(ch).or_insert(0);
+    *x += 1;
+  }
+  if table.len() == 1 {
+    println!("{}", if n == 1 { "YES" } else { "NO" });
+  } else if table.len() == 2 {
+    println!("{}", if n == 2 { "YES" } else { "NO" });
+  } else {
+    let &min = table.values().min().unwrap();
+    let &max = table.values().max().unwrap();
+    println!("{}", if min + 1 >= max { "YES" } else { "NO" });
+  }
 }
