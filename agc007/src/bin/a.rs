5b069be32638ea91195515454e7f5431bfcd511d
emanon001 <emanon001@gmail.com>
2020-03-25 16:40:53 +0900

    solve agc007/a

--- agc007/src/bin/a.rs
+++ agc007/src/bin/a.rs
@@ -1,6 +1,25 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    h: usize, w: usize,
+    grid: [Chars; h]
   };
+
+  let mut count = 0;
+  for i in 0..h {
+    for j in 0..w {
+      if grid[i][j] == '#' {
+        count += 1;
+      }
+    }
+  }
+
+  let res = if count == h + w - 1 {
+    "Possible"
+  } else {
+    "Impossible"
+  };
+  println!("{}", res);
 }
