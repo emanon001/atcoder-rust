19f7fe222f6222fb321bd572f6671afe075f4dfc
emanon001 <emanon001@gmail.com>
2020-03-25 10:40:48 +0900

    solve agc028/a

--- agc028/src/bin/a.rs
+++ agc028/src/bin/a.rs
@@ -1,6 +1,27 @@
+use num::*;
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    n: usize, m: usize,
+    s: Chars,
+    t: Chars,
   };
+
+  let lcm = n.lcm(&m);
+  let gcd = n.gcd(&m);
+  let a = n / gcd;
+  let b = m / gcd;
+  let mut i = 0;
+  let mut j = 0;
+  while i < n && j < m {
+    if s[i] != t[j] {
+      println!("-1");
+      std::process::exit(0);
+    }
+    i += a;
+    j += b;
+  }
+  println!("{}", lcm);
 }
