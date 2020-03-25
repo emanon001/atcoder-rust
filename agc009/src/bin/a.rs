61e400dbcfb274c7396f03cccafac0e5d24f98f5
emanon001 <emanon001@gmail.com>
2020-03-25 10:58:27 +0900

    solve agc009/a

--- agc009/src/bin/a.rs
+++ agc009/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    ab_l: [(u64, u64); n]
   };
+
+  let mut res = 0_u64;
+  for (a, b) in ab_l.into_iter().rev() {
+    let x = (a + res) % b;
+    if x > 0 {
+      res += b - x;
+    }
+  }
+  println!("{}", res);
 }
