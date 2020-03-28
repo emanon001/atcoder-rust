52d7ac0bf2474eba1d2d5db96db3724fa6e2de3d
emanon001 <emanon001@gmail.com>
2020-03-28 16:45:00 +0900

    solve abc042/a

--- abc042/src/bin/a.rs
+++ abc042/src/bin/a.rs
@@ -2,5 +2,14 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: usize, b: usize, c: usize
   };
+  let count5 = vec![a, b, c].into_iter().filter(|&x| x == 5).count();
+  let count7 = vec![a, b, c].into_iter().filter(|&x| x == 7).count();
+  let res = if count5 == 2 && count7 == 1 {
+    "YES"
+  } else {
+    "NO"
+  };
+  println!("{}", res);
 }
