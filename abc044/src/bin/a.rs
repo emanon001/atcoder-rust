de08b3aa446f56ae38763e29503deeebec73e487
emanon001 <emanon001@gmail.com>
2020-03-28 16:30:15 +0900

    solve abc044/a

--- abc044/src/bin/a.rs
+++ abc044/src/bin/a.rs
@@ -2,5 +2,16 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    k: usize,
+    x: usize,
+    y: usize,
   };
+
+  let d = std::cmp::min(n, k);
+  let mut res = d * x;
+  if n - d > 0 {
+    res += (n - d) * y;
+  }
+  println!("{}", res);
 }
