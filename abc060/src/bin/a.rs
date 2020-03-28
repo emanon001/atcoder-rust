e6fe99272c14505efb185ef4f39cd144fbffe778
emanon001 <emanon001@gmail.com>
2020-03-28 16:37:46 +0900

    solve abc060/a

--- abc060/src/bin/a.rs
+++ abc060/src/bin/a.rs
@@ -1,6 +1,17 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    a: Chars,
+    b: Chars,
+    c: Chars
   };
+
+  let res = if a[a.len() - 1] == b[0] && b[b.len() - 1] == c[0] {
+    "YES"
+  } else {
+    "NO"
+  };
+  println!("{}", res);
 }
