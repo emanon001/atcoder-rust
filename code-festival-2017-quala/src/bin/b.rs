2ccab2a2fe2a5ea7449624280e1f03567ebe1277
emanon001 <emanon001@gmail.com>
2020-03-30 01:22:21 +0900

    solve code-festival-2017-quala/b

--- code-festival-2017-quala/src/bin/b.rs
+++ code-festival-2017-quala/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    h: usize, w: usize, k: usize
   };
+
+  for a in 0..=h {
+    for b in 0..=w {
+      let x = a * w + b * h - a * b * 2;
+      if x == k {
+        println!("Yes");
+        std::process::exit(0);
+      }
+    }
+  }
+  println!("No");
 }
