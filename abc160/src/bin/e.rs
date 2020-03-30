34aefd687366a2e512963e82c03c7a8c488b82c8
emanon001 <emanon001@gmail.com>
2020-03-31 08:02:00 +0900

    update abc160/e

--- abc160/src/bin/e.rs
+++ abc160/src/bin/e.rs
@@ -3,35 +3,16 @@ use proconio::input;
 fn main() {
   input! {
     x: usize, y: usize, a: usize, b: usize, c: usize,
-    p_l: [isize; a],
-    q_l: [isize; b],
-    r_l: [isize; c]
+    mut pv: [i64; a],
+    mut qv: [i64; b],
+    mut rv: [i64; c]
   };
 
-  let mut pq_que = std::collections::BinaryHeap::new();
-  let mut p_l = p_l;
-  p_l.sort_by_key(|&x| -x);
-  for &p in &p_l[..x] {
-    pq_que.push(std::cmp::Reverse(p));
-  }
-  let mut q_l = q_l;
-  q_l.sort_by_key(|&x| -x);
-  for &q in &q_l[..y] {
-    pq_que.push(std::cmp::Reverse(q));
-  }
-
-  let mut r_l = r_l;
-  r_l.sort_by_key(|&x| -x);
-  for r in r_l {
-    if let Some(&std::cmp::Reverse(x)) = pq_que.peek() {
-      if r > x {
-        pq_que.pop();
-        pq_que.push(std::cmp::Reverse(r));
-      }
-    }
-  }
-  let res = pq_que
-    .into_iter()
-    .fold(0_i64, |acc, std::cmp::Reverse(x)| acc + (x as i64));
+  pv.sort_by_key(|&x| -x);
+  qv.sort_by_key(|&x| -x);
+  rv.sort_by_key(|&x| -x);
+  let mut v = [&pv[0..x], &qv[0..y], &rv[..]].concat();
+  v.sort_by_key(|&x| -x);
+  let res: i64 = v[..(x + y)].iter().sum();
   println!("{}", res);
 }
