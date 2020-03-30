3b33cd03cb1a0adf6d847eaf5b1f19ceb87ff46d
emanon001 <emanon001@gmail.com>
2020-03-30 17:44:57 +0900

    update abc160/d

--- abc160/src/bin/d.rs
+++ abc160/src/bin/d.rs
@@ -1,27 +1,47 @@
 use proconio::input;
 
-pub type Graph = [Vec<usize>];
+pub struct Graph {
+  graph: Vec<Vec<usize>>,
+  v: usize,
+}
 
-pub fn shortest_path(
-  graph: &Graph,
-  start: usize,
-  res: &mut std::collections::HashMap<usize, usize>,
-) {
-  let mut visited = std::collections::HashSet::new();
-  let mut que = std::collections::VecDeque::new();
-  visited.insert(start);
-  que.push_back((start, 0));
-  while let Some((u, cost)) = que.pop_front() {
-    for &v in &graph[u] {
-      if visited.contains(&v) {
-        continue;
-      }
-      visited.insert(v);
-      let new_cost = cost + 1;
-      let x = res.entry(new_cost).or_insert(0);
-      *x += 1;
-      que.push_back((v, new_cost));
+impl Graph {
+  pub fn new(edges: &[(usize, usize)], v: usize) -> Self {
+    let mut graph = vec![Vec::new(); v];
+    for &(u, v) in edges {
+      graph[u].push(v);
+      graph[v].push(u);
     }
+    Self { graph, v }
+  }
+
+  pub fn new_directed(edges: &[(usize, usize)], v: usize) -> Self {
+    let mut graph = vec![Vec::new(); v];
+    for &(u, v) in edges {
+      graph[u].push(v);
+    }
+    Self { graph, v }
+  }
+
+  pub fn shortest_path(&self, start: usize) -> Vec<usize> {
+    let mut visited = std::collections::HashSet::new();
+    let mut d = vec![std::usize::MAX; self.v];
+    let mut que = std::collections::VecDeque::new();
+    d[start] = 0;
+    visited.insert(start);
+    que.push_back(start);
+    while let Some(u) = que.pop_front() {
+      for &v in &self.graph[u] {
+        if visited.contains(&v) {
+          continue;
+        }
+        visited.insert(v);
+        let new_cost = d[u] + 1;
+        d[v] = new_cost;
+        que.push_back(v);
+      }
+    }
+    d
   }
 }
 
@@ -30,21 +50,22 @@ fn main() {
     n: usize, x: usize, y: usize
   };
 
-  let mut graph = vec![Vec::new(); n];
+  let mut edges = Vec::new();
   for i in 0..(n - 1) {
-    graph[i].push(i + 1);
-    graph[i + 1].push(i);
+    edges.push((i, i + 1));
   }
-  graph[x - 1].push(y - 1);
-  graph[y - 1].push(x - 1);
+  edges.push((x - 1, y - 1));
 
-  // println!("{:?}", graph);
-  let mut res = std::collections::HashMap::new();
+  let graph = Graph::new(&edges, n);
+  let mut table = std::collections::HashMap::new();
   for i in 0..n {
-    shortest_path(&graph, i, &mut res);
+    for c in graph.shortest_path(i) {
+      let x = table.entry(c).or_insert(0);
+      *x += 1;
+    }
   }
   for i in 1..n {
-    let x = res.get(&i).unwrap_or(&0) / 2;
+    let x = table.get(&i).unwrap_or(&0) / 2;
     println!("{}", x);
   }
 }
