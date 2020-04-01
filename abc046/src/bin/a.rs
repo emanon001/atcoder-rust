dd2b9e753e2623226e96a61fd2d872ef4faae7ab
emanon001 <emanon001@gmail.com>
2020-04-01 21:50:41 +0900

    solve abc046/a

--- abc046/src/bin/a.rs
+++ abc046/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: usize, b: usize, c: usize
   };
+
+  let res = [a, b, c]
+    .iter()
+    .collect::<std::collections::HashSet<_>>()
+    .len();
+  println!("{}", res);
 }
