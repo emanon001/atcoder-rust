042526c9e89ba3eb15013c0c07decdea0fd235d8
emanon001 <emanon001@gmail.com>
2020-04-03 05:06:49 +0900

    solve agc001/a

--- agc001/src/bin/a.rs
+++ agc001/src/bin/a.rs
@@ -2,5 +2,14 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    mut lv: [usize; n * 2]
   };
+
+  lv.sort();
+  let mut res = 0;
+  for i in 0..n {
+    res += std::cmp::min(lv[i * 2], lv[i * 2 + 1]);
+  }
+  println!("{}", res);
 }
