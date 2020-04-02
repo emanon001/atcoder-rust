6bed0a9464e5669ea27275ac535d720dd3df978e
emanon001 <emanon001@gmail.com>
2020-04-03 04:37:43 +0900

    solve cf16-final/a

--- cf16-final/src/bin/a.rs
+++ cf16-final/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    h: usize, w: usize,
+    grid: [[String; w]; h]
   };
+
+  for i in 0..h {
+    for j in 0..w {
+      if grid[i][j] == "snuke" {
+        let ch: char = (0x41_u8 + j as u8).into();
+        println!("{}{}", ch, i + 1);
+        std::process::exit(0);
+      }
+    }
+  }
 }
