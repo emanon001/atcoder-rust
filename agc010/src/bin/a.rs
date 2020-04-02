0f83b5a0957d5f224536364154098418c32d199f
emanon001 <emanon001@gmail.com>
2020-04-03 04:04:22 +0900

    solve agc010/a

--- agc010/src/bin/a.rs
+++ agc010/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    av: [usize; n]
   };
+
+  let even_count = av.iter().filter(|&x| x % 2 == 0).count();
+  let odd_count = n - even_count;
+  let res = if odd_count % 2 == 0 { "YES" } else { "NO" };
+  println!("{}", res);
 }
