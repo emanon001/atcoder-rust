1167a66d2e0cd5b7154df5ec35c3b5efaa54dd21
emanon001 <emanon001@gmail.com>
2020-04-03 04:42:36 +0900

    solve code-festival-2016-qualb/a

--- code-festival-2016-qualb/src/bin/a.rs
+++ code-festival-2016-qualb/src/bin/a.rs
@@ -1,6 +1,17 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars
   };
+
+  let expected: Vec<char> = "CODEFESTIVAL2016".chars().collect();
+  let mut res = 0;
+  for i in 0..s.len() {
+    if s[i] != expected[i] {
+      res += 1;
+    }
+  }
+  println!("{}", res);
 }
