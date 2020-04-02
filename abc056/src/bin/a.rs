7c00d69648e73a241194c11afb00abc9205477ed
emanon001 <emanon001@gmail.com>
2020-04-02 23:54:43 +0900

    solve abc056/a

--- abc056/src/bin/a.rs
+++ abc056/src/bin/a.rs
@@ -2,5 +2,12 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: char, b: char
   };
+  let res = if (a == 'H' && b == 'H') || (a == 'D' && b == 'D') {
+    "H"
+  } else {
+    "D"
+  };
+  println!("{}", res);
 }
