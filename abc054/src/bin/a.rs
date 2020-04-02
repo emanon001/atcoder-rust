09685b575cd49f9696c147481618a474edd037c4
emanon001 <emanon001@gmail.com>
2020-04-03 00:02:38 +0900

    solve abc054/a

--- abc054/src/bin/a.rs
+++ abc054/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: String, b: String
   };
+
+  let a = i32::from_str_radix(&a, 10).unwrap();
+  let b = i32::from_str_radix(&b, 10).unwrap();
+  let res = if a == b {
+    "Draw"
+  } else if a == 1 || (b != 1 && a > b) {
+    "Alice"
+  } else {
+    "Bob"
+  };
+  println!("{}", res);
 }
