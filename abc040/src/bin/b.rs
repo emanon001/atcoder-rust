8bc1d775797b8202ac7201a5bb5ad55c1ebdb59d
emanon001 <emanon001@gmail.com>
2020-04-06 17:13:50 +0900

    solve abc040/b

--- abc040/src/bin/b.rs
+++ abc040/src/bin/b.rs
@@ -2,5 +2,18 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize
   };
+
+  let mut res = std::isize::MAX;
+  let mut x = 1_isize;
+  let n = n as isize;
+  while x * x <= n {
+    let sum = (x - n / x).abs() + n % x;
+    if sum < res {
+      res = sum;
+    }
+    x += 1;
+  }
+  println!("{}", res);
 }
