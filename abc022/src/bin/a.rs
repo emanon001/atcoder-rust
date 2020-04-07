6a463f3e6e1076b8106663017c5e56513038721b
emanon001 <emanon001@gmail.com>
2020-04-07 19:34:31 +0900

    solve abc022/a

--- abc022/src/bin/a.rs
+++ abc022/src/bin/a.rs
@@ -1,6 +1,23 @@
 use proconio::input;
 
+fn is_ok(w: isize, s: isize, t: isize) -> bool {
+  w >= s && w <= t
+}
+
 fn main() {
   input! {
+    n: usize, s: isize, t: isize,
+    w: isize,
+    av: [isize; n - 1]
   };
+
+  let mut weight = w;
+  let mut res = 0;
+  for a in vec![vec![0], av].concat() {
+    weight += a;
+    if is_ok(weight, s, t) {
+      res += 1;
+    }
+  }
+  println!("{}", res);
 }
