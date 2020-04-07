5109e1ccdd2dca183c83556c03a8a6f58326cbcc
emanon001 <emanon001@gmail.com>
2020-04-07 22:35:11 +0900

    solve abc024/b

--- abc024/src/bin/b.rs
+++ abc024/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize, t: usize,
+    av: [usize; n]
   };
+
+  let mut res = 0;
+  let mut cur = av[0];
+  for a in av
+    .into_iter()
+    .skip(1)
+    .chain(vec![1_000_000 * 2].into_iter())
+  {
+    res += std::cmp::min(a - cur, t);
+    cur = a;
+  }
+  println!("{}", res);
 }
