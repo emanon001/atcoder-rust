33057ff91f8d3374480f5751a3bbc538d3d17267
emanon001 <emanon001@gmail.com>
2020-04-07 19:41:01 +0900

    solve abc022/b

--- abc022/src/bin/b.rs
+++ abc022/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    av: [usize; n]
   };
+
+  let mut map = std::collections::HashMap::new();
+  for a in av {
+    let c = map.entry(a).or_insert(0_usize);
+    *c += 1;
+  }
+  let res: usize = map.values().into_iter().map(|c| c - 1).sum();
+  println!("{}", res);
 }
