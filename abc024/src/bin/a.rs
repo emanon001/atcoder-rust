ae16b10563857df389482fddc0c9df81e430e277
emanon001 <emanon001@gmail.com>
2020-04-07 22:24:07 +0900

    solve abc024/a

--- abc024/src/bin/a.rs
+++ abc024/src/bin/a.rs
@@ -2,5 +2,10 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: usize, b: usize, c: usize, k: usize,
+    s: usize, t: usize
   };
+
+  let res = a * s + b * t - (if s + t >= k { (s + t) * c } else { 0 });
+  println!("{}", res);
 }
