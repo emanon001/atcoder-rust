a2ef3a85b8878c8b610dc9cc76d292b7d4614e60
emanon001 <emanon001@gmail.com>
2020-04-08 17:06:12 +0900

    solve abc020/b

--- abc020/src/bin/b.rs
+++ abc020/src/bin/b.rs
@@ -2,5 +2,10 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: usize, b: usize
   };
+
+  let ab = a.to_string() + &b.to_string();
+  let res = i32::from_str_radix(&ab, 10).unwrap() * 2;
+  println!("{}", res);
 }
