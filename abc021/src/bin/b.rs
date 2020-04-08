93e9132b523206d2a6e285cfe388b09fcb114275
emanon001 <emanon001@gmail.com>
2020-04-08 14:10:48 +0900

    solve abc021/b

--- abc021/src/bin/b.rs
+++ abc021/src/bin/b.rs
@@ -2,5 +2,22 @@ use proconio::input;
 
 fn main() {
   input! {
+    _: usize,
+    a: usize, b: usize,
+    k: usize,
+    pv: [usize; k]
   };
+
+  let mut map = std::collections::HashMap::new();
+  for p in pv {
+    let c = map.entry(p).or_insert(0);
+    *c += 1;
+  }
+  if map.contains_key(&a) || map.contains_key(&b) {
+    println!("NO");
+  } else if map.values().any(|&c| c > 1) {
+    println!("NO");
+  } else {
+    println!("YES");
+  }
 }
