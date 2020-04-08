da09daedd8f75084e070073cc9c7f78c715155e8
emanon001 <emanon001@gmail.com>
2020-04-08 14:02:49 +0900

    solve abc021/a

--- abc021/src/bin/a.rs
+++ abc021/src/bin/a.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize
   };
+
+  let two_count = n / 2;
+  if n % 2 == 0 {
+    println!("{}", two_count);
+  } else {
+    println!("{}", two_count + 1);
+    println!("1");
+  }
+  for _ in 0..two_count {
+    println!("2");
+  }
 }
