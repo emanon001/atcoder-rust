5889979e049949101c32f1a40877d1ecfadd1e66
emanon001 <emanon001@gmail.com>
2020-04-09 21:33:17 +0900

    solve abc019/c

--- abc019/src/bin/c.rs
+++ abc019/src/bin/c.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    av: [usize; n]
   };
+
+  let mut set = std::collections::HashSet::new();
+  for a in av {
+    let mut x = a;
+    while x % 2 == 0 {
+      x /= 2;
+    }
+    set.insert(x);
+  }
+  println!("{}", set.len());
 }
