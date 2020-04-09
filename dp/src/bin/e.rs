66a20c4b0c5d3aea0b47bb8f05a05b920903a6c0
emanon001 <emanon001@gmail.com>
2020-04-09 18:06:34 +0900

    solve dp/e

--- dp/src/bin/e.rs
+++ dp/src/bin/e.rs
@@ -2,5 +2,33 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize, w: usize,
+    wvv: [(usize, usize); n]
   };
+
+  let max_v = 100_000;
+  let mut dp = vec![vec![w + 1; max_v + 1]; n + 1];
+  dp[0][0] = 0;
+  for i in 0..n {
+    let (w1, v1) = wvv[i];
+    for j in 0..=max_v {
+      if dp[i][j] < dp[i + 1][j] {
+        dp[i + 1][j] = dp[i][j];
+      }
+      if j + v1 > max_v {
+        continue;
+      }
+      let cost = dp[i][j] + w1;
+      if cost < dp[i + 1][j + v1] {
+        dp[i + 1][j + v1] = cost;
+      }
+    }
+  }
+  let mut res = 0;
+  for i in 0..=max_v {
+    if dp[n][i] <= w {
+      res = i;
+    }
+  }
+  println!("{}", res);
 }
