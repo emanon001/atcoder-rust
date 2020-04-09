76f0e7c45e06156a58fceed835b76f6dfa6eca61
emanon001 <emanon001@gmail.com>
2020-04-09 16:40:47 +0900

    solve dp/d

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -2,5 +2,24 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize, w: usize,
+    wvv: [(usize, u64); n]
   };
+
+  let mut dp = vec![vec![0_u64; w + 1]; n + 1];
+  for i in 0..n {
+    let (w1, v1) = wvv[i];
+    for j in 0..=w {
+      if dp[i][j] > dp[i + 1][j] {
+        dp[i + 1][j] = dp[i][j];
+      }
+      if j + w1 <= w {
+        let cost = dp[i][j] + v1;
+        if cost > dp[i + 1][j + w1] {
+          dp[i + 1][j + w1] = cost;
+        }
+      }
+    }
+  }
+  println!("{}", dp[n][w]);
 }
