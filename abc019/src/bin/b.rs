e5b6bebe58adc4dd62de45c2db7b99e1e020ee4d
emanon001 <emanon001@gmail.com>
2020-04-09 21:15:44 +0900

    solve abc019/b

--- abc019/src/bin/b.rs
+++ abc019/src/bin/b.rs
@@ -1,6 +1,23 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars
   };
+
+  let mut res = "".to_string();
+  let mut c = 1;
+  let mut prev = s[0];
+  for &ch in &s[1..] {
+    if ch == prev {
+      c += 1;
+    } else {
+      res = res + &format!("{}{}", prev, c);
+      c = 1;
+    }
+    prev = ch;
+  }
+  res = res + &format!("{}{}", prev, c);
+  println!("{}", res);
 }
