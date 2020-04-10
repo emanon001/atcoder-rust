fbf2a7b308dbad28616f6e1d2ab6cd88c60464f1
emanon001 <emanon001@gmail.com>
2020-04-10 20:46:22 +0900

    solve abc016/b

--- abc016/src/bin/b.rs
+++ abc016/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
   input! {
+    a: isize, b: isize, c: isize
   };
+  let is_plus = a + b == c;
+  let is_minus = a - b == c;
+  let res = match (is_plus, is_minus) {
+    (true, true) => '?',
+    (true, false) => '+',
+    (false, true) => '-',
+    _ => '!',
+  };
+  println!("{}", res);
 }
