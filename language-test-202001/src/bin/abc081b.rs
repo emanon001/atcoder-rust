f9408d31e9542db46a6854ef19812f7449f100cf
emanon001 <emanon001@gmail.com>
2020-04-11 22:26:57 +0900

    solve language-test-202001/abc081b

--- language-test-202001/src/bin/abc081b.rs
+++ language-test-202001/src/bin/abc081b.rs
@@ -8,5 +8,20 @@ use proconio::marker::*;
 
 fn main() {
   input! {
+    n: usize,
+    mut av: [usize; n]
   };
+
+  let mut m = 2;
+  let mut res = 0;
+  loop {
+    let is_ok = av.iter().all(|&a| a % m == 0);
+    if is_ok {
+      res += 1;
+      m *= 2;
+    } else {
+      break;
+    }
+  }
+  println!("{}", res);
 }
