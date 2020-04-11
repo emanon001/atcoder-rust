30a489d42ec135577812fc1205708bcb65b51718
emanon001 <emanon001@gmail.com>
2020-04-11 15:35:13 +0900

    solve abc019/d

--- abc019/src/bin/d.rs
+++ abc019/src/bin/d.rs
@@ -1,6 +1,35 @@
-use proconio::input;
+use std::io::*;
+
+fn read() -> String {
+  let mut s = String::new();
+  stdin().read_line(&mut s).ok();
+  s.trim().to_string()
+}
 
 fn main() {
-  input! {
-  };
+  let n = read().parse::<usize>().unwrap();
+
+  let mut longest = (1, 0);
+  for v in 2..=n {
+    println!("? {} {}", 1, v);
+    stdout().flush().ok();
+    let d = read().parse::<usize>().unwrap();
+    if d > longest.1 {
+      longest = (v, d);
+    }
+  }
+  let mut res = 0;
+  let u = longest.0;
+  for v in 1..=n {
+    if u == v {
+      continue;
+    }
+    println!("? {} {}", u, v);
+    stdout().flush().ok();
+    let d = read().parse::<usize>().unwrap();
+    if d > res {
+      res = d;
+    }
+  }
+  println!("! {}", res);
 }
