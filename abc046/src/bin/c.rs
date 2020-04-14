721a2a47a18da6bcee38db91f5d28971bcd07ca6
emanon001 <emanon001@gmail.com>
2020-04-14 23:32:52 +0900

    solve abc046/c

--- abc046/src/bin/c.rs
+++ abc046/src/bin/c.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    tav: [(u64, u64); n]
   };
+
+  let mut tc = 1_u64;
+  let mut ac = 1_u64;
+  for (t, a) in tav {
+    let x = std::cmp::max(
+      if tc % t == 0 { tc / t } else { tc / t + 1 },
+      if ac % a == 0 { ac / a } else { ac / a + 1 },
+    );
+    tc = t * x;
+    ac = a * x;
+  }
+  println!("{}", tc + ac);
 }
