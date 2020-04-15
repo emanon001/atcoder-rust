1596b8cd9d52a5f9a67c4834ab2df651207d6e82
emanon001 <emanon001@gmail.com>
2020-04-16 00:25:38 +0900

    solve abc015/d

--- abc015/src/bin/d.rs
+++ abc015/src/bin/d.rs
@@ -8,5 +8,30 @@ use proconio::marker::*;
 
 fn main() {
   input! {
+    w: usize,
+    n: usize, k: usize,
+    abv: [(usize, usize); n]
   };
+
+  let mut dp = vec![vec![vec![0; k + 1]; w + 1]; n + 1];
+  for i in 0..n {
+    let (a, b) = abv[i];
+    for j in 0..=w {
+      for k2 in 0..=k {
+        dp[i + 1][j][k2] = std::cmp::max(dp[i + 1][j][k2], dp[i][j][k2]);
+        if k2 + 1 > k {
+          continue;
+        }
+        if j + a > w {
+          continue;
+        }
+        dp[i + 1][j + a][k2 + 1] = std::cmp::max(dp[i + 1][j + a][k2 + 1], dp[i][j][k2] + b);
+      }
+    }
+  }
+  let mut res = 0;
+  for i in 0..=k {
+    res = std::cmp::max(dp[n][w][i], res);
+  }
+  println!("{}", dp[n][w][k]);
 }
