593518440d8044425e210a32bb6e22e0ad4f60f8
emanon001 <emanon001@gmail.com>
2020-04-15 23:55:54 +0900

    solve abc015/c

--- abc015/src/bin/c.rs
+++ abc015/src/bin/c.rs
@@ -6,7 +6,25 @@ use proconio::input;
 #[allow(unused_imports)]
 use proconio::marker::*;
 
+fn dfs(i: usize, acc: u8, n: usize, k: usize, answers: &[Vec<u8>]) -> bool {
+  if i == n {
+    return acc == 0;
+  }
+  (0..k)
+    .into_iter()
+    .any(|j| dfs(i + 1, acc ^ answers[i][j], n, k, answers))
+}
+
 fn main() {
   input! {
+    n: usize, k: usize,
+    answers: [[u8; k]; n]
   };
+
+  let res = if dfs(0, 0, n, k, &answers) {
+    "Found"
+  } else {
+    "Nothing"
+  };
+  println!("{}", res);
 }
