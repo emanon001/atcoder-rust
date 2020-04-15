f832a4609734b372a325077d68f265e1a692db41
emanon001 <emanon001@gmail.com>
2020-04-15 23:37:13 +0900

    solve abc015/b

--- abc015/src/bin/b.rs
+++ abc015/src/bin/b.rs
@@ -8,5 +8,18 @@ use proconio::marker::*;
 
 fn main() {
   input! {
+    n: usize,
+    av: [usize; n]
   };
+
+  let mut s_count = 0;
+  let mut bug_count = 0;
+  for a in av {
+    if a > 0 {
+      s_count += 1;
+      bug_count += a;
+    }
+  }
+  let res = (bug_count + s_count - 1) / s_count;
+  println!("{}", res);
 }
