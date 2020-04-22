ecc79f1786594a59157ebcae2a6b18f63051e038
emanon001 <emanon001@gmail.com>
2020-04-22 10:37:26 +0900

    cargo atcoder new abc014

---
+++ abc014/src/bin/d.rs
@@ -0,0 +1,12 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::*;
+
+fn main() {
+  input! {
+  };
+}
