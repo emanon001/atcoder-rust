342005a116fe2704a1ca3186a3a971fab7965a82
emanon001 <emanon001@gmail.com>
2020-04-22 11:03:38 +0900

    solve abc014/c

--- abc014/src/bin/c.rs
+++ abc014/src/bin/c.rs
@@ -8,5 +8,22 @@ use proconio::marker::*;
 
 fn main() {
   input! {
+    n: usize,
+    abv: [(usize, usize); n]
   };
+
+  let color_size = 1_000_001;
+  let mut imos = vec![0_isize; color_size + 1];
+  for (a, b) in abv {
+    imos[a] += 1;
+    imos[b + 1] -= 1;
+  }
+  let mut v = vec![0; color_size];
+  let mut cur = 0;
+  for i in 0..color_size {
+    cur = cur + imos[i];
+    v[i] = cur;
+  }
+  let res = v.iter().max().unwrap();
+  println!("{}", res);
 }
