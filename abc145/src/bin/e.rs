3cd72f2c378a42c57bbc5ae4f9bd753f27ed5045
emanon001 <emanon001@gmail.com>
2020-04-22 10:25:01 +0900

    solve abc145/e

--- abc145/src/bin/e.rs
+++ abc145/src/bin/e.rs
@@ -2,5 +2,21 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize, t: usize,
+    mut abv: [(usize, usize); n]
   };
+
+  abv.sort();
+  let mut dp = vec![vec![0; 6000]; n + 1];
+  for i in 0..n {
+    let (a, b) = abv[i];
+    for j in 0..6000 {
+      dp[i + 1][j] = std::cmp::max(dp[i + 1][j], dp[i][j]);
+      if j < t {
+        dp[i + 1][j + a] = std::cmp::max(dp[i + 1][j + a], dp[i][j] + b);
+      }
+    }
+  }
+  let res = dp[n].iter().max().unwrap();
+  println!("{}", res);
 }
