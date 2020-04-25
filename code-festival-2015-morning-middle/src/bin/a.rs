536309c585f70e804eac4ac00e8d6f85a192d9d2
emanon001 <emanon001@gmail.com>
2020-04-25 23:41:09 +0900

    solve code-festival-2015-morning-middle/a

--- code-festival-2015-morning-middle/src/bin/a.rs
+++ code-festival-2015-morning-middle/src/bin/a.rs
@@ -10,5 +10,29 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, k: usize, m: usize, r: usize,
+    mut sv: [usize; n - 1]
   };
+
+  let required = k * r;
+  sv.sort_by_key(|&x| -(x as isize));
+  if n == k {
+    let sum: usize = sv.into_iter().sum();
+    if sum >= required {
+      println!("0");
+    } else if sum + m < required {
+      println!("-1");
+    } else {
+      println!("{}", required - sum);
+    }
+  } else {
+    let sum: usize = sv[0..k].into_iter().sum();
+    if sum >= required {
+      println!("0");
+    } else if sum - sv[k - 1] + m < required {
+      println!("-1");
+    } else {
+      println!("{}", required - (sum - sv[k - 1]));
+    }
+  }
 }
