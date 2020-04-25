206cc2c2ee297c2be381bfc323efa06ea393898c
emanon001 <emanon001@gmail.com>
2020-04-25 16:07:08 +0900

    solve abc027/b

--- abc027/src/bin/b.rs
+++ abc027/src/bin/b.rs
@@ -10,5 +10,24 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    av: [isize; n]
   };
+
+  let sum: isize = av.iter().sum();
+  if sum % n as isize != 0 {
+    println!("-1");
+    std::process::exit(0);
+  }
+
+  let m = sum / n as isize;
+  let mut res = 0;
+  let mut insufficient = 0;
+  for a in av {
+    if a - insufficient != m {
+      res += 1;
+    }
+    insufficient += m - a;
+  }
+  println!("{}", res);
 }
