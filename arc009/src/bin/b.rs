194db9647e59d22e7563f3e119df77259145efe8
emanon001 <emanon001@gmail.com>
2020-04-25 22:33:39 +0900

    solve arc009/b

--- arc009/src/bin/b.rs
+++ arc009/src/bin/b.rs
@@ -8,7 +8,26 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn convert(n: usize, table: &[usize]) -> usize {
+  n.to_string()
+    .chars()
+    .map(|ch| table[ch.to_digit(10).unwrap() as usize])
+    .fold(0, |acc, x| acc * 10 + x)
+}
+
 fn main() {
   input! {
+    bv: [usize; 10],
+    n: usize,
+    mut av: [usize; n]
   };
+
+  let mut table = vec![0; 10];
+  for (i, b) in bv.into_iter().enumerate() {
+    table[b] = i;
+  }
+  av.sort_by_key(|&a| convert(a, &table));
+  for a in av {
+    println!("{}", a);
+  }
 }
