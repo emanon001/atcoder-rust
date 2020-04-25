13e2d2cae64b10f0d8dbb1aefca9ce7d652166de
emanon001 <emanon001@gmail.com>
2020-04-25 20:26:27 +0900

    solve arc042/a

--- arc042/src/bin/a.rs
+++ arc042/src/bin/a.rs
@@ -10,5 +10,26 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, m: usize,
+    av: [usize; m]
   };
+
+  let mut used = HashSet::new();
+  let mut res = Vec::new();
+  for a in av.into_iter().rev() {
+    if used.contains(&a) {
+      continue;
+    }
+    used.insert(a);
+    res.push(a);
+  }
+  for a in 1..=n {
+    if used.contains(&a) {
+      continue;
+    }
+    res.push(a);
+  }
+  for a in res {
+    println!("{}", a);
+  }
 }
