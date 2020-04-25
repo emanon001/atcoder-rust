656d7583ea03f98a428954ab79b596a8e96c1f21
emanon001 <emanon001@gmail.com>
2020-04-25 20:04:24 +0900

    solve arc008/b

--- arc008/src/bin/b.rs
+++ arc008/src/bin/b.rs
@@ -10,5 +10,32 @@ use std::collections::*;
 
 fn main() {
   input! {
+    _: usize, _: usize,
+    name: Chars,
+    kit: Chars
   };
+
+  let mut n_table = HashMap::new();
+  for ch in name {
+    let x = n_table.entry(ch).or_insert(0);
+    *x += 1;
+  }
+  let mut k_table = HashMap::new();
+  for ch in kit {
+    let x = k_table.entry(ch).or_insert(0);
+    *x += 1;
+  }
+  let mut res = 0;
+  for (ch, c) in n_table {
+    if let Some(c2) = k_table.get(&ch) {
+      let c3 = (c + c2 - 1) / c2;
+      if c3 > res {
+        res = c3;
+      }
+    } else {
+      println!("-1");
+      std::process::exit(0);
+    }
+  }
+  println!("{}", res);
 }
