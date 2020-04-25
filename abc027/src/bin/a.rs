36764471503599db6200c667eceb03620bd1206c
emanon001 <emanon001@gmail.com>
2020-04-25 15:54:35 +0900

    solve abc027/a

--- abc027/src/bin/a.rs
+++ abc027/src/bin/a.rs
@@ -10,5 +10,17 @@ use std::collections::*;
 
 fn main() {
   input! {
+    lv: [usize; 3]
   };
+
+  let mut table = HashMap::new();
+  for l in lv {
+    let x = table.entry(l).or_insert(0);
+    *x += 1;
+  }
+  for (l, c) in table {
+    if c == 1 || c == 3 {
+      println!("{}", l);
+    }
+  }
 }
