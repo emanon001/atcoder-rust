4f1d1b20b3e49a302890a869b7a8497d87ae1893
emanon001 <emanon001@gmail.com>
2020-04-25 15:46:03 +0900

    solve arc056/a

--- arc056/src/bin/a.rs
+++ arc056/src/bin/a.rs
@@ -10,5 +10,12 @@ use std::collections::*;
 
 fn main() {
   input! {
+    a: u64, b: u64, k: u64, l: u64
   };
+
+  let res = vec![a * k, b * ((k + l - 1) / l), b * (k / l) + a * (k % l)]
+    .into_iter()
+    .min()
+    .unwrap();
+  println!("{}", res);
 }
