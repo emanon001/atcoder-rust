51c1fcd1b84227df4656afdccc4e465ce261ae9f
emanon001 <emanon001@gmail.com>
2020-04-25 19:52:07 +0900

    solve abc005/c

--- abc005/src/bin/c.rs
+++ abc005/src/bin/c.rs
@@ -10,5 +10,47 @@ use std::collections::*;
 
 fn main() {
   input! {
+    t: usize,
+    n: usize,
+    av: [usize; n],
+    m: usize,
+    bv: [usize; m]
   };
+
+  let mut events = Vec::new();
+  for a in av {
+    events.push((a, 0));
+    events.push((a + t + 1, 1));
+  }
+  for b in bv {
+    events.push((b, 2));
+  }
+  events.sort();
+  let mut c = 0;
+  let mut ate_c = 0;
+  for (_, k) in events {
+    match k {
+      0 => {
+        c += 1;
+      }
+      1 => {
+        if ate_c == 0 {
+          c -= 1
+        } else {
+          ate_c -= 1;
+        }
+      }
+      2 => {
+        if c == 0 {
+          println!("no");
+          std::process::exit(0);
+        } else {
+          c -= 1;
+          ate_c += 1;
+        }
+      }
+      _ => unreachable!(),
+    }
+  }
+  println!("yes");
 }
