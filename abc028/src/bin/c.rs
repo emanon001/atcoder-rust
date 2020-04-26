a10dcb495ecf431a8057e065fce9662df76973ee
emanon001 <emanon001@gmail.com>
2020-04-26 14:30:17 +0900

    solve abc028/c

--- abc028/src/bin/c.rs
+++ abc028/src/bin/c.rs
@@ -10,5 +10,14 @@ use std::collections::*;
 
 fn main() {
   input! {
+    av: [usize; 5]
   };
+
+  let mut sums = av
+    .into_iter()
+    .combinations(3)
+    .map(|c| c.into_iter().sum::<usize>())
+    .collect::<Vec<_>>();
+  sums.sort_by_key(|&a| -(a as isize));
+  println!("{}", sums[2]);
 }
