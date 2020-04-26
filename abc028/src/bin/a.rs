1060cd0df8e0ceec70b153af3a247bbe35af7c2f
emanon001 <emanon001@gmail.com>
2020-04-26 14:24:13 +0900

    solve abc028/a

--- abc028/src/bin/a.rs
+++ abc028/src/bin/a.rs
@@ -10,5 +10,17 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
   };
+
+  let res = if n <= 59 {
+    "Bad"
+  } else if n >= 60 && n <= 89 {
+    "Good"
+  } else if n >= 90 && n <= 99 {
+    "Great"
+  } else {
+    "Perfect"
+  };
+  println!("{}", res);
 }
