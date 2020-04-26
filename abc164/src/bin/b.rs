c7c0438de7e8678be93cd4258bd9ccff73362125
emanon001 <emanon001@gmail.com>
2020-04-26 23:04:10 +0900

    solve abc164/{a-c}

--- abc164/src/bin/b.rs
+++ abc164/src/bin/b.rs
@@ -10,5 +10,19 @@ use std::collections::*;
 
 fn main() {
   input! {
+    mut a: isize, b: isize, mut c: isize, d: isize
   };
+
+  loop {
+    c -= b;
+    if c <= 0 {
+      println!("Yes");
+      std::process::exit(0);
+    }
+    a -= d;
+    if a <= 0 {
+      println!("No");
+      std::process::exit(0);
+    }
+  }
 }
--- abc164/src/bin/c.rs
+++ abc164/src/bin/c.rs
@@ -10,5 +10,10 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    sv: [String; n]
   };
+
+  let res = sv.into_iter().collect::<HashSet<_>>().len();
+  println!("{}", res);
 }
