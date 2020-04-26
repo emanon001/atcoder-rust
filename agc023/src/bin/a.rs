14249a617cdb11fc49e6baa8263fc4571e9f265b
emanon001 <emanon001@gmail.com>
2020-04-27 01:35:36 +0900

    solve agc023/a

--- agc023/src/bin/a.rs
+++ agc023/src/bin/a.rs
@@ -10,5 +10,21 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    av: [i64; n]
   };
+
+  let mut table = HashMap::new();
+  table.insert(0, 1);
+  let mut cur = 0_i64;
+  for a in av {
+    cur += a;
+    let x = table.entry(cur).or_insert(0);
+    *x += 1;
+  }
+  let mut res = 0_u64;
+  for (_, c) in table {
+    res += c * (c - 1) / 2;
+  }
+  println!("{}", res);
 }
