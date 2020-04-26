80c2d747f85fd4ecdc107fe1805fa5a3c88420a6
emanon001 <emanon001@gmail.com>
2020-04-26 14:24:45 +0900

    solve abc028/b

--- abc028/src/bin/b.rs
+++ abc028/src/bin/b.rs
@@ -10,5 +10,17 @@ use std::collections::*;
 
 fn main() {
   input! {
+    s: Chars
   };
+
+  let mut table = HashMap::new();
+  for ch in s {
+    let c = table.entry(ch).or_insert(0);
+    *c += 1;
+  }
+  let res = vec!['A', 'B', 'C', 'D', 'E', 'F']
+    .into_iter()
+    .map(|ch| table.get(&ch).unwrap_or(&0))
+    .join(" ");
+  println!("{}", res);
 }
