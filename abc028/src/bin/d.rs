990459d4983a19b436d0aab948c3357f37ce8fcd
emanon001 <emanon001@gmail.com>
2020-04-26 14:41:35 +0900

    solve abc028/d

--- abc028/src/bin/d.rs
+++ abc028/src/bin/d.rs
@@ -10,5 +10,23 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    k: usize
   };
+
+  let all = n * n * n;
+  // k, k, k
+  let c3 = 1;
+  // (k, k, _), (k, _, k), (_, k, k)
+  let c2 = (n - 1) * 3;
+  // (k, < k, > k), (k, > k, < k)
+  // (< k, k, > k), (> k, , < k)
+  // (< k, > k, k), (> k, < k, k)
+  let c1 = if k == 1 || k == n {
+    0
+  } else {
+    (k - 1) * (n - k) * 6
+  };
+  let res = (c3 + c2 + c1) as f64 / all as f64;
+  println!("{}", res);
 }
