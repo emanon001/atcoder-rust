6ce1707c94fd596f2399b48a8777c94cb110f006
emanon001 <emanon001@gmail.com>
2020-04-26 13:22:57 +0900

    solve code-festival-2016-qualc/b

--- code-festival-2016-qualc/src/bin/b.rs
+++ code-festival-2016-qualc/src/bin/b.rs
@@ -2,5 +2,26 @@ use proconio::input;
 
 fn main() {
   input! {
+    k: usize, t: usize,
+    mut av: [usize; t]
   };
+
+  if t == 1 {
+    println!("{}", k - 1);
+    std::process::exit(0);
+  }
+  av.sort();
+  while av.len() > 1 {
+    let x = av.pop().unwrap() - 1;
+    let y = av.pop().unwrap() - 1;
+    if x > 0 {
+      av.push(x);
+    }
+    if y > 0 {
+      av.push(y);
+    }
+    av.sort();
+  }
+  let res = if av.is_empty() { 0 } else { av[0] - 1 };
+  println!("{}", res);
 }
