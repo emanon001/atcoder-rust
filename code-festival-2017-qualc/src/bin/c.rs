5c7e0bd8d4e9128770392ec341b0636fbbe83007
emanon001 <emanon001@gmail.com>
2020-04-27 21:44:25 +0900

    solve code-festival-2017-qualc/c

--- code-festival-2017-qualc/src/bin/c.rs
+++ code-festival-2017-qualc/src/bin/c.rs
@@ -1,6 +1,28 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars
   };
+
+  let mut res = 0_isize;
+  let mut i = 0;
+  let mut j = s.len() - 1;
+  while i < j {
+    if s[i] == s[j] {
+      i += 1;
+      j -= 1;
+    } else if s[i] == 'x' {
+      i += 1;
+      res += 1;
+    } else if s[j] == 'x' {
+      j -= 1;
+      res += 1;
+    } else {
+      res = -1;
+      break;
+    }
+  }
+  println!("{}", res);
 }
