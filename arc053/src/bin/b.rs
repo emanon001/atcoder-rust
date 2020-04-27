6804ab479359206a19ae2eea728cffb6c2744001
emanon001 <emanon001@gmail.com>
2020-04-28 03:24:12 +0900

    solve arc053/b

--- arc053/src/bin/b.rs
+++ arc053/src/bin/b.rs
@@ -10,5 +10,25 @@ use std::collections::*;
 
 fn main() {
   input! {
+    s: Chars
   };
+
+  let all_count = s.len();
+  let mut table = HashMap::new();
+  for ch in s {
+    let c = table.entry(ch).or_insert(0);
+    *c += 1;
+  }
+  let mut n = 0;
+  for &c in table.values() {
+    if c % 2 == 1 {
+      n += 1;
+    }
+  }
+  let res = if n <= 1 {
+    all_count
+  } else {
+    (all_count - n) / 2 / n * 2 + 1
+  };
+  println!("{}", res);
 }
