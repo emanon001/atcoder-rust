25e803add5502df4699731aa39d4c1e97bc6dd85
emanon001 <emanon001@gmail.com>
2020-04-27 21:31:48 +0900

    solve arc011/b

--- arc011/src/bin/b.rs
+++ arc011/src/bin/b.rs
@@ -10,5 +10,41 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    wv: [Chars; n]
   };
+
+  let mut table = HashMap::new();
+  let pairs = vec![
+    ('z', 'r'),
+    ('b', 'c'),
+    ('d', 'w'),
+    ('t', 'j'),
+    ('f', 'q'),
+    ('l', 'v'),
+    ('s', 'x'),
+    ('p', 'm'),
+    ('h', 'k'),
+    ('n', 'g'),
+  ];
+  for (i, pair) in pairs.into_iter().enumerate() {
+    table.insert(pair.0, i);
+    table.insert(pair.1, i);
+  }
+
+  let res = wv
+    .into_iter()
+    .flat_map(|chars| {
+      let res = chars
+        .into_iter()
+        .flat_map(|ch| table.get(&ch.to_ascii_lowercase()).map(|ch| ch.to_string()))
+        .join("");
+      if res.len() == 0 {
+        None
+      } else {
+        Some(res)
+      }
+    })
+    .join(" ");
+  println!("{}", res);
 }
