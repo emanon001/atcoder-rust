7ff8be58e8bc0dc90f326763b862e5a7eb718afc
emanon001 <emanon001@gmail.com>
2020-04-27 20:51:19 +0900

    solve agc011/b

--- agc011/src/bin/b.rs
+++ agc011/src/bin/b.rs
@@ -2,5 +2,20 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    mut av: [usize; n]
   };
+
+  av.sort();
+  let mut minus = 0;
+  let mut cur = av[0];
+  for (i, a) in av.into_iter().skip(1).enumerate() {
+    let i = i + 1;
+    if cur * 2 < a {
+      minus = i;
+    }
+    cur += a;
+  }
+  let res = n - minus;
+  println!("{}", res);
 }
