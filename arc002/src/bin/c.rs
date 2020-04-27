ed5e75f53060c86615dbdff8b8c10e4aa09f38b3
emanon001 <emanon001@gmail.com>
2020-04-28 01:22:25 +0900

    solve arc002/c

--- arc002/src/bin/c.rs
+++ arc002/src/bin/c.rs
@@ -10,5 +10,27 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    cs: String
   };
+
+  let commands = vec!['A', 'B', 'X', 'Y'];
+  let mut cv = Vec::new();
+  for i in 0..4 {
+    for j in 0..4 {
+      cv.push(format!("{}{}", commands[i], commands[j]));
+    }
+  }
+  let mut res = 1 << 30;
+  for lc in &cv {
+    for rc in &cv {
+      let s = cs.clone();
+      let s = s.replace(lc, "_").replace(rc, "_").replace("_", "");
+      let count = s.len() + (n - s.len()) / 2;
+      if count < res {
+        res = count;
+      }
+    }
+  }
+  println!("{}", res);
 }
