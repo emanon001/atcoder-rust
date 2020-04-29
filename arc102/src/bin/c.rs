74d829021264cf94abd7ab1709f0bdfb3a217108
emanon001 <emanon001@gmail.com>
2020-04-29 23:01:52 +0900

    solve arc102/c

--- arc102/src/bin/c.rs
+++ arc102/src/bin/c.rs
@@ -10,5 +10,20 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, k: usize
   };
+
+  let k_is_even = k % 2 == 0;
+  let mut zero_c = 0_u64;
+  let mut halfk_c = 0_u64;
+  for x in 1..=n {
+    if x % k == 0 {
+      zero_c += 1;
+    }
+    if k_is_even && x % k == k / 2 {
+      halfk_c += 1;
+    }
+  }
+  let res = zero_c.pow(3) + halfk_c.pow(3);
+  println!("{}", res);
 }
