dca96245b355b3979f7bf2189bec71ae0aeb710c
emanon001 <emanon001@gmail.com>
2020-04-30 23:46:45 +0900

    solve abc008/b

--- abc008/src/bin/b.rs
+++ abc008/src/bin/b.rs
@@ -10,5 +10,16 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    sv: [String; n]
   };
+
+  let mut table = HashMap::new();
+  for s in sv {
+    let c = table.entry(s).or_insert(0);
+    *c += 1;
+  }
+  let mut vec = table.into_iter().collect::<Vec<_>>();
+  vec.sort_by_key(|(_, c)| -(*c as isize));
+  println!("{}", vec[0].0);
 }
