405f5ddb9849b899bbfb01d07d23cf373faec57a
emanon001 <emanon001@gmail.com>
2020-05-02 04:39:07 +0900

    solve digitalarts2012/a

--- digitalarts2012/src/bin/a.rs
+++ digitalarts2012/src/bin/a.rs
@@ -7,8 +7,31 @@ use proconio::input;
 use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
+use whiteread;
 
 fn main() {
+  let sv: Vec<String> = whiteread::parse_line().unwrap();
   input! {
+    n: usize,
+    tv: [String; n]
   };
+
+  let mut res = Vec::new();
+  for s in sv {
+    let sc = s.chars().collect::<Vec<_>>();
+    let filtered = tv.iter().any(|t| {
+      if s.len() != t.len() {
+        return false;
+      }
+      let tc = t.chars().collect::<Vec<_>>();
+      for i in 0..s.len() {
+        if tc[i] != '*' && sc[i] != tc[i] {
+          return false;
+        }
+      }
+      true
+    });
+    res.push(if filtered { "*".repeat(s.len()) } else { s });
+  }
+  println!("{}", res.join(" "));
 }
