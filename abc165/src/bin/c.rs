a62ba9d6c01b7ad3860b12417fcb9e29f7e8c2c2
emanon001 <emanon001@gmail.com>
2020-05-03 00:54:35 +0900

    solve abc165/c

--- abc165/src/bin/c.rs
+++ abc165/src/bin/c.rs
@@ -9,5 +9,24 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {};
+  input! {
+    n: usize, m: usize, q: usize,
+    abcdv: [(Usize1, Usize1, usize, usize); q]
+  };
+
+  let mut res = 0;
+  for v in (1..=m).combinations_with_replacement(n) {
+    let mut v = v;
+    v.sort();
+    let mut sum = 0;
+    for (a, b, c, d) in &abcdv {
+      if v[*b] - v[*a] == *c {
+        sum += d;
+      }
+    }
+    if sum > res {
+      res = sum;
+    }
+  }
+  println!("{}", res);
 }
