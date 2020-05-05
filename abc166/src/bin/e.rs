a8149bbabaf39d0ae5faa0c0aa2050438ffa6971
emanon001 <emanon001@gmail.com>
2020-05-06 02:39:42 +0900

    solve abc166/e

--- abc166/src/bin/e.rs
+++ abc166/src/bin/e.rs
@@ -10,5 +10,23 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    av: [isize; n]
   };
+
+  let mut add_table: HashMap<isize, u64> = HashMap::new();
+  for (i, &a) in av.iter().enumerate() {
+    let i = (i + 1) as isize;
+    *add_table.entry(i + a).or_insert(0) += 1;
+  }
+  let mut sub_table = HashMap::new();
+  for (i, &a) in av.iter().enumerate() {
+    let i = (i + 1) as isize;
+    *sub_table.entry(i - a).or_insert(0) += 1;
+  }
+  let mut res = 0_u64;
+  for (&a, &c) in &add_table {
+    res += c * sub_table.get(&a).unwrap_or(&0);
+  }
+  println!("{}", res);
 }
