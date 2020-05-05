42dc5f8b30e6143e81b2cd3c8c83e48ec7ad7cf9
emanon001 <emanon001@gmail.com>
2020-05-06 02:39:30 +0900

    solve apc001/b

--- apc001/src/bin/b.rs
+++ apc001/src/bin/b.rs
@@ -2,5 +2,33 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    av: [usize; n],
+    bv: [usize; n]
   };
+
+  let asum = av.iter().sum::<usize>();
+  let bsum = bv.iter().sum::<usize>();
+  if asum > bsum {
+    println!("No");
+    std::process::exit(0);
+  }
+  let op_c = bsum - asum;
+  let mut c1 = 0_usize;
+  let mut c2 = 0_usize;
+  for i in 0..n {
+    let a = av[i];
+    let b = bv[i];
+    if a > b {
+      c1 += a - b;
+    } else {
+      c2 += (b - a + 1) / 2;
+    }
+  }
+  let res = if c1 <= op_c && c2 <= op_c {
+    "Yes"
+  } else {
+    "No"
+  };
+  println!("{}", res);
 }
