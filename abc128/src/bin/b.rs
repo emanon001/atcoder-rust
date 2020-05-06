d8dcfa3396a75aaca6fe207abff380741e87950e
emanon001 <emanon001@gmail.com>
2020-05-07 02:31:25 +0900

    solve abc128/b

--- abc128/src/bin/b.rs
+++ abc128/src/bin/b.rs
@@ -2,5 +2,13 @@ use proconio::input;
 
 fn main() {
   input! {
+    n: usize,
+    spv: [(String, isize); n]
   };
+
+  let mut v = spv.into_iter().enumerate().collect::<Vec<_>>();
+  v.sort_by_key(|(_, sp)| (sp.0.clone(), -sp.1));
+  for (i, _) in v {
+    println!("{}", i + 1);
+  }
 }
