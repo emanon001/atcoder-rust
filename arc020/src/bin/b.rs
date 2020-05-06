d7f3f8f24ac6f7e15cad1be5a323c760cd02b6ec
emanon001 <emanon001@gmail.com>
2020-05-07 06:32:36 +0900

    solve arc020/b

--- arc020/src/bin/b.rs
+++ arc020/src/bin/b.rs
@@ -10,5 +10,36 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, c: usize,
+    av: [i32; n]
   };
+
+  let mut res = 1 << 32;
+  for cv in (1..=10).combinations(2) {
+    let cl1 = cv[0];
+    let cl2 = cv[1];
+    // c1, c2, c1, ...
+    let mut sum = 0;
+    for i in 0..n {
+      let cl = if i % 2 == 0 { cl1 } else { cl2 };
+      if av[i] != cl {
+        sum += c;
+      }
+    }
+    if sum < res {
+      res = sum;
+    }
+    // c2, c1, c2, ...
+    let mut sum = 0;
+    for i in 0..n {
+      let cl = if i % 2 == 0 { cl2 } else { cl1 };
+      if av[i] != cl {
+        sum += c;
+      }
+    }
+    if sum < res {
+      res = sum;
+    }
+  }
+  println!("{}", res);
 }
