8fec8c2a369e9afc9f51c2720e9b7549ba5f6255
emanon001 <emanon001@gmail.com>
2020-05-08 08:46:33 +0900

    solve abc112/c

--- abc112/src/bin/c.rs
+++ abc112/src/bin/c.rs
@@ -10,5 +10,42 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    xyhv: [(isize, isize, isize); n],
   };
+
+  for cx in 0_isize..=100 {
+    for cy in 0_isize..=100 {
+      let mut max = 1_isize << 60;
+      for &(x, y, h) in &xyhv {
+        if h == 0 {
+          let d = (x - cx).abs() + (y - cy).abs();
+          if d < max {
+            max = d;
+          }
+        }
+      }
+      let mut is_ok = true;
+      let mut height = -1_isize;
+      for &(x, y, h) in &xyhv {
+        if h > 0 {
+          let d = (x - cx).abs() + (y - cy).abs() + h;
+          if d > max {
+            is_ok = false;
+            break;
+          }
+          if height == -1 {
+            height = d;
+          }
+          if height != d {
+            is_ok = false;
+            break;
+          }
+        }
+      }
+      if is_ok {
+        println!("{} {} {}", cx, cy, height);
+      }
+    }
+  }
 }
