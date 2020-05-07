0c13e1f56049824da1e591a67210957e04a5e41a
emanon001 <emanon001@gmail.com>
2020-05-08 05:10:19 +0900

    solve abc110/b

--- abc110/src/bin/b.rs
+++ abc110/src/bin/b.rs
@@ -10,5 +10,24 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, m: usize, x: isize, y: isize,
+    xv: [isize; n],
+    yv: [isize; m],
   };
+
+  let is_ok = (x + 1..=y).any(|z| {
+    for i in 0..n {
+      if xv[i] >= z {
+        return false;
+      }
+    }
+    for i in 0..m {
+      if yv[i] < z {
+        return false;
+      }
+    }
+    true
+  });
+  let res = if is_ok { "No War" } else { "War" };
+  println!("{}", res);
 }
