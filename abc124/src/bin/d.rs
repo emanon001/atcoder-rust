ac13f511337ebc2f4ef67b4f76a68593e543b59e
emanon001 <emanon001@gmail.com>
2020-05-08 04:48:39 +0900

    solve abc124/d

--- abc124/src/bin/d.rs
+++ abc124/src/bin/d.rs
@@ -10,5 +10,55 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, k: usize,
+    s: Chars
   };
+
+  // ('0'|'1', count)
+  let mut cv = Vec::new();
+  let mut prev = s[0];
+  let mut c = 1;
+  for i in 1..n {
+    if prev == s[i] {
+      c += 1;
+    } else {
+      cv.push((prev, c));
+      c = 1;
+    }
+    prev = s[i];
+  }
+  cv.push((prev, c));
+
+  let mut cusum = Vec::new();
+  let mut sum = 0;
+  for i in 0..cv.len() {
+    let (ch, c) = cv[i];
+    sum += c;
+    if ch == '0' {
+      cusum.push((i, sum));
+    }
+  }
+
+  if cusum.is_empty() {
+    println!("{}", cv[0].1);
+    std::process::exit(0);
+  }
+
+  let mut res = 0;
+  for i in 0..cusum.len() {
+    let j = std::cmp::min(i + k - 1, cusum.len() - 1);
+    let mut sum = if i > 0 {
+      cusum[j].1 - cusum[i - 1].1
+    } else {
+      cusum[j].1
+    };
+    let ci = cusum[j].0;
+    if ci + 1 < cv.len() {
+      sum += cv[ci + 1].1;
+    }
+    if sum > res {
+      res = sum;
+    }
+  }
+  println!("{}", res);
 }
