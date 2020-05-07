b442f55f901e1e52945204ff9c6edaeff350a314
emanon001 <emanon001@gmail.com>
2020-05-08 05:01:01 +0900

    solve abc110/a

--- abc110/src/bin/a.rs
+++ abc110/src/bin/a.rs
@@ -10,5 +10,10 @@ use std::collections::*;
 
 fn main() {
   input! {
+    a: usize, b: usize, c: usize
   };
+
+  let max = a.max(b).max(c);
+  let res = max * 10 + a + b + c - max;
+  println!("{}", res);
 }
