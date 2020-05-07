bfe6507b7978591a046c7d24b0a4a64592a5ed64
emanon001 <emanon001@gmail.com>
2020-05-08 08:21:39 +0900

    solve abc112/{a-b}

--- abc112/src/bin/a.rs
+++ abc112/src/bin/a.rs
@@ -10,5 +10,16 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize
   };
+
+  if n == 1 {
+    println!("Hello World");
+  } else {
+    input! {
+      a: usize,
+      b: usize
+    };
+    println!("{}", a + b);
+  }
 }
--- abc112/src/bin/b.rs
+++ abc112/src/bin/b.rs
@@ -8,7 +8,24 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+const INF: usize = 1 << 60;
+
 fn main() {
   input! {
+    n: usize, t: usize,
+    ctv: [(usize, usize); n]
   };
+
+  let mut res = INF;
+  for (c, t2) in ctv {
+    if t2 <= t && c < res {
+      res = c;
+    }
+  }
+  let res = if res == INF {
+    "TLE".to_string()
+  } else {
+    res.to_string()
+  };
+  println!("{}", res);
 }
