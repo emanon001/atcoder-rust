f2cffbb9e32b2b2186c77f7842df39b3cbf9dfaa
emanon001 <emanon001@gmail.com>
2020-05-09 06:58:08 +0900

    solve abc115/c

--- abc115/src/bin/c.rs
+++ abc115/src/bin/c.rs
@@ -10,5 +10,21 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, k: usize,
+    mut hv: [usize; n]
   };
+
+  hv.sort();
+  let mut res = 1 << 30;
+  for i in 0..n {
+    let j = i + k - 1;
+    if j >= n {
+      break;
+    }
+    let diff = hv[j] - hv[i];
+    if diff < res {
+      res = diff;
+    }
+  }
+  println!("{}", res);
 }
