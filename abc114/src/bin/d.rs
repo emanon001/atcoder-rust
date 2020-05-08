cc5009ca8acf869548bb99f2db649e5f98195f7d
emanon001 <emanon001@gmail.com>
2020-05-08 13:23:56 +0900

    solve abc114/d

--- abc114/src/bin/d.rs
+++ abc114/src/bin/d.rs
@@ -8,7 +8,55 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn prime_factor(n: u64) -> std::collections::HashMap<u64, u64> {
+  if n < 2 {
+    return std::collections::HashMap::new();
+  }
+  let mut res = std::collections::HashMap::new();
+  let mut n = n;
+  let mut i = 2;
+  while i * i <= n {
+    while n % i == 0 {
+      let c = res.entry(i).or_insert(0);
+      *c += 1;
+      n /= i;
+    }
+    i += 1;
+  }
+  if n != 1 {
+    res.insert(n, 1);
+  }
+  res
+}
+
 fn main() {
   input! {
+    n: usize
   };
+
+  let mut pf = HashMap::new();
+  for a in 1..=n {
+    let pf2 = prime_factor(a as u64);
+    for (k, v) in pf2 {
+      *pf.entry(k).or_insert(0) += v;
+    }
+  }
+
+  // 3 * 5 * 5
+  // 3 * 25
+  // 5 * 15
+  // 75
+  let c3 = pf.iter().filter(|&(_, c)| *c >= 2).count() as isize;
+  let c5 = pf.iter().filter(|&(_, c)| *c >= 4).count() as isize;
+  let c15 = pf.iter().filter(|&(_, c)| *c >= 14).count() as isize;
+  let c25 = pf.iter().filter(|&(_, c)| *c >= 24).count() as isize;
+  let c75 = pf.iter().filter(|&(_, c)| *c >= 74).count() as isize;
+  let res = (c3 - c5) * c5 * std::cmp::max(c5 - 1, 0) / 2
+    + c5 * std::cmp::max(c5 - 1, 0) * std::cmp::max(c5 - 2, 0) / 2
+    + (c3 - c25) * c25
+    + c25 * std::cmp::max(c25 - 1, 0)
+    + (c5 - c15) * c15
+    + c15 * std::cmp::max(c15 - 1, 0)
+    + c75;
+  println!("{}", res);
 }
