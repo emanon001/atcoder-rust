47823ad0ea2cdaa1ffb49476b94983d2ca04fca4
emanon001 <emanon001@gmail.com>
2020-05-08 12:01:37 +0900

    solve abc114/b

--- abc114/src/bin/b.rs
+++ abc114/src/bin/b.rs
@@ -10,5 +10,18 @@ use std::collections::*;
 
 fn main() {
   input! {
+    s: String
   };
+
+  let mut res = 1000;
+  for i in 0..s.len() {
+    if i + 3 > s.len() {
+      break;
+    }
+    let n = (753 - &s[i..i + 3].parse::<isize>().unwrap()).abs();
+    if n < res {
+      res = n;
+    }
+  }
+  println!("{}", res);
 }
