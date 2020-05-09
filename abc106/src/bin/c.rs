755a725bab31e17de0d80aa524b5fe258cd84483
emanon001 <emanon001@gmail.com>
2020-05-09 15:27:15 +0900

    solve abc106/c

--- abc106/src/bin/c.rs
+++ abc106/src/bin/c.rs
@@ -1,6 +1,16 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
   input! {
+    s: Chars,
+    k: u64
   };
+
+  let prefix_one_count = s.iter().take_while(|&ch| *ch == '1').count();
+  if prefix_one_count as u64 >= k {
+    println!("1");
+  } else {
+    println!("{}", s[prefix_one_count]);
+  }
 }
