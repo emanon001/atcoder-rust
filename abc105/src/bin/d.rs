9202e29eb8bd91dadd6298e85e4742bc66d7a795
emanon001 <emanon001@gmail.com>
2020-05-09 20:24:41 +0900

    update abc105/d

--- abc105/src/bin/d.rs
+++ abc105/src/bin/d.rs
@@ -15,28 +15,15 @@ fn main() {
   };
 
   let mut table = HashMap::new();
+  table.insert(0, 1);
   let mut cur = 0;
   for &a in &av {
     cur = (cur + a) % m;
     *table.entry(cur).or_insert(0) += 1;
   }
   let mut res = 0_u64;
-  let mut cur = 0;
-  for a in av {
-    cur = (cur + a) % m;
-    let b = if a % m >= cur {
-      (m - (a % m - cur)) % m
-    } else {
-      cur - a % m
-    };
-    if let Some(x) = table.get(&b) {
-      res += *x;
-    }
-    if let Some(x) = table.get_mut(&cur) {
-      if *x > 0 {
-        *x -= 1;
-      }
-    }
+  for (_, c) in table {
+    res += c * (c - 1) / 2;
   }
   println!("{}", res);
 }
