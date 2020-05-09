4ddfed00f359ea303a36669fd5c377cae4c72d6c
emanon001 <emanon001@gmail.com>
2020-05-09 10:25:16 +0900

    solve abc094/c

--- abc094/src/bin/c.rs
+++ abc094/src/bin/c.rs
@@ -10,5 +10,23 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    xv: [usize; n]
   };
+
+  let mut xv = xv.into_iter().enumerate().collect::<Vec<_>>();
+  xv.sort_by_key(|(_, x)| *x);
+  let mut indexes = vec![0; n];
+  for i in 0..n {
+    indexes[xv[i].0] = i;
+  }
+  for i in 0..n {
+    let j = indexes[i];
+    let res = if j < n / 2 {
+      xv[n / 2].1
+    } else {
+      xv[n / 2 - 1].1
+    };
+    println!("{}", res);
+  }
 }
