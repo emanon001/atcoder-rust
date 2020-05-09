a8bc9c18681c8a86067fe3cf68f03f7a09955886
emanon001 <emanon001@gmail.com>
2020-05-09 10:42:02 +0900

    solve abc094/d

--- abc094/src/bin/d.rs
+++ abc094/src/bin/d.rs
@@ -10,5 +10,24 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize,
+    mut av: [usize; n]
   };
+
+  let i = *av.iter().max().unwrap();
+  let mut skipped = false;
+  let mut rj = 0;
+  let mut diff = 1 << 30;
+  for j in av {
+    if i == j && !skipped {
+      skipped = true;
+      continue;
+    }
+    let d = if j > i - j { j - (i - j) } else { (i - j) - j };
+    if d < diff {
+      rj = j;
+      diff = d;
+    }
+  }
+  println!("{} {}", i, rj);
 }
