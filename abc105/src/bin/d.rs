eaafb09451170a794870eb86e34b1caa108a35a8
emanon001 <emanon001@gmail.com>
2020-05-09 19:58:34 +0900

    solve abc105/d

--- abc105/src/bin/d.rs
+++ abc105/src/bin/d.rs
@@ -10,5 +10,33 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, m: usize,
+    av: [usize; n]
   };
+
+  let mut table = HashMap::new();
+  let mut cur = 0;
+  for &a in &av {
+    cur = (cur + a) % m;
+    *table.entry(cur).or_insert(0) += 1;
+  }
+  let mut res = 0_u64;
+  let mut cur = 0;
+  for a in av {
+    cur = (cur + a) % m;
+    let b = if a % m >= cur {
+      (m - (a % m - cur)) % m
+    } else {
+      cur - a % m
+    };
+    if let Some(x) = table.get(&b) {
+      res += *x;
+    }
+    if let Some(x) = table.get_mut(&cur) {
+      if *x > 0 {
+        *x -= 1;
+      }
+    }
+  }
+  println!("{}", res);
 }
