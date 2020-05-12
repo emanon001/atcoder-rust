ec3d6f82b7054415d2984aba8e9e2298b7fdba54
emanon001 <emanon001@gmail.com>
2020-05-12 23:40:26 +0900

    solve abc084/d

--- abc084/src/bin/d.rs
+++ abc084/src/bin/d.rs
@@ -1,6 +1,7 @@
 use proconio::input;
+use std::collections::HashSet;
 
-pub fn primes(n: usize) -> Vec<u64> {
+pub fn primes(n: usize) -> Vec<usize> {
   if n < 2 {
     return Vec::new();
   }
@@ -10,7 +11,7 @@ pub fn primes(n: usize) -> Vec<u64> {
   let mut res = Vec::new();
   for i in 2..=n {
     if is_prime[i] {
-      res.push(i as u64);
+      res.push(i);
       let mut j = 2 * i;
       while j <= n {
         is_prime[j] = false;
@@ -24,26 +25,20 @@ pub fn primes(n: usize) -> Vec<u64> {
 fn main() {
   input! {
     q: usize,
-    queries: [(usize, usize); q]
+    lrv: [(usize, usize); q]
   };
 
-  let n = 100_000;
-  let primes = primes(n)
-    .into_iter()
-    .collect::<std::collections::HashSet<_>>();
-  let mut cusum = vec![0; n + 1];
-  for i in 2..=n {
-    if i % 2 == 0 {
-      cusum[i] = cusum[i - 1];
-    } else {
-      if primes.contains(&(i as u64)) && primes.contains(&(((i + 1) / 2) as u64)) {
-        cusum[i] = cusum[i - 1] + 1;
-      } else {
-        cusum[i] = cusum[i - 1];
-      };
-    }
-  }
-  for (l, r) in queries {
-    println!("{}", cusum[r] - cusum[l - 1]);
+  let max = 100_000;
+  let prime_set = primes(max).into_iter().collect::<HashSet<_>>();
+  let cusum = (0..=max)
+    .scan(0, |acc, x| {
+      let is_ok = x % 2 == 1 && prime_set.contains(&x) && prime_set.contains(&((x + 1) / 2));
+      *acc += if is_ok { 1 } else { 0 };
+      Some(*acc)
+    })
+    .collect::<Vec<_>>();
+  for (l, r) in lrv {
+    let res = cusum[r] - cusum[l - 1];
+    println!("{}", res);
   }
 }
