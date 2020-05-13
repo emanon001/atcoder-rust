e0c18f14725116de3d5b1c180dbc07847773a9c8
emanon001 <emanon001@gmail.com>
2020-05-13 21:00:02 +0900

    solve abc123/{a-b}

--- abc123/src/bin/a.rs
+++ abc123/src/bin/a.rs
@@ -10,5 +10,17 @@ use std::collections::*;
 
 fn main() {
   input! {
+    v: [usize; 5],
+    k: usize
   };
+
+  for i in 0..v.len() - 1 {
+    for j in i + 1..v.len() {
+      if v[j] - v[i] > k {
+        println!(":(");
+        std::process::exit(0);
+      }
+    }
+  }
+  println!("Yay!");
 }
--- abc123/src/bin/b.rs
+++ abc123/src/bin/b.rs
@@ -10,5 +10,29 @@ use std::collections::*;
 
 fn main() {
   input! {
+    a: usize,
+    b: usize,
+    c: usize,
+    d: usize,
+    e: usize
   };
+
+  let mut res = 1_usize << 30;
+  let cv = vec![a, b, c, d, e];
+  for v in cv.into_iter().permutations(5) {
+    let mut cur = 0;
+    for x in v {
+      if cur % 10 == 0 {
+        cur += x;
+      } else {
+        cur += 10;
+        cur -= cur % 10;
+        cur += x;
+      }
+    }
+    if cur < res {
+      res = cur;
+    }
+  }
+  println!("{}", res);
 }
