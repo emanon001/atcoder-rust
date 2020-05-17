18ebbe3399cf12d266c067e35c85f1febe8a927f
emanon001 <emanon001@gmail.com>
2020-05-17 20:41:07 +0900

    solve abc129/a

--- abc129/src/bin/a.rs
+++ abc129/src/bin/a.rs
@@ -10,5 +10,19 @@ use std::collections::*;
 
 fn main() {
   input! {
+    p: usize, q: usize, r: usize
   };
+
+  let res = vec![
+    p + q, // a -> b -> c
+    r + q, // a -> c -> b
+    p + r, // b -> a -> c
+    q + r, // b -> c -> a
+    r + p, // c -> a -> b
+    q + p, // c -> b -> a
+  ]
+  .into_iter()
+  .min()
+  .unwrap();
+  println!("{}", res);
 }
