f44b7b77f6b4edd4ee7441ad8e1f90227abe405b
emanon001 <emanon001@gmail.com>
2020-05-22 05:58:16 +0900

    solve abc066/d

--- abc066/src/bin/d.rs
+++ abc066/src/bin/d.rs
@@ -8,7 +8,241 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct ModInt(u32);
+
+impl ModInt {
+  pub const MOD: u32 = 1_000_000_007;
+
+  pub fn inv(self) -> Self {
+    if self.0 == 0 {
+      panic!();
+    }
+    self.pow(Self::MOD - 2)
+  }
+
+  pub fn pow(self, e: u32) -> Self {
+    if e == 0 {
+      return Self::new(1);
+    }
+    let mut res = self.pow(e >> 1);
+    res *= res;
+    if e & 1 == 1 {
+      res *= self;
+    }
+    res
+  }
+
+  fn new(n: i64) -> Self {
+    let mut n = n % (Self::MOD as i64);
+    if n.is_negative() {
+      n += Self::MOD as i64;
+    }
+    Self(n as u32)
+  }
+}
+
+impl From<i32> for ModInt {
+  fn from(n: i32) -> Self {
+    ModInt::from(n as i64)
+  }
+}
+
+impl From<i64> for ModInt {
+  fn from(n: i64) -> Self {
+    Self::new(n)
+  }
+}
+
+impl From<isize> for ModInt {
+  fn from(n: isize) -> Self {
+    ModInt::from(n as i64)
+  }
+}
+
+impl From<u32> for ModInt {
+  fn from(n: u32) -> Self {
+    ModInt::from(n as u64)
+  }
+}
+
+impl From<u64> for ModInt {
+  fn from(n: u64) -> Self {
+    Self::new(n as i64)
+  }
+}
+
+impl From<usize> for ModInt {
+  fn from(n: usize) -> Self {
+    ModInt::from(n as u64)
+  }
+}
+
+impl Into<i32> for ModInt {
+  fn into(self) -> i32 {
+    self.0 as i32
+  }
+}
+
+impl Into<i64> for ModInt {
+  fn into(self) -> i64 {
+    self.0 as i64
+  }
+}
+
+impl Into<isize> for ModInt {
+  fn into(self) -> isize {
+    self.0 as isize
+  }
+}
+
+impl Into<u32> for ModInt {
+  fn into(self) -> u32 {
+    self.0
+  }
+}
+
+impl Into<u64> for ModInt {
+  fn into(self) -> u64 {
+    self.0 as u64
+  }
+}
+
+impl Into<usize> for ModInt {
+  fn into(self) -> usize {
+    self.0 as usize
+  }
+}
+
+impl std::fmt::Display for ModInt {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", self.0)
+  }
+}
+
+impl std::ops::Add for ModInt {
+  type Output = Self;
+
+  fn add(self, rhs: Self) -> Self {
+    Self::new((self.0 + rhs.0) as i64)
+  }
+}
+
+impl std::ops::AddAssign for ModInt {
+  fn add_assign(&mut self, rhs: Self) {
+    *self = *self + rhs;
+  }
+}
+
+impl std::ops::Div for ModInt {
+  type Output = Self;
+
+  fn div(self, rhs: Self) -> Self {
+    self * rhs.inv()
+  }
+}
+
+impl std::ops::DivAssign for ModInt {
+  fn div_assign(&mut self, rhs: Self) {
+    *self = *self / rhs;
+  }
+}
+
+impl std::ops::Mul for ModInt {
+  type Output = Self;
+
+  fn mul(self, rhs: Self) -> Self {
+    Self::new((self.0 as i64) * (rhs.0 as i64))
+  }
+}
+
+impl std::ops::MulAssign for ModInt {
+  fn mul_assign(&mut self, rhs: Self) {
+    *self = *self * rhs;
+  }
+}
+
+impl std::ops::Sub for ModInt {
+  type Output = Self;
+
+  fn sub(self, rhs: Self) -> Self {
+    Self::new((self.0 as i64) - (rhs.0 as i64))
+  }
+}
+
+impl std::ops::SubAssign for ModInt {
+  fn sub_assign(&mut self, rhs: Self) {
+    *self = *self - rhs;
+  }
+}
+
+pub struct ModComb {
+  max: usize,
+  modulo: usize,
+  fac: Vec<usize>,
+  finv: Vec<usize>,
+}
+
+impl ModComb {
+  pub fn new(max: usize, modulo: usize) -> Self {
+    let mut fac = vec![0_usize; max + 1];
+    let mut finv = vec![0_usize; max + 1];
+    let mut inv = vec![0_usize; max + 1];
+    fac[0] = 1;
+    fac[1] = 1;
+    finv[0] = 1;
+    finv[1] = 1;
+    inv[1] = 1;
+    for i in 2..=max {
+      fac[i] = (fac[i - 1] * i) % modulo;
+      inv[i] = modulo - ((inv[modulo % i] * (modulo / i)) % modulo);
+      finv[i] = (finv[i - 1] * inv[i]) % modulo;
+    }
+    Self {
+      max,
+      modulo,
+      fac,
+      finv,
+    }
+  }
+
+  pub fn comb(&self, n: usize, k: usize) -> usize {
+    if n > self.max {
+      panic!();
+    }
+    if n < k {
+      return 0;
+    }
+    (self.fac[n] * ((self.finv[k] * self.finv[n - k]) % self.modulo)) % self.modulo
+  }
+}
+
+const MOD: usize = 1_000_000_007;
+
 fn main() {
   input! {
+    n: usize,
+    av: [usize; n + 1]
   };
+
+  let f = || {
+    let mut map = HashMap::new();
+    for (i, a) in av.iter().enumerate() {
+      let i = i + 1;
+      if let Some(&p) = map.get(a) {
+        return (p, i);
+      } else {
+        map.insert(*a, i);
+      }
+    }
+    unreachable!();
+  };
+  let (l, r) = f();
+
+  let comb = ModComb::new(n + 1, MOD);
+  for k in 1..=n + 1 {
+    let c1 = comb.comb(n + 1, k);
+    let c2 = comb.comb(l - 1 + (n + 1 - r), k - 1);
+    println!("{}", ModInt::from(c1) - ModInt::from(c2));
+  }
 }
