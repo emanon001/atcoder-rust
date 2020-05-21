5495e2c5df4213a2278cd62cbf684d740c7af488
emanon001 <emanon001@gmail.com>
2020-05-22 06:21:45 +0900

    ModComb で ModInt を使用するよう修正

--- competitive/src/mod_comb.rs
+++ competitive/src/mod_comb.rs
@@ -1,41 +1,38 @@
+use crate::mod_int::ModInt;
+
 pub struct ModComb {
   max: usize,
-  modulo: usize,
-  fac: Vec<usize>,
-  finv: Vec<usize>,
+  fac: Vec<ModInt>,
+  finv: Vec<ModInt>,
 }
 
 impl ModComb {
-  pub fn new(max: usize, modulo: usize) -> Self {
-    let mut fac = vec![0_usize; max + 1];
-    let mut finv = vec![0_usize; max + 1];
-    let mut inv = vec![0_usize; max + 1];
-    fac[0] = 1;
-    fac[1] = 1;
-    finv[0] = 1;
-    finv[1] = 1;
-    inv[1] = 1;
+  pub fn new(max: usize) -> Self {
+    let mut fac = vec![ModInt::from(0); max + 1];
+    let mut finv = vec![ModInt::from(0); max + 1];
+    let mut inv = vec![ModInt::from(0); max + 1];
+    fac[0] = ModInt::from(1);
+    fac[1] = ModInt::from(1);
+    finv[0] = ModInt::from(1);
+    finv[1] = ModInt::from(1);
+    inv[1] = ModInt::from(1);
+    let modulo = ModInt::MOD as usize;
     for i in 2..=max {
-      fac[i] = (fac[i - 1] * i) % modulo;
-      inv[i] = modulo - ((inv[modulo % i] * (modulo / i)) % modulo);
-      finv[i] = (finv[i - 1] * inv[i]) % modulo;
+      fac[i] = fac[i - 1] * ModInt::from(i);
+      inv[i] = ModInt::from(ModInt::from(modulo) - (inv[modulo % i] * ModInt::from(modulo / i)));
+      finv[i] = finv[i - 1] * inv[i]
     }
-    Self {
-      max,
-      modulo,
-      fac,
-      finv,
-    }
+    Self { max, fac, finv }
   }
 
-  pub fn comb(&self, n: usize, k: usize) -> usize {
+  pub fn comb(&self, n: usize, k: usize) -> ModInt {
     if n > self.max {
       panic!();
     }
     if n < k {
-      return 0;
+      return ModInt::from(0);
     }
-    (self.fac[n] * ((self.finv[k] * self.finv[n - k]) % self.modulo)) % self.modulo
+    self.fac[n] * self.finv[k] * self.finv[n - k]
   }
 }
 
@@ -45,14 +42,14 @@ mod tests {
 
   #[test]
   fn test_comb() {
-    let comb = ModComb::new(40, 1_000_000_007);
-    assert_eq!(1, comb.comb(0, 0));
-    assert_eq!(1, comb.comb(1, 0));
-    assert_eq!(1, comb.comb(1, 1));
-    assert_eq!(2, comb.comb(2, 1));
-    assert_eq!(635_745_396, comb.comb(39, 10));
-    assert_eq!(676_056_037, comb.comb(39, 11));
-    assert_eq!(847_660_528, comb.comb(40, 10));
-    assert_eq!(311_801_426, comb.comb(40, 11));
+    let comb = ModComb::new(40);
+    assert_eq!(1, comb.comb(0, 0).into());
+    assert_eq!(1, comb.comb(1, 0).into());
+    assert_eq!(1, comb.comb(1, 1).into());
+    assert_eq!(2, comb.comb(2, 1).into());
+    assert_eq!(635_745_396, comb.comb(39, 10).into());
+    assert_eq!(676_056_037, comb.comb(39, 11).into());
+    assert_eq!(847_660_528, comb.comb(40, 10).into());
+    assert_eq!(311_801_426, comb.comb(40, 11).into());
   }
 }
