73ec3e56585b93a8f3fdb93ef7b2ab0e032605d6
emanon001 <emanon001@gmail.com>
2020-05-24 07:31:38 +0900

    solve abc117/d

--- abc117/src/bin/d.rs
+++ abc117/src/bin/d.rs
@@ -10,5 +10,41 @@ use std::collections::*;
 
 fn main() {
   input! {
+    n: usize, k: usize,
+    av: [u64; n]
   };
+
+  let size = 60;
+  // 0: same, 1: lower
+  let mut dp = vec![vec![-1_i64; 2]; size + 1];
+  dp[0][0] = 0;
+  for i in 0..size {
+    let d = size - 1 - i;
+    let mut zero_c = 0;
+    let mut one_c = 0;
+    for &a in &av {
+      if (a >> d) & 1 == 1 {
+        one_c += 1;
+      } else {
+        zero_c += 1;
+      }
+    }
+    let value = |b| -> i64 {
+      let v = if b == 1 { zero_c } else { one_c };
+      v * 2_i64.pow(d as u32)
+    };
+    let bit = (k >> d) & 1;
+    // same -> same
+    dp[i + 1][0] = dp[i][0] + value(bit);
+    // same -> lower
+    if bit == 1 {
+      dp[i + 1][1] = dp[i][0] + value(0);
+    }
+    // lower -> lower
+    if dp[i][1] != -1 {
+      dp[i + 1][1] = std::cmp::max(dp[i + 1][1], dp[i][1] + std::cmp::max(value(0), value(1)));
+    }
+  }
+  let res = std::cmp::max(dp[size][0], dp[size][1]);
+  println!("{}", res);
 }
