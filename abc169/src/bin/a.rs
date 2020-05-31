332b1230718d2daf57432eb4386b68cb238e5d0a
emanon001 <emanon001@gmail.com>
2020-05-31 21:01:02 +0900

    cargo atcoder new abc169

---
+++ abc169/src/bin/a.rs
@@ -0,0 +1,19 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+fn main() {
+    input! {
+        a: usize,
+        b: usize
+    };
+
+    let res = a * b;
+    println!("{}", res);
+}
