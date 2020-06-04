b9d8c05ba93eaa54570b6d8246d457abd63aa1bb
emanon001 <emanon001@gmail.com>
2020-06-05 03:22:04 +0900

    update abc098/d

--- abc098/src/bin/d.rs
+++ abc098/src/bin/d.rs
@@ -8,18 +8,32 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn add(a: u128, c: &mut [usize], digit: usize) {
     for i in 0..digit {
         if (a >> i) & 1 == 1 {
             c[i] += 1;
-        }
-    }
-}
-
-fn subtract(a: u128, c: &mut [usize], digit: usize) {
-    for i in 0..digit {
-        if (a >> i) & 1 == 1 {
-            c[i] -= 1;
         }
     }
 }
@@ -31,25 +45,30 @@ fn solve() {
     };
 
     let digit = 20;
-    let mut res = 0;
-    let mut l = 0;
-    let mut r = 0;
-    let mut c = vec![0; digit];
-    while l <= r && r < n {
-        let a = av[r];
-        add(a, &mut c, digit);
-        let is_ok = c.iter().all(|&x| x <= 1);
-        if is_ok {
-            res += r - l + 1;
-            r += 1;
-        } else {
-            subtract(av[l], &mut c, digit);
-            subtract(av[r], &mut c, digit);
-            l += 1;
-        }
-        // println!("({}, {}), {:?}", l, r, c);
-    }
+    let empty = vec![0; digit];
+    let cusum = av
+        .into_iter()
+        .scan(empty.clone(), |acc, x| {
+            add(x, acc, digit);
+            Some(acc.clone())
+        })
+        .collect::<Vec<_>>();
 
+    let mut res = 0;
+    for i in 0..n {
+        let v = if i == 0 {
+            &empty
+        } else {
+            &cusum[i - 1 as usize]
+        };
+        let j = bsearch(i as i64 - 1, n as i64, |x| {
+            let x = x as usize;
+            let v2 = &cusum[x as usize];
+            (0..digit).all(|d| v2[d] - v[d] <= 1)
+        })
+        .unwrap();
+        res += j as usize - i + 1;
+    }
     println!("{}", res);
 }
 
