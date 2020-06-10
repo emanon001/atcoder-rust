ee7f0aab5aaf5f1255698c8c1f0e1cc3961d96b5
emanon001 <emanon001@gmail.com>
2020-06-10 23:15:38 +0900

    use `ModInt::zero()` & `ModInt::one()`

--- competitive/src/mod_comb.rs
+++ competitive/src/mod_comb.rs
@@ -8,14 +8,14 @@ pub struct ModComb {
 
 impl ModComb {
     pub fn new(max: usize) -> Self {
-        let mut fac = vec![ModInt::from(0); max + 1];
-        let mut finv = vec![ModInt::from(0); max + 1];
-        let mut inv = vec![ModInt::from(0); max + 1];
-        fac[0] = ModInt::from(1);
-        fac[1] = ModInt::from(1);
-        finv[0] = ModInt::from(1);
-        finv[1] = ModInt::from(1);
-        inv[1] = ModInt::from(1);
+        let mut fac = vec![ModInt::zero(); max + 1];
+        let mut finv = vec![ModInt::zero(); max + 1];
+        let mut inv = vec![ModInt::zero(); max + 1];
+        fac[0] = ModInt::one();
+        fac[1] = ModInt::one();
+        finv[0] = ModInt::one();
+        finv[1] = ModInt::one();
+        inv[1] = ModInt::one();
         let modulo = ModInt::MOD as usize;
         for i in 2..=max {
             fac[i] = fac[i - 1] * ModInt::from(i);
@@ -31,7 +31,7 @@ impl ModComb {
             panic!();
         }
         if n < k {
-            return ModInt::from(0);
+            return ModInt::zero();
         }
         self.fac[n] * self.finv[k] * self.finv[n - k]
     }
@@ -44,9 +44,9 @@ mod tests {
     #[test]
     fn test_comb() {
         let comb = ModComb::new(40);
-        assert_eq!(comb.comb(0, 0), ModInt::from(1));
-        assert_eq!(comb.comb(1, 0), ModInt::from(1));
-        assert_eq!(comb.comb(1, 1), ModInt::from(1));
+        assert_eq!(comb.comb(0, 0), ModInt::one());
+        assert_eq!(comb.comb(1, 0), ModInt::one());
+        assert_eq!(comb.comb(1, 1), ModInt::one());
         assert_eq!(comb.comb(2, 1), ModInt::from(2));
         assert_eq!(comb.comb(39, 10), ModInt::from(635_745_396));
         assert_eq!(comb.comb(39, 11), ModInt::from(676_056_037));
