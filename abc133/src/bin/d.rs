eb573cf30c7a60e5a4d9e1cde18ec6893efc7325
emanon001 <emanon001@gmail.com>
2020-06-16 15:19:45 +0900

    solve abc133/d

--- abc133/src/bin/d.rs
+++ abc133/src/bin/d.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [u64; n]
     };
+
+    let sum = av.iter().sum::<u64>();
+    let mut x = sum;
+    for i in 0..n / 2 {
+        x -= av[i * 2 + 1] * 2;
+    }
+    let mut res = Vec::new();
+    res.push(x);
+    for i in 0..n - 1 {
+        x = (av[i] * 2) - x;
+        res.push(x);
+    }
+    println!("{}", res.into_iter().join(" "));
 }
 
 fn main() {
