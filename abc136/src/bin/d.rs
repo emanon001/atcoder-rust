772a5b431e94be387fb9a0ad7598800106a6dd8f
emanon001 <emanon001@gmail.com>
2020-06-17 14:24:35 +0900

    solve abc136/d

--- abc136/src/bin/d.rs
+++ abc136/src/bin/d.rs
@@ -10,7 +10,39 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        mut s: Chars
     };
+
+    s.push('R');
+    let mut res = vec![0; s.len()];
+    let mut is_right = true;
+    let mut l = 0;
+    for i in 1..s.len() {
+        match s[i] {
+            'R' => {
+                if !is_right {
+                    let len = i - l;
+                    let c = len / 2;
+                    res[l - 1] += c;
+                    res[l] += len - c;
+                    is_right = true;
+                    l = i;
+                }
+            }
+            'L' => {
+                if is_right {
+                    let len = i - l;
+                    let c = len / 2;
+                    res[i] += c;
+                    res[i - 1] += len - c;
+                    is_right = false;
+                    l = i;
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+    println!("{}", res.into_iter().take(s.len() - 1).join(" "));
 }
 
 fn main() {
