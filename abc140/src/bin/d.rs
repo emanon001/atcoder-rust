b1ae0b448fafd707d2a32119379701f87f0cece7
emanon001 <emanon001@gmail.com>
2020-06-24 19:35:09 +0900

    solve abc140/d

--- abc140/src/bin/d.rs
+++ abc140/src/bin/d.rs
@@ -10,7 +10,31 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, mut k: usize,
+        s: Chars
     };
+
+    let other_ch = 'X';
+    let mut sum = 0;
+    let mut prev = other_ch;
+    let mut count = 0;
+    let mut section = 0;
+    for ch in s.into_iter().chain(vec![other_ch].into_iter()) {
+        if ch != prev {
+            section += 1;
+            if count > 1 {
+                sum += count - 1;
+            }
+            count = 1;
+        } else {
+            count += 1;
+        }
+        prev = ch;
+    }
+
+    let m = std::cmp::min(section / 2, k);
+    let res = std::cmp::min(sum + m * 2, n - 1);
+    println!("{}", res);
 }
 
 fn main() {
