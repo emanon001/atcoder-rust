5b5e97f662bf774cf97c8c0369b4095f986dd73e
emanon001 <emanon001@gmail.com>
2020-06-26 04:27:17 +0900

    solve abc142/d

--- abc142/src/bin/d.rs
+++ abc142/src/bin/d.rs
@@ -8,9 +8,36 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn prime_factor(n: u64) -> std::collections::HashMap<u64, u64> {
+    if n < 2 {
+        return std::collections::HashMap::new();
+    }
+    let mut res = std::collections::HashMap::new();
+    let mut n = n;
+    let mut i = 2;
+    while i * i <= n {
+        while n % i == 0 {
+            let c = res.entry(i).or_insert(0);
+            *c += 1;
+            n /= i;
+        }
+        i += 1;
+    }
+    if n != 1 {
+        res.insert(n, 1);
+    }
+    res
+}
+
 fn solve() {
     input! {
+        a: u64, b: u64
     };
+
+    let aset = prime_factor(a).keys().copied().collect::<HashSet<_>>();
+    let bset = prime_factor(b).keys().copied().collect::<HashSet<_>>();
+    let res = aset.intersection(&bset).count() + 1;
+    println!("{}", res);
 }
 
 fn main() {
