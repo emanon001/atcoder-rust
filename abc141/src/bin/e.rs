5a6b29acc05d9255ff99c531133d1ff8dc5f1b16
emanon001 <emanon001@gmail.com>
2020-06-26 01:23:13 +0900

    solve abc141/e

--- abc141/src/bin/e.rs
+++ abc141/src/bin/e.rs
@@ -8,9 +8,47 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn zalgo(s: &[char]) -> Vec<usize> {
+    let len = s.len();
+    let mut res = vec![0; len];
+    res[0] = len;
+    let mut i = 1;
+    let mut j = 0;
+    while i < len {
+        while i + j < len && s[j] == s[i + j] {
+            j += 1;
+        }
+        res[i] = j;
+        if j == 0 {
+            i += 1;
+            continue;
+        }
+        let mut k = 1;
+        while i + k < len && k + res[k] < j {
+            res[i + k] = res[k];
+            k += 1;
+        }
+        i += k;
+        j -= k;
+    }
+    res
+}
+
 fn solve() {
     input! {
+        n: usize,
+        s: Chars
     };
+
+    let mut res = 0;
+    for i in 0..n {
+        let lv = zalgo(&s[i..]);
+        for j in 0..n - i {
+            let l = lv[j].min(j);
+            res = res.max(l);
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
