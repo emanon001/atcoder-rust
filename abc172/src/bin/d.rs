2f0284861dbaf634d04a1466009d24da305594be
emanon001 <emanon001@gmail.com>
2020-06-27 21:23:18 +0900

    solve abc172/d

--- abc172/src/bin/d.rs
+++ abc172/src/bin/d.rs
@@ -10,7 +10,20 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize
     };
+
+    let mut res = 0_u64;
+    let mut v = vec![0_u64; n + 1];
+    for a in 1..=n {
+        let mut b = a;
+        while b <= n {
+            v[b] += 1;
+            b += a;
+        }
+        res += a as u64 * v[a];
+    }
+    println!("{}", res);
 }
 
 fn main() {
