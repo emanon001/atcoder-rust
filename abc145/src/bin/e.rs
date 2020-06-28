da4bb6d5b3c36b97043e26c25b755d335880f724
emanon001 <emanon001@gmail.com>
2020-06-29 07:19:39 +0900

    solve abc145/e

--- abc145/src/bin/e.rs
+++ abc145/src/bin/e.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, t: usize,
+        mut abv: [(usize, usize); n]
     };
+
+    abv.sort();
+    let mut dp = vec![vec![0; t + 1]; n + 1];
+    for i in 0..n {
+        let (a, b) = abv[i];
+        for j in 0..=t {
+            let to_i = i + 1;
+            let from_v = dp[i][j];
+
+            // 食べない場合
+            let to_j = j;
+            let to_v = dp[to_i][to_j];
+            dp[to_i][to_j] = to_v.max(from_v);
+
+            // 食べた場合
+            if j < t {
+                let to_j = (j + a).min(t);
+                let to_v = dp[to_i][to_j];
+                dp[to_i][to_j] = to_v.max(from_v + b);
+            }
+        }
+    }
+    println!("{}", dp[n][t]);
 }
 
 fn main() {
