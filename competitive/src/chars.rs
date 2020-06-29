fcc415e913d333f7cb01bca8db46ad20eda12476
emanon001 <emanon001@gmail.com>
2020-06-29 23:05:26 +0900

    `chars::shift_*_char` で左方向にシフトできるよう変更

--- competitive/src/chars.rs
+++ competitive/src/chars.rs
@@ -12,19 +12,29 @@ pub fn make_ascii_uppercase_chars() -> Vec<char> {
         .collect::<Vec<_>>()
 }
 
-pub fn shift_ascii_lowercase_char(ch: char, n: usize) -> char {
+pub fn shift_ascii_lowercase_char(ch: char, n: isize) -> char {
     assert!(ch.is_ascii_lowercase());
     let base = 0x61_u8;
-    let m = 26;
-    let offset = (ch as u8 - base + (n % m) as u8) % m as u8;
+    let m = 26_isize;
+    let ch_pos = ch as u8 - base;
+    let offset = if n >= 0 {
+        (ch_pos as isize + n) % m
+    } else {
+        (m - (ch_pos as isize - n.abs()).abs() % m) % m
+    } as u8;
     char::from(base + offset)
 }
 
-pub fn shift_ascii_uppercase_char(ch: char, n: usize) -> char {
+pub fn shift_ascii_uppercase_char(ch: char, n: isize) -> char {
     assert!(ch.is_ascii_uppercase());
     let base = 0x41_u8;
-    let m = 26;
-    let offset = (ch as u8 - base + (n % m) as u8) % m as u8;
+    let m = 26_u8;
+    let ch_pos = ch as u8 - base;
+    let offset = if n >= 0 {
+        (ch_pos + (n % m as isize) as u8) % m as u8
+    } else {
+        (m as u8 - (ch_pos as i8 - (n.abs() % m as isize) as i8).abs() as u8) % m
+    };
     char::from(base + offset)
 }
 
@@ -57,6 +67,7 @@ mod tests {
     #[test]
     fn test_shift_ascii_lowercase_char() {
         let ch = 'a';
+        // right
         assert_eq!(shift_ascii_lowercase_char(ch, 0), 'a');
         assert_eq!(shift_ascii_lowercase_char(ch, 1), 'b');
         assert_eq!(shift_ascii_lowercase_char(ch, 25), 'z');
@@ -64,11 +75,19 @@ mod tests {
         assert_eq!(shift_ascii_lowercase_char(ch, 27), 'b');
         assert_eq!(shift_ascii_lowercase_char(ch, 51), 'z');
         assert_eq!(shift_ascii_lowercase_char(ch, 52), 'a');
+        // left
+        assert_eq!(shift_ascii_lowercase_char(ch, -1), 'z');
+        assert_eq!(shift_ascii_lowercase_char(ch, -25), 'b');
+        assert_eq!(shift_ascii_lowercase_char(ch, -26), 'a');
+        assert_eq!(shift_ascii_lowercase_char(ch, -27), 'z');
+        assert_eq!(shift_ascii_lowercase_char(ch, -51), 'b');
+        assert_eq!(shift_ascii_lowercase_char(ch, -52), 'a');
     }
 
     #[test]
     fn test_shift_ascii_uppercase_char() {
         let ch = 'A';
+        // right
         assert_eq!(shift_ascii_uppercase_char(ch, 0), 'A');
         assert_eq!(shift_ascii_uppercase_char(ch, 1), 'B');
         assert_eq!(shift_ascii_uppercase_char(ch, 25), 'Z');
@@ -76,5 +95,12 @@ mod tests {
         assert_eq!(shift_ascii_uppercase_char(ch, 27), 'B');
         assert_eq!(shift_ascii_uppercase_char(ch, 51), 'Z');
         assert_eq!(shift_ascii_uppercase_char(ch, 52), 'A');
+        // left
+        assert_eq!(shift_ascii_uppercase_char(ch, -1), 'Z');
+        assert_eq!(shift_ascii_uppercase_char(ch, -25), 'B');
+        assert_eq!(shift_ascii_uppercase_char(ch, -26), 'A');
+        assert_eq!(shift_ascii_uppercase_char(ch, -27), 'Z');
+        assert_eq!(shift_ascii_uppercase_char(ch, -51), 'B');
+        assert_eq!(shift_ascii_uppercase_char(ch, -52), 'A');
     }
 }
