f7f137a876c31de8d0b806051f89280db0c3db8e
emanon001 <emanon001@gmail.com>
2020-06-30 08:26:57 +0900

    solve abc146/e

--- abc146/src/bin/e.rs
+++ abc146/src/bin/e.rs
@@ -10,7 +10,33 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        av: [usize; n]
     };
+
+    let iter = av.into_iter().map(|a| a - 1);
+    let cusum = vec![0]
+        .into_iter()
+        .chain(iter)
+        .scan(0, |acc, a| {
+            *acc = (*acc + a) % k;
+            Some(*acc)
+        })
+        .collect::<Vec<_>>();
+    let mut counts = HashMap::new();
+    let mut que = VecDeque::new();
+    let mut res = 0_u64;
+    for r in 0..n + 1 {
+        let a = cusum[r];
+        res += counts.get(&a).unwrap_or(&0);
+        *counts.entry(a).or_insert(0) += 1;
+        que.push_back(a);
+        if que.len() == k {
+            let b = que.pop_front().unwrap();
+            *counts.entry(b).or_insert(1) -= 1;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
