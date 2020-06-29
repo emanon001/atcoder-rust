1e3d7ff35c6cb75d200201b42d67c8275869bca0
emanon001 <emanon001@gmail.com>
2020-06-30 02:25:21 +0900

    solve abc146/d

--- abc146/src/bin/d.rs
+++ abc146/src/bin/d.rs
@@ -10,7 +10,39 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        edges: [(Usize1, Usize1); n - 1]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for &(u, v) in &edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut colors = HashMap::new();
+    let mut que = VecDeque::new();
+    let not_found = std::usize::MAX;
+    // (u, parent)
+    que.push_back((0, not_found));
+    while let Some((u, parent)) = que.pop_front() {
+        let pcolor = *colors.get(&(parent, u)).unwrap_or(&not_found);
+        let mut color = 1;
+        for &v in &graph[u] {
+            if v == parent {
+                continue;
+            }
+            if color == pcolor {
+                color += 1;
+            }
+            colors.insert((u, v), color);
+            que.push_back((v, u));
+            color += 1;
+        }
+    }
+    println!("{}", colors.values().max().unwrap());
+    for e in edges {
+        println!("{}", colors.get(&e).unwrap());
+    }
 }
 
 fn main() {
