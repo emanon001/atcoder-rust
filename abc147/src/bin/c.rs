6c469060fe492d31f8b7efe078dbffd0e9f6ae21
emanon001 <emanon001@gmail.com>
2020-07-02 19:09:11 +0900

    solve abc147/c

--- abc147/src/bin/c.rs
+++ abc147/src/bin/c.rs
@@ -10,7 +10,37 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        xyvv: [[(Usize1, usize)]]
     };
+
+    let n = xyvv.len();
+    let mut res = 0;
+    for bits in 0..(1 << n) {
+        let mut honest_set = HashSet::new();
+        let mut unkindness_set = HashSet::new();
+        for i in 0..n {
+            let is_honest = (bits >> i) & 1 == 1;
+            if is_honest {
+                honest_set.insert(i);
+            } else {
+                unkindness_set.insert(i);
+            };
+            for &(x, y) in &xyvv[i] {
+                if is_honest {
+                    if y == 0 {
+                        unkindness_set.insert(x);
+                    } else {
+                        honest_set.insert(x);
+                    }
+                }
+            }
+        }
+        let is_ok = honest_set.intersection(&unkindness_set).count() == 0;
+        if is_ok && honest_set.len() > res {
+            res = honest_set.len();
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
