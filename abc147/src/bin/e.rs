118d9dd922723dc28393261529907c75d7d60920
emanon001 <emanon001@gmail.com>
2020-07-03 12:10:44 +0900

    solve abc147/e

--- abc147/src/bin/e.rs
+++ abc147/src/bin/e.rs
@@ -10,7 +10,50 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid_a: [[usize; w]; h],
+        grid_b: [[usize; w]; h],
     };
+
+    let max_v = 80_usize * 160;
+    let mut used = vec![vec![vec![false; max_v * 2 + 1]; w]; h];
+    used[0][0][max_v] = true;
+    let mut res = std::isize::MAX;
+    for i in 0..h {
+        for j in 0..w {
+            let a = grid_a[i][j];
+            let b = grid_b[i][j];
+            // ex: max_v: 2
+            // -2, -1, 0, 1, 2
+            for k in 0..max_v * 2 + 1 {
+                if used[i][j][k] {
+                    if i == h - 1 && j == w - 1 {
+                        let v = ((k + a - b) as isize - max_v as isize).abs();
+                        if v < res {
+                            res = v;
+                        }
+                        let v = ((k + b - a) as isize - max_v as isize).abs();
+                        if v < res {
+                            res = v;
+                        }
+                    }
+                    if i + 1 < h {
+                        let new_i = i + 1;
+                        let new_j = j;
+                        used[new_i][new_j][k + a - b] = true;
+                        used[new_i][new_j][k + b - a] = true;
+                    }
+                    if j + 1 < w {
+                        let new_i = i;
+                        let new_j = j + 1;
+                        used[new_i][new_j][k + a - b] = true;
+                        used[new_i][new_j][k + b - a] = true;
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
