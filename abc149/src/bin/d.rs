d38ed1012e0449a0a21da3d0c1d5493b867ea830
emanon001 <emanon001@gmail.com>
2020-07-03 18:07:50 +0900

    solve abc149/d

--- abc149/src/bin/d.rs
+++ abc149/src/bin/d.rs
@@ -10,7 +10,50 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        r: usize, s: usize, p: usize,
+        t: Chars
     };
+
+    let mut res = 0_usize;
+    for i in 0..k {
+        let mut prev = 'x';
+        let mut i = i;
+        let mut sum = 0;
+        while i < n {
+            let ch = t[i];
+            match ch {
+                'r' => {
+                    if prev != 'p' {
+                        sum += p;
+                        prev = 'p';
+                    } else {
+                        prev = 'x';
+                    }
+                }
+                's' => {
+                    if prev != 'r' {
+                        sum += r;
+                        prev = 'r';
+                    } else {
+                        prev = 'x';
+                    }
+                }
+                'p' => {
+                    if prev != 's' {
+                        sum += s;
+                        prev = 's';
+                    } else {
+                        prev = 'x';
+                    }
+                }
+                _ => unreachable!(),
+            }
+            i += k;
+        }
+        res += sum;
+    }
+    println!("{}", res);
 }
 
 fn main() {
