e0e0db7b1779b9ca16f445f5084c26737aca7e93
emanon001 <emanon001@gmail.com>
2020-07-05 21:24:03 +0900

    solve abc173/d

--- abc173/src/bin/d.rs
+++ abc173/src/bin/d.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        mut av: [u64; n]
     };
+
+    av.sort_by_key(|&x| -(x as i64));
+    let mut res = av[0];
+    let mut rest = n - 2;
+    let mut i = 1;
+    while rest > 0 {
+        rest -= 1;
+        res += av[i];
+        if rest > 0 {
+            rest -= 1;
+            res += av[i];
+        }
+        i += 1;
+    }
+    println!("{}", res);
 }
 
 fn main() {
