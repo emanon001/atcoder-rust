c6dbfcfaa6b3af23157d82827511551e8a406a62
emanon001 <emanon001@gmail.com>
2020-07-05 21:00:55 +0900

    cargo atcoder new abc173

---
+++ abc173/src/bin/a.rs
@@ -0,0 +1,30 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+fn solve() {
+    input! {
+        n: usize
+    };
+
+    let res = n % 1000;
+    println!("{}", res);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("big stack size".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(|| {
+            solve();
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+}
