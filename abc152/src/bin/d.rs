ce71f6d031ea6b7c23d6078461b9fba9c38dc664
emanon001 <emanon001@gmail.com>
2020-07-08 13:46:13 +0900

    solve abc152/d

--- abc152/src/bin/d.rs
+++ abc152/src/bin/d.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize
     };
+
+    let mut ht_counts = HashMap::new();
+    for x in 1..=n {
+        let chars = x
+            .to_string()
+            .chars()
+            .map(|ch| ch.to_digit(10).unwrap() as usize)
+            .collect::<Vec<_>>();
+        let head = chars[0];
+        let tail = chars[chars.len() - 1];
+        *ht_counts.entry((head, tail)).or_insert(0) += 1;
+    }
+    let mut res = 0_usize;
+    for h in 1..=9 {
+        for t in 1..=9 {
+            match (ht_counts.get(&(h, t)), ht_counts.get(&(t, h))) {
+                (Some(&a), Some(&b)) => {
+                    res += a * b;
+                }
+                _ => {}
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
