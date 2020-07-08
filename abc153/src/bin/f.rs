cc9a6160d5b51e3be8d701144dc6e7cbd491ddb4
emanon001 <emanon001@gmail.com>
2020-07-08 19:45:44 +0900

    solve abc153/f

--- abc153/src/bin/f.rs
+++ abc153/src/bin/f.rs
@@ -8,9 +8,62 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, d: u64, a: u64,
+        mut xhv: [(u64, u64); n]
     };
+
+    xhv.sort();
+    let res = bsearch(10.pow(16), 0, |x| {
+        let x = x as u64;
+        let mut all_count = 0_u64;
+        let mut bomb_area: VecDeque<(u64, u64)> = VecDeque::new();
+        let mut cur_count = 0_u64;
+        for &(x, h) in &xhv {
+            while let Some((bx, c)) = bomb_area.pop_front() {
+                if x > bx {
+                    cur_count -= c;
+                } else {
+                    bomb_area.push_front((bx, c));
+                    break;
+                }
+            }
+            if cur_count * a >= h {
+                continue;
+            }
+            let rest = h - cur_count * a;
+            let c = (rest + a - 1) / a;
+            all_count += c;
+            cur_count += c;
+            bomb_area.push_back((x + d * 2, c));
+        }
+        all_count <= x
+    })
+    .unwrap();
+    println!("{}", res);
 }
 
 fn main() {
