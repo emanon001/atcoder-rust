d042acf6a361abbd7d66abc28a29ecd8d2b42e3e
emanon001 <emanon001@gmail.com>
2020-07-10 14:57:20 +0900

    solve abc154/d

--- abc154/src/bin/d.rs
+++ abc154/src/bin/d.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        pv: [usize; n]
     };
+
+    let cusum = vec![0_f64]
+        .into_iter()
+        .chain(pv.into_iter().scan(0_f64, |acc, p| {
+            *acc += ((1 + p) * p) as f64 / 2_f64 / p as f64;
+            Some(*acc)
+        }))
+        .collect::<Vec<_>>();
+    let mut res = 0_f64;
+    for i in 0..n {
+        if i + k > n {
+            continue;
+        }
+        let sum = cusum[i + k] - cusum[i];
+        if sum > res {
+            res = sum;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
