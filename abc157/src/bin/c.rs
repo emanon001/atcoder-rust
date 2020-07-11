18ef103107e735a84784a2e07049837da23ecbca
emanon001 <emanon001@gmail.com>
2020-07-12 04:36:37 +0900

    solve abc157/c

--- abc157/src/bin/c.rs
+++ abc157/src/bin/c.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        scv: [(Usize1, char); m]
     };
+
+    let mut res = std::usize::MAX;
+    for x in 0..10.pow(n as u32) {
+        let digits = x.to_string().chars().collect::<Vec<char>>();
+        if digits.len() != n {
+            continue;
+        }
+        let is_ok = scv.iter().all(|&(s, c)| s < digits.len() && digits[s] == c);
+        if is_ok && x < res {
+            res = x;
+        }
+    }
+    let res = if res == std::usize::MAX {
+        -1
+    } else {
+        res as isize
+    };
+    println!("{}", res);
 }
 
 fn main() {
