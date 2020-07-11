dfaf6697e18e2e22702f51de4a8833611260ba37
emanon001 <emanon001@gmail.com>
2020-07-12 04:26:43 +0900

    solve abc157/{a-b}

--- abc157/src/bin/b.rs
+++ abc157/src/bin/b.rs
@@ -10,7 +10,43 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        grid: [[usize; 3]; 3],
+        n: usize,
+        bv: [usize; n]
     };
+
+    let mut marks = vec![vec![false; 3]; 3];
+    for b in bv {
+        for i in 0..3 {
+            for j in 0..3 {
+                if grid[i][j] == b {
+                    marks[i][j] = true;
+                }
+            }
+        }
+    }
+    let mut count = 0;
+    // 横
+    for i in 0..3 {
+        if (0..3).all(|j| marks[i][j]) {
+            count += 1;
+        }
+    }
+    // 縦
+    for j in 0..3 {
+        if (0..3).all(|i| marks[i][j]) {
+            count += 1;
+        }
+    }
+    // 斜め
+    if (0..3).all(|ij| marks[ij][ij]) {
+        count += 1;
+    }
+    if marks[0][2] && marks[1][1] && marks[2][0] {
+        count += 1;
+    }
+    let res = if count > 0 { "Yes" } else { "No" };
+    println!("{}", res);
 }
 
 fn main() {
