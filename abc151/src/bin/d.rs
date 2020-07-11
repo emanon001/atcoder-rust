494c861244923153ef0b1200495a6a8e44bd26f3
emanon001 <emanon001@gmail.com>
2020-07-11 20:38:51 +0900

    update abc151/d

--- abc151/src/bin/d.rs
+++ abc151/src/bin/d.rs
@@ -8,46 +8,40 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
-pub struct Graph {
-    graph: Vec<Vec<usize>>,
-    vn: usize,
+#[derive(Clone)]
+pub struct UnweightedGraph {
+    graph: WeightedGraph,
 }
 
-type Edge = (usize, usize);
-impl Graph {
-    pub fn new(edges: &[Edge], vn: usize) -> Self {
-        let mut graph = vec![Vec::new(); vn];
-        for &(u, v) in edges {
-            graph[u].push(v);
-            graph[v].push(u);
-        }
-        Self { graph, vn }
+pub type UnweightedEdge = (usize, usize);
+impl UnweightedGraph {
+    pub fn new(edges: &[UnweightedEdge], vn: usize) -> Self {
+        let edges = Self::to_weighted_edges(&edges);
+        let graph = WeightedGraph::new(&edges, vn);
+        Self { graph }
     }
 
-    pub fn new_directed(edges: &[Edge], vn: usize) -> Self {
-        let mut graph = vec![Vec::new(); vn];
-        for &(u, v) in edges {
-            graph[u].push(v);
-        }
-        Self { graph, vn }
+    pub fn new_directed(edges: &[UnweightedEdge], vn: usize) -> Self {
+        let edges = Self::to_weighted_edges(&edges);
+        let graph = WeightedGraph::new_directed(&edges, vn);
+        Self { graph }
     }
 
-    pub fn add_directed_edge(&mut self, e: Edge) {
-        self.graph[e.0].push(e.1);
+    pub fn add_directed_edge(&mut self, e: UnweightedEdge) {
+        self.graph.add_directed_edge((e.0, e.1, 1));
     }
 
-    pub fn add_edge(&mut self, e: Edge) {
-        self.graph[e.0].push(e.1);
-        self.graph[e.1].push(e.0);
+    pub fn add_edge(&mut self, e: UnweightedEdge) {
+        self.graph.add_edge((e.0, e.1, 1));
     }
 
     pub fn shortest_path(&self, start: usize) -> Vec<Option<usize>> {
-        let mut cost_list = vec![None; self.vn];
+        let mut cost_list = vec![None; self.graph.vn];
         let mut que = std::collections::VecDeque::new();
         cost_list[start] = Some(0);
         que.push_back(start);
         while let Some(u) = que.pop_front() {
-            for &v in &self.graph[u] {
+            for &(v, _) in &self.graph.graph[u] {
                 if cost_list[v].is_some() {
                     continue;
                 }
@@ -57,6 +51,181 @@ impl Graph {
             }
         }
         cost_list
+    }
+
+    pub fn to_weighted_graph(&self) -> WeightedGraph {
+        self.graph.clone()
+    }
+
+    fn to_weighted_edges(edges: &[UnweightedEdge]) -> Vec<WeightedEdge> {
+        edges
+            .into_iter()
+            .map(|(u, v)| (*u, *v, 1))
+            .collect::<Vec<_>>()
+    }
+}
+
+#[derive(Clone)]
+pub struct WeightedGraph {
+    graph: Vec<Vec<(usize, i64)>>,
+    vn: usize,
+}
+
+pub type WeightedEdge = (usize, usize, i64);
+impl WeightedGraph {
+    const INF: i64 = 1 << 60;
+
+    pub fn new(edges: &[WeightedEdge], vn: usize) -> Self {
+        let mut graph = vec![Vec::new(); vn];
+        for &(u, v, w) in edges {
+            graph[u].push((v, w));
+            graph[v].push((u, w));
+        }
+        Self { graph, vn }
+    }
+
+    pub fn new_directed(edges: &[WeightedEdge], vn: usize) -> Self {
+        let mut graph = vec![Vec::new(); vn];
+        for &(u, v, w) in edges {
+            graph[u].push((v, w));
+        }
+        Self { graph, vn }
+    }
+
+    pub fn add_directed_edge(&mut self, e: WeightedEdge) {
+        self.graph[e.0].push((e.1, e.2));
+    }
+
+    pub fn add_edge(&mut self, e: WeightedEdge) {
+        self.graph[e.0].push((e.1, e.2));
+        self.graph[e.1].push((e.0, e.2));
+    }
+
+    pub fn bellman_ford(&self, s: usize) -> Option<Vec<Option<i64>>> {
+        let vn = self.vn;
+        let inf = Self::INF;
+        let mut cost_list = vec![inf; vn];
+        cost_list[s] = 0;
+        for c in 0..vn {
+            for u in 0..vn {
+                for &(v, w) in &self.graph[u] {
+                    if cost_list[u] == inf {
+                        continue;
+                    }
+                    let new_cost = cost_list[u] + w;
+                    if new_cost < cost_list[v] {
+                        cost_list[v] = new_cost;
+                        if c == vn - 1 {
+                            return None;
+                        }
+                    }
+                }
+            }
+        }
+        Some(self.optionalize(cost_list))
+    }
+
+    pub fn prim(&self) -> i64 {
+        let mut used = std::collections::HashSet::new();
+        let mut heap = std::collections::BinaryHeap::new();
+
+        let mut res = 0_i64;
+        heap.push(std::cmp::Reverse((0_i64, 0)));
+        while let Some(std::cmp::Reverse((weight, u))) = heap.pop() {
+            if used.contains(&u) {
+                continue;
+            }
+            used.insert(u);
+            res += weight;
+            for &(v, w) in &self.graph[u] {
+                if used.contains(&v) {
+                    continue;
+                }
+                heap.push(std::cmp::Reverse((w, v)));
+            }
+        }
+        res
+    }
+
+    pub fn reachable_vertexes(&self, s: usize) -> std::collections::HashSet<usize> {
+        let mut visited = std::collections::HashSet::new();
+        let mut queue = std::collections::VecDeque::new();
+        visited.insert(s);
+        queue.push_back(s);
+        while let Some(u) = queue.pop_front() {
+            for &(v, _) in &self.graph[u] {
+                if visited.contains(&v) {
+                    continue;
+                }
+                visited.insert(v);
+                queue.push_back(v);
+            }
+        }
+        visited
+    }
+
+    pub fn rev(&self) -> WeightedGraph {
+        let mut edges = Vec::new();
+        for u in 0..self.vn {
+            for &(v, w) in &self.graph[u] {
+                edges.push((v, u, w));
+            }
+        }
+        Self::new_directed(&edges, self.vn)
+    }
+
+    pub fn shortest_path(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![Self::INF; self.vn];
+        let mut heap = std::collections::BinaryHeap::new();
+
+        cost_list[start] = 0;
+        heap.push(std::cmp::Reverse((0_i64, start)));
+
+        while let Some(std::cmp::Reverse((cost, u))) = heap.pop() {
+            if cost > cost_list[u] {
+                continue;
+            }
+            for &(v, w) in &self.graph[u] {
+                let new_cost = cost + w;
+                if new_cost < cost_list[v] {
+                    heap.push(std::cmp::Reverse((new_cost, v)));
+                    cost_list[v] = new_cost;
+                }
+            }
+        }
+        self.optionalize(cost_list)
+    }
+
+    pub fn warshall_floyd(&self) -> Vec<Vec<Option<i64>>> {
+        let inf = Self::INF;
+        let vn = self.vn;
+        let mut cost_list = vec![vec![inf; vn]; vn];
+        for u in 0..vn {
+            for &(v, w) in &self.graph[u] {
+                cost_list[u][v] = w;
+            }
+        }
+        for i in 0..vn {
+            cost_list[i][i] = 0;
+        }
+        for k in 0..vn {
+            for i in 0..vn {
+                for j in 0..vn {
+                    cost_list[i][j] =
+                        std::cmp::min(cost_list[i][j], cost_list[i][k] + cost_list[k][j]);
+                }
+            }
+        }
+        cost_list
+            .into_iter()
+            .map(|v| self.optionalize(v))
+            .collect::<Vec<_>>()
+    }
+
+    fn optionalize(&self, v: Vec<i64>) -> Vec<Option<i64>> {
+        v.into_iter()
+            .map(|x| if x == Self::INF { None } else { Some(x) })
+            .collect::<Vec<_>>()
     }
 }
 
@@ -68,7 +237,7 @@ pub struct Grid {
     dirs: Vec<(isize, isize)>,
 }
 
-type VertexTable = std::collections::HashMap<(usize, usize), usize>;
+pub type VertexTable = std::collections::HashMap<(usize, usize), usize>;
 impl Grid {
     // 上下左右 (i, j)
     #[allow(dead_code)]
@@ -103,7 +272,7 @@ impl Grid {
         }
     }
 
-    pub fn to_graph(&self) -> (Graph, VertexTable) {
+    pub fn to_graph(&self) -> (UnweightedGraph, VertexTable) {
         let mut edges = Vec::new();
         let mut vertex_table = std::collections::HashMap::new();
         let mut v = 0;
@@ -133,7 +302,7 @@ impl Grid {
                 }
             }
         }
-        let graph = Graph::new_directed(&edges, vertex_table.len());
+        let graph = UnweightedGraph::new_directed(&edges, vertex_table.len());
         (graph, vertex_table)
     }
 
