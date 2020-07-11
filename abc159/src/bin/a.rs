9e11b086602e0ea90d9665ea144c27a245ff50a2
emanon001 <emanon001@gmail.com>
2020-07-12 06:21:15 +0900

    solve abc159/{a-b}

--- abc159/src/bin/a.rs
+++ abc159/src/bin/a.rs
@@ -10,7 +10,13 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize
     };
+
+    let nc = if n >= 2 { n * (n - 1) / 2 } else { 0 };
+    let mc = if m >= 2 { m * (m - 1) / 2 } else { 0 };
+    let res = nc + mc;
+    println!("{}", res);
 }
 
 fn main() {
--- abc159/src/bin/b.rs
+++ abc159/src/bin/b.rs
@@ -8,9 +8,21 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn is_palindrome(s: &[char]) -> bool {
+    let rev = s.into_iter().copied().rev().collect::<Vec<_>>();
+    s == &rev[..]
+}
+
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let len = s.len();
+    let mid = len / 2;
+    let is_ok = is_palindrome(&s) && is_palindrome(&s[0..mid]) && is_palindrome(&s[mid + 1..]);
+    let res = if is_ok { "Yes" } else { "No" };
+    println!("{}", res);
 }
 
 fn main() {
