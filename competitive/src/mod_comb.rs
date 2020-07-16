44bb38d5dfde65e876d2dfec3d1d80b3ae7365cf
emanon001 <emanon001@gmail.com>
2020-07-17 06:18:18 +0900

    `comb` -> `c`

--- competitive/src/mod_comb.rs
+++ competitive/src/mod_comb.rs
@@ -26,7 +26,7 @@ impl ModComb {
         Self { n, fact, ifact }
     }
 
-    pub fn comb(&self, n: usize, k: usize) -> ModInt {
+    pub fn c(&self, n: usize, k: usize) -> ModInt {
         assert!(n <= self.n);
         if k > n {
             return ModInt::zero();
@@ -42,13 +42,13 @@ mod tests {
     #[test]
     fn test_comb() {
         let comb = ModComb::new(40);
-        assert_eq!(comb.comb(0, 0), ModInt::one());
-        assert_eq!(comb.comb(1, 0), ModInt::one());
-        assert_eq!(comb.comb(1, 1), ModInt::one());
-        assert_eq!(comb.comb(2, 1), ModInt::from(2));
-        assert_eq!(comb.comb(39, 10), ModInt::from(635_745_396));
-        assert_eq!(comb.comb(39, 11), ModInt::from(676_056_037));
-        assert_eq!(comb.comb(40, 10), ModInt::from(847_660_528));
-        assert_eq!(comb.comb(40, 11), ModInt::from(311_801_426));
+        assert_eq!(comb.c(0, 0), ModInt::one());
+        assert_eq!(comb.c(1, 0), ModInt::one());
+        assert_eq!(comb.c(1, 1), ModInt::one());
+        assert_eq!(comb.c(2, 1), ModInt::from(2));
+        assert_eq!(comb.c(39, 10), ModInt::from(635_745_396));
+        assert_eq!(comb.c(39, 11), ModInt::from(676_056_037));
+        assert_eq!(comb.c(40, 10), ModInt::from(847_660_528));
+        assert_eq!(comb.c(40, 11), ModInt::from(311_801_426));
     }
 }
