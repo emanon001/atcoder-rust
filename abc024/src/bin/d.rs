1bab6ec5bb635943d81df8a86e93fa7ed9571720
emanon001 <emanon001@gmail.com>
2020-07-17 09:24:56 +0900

    update Cargo.toml

--- abc024/src/bin/d.rs
+++ abc024/src/bin/d.rs
@@ -1,6 +1,19 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
 use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+enum InputType {
+    a,
+    b,
+    c,
+}
 
 fn main() {
-  input! {
-  };
+    input! {};
 }
