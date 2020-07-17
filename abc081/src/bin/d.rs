8516a8c3b8699a8d4c138144849a29adb33bc813
emanon001 <emanon001@gmail.com>
2020-07-18 00:45:55 +0900

    solve abc081/d

--- abc081/src/bin/d.rs
+++ abc081/src/bin/d.rs
@@ -10,7 +10,65 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [isize; n]
     };
+
+    let mut min = std::isize::MAX;
+    let mut min_i = 0;
+    for i in 0..n {
+        let a = av[i];
+        if a < min {
+            min = a;
+            min_i = i;
+        }
+    }
+    let mut max = std::isize::MIN;
+    let mut max_i = 0;
+    for i in 0..n {
+        let a = av[i];
+        if a > max {
+            max = a;
+            max_i = i;
+        }
+    }
+    if min.abs() > max.abs() {
+        let mut res = Vec::new();
+        res.push((min_i + 1, n));
+        res.push((min_i + 1, n));
+        for i in (0..n - 1).rev() {
+            if av[i] > 0 {
+                let i = i + 1;
+                res.push((i + 1, i));
+                res.push((i + 1, i));
+            } else {
+                let i = i + 1;
+                res.push((i + 1, i));
+            }
+        }
+        println!("{}", res.len());
+        for (x, y) in res {
+            println!("{} {}", x, y);
+        }
+    } else {
+        let mut res = Vec::new();
+        res.push((max_i + 1, 1));
+        res.push((max_i + 1, 1));
+        for i in 1..n {
+            if av[i] > 0 {
+                let i = i + 1;
+                res.push((i - 1, i));
+            } else {
+                let i = i + 1;
+                res.push((i - 1, i));
+                res.push((i - 1, i));
+            }
+        }
+        println!("{}", res.len());
+        for (x, y) in res {
+            println!("{} {}", x, y);
+        }
+    }
 }
 
 fn main() {
