b73fb6d595f3746f0d9ecde3ae3a28b6f49de6a6
emanon001 <emanon001@gmail.com>
2020-07-17 11:19:19 +0900

    solve abc054/d

--- abc054/src/bin/d.rs
+++ abc054/src/bin/d.rs
@@ -1,6 +1,50 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
 use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: usize, ma: usize, mb: usize,
+        abcv: [(usize, usize, usize); n]
+    };
+
+    let inf = 1_usize << 30;
+    let max_g = 10 * n;
+    let mut dp = vec![vec![vec![inf; max_g + 1]; max_g + 1]; n + 1];
+    dp[0][0][0] = 0;
+    for i in 0..n {
+        let (a, b, c) = abcv[i];
+        for j in 0..max_g + 1 {
+            for k in 0..max_g + 1 {
+                dp[i + 1][j][k] = dp[i + 1][j][k].min(dp[i][j][k]);
+                if j + a > max_g || k + b > max_g {
+                    continue;
+                }
+                dp[i + 1][j + a][k + b] = dp[i + 1][j + a][k + b].min(dp[i][j][k] + c);
+            }
+        }
+    }
+    let mut res = inf;
+    for i in 1..max_g + 1 {
+        for j in 1..max_g + 1 {
+            let c = dp[n][i][j];
+            if c == inf {
+                continue;
+            }
+            let gcd = i.gcd(&j);
+            if i / gcd == ma && j / gcd == mb {
+                if c < res {
+                    res = c;
+                }
+            }
+        }
+    }
+    let res = if res == inf { -1 } else { res as isize };
+    println!("{}", res);
 }
