4c0a64cd86a5e002ccd35db771950ece33be2c04
emanon001 <emanon001@gmail.com>
2020-07-21 19:49:10 +0900

    solve abc165/e

--- abc165/src/bin/e.rs
+++ abc165/src/bin/e.rs
@@ -9,6 +9,29 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: usize, m: usize
+    };
+
+    let mut res = Vec::new();
+    if n % 2 == 0 {
+        let mut i = 1;
+        let mut changed = false;
+        for _ in 0..m {
+            let j = n - i + if changed { 1 } else { 0 };
+            if !changed && n - j + i >= j - i {
+                changed = true;
+                i += 1;
+            }
+            res.push((i, j));
+            i += 1;
+        }
+    } else {
+        for i in 1..m + 1 {
+            res.push((i, n - i));
+        }
+    }
+    for (a, b) in res {
+        println!("{} {}", a, b);
+    }
 }
