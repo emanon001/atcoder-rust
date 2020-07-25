deba547435636bc6022d42b5d51ef0d631ca56dd
emanon001 <emanon001@gmail.com>
2020-07-26 03:05:54 +0900

    solve m-solutions2020/d

--- m-solutions2020/src/bin/d.rs
+++ m-solutions2020/src/bin/d.rs
@@ -10,7 +10,36 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n]
     };
+
+    let mut x = av[0];
+    let mut y = av[0];
+    let mut pairs = Vec::new();
+    for &a in av.iter().skip(1).chain(vec![0].iter()) {
+        if a > y {
+            y = a;
+        } else {
+            pairs.push((x, y));
+            x = a;
+            y = a;
+        }
+    }
+    let pairs = pairs
+        .into_iter()
+        .filter(|(x, y)| x != y)
+        .collect::<Vec<_>>();
+
+    let mut money = 1000;
+    for (x, y) in pairs {
+        let v = money / x * y;
+        let rest = money - (money / x * x);
+        if v + rest > money {
+            money = v + rest;
+        }
+    }
+    println!("{}", money);
 }
 
 fn main() {
--- m-solutions2020/src/bin/e.rs
+++ m-solutions2020/src/bin/e.rs
@@ -8,9 +8,64 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn distance(lines: &[(isize, bool)], xyp: (isize, isize, isize)) -> isize {
+    let (x, y, p) = xyp;
+    let mut res = std::isize::MAX;
+    for &(point, is_v) in lines {
+        let v = if is_v {
+            // vertical
+            (point - x).abs() * p
+        } else {
+            // horizontal
+            (point - y).abs() * p
+        };
+        if v < res {
+            res = v;
+        }
+    }
+    res
+}
+
 fn solve() {
     input! {
+        n: usize,
+        xypv: [(isize, isize, isize); n]
     };
+
+    let mut sum = 0;
+    let lines = vec![(0, true), (0, false)];
+    for &xyp in &xypv {
+        sum += distance(&lines, xyp);
+    }
+    println!("{}", sum);
+    for i in 1..=n {
+        let mut res = std::isize::MAX;
+        for v in (0..n).combinations(i) {
+            for bits in 0..2 << i {
+                let mut lines = lines.clone();
+                for j in 0..i {
+                    if (bits >> j) & 1 == 1 {
+                        // vertical
+                        let x = xypv[v[j]].0;
+                        lines.push((x, true));
+                    } else {
+                        // horizontal
+                        let y = xypv[v[j]].1;
+                        lines.push((y, false));
+                    }
+                }
+                // println!("{:?}", lines);
+                let mut sum = 0;
+                for &xyp in &xypv {
+                    sum += distance(&lines, xyp);
+                }
+                if sum < res {
+                    res = sum;
+                }
+            }
+        }
+        println!("{}", res);
+    }
 }
 
 fn main() {
