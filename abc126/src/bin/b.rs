fb150a11a3ee4b2b8ef638796ad8036366623dcd
emanon001 <emanon001@gmail.com>
2020-07-28 22:24:31 +0900

    solve abc126/b

--- abc126/src/bin/b.rs
+++ abc126/src/bin/b.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let head2 = s
+        .iter()
+        .take(2)
+        .map(|&ch| ch.to_digit(10).unwrap() as usize)
+        .fold(0, |acc, x| acc * 10 + x);
+    let tail2 = s
+        .iter()
+        .skip(2)
+        .map(|&ch| ch.to_digit(10).unwrap() as usize)
+        .fold(0, |acc, x| acc * 10 + x);
+    let ok_yymm = (1..=12).contains(&tail2);
+    let ok_mmyy = (1..=12).contains(&head2);
+    let res = match (ok_yymm, ok_mmyy) {
+        (true, true) => "AMBIGUOUS",
+        (true, _) => "YYMM",
+        (_, true) => "MMYY",
+        _ => "NA",
+    };
+    println!("{}", res);
 }
 
 fn main() {
