9a0f65adecb738224705949ba05307f55489bd2d
emanon001 <emanon001@gmail.com>
2020-07-28 23:56:12 +0900

    solve abc126/c

--- abc126/src/bin/c.rs
+++ abc126/src/bin/c.rs
@@ -10,7 +10,18 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
     };
+
+    let mut res = 0_f64;
+    for a in 1..n + 1 {
+        let mut c = 0;
+        while a * 2.pow(c) < k {
+            c += 1;
+        }
+        res += (1.0 / n as f64) * (1_f64 / 2.pow(c as u32) as f64);
+    }
+    println!("{}", res);
 }
 
 fn main() {
