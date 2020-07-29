b59b61e0fbe51e36e4066600665fa8f7c86a0061
emanon001 <emanon001@gmail.com>
2020-07-29 22:48:42 +0900

    solve arc017/a

--- arc017/src/bin/a.rs
+++ arc017/src/bin/a.rs
@@ -8,9 +8,27 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn is_prime(n: u64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
 fn solve() {
     input! {
+        n: u64
     };
+
+    let res = if is_prime(n) { "YES" } else { "NO" };
+    println!("{}", res);
 }
 
 fn main() {
