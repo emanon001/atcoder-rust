1e363b04b0a40f84e0782b508dd5422ea6628290
emanon001 <emanon001@gmail.com>
2020-08-18 04:26:46 +0900

    solve arc034/b

--- arc034/src/bin/b.rs
+++ arc034/src/bin/b.rs
@@ -10,7 +10,30 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: i64
     };
+
+    let mut res = BTreeSet::new();
+    let len = n.to_string().len();
+    for v in (0..=9).combinations_with_replacement(len) {
+        let digit_sum = v.iter().sum::<i64>();
+        let m = n - digit_sum;
+        if m <= 0 {
+            continue;
+        }
+        let m_digit_sum = m
+            .to_string()
+            .chars()
+            .map(|ch| ch.to_digit(10).unwrap() as i64)
+            .sum::<i64>();
+        if m_digit_sum == digit_sum {
+            res.insert(m);
+        }
+    }
+    println!("{}", res.len());
+    for x in res {
+        println!("{}", x);
+    }
 }
 
 fn main() {
