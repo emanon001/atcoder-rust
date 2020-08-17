bb6fc517488045c97710e35617c8341e1be5e5f1
emanon001 <emanon001@gmail.com>
2020-08-18 03:17:54 +0900

    solve abc036/c

--- abc036/src/bin/c.rs
+++ abc036/src/bin/c.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n]
     };
+
+    let mut map = BTreeMap::new();
+    for i in 0..n {
+        let v = av[i];
+        map.entry(v).or_insert(Vec::new()).push(i);
+    }
+    let mut res = vec![0; n];
+    for (a, (_, v)) in map.into_iter().enumerate() {
+        for i in v {
+            res[i] = a;
+        }
+    }
+    for a in res {
+        println!("{}", a);
+    }
 }
 
 fn main() {
