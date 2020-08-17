8a7bcf1c84954575ed8530342ad25a1263c89137
emanon001 <emanon001@gmail.com>
2020-08-17 23:27:04 +0900

    solve arc036/b

--- arc036/src/bin/b.rs
+++ arc036/src/bin/b.rs
@@ -8,9 +8,44 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(PartialEq, Eq)]
+enum Mode {
+    Up,
+    Down,
+}
+
 fn solve() {
     input! {
+        n: usize,
+        hv: [usize; n]
     };
+
+    let mut res = 0;
+    let mut mode = Mode::Up;
+    let mut s = 0;
+    let mut prev = 0;
+    for i in 0..n {
+        let h = hv[i];
+        match mode {
+            Mode::Up => {
+                if prev > h {
+                    mode = Mode::Down;
+                }
+            }
+            Mode::Down => {
+                if prev < h {
+                    mode = Mode::Up;
+                    s = i - 1;
+                }
+            }
+        }
+        let score = i - s + 1;
+        if score > res {
+            res = score;
+        }
+        prev = h;
+    }
+    println!("{}", res);
 }
 
 fn main() {
