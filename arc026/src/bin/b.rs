7dfa26a31035b73af1c9f664117033ca8cd0934b
emanon001 <emanon001@gmail.com>
2020-08-18 15:44:52 +0900

    solve arc026/b

--- arc026/src/bin/b.rs
+++ arc026/src/bin/b.rs
@@ -8,9 +8,36 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: u64) -> Vec<u64> {
+    let mut res = Vec::new();
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            res.push(x);
+            let y = n / x;
+            if y != x {
+                res.push(y);
+            }
+        }
+        x += 1;
+    }
+    res
+}
+
 fn solve() {
     input! {
+        n: u64
     };
+
+    let m = divisors(n).into_iter().sum::<u64>() - n;
+    let res = if m == n {
+        "Perfect"
+    } else if m < n {
+        "Deficient"
+    } else {
+        "Abundant"
+    };
+    println!("{}", res);
 }
 
 fn main() {
