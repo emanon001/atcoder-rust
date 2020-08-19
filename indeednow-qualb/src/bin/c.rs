9d7ff6e0894992ff0b50431e3eca3aa673a4c518
emanon001 <emanon001@gmail.com>
2020-08-19 11:08:56 +0900

    solve indeednow-qualb/c

--- indeednow-qualb/src/bin/c.rs
+++ indeednow-qualb/src/bin/c.rs
@@ -8,9 +8,33 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+use std::cmp::Reverse as Rev;
+
 fn solve() {
     input! {
+        n: usize,
+        edges: [(Usize1, Usize1); n - 1]
     };
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    let mut res = Vec::new();
+    let mut heap = BinaryHeap::new();
+    heap.push(Rev((0, 0)));
+    while let Some(Rev((u, parent))) = heap.pop() {
+        res.push(u + 1);
+        for &v in &graph[u] {
+            if v == parent {
+                continue;
+            }
+            heap.push(Rev((v, u)));
+        }
+    }
+    let res = res.into_iter().join(" ");
+    println!("{}", res);
 }
 
 fn main() {
