81f47d2e98d68850cbad9c32b0c41e52602b83c6
emanon001 <emanon001@gmail.com>
2020-08-20 19:36:30 +0900

    solve abc006/c

--- abc006/src/bin/c.rs
+++ abc006/src/bin/c.rs
@@ -9,6 +9,33 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: i64, m: i64
+    };
+
+    let x = m - 2 * n;
+    if x < 0 {
+        println!("-1 -1 -1");
+        return;
+    }
+
+    for b in 0..=x {
+        let c = (x - b) / 2;
+        if c < 0 {
+            continue;
+        }
+        if b + 2 * c != x {
+            continue;
+        }
+        let a = (m - (3 * b + 4 * c)) / 2;
+        if a < 0 {
+            continue;
+        }
+        if a + b + c != n || 2 * a + 3 * b + 4 * c != m {
+            continue;
+        }
+        println!("{} {} {}", a, b, c);
+        return;
+    }
+    println!("-1 -1 -1");
 }
