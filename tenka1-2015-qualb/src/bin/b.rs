724b9b8654e39a36d96a014fec2d235c27955a9e
emanon001 <emanon001@gmail.com>
2020-08-20 13:11:49 +0900

    solve tenka1-2015-qualb/b

--- tenka1-2015-qualb/src/bin/b.rs
+++ tenka1-2015-qualb/src/bin/b.rs
@@ -8,9 +8,67 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(PartialEq, Eq)]
+enum Type {
+    Dict,
+    Set,
+    Integer,
+}
+
+fn parse_expr(s: &[char], pos: &mut usize) -> Type {
+    match s[*pos] {
+        '{' => {
+            // dict | set
+            if s[*pos + 1] == '}' {
+                *pos += 1;
+                return Type::Dict;
+            }
+            *pos += 1;
+            parse_expr(s, pos);
+            if s[*pos + 1] == ':' {
+                // dict
+                *pos += 2;
+                parse_expr(s, pos);
+                while s[*pos + 1] == ',' {
+                    *pos += 2;
+                    parse_expr(s, pos);
+                    *pos += 2;
+                    parse_expr(s, pos);
+                }
+                *pos += 1;
+                Type::Dict
+            } else {
+                // set
+                while s[*pos + 1] == ',' {
+                    *pos += 2;
+                    parse_expr(s, pos);
+                }
+                *pos += 1;
+                Type::Set
+            }
+        }
+        _ => {
+            // integer
+            while s[*pos + 1].is_ascii_digit() {
+                *pos += 1;
+            }
+            Type::Integer
+        }
+    }
+}
+
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let mut pos = 0;
+    let res = if parse_expr(&s, &mut pos) == Type::Dict {
+        "dict"
+    } else {
+        "set"
+    };
+    println!("{}", res);
 }
 
 fn main() {
