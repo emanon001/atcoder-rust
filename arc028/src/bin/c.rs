296aa3a0abab8bc49553d81248e2c66e175a7149
emanon001 <emanon001@gmail.com>
2020-08-21 16:07:19 +0900

    solve arc028/c

--- arc028/src/bin/c.rs
+++ arc028/src/bin/c.rs
@@ -10,7 +10,51 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        pv: [usize; n - 1]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    let mut indegree = vec![0; n];
+    for (u, v) in pv.into_iter().enumerate() {
+        let u = u + 1;
+        graph[u].push(v);
+        graph[v].push(u);
+        indegree[u] += 1;
+        indegree[v] += 1;
+    }
+
+    let mut res = vec![0; n];
+    let mut que = VecDeque::new();
+    for i in 0..n {
+        if indegree[i] == 1 {
+            que.push_back((i, i));
+            res[i] = n - 1;
+        }
+    }
+    let mut childs = vec![0; n];
+    while let Some((u, parent)) = que.pop_front() {
+        for &v in &graph[u] {
+            if v == parent {
+                continue;
+            }
+            if indegree[v] == 1 {
+                continue;
+            }
+            indegree[v] -= 1;
+            res[v] = res[v].max(childs[u] + 1);
+            if indegree[v] > 0 {
+                childs[v] += childs[u] + 1;
+            }
+            if indegree[v] == 1 {
+                res[v] = res[v].max(n - childs[v] - 1);
+                que.push_back((v, u));
+            }
+        }
+    }
+    for i in 0..n {
+        println!("{}", res[i]);
+    }
 }
 
 fn main() {
