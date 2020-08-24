bcec414013be1ed475560237415c8500d5d0b106
emanon001 <emanon001@gmail.com>
2020-08-24 22:27:52 +0900

    solve arc006/b

--- arc006/src/bin/b.rs
+++ arc006/src/bin/b.rs
@@ -2,15 +2,47 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::input;
 #[allow(unused_imports)]
 use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
+use whiteread::parse_line;
+
+fn read_line(width: usize) -> String {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s[..width].to_string()
+}
 
 fn solve() {
-    input! {
-    };
+    let (n, l): (usize, usize) = parse_line().unwrap();
+    let width = 2 * n - 1;
+    let mut grid = Vec::new();
+    for _ in 0..l {
+        let line = read_line(width).chars().into_iter().collect::<Vec<char>>();
+        grid.push(line);
+    }
+    let mut goal = read_line(width).chars().into_iter().collect::<Vec<char>>();
+
+    let grid = grid
+        .into_iter()
+        .map(|mut rows| {
+            rows.reverse();
+            rows
+        })
+        .rev()
+        .collect::<Vec<_>>();
+    goal.reverse();
+    let mut pos = goal.into_iter().position(|ch| ch == 'o').unwrap();
+    for line in grid {
+        if pos > 0 && line[pos - 1] == '-' {
+            pos -= 2;
+        } else if pos < width - 1 && line[pos + 1] == '-' {
+            pos += 2;
+        }
+    }
+    let res = n - (pos / 2);
+    println!("{}", res);
 }
 
 fn main() {
