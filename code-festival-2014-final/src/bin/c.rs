9d1178e573f0b3d0702dfaf2ac382cdc1cc07fab
emanon001 <emanon001@gmail.com>
2020-08-26 01:37:27 +0900

    solve code-festival-2014-final/c

--- code-festival-2014-final/src/bin/c.rs
+++ code-festival-2014-final/src/bin/c.rs
@@ -9,6 +9,25 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        a: i64
+    };
+
+    for k in 10_i64..=10000 {
+        let mut n = 0;
+        let mut m = 1;
+        let mut rest = k;
+        while rest > 0 {
+            n += (rest % 10) * m;
+            rest = rest / 10;
+            if rest > 0 {
+                m *= k;
+            }
+        }
+        if n == a {
+            println!("{}", k);
+            return;
+        }
+    }
+    println!("-1");
 }
