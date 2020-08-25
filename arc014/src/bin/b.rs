37c25570ae8635312c83da29aa262f1e7f298995
emanon001 <emanon001@gmail.com>
2020-08-25 15:30:23 +0900

    solve arc014/b

--- arc014/src/bin/b.rs
+++ arc014/src/bin/b.rs
@@ -10,7 +10,29 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        wv: [Chars; n]
     };
+
+    let mut turn = 1;
+    let mut used = HashSet::new();
+    let mut prev_suffix = wv[0][0];
+    for w in wv {
+        if used.contains(&w) {
+            let res = if turn % 2 == 1 { "LOSE" } else { "WIN" };
+            println!("{}", res);
+            return;
+        }
+        if w[0] != prev_suffix {
+            let res = if turn % 2 == 1 { "LOSE" } else { "WIN" };
+            println!("{}", res);
+            return;
+        }
+        prev_suffix = w[w.len() - 1];
+        used.insert(w);
+        turn += 1;
+    }
+    println!("DRAW");
 }
 
 fn main() {
