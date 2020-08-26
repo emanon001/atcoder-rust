af711a4e0d766d44bb74a701b1a34ad0e76cb762
emanon001 <emanon001@gmail.com>
2020-08-26 23:22:32 +0900

    update arc022

--- arc022/src/bin/c.rs
+++ arc022/src/bin/c.rs
@@ -8,168 +8,233 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
-pub trait Monoid {
-    fn empty() -> Self;
-    fn append(&self, other: &Self) -> Self;
+#[derive(Clone)]
+pub struct Graph {
+    graph: WeightedGraph,
 }
-pub struct SegmentTree<T>
-where
-    T: Monoid + Clone,
-{
-    size: usize,
-    data: Vec<T>,
-}
-impl<T> SegmentTree<T>
-where
-    T: Monoid + Clone,
-{
-    pub fn new(size: usize) -> Self {
-        let size = Self::normalize_data_size(size);
-        let data = vec![T::empty(); size * 2 - 1];
-        Self { size, data }
+pub type Edge = (usize, usize);
+impl Graph {
+    pub fn new(edges: &[Edge], vn: usize) -> Self {
+        let edges = Self::to_weighted_edges(&edges);
+        let graph = WeightedGraph::new(&edges, vn);
+        Graph { graph }
     }
-    pub fn from_slice(values: &[T]) -> Self {
-        let mut st = SegmentTree::new(values.len());
-        for (i, v) in values.into_iter().enumerate() {
-            st.data[i + st.size - 1] = v.clone();
-        }
-        if st.size < 2 {
-            return st;
-        }
-        for i in (0..=(st.size - 2)).rev() {
-            st.data[i] = st.data[i * 2 + 1].append(&st.data[i * 2 + 2]);
-        }
-        st
+    pub fn new_directed(edges: &[Edge], vn: usize) -> Self {
+        let edges = Self::to_weighted_edges(&edges);
+        let graph = WeightedGraph::new_directed(&edges, vn);
+        Graph { graph }
     }
-    pub fn update(&mut self, i: usize, v: T) {
-        let mut i = i + self.size - 1;
-        self.data[i] = v;
-        while i > 0 {
-            i = (i - 1) / 2;
-            self.data[i] = self.data[i * 2 + 1].append(&self.data[i * 2 + 2]);
-        }
+    pub fn add_directed_edge(&mut self, e: Edge) {
+        self.graph.add_directed_edge((e.0, e.1, 1));
     }
-    pub fn query(&self, a: usize, b: usize) -> T {
-        self.execute_query(a, b, 0, 0, self.size)
+    pub fn add_edge(&mut self, e: Edge) {
+        self.graph.add_edge((e.0, e.1, 1));
     }
-    fn normalize_data_size(size: usize) -> usize {
-        let mut n = 1;
-        while n < size {
-            n *= 2;
-        }
-        n
+    pub fn shortest_path(&self, start: usize) -> Vec<Option<i64>> {
+        self.graph.shortest_path_1(start)
     }
-    fn execute_query(&self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
-        if r <= a || b <= l {
-            return T::empty();
-        }
-        if a <= l && r <= b {
-            return self.data[i].clone();
-        }
-        let vl = self.execute_query(a, b, i * 2 + 1, l, (l + r) / 2);
-        let vr = self.execute_query(a, b, i * 2 + 2, (l + r) / 2, r);
-        vl.append(&vr)
+    pub fn to_weighted_graph(&self) -> WeightedGraph {
+        self.graph.clone()
+    }
+    fn to_weighted_edges(edges: &[Edge]) -> Vec<WeightedEdge> {
+        edges
+            .into_iter()
+            .map(|(u, v)| (*u, *v, 1))
+            .collect::<Vec<_>>()
     }
 }
-#[derive(Debug, Copy, Clone)]
-pub struct LcaDepth {
-    depth: usize,
-    idx: usize,
-}
-impl Monoid for LcaDepth {
-    fn empty() -> Self {
-        Self {
-            depth: std::usize::MAX,
-            idx: 0,
-        }
-    }
-    fn append(&self, other: &Self) -> Self {
-        if self.depth <= other.depth {
-            *self
-        } else {
-            *other
-        }
-    }
-}
-pub struct Lca {
-    vs: Vec<usize>,
-    vdepth: Vec<usize>,
-    vidx: Vec<usize>,
+#[derive(Clone)]
+pub struct WeightedGraph {
+    graph: Vec<Vec<(usize, i64)>>,
     vn: usize,
-    st: SegmentTree<LcaDepth>,
 }
-impl Lca {
-    pub fn new(edges: &[(usize, usize)], vn: usize, root: usize) -> Self {
+pub type WeightedEdge = (usize, usize, i64);
+impl WeightedGraph {
+    const INF: i64 = 1 << 60;
+    pub fn new(edges: &[WeightedEdge], vn: usize) -> Self {
         let mut graph = vec![Vec::new(); vn];
-        for &(u, v) in edges {
-            graph[u].push(v);
-            graph[v].push(u);
+        for &(u, v, w) in edges {
+            graph[u].push((v, w));
+            graph[v].push((u, w));
         }
-        let mut vidx = vec![0; vn];
-        let mut vs = vec![0; vn * 2 - 1];
-        let mut vdepth = vec![0; vn * 2 - 1];
-        Self::traverse(
-            root,
-            root,
-            0,
-            &mut 0,
-            &graph,
-            &mut vidx,
-            &mut vs,
-            &mut vdepth,
-        );
-        let lca_depth = vdepth
-            .iter()
-            .copied()
-            .enumerate()
-            .map(|(i, d)| LcaDepth { depth: d, idx: i })
-            .collect::<Vec<_>>();
-        let st = SegmentTree::from_slice(&lca_depth);
-        Self {
-            vs,
-            vdepth,
-            vidx,
-            vn,
-            st,
+        Self { graph, vn }
+    }
+    pub fn new_directed(edges: &[WeightedEdge], vn: usize) -> Self {
+        let mut graph = vec![Vec::new(); vn];
+        for &(u, v, w) in edges {
+            graph[u].push((v, w));
         }
+        Self { graph, vn }
     }
-    pub fn depth(&self, u: usize) -> usize {
-        if u >= self.vn {
-            panic!("u >= self.vn");
-        }
-        let i = self.vidx[u];
-        self.vdepth[i]
+    pub fn add_directed_edge(&mut self, e: WeightedEdge) {
+        self.graph[e.0].push((e.1, e.2));
     }
-    pub fn query(&self, u: usize, v: usize) -> usize {
-        let ui = self.vidx[u];
-        let vi = self.vidx[v];
-        let LcaDepth { idx, .. } = self
-            .st
-            .query(std::cmp::min(ui, vi), std::cmp::max(ui, vi) + 1);
-        self.vs[idx]
+    pub fn add_edge(&mut self, e: WeightedEdge) {
+        self.graph[e.0].push((e.1, e.2));
+        self.graph[e.1].push((e.0, e.2));
     }
-    fn traverse(
-        u: usize,
-        parent: usize,
-        depth: usize,
-        vid: &mut usize,
-        graph: &[Vec<usize>],
-        vids: &mut [usize],
-        vs: &mut [usize],
-        vdepth: &mut [usize],
-    ) {
-        vids[u] = *vid;
-        vs[*vid] = u;
-        vdepth[*vid] = depth;
-        *vid += 1;
-        for &v in &graph[u] {
-            if v != parent {
-                Self::traverse(v, u, depth + 1, vid, graph, vids, vs, vdepth);
-                vs[*vid] = u;
-                vdepth[*vid] = depth;
-                *vid += 1;
+    pub fn bellman_ford(&self, s: usize) -> Option<Vec<Option<i64>>> {
+        let vn = self.vn;
+        let inf = Self::INF;
+        let mut cost_list = vec![inf; vn];
+        cost_list[s] = 0;
+        for c in 0..vn {
+            for u in 0..vn {
+                for &(v, w) in &self.graph[u] {
+                    if cost_list[u] == inf {
+                        continue;
+                    }
+                    let new_cost = cost_list[u] + w;
+                    if new_cost < cost_list[v] {
+                        cost_list[v] = new_cost;
+                        if c == vn - 1 {
+                            return None;
+                        }
+                    }
+                }
             }
         }
+        Some(self.optionalize(cost_list))
+    }
+    pub fn prim(&self) -> i64 {
+        let mut used = std::collections::HashSet::new();
+        let mut heap = std::collections::BinaryHeap::new();
+        let mut res = 0_i64;
+        heap.push(std::cmp::Reverse((0_i64, 0)));
+        while let Some(std::cmp::Reverse((weight, u))) = heap.pop() {
+            if used.contains(&u) {
+                continue;
+            }
+            used.insert(u);
+            res += weight;
+            for &(v, w) in &self.graph[u] {
+                if used.contains(&v) {
+                    continue;
+                }
+                heap.push(std::cmp::Reverse((w, v)));
+            }
+        }
+        res
+    }
+    pub fn reachable_vertexes(&self, s: usize) -> std::collections::HashSet<usize> {
+        let mut visited = std::collections::HashSet::new();
+        let mut queue = std::collections::VecDeque::new();
+        visited.insert(s);
+        queue.push_back(s);
+        while let Some(u) = queue.pop_front() {
+            for &(v, _) in &self.graph[u] {
+                if visited.contains(&v) {
+                    continue;
+                }
+                visited.insert(v);
+                queue.push_back(v);
+            }
+        }
+        visited
+    }
+    pub fn rev(&self) -> WeightedGraph {
+        let mut edges = Vec::new();
+        for u in 0..self.vn {
+            for &(v, w) in &self.graph[u] {
+                edges.push((v, u, w));
+            }
+        }
+        Self::new_directed(&edges, self.vn)
+    }
+    pub fn shortest_path(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![Self::INF; self.vn];
+        let mut heap = std::collections::BinaryHeap::new();
+        cost_list[start] = 0;
+        heap.push(std::cmp::Reverse((0_i64, start)));
+        while let Some(std::cmp::Reverse((cost, u))) = heap.pop() {
+            if cost > cost_list[u] {
+                continue;
+            }
+            for &(v, w) in &self.graph[u] {
+                let new_cost = cost + w;
+                if new_cost < cost_list[v] {
+                    heap.push(std::cmp::Reverse((new_cost, v)));
+                    cost_list[v] = new_cost;
+                }
+            }
+        }
+        self.optionalize(cost_list)
+    }
+    pub fn shortest_path_1(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![None; self.vn];
+        let mut que = std::collections::VecDeque::new();
+        cost_list[start] = Some(0);
+        que.push_back(start);
+        while let Some(u) = que.pop_front() {
+            for &(v, w) in &self.graph[u] {
+                if w != 1 {
+                    panic!("weight is not 1");
+                }
+                if cost_list[v].is_some() {
+                    continue;
+                }
+                let new_cost = cost_list[u].unwrap() + 1;
+                cost_list[v] = Some(new_cost);
+                que.push_back(v);
+            }
+        }
+        cost_list
+    }
+    pub fn shortest_path_01(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![Self::INF; self.vn];
+        let mut que = std::collections::VecDeque::new();
+        cost_list[start] = 0;
+        que.push_front((start, 0_i64));
+        while let Some((u, cost)) = que.pop_front() {
+            if cost > cost_list[u] {
+                continue;
+            }
+            for &(v, w) in &self.graph[u] {
+                if w != 0 && w != 1 {
+                    panic!("weight is not 01");
+                }
+                let new_cost = cost + w;
+                if new_cost < cost_list[v] {
+                    cost_list[v] = new_cost;
+                    if w == 0 {
+                        que.push_front((v, new_cost));
+                    } else {
+                        que.push_back((v, new_cost));
+                    }
+                }
+            }
+        }
+        self.optionalize(cost_list)
+    }
+    pub fn warshall_floyd(&self) -> Vec<Vec<Option<i64>>> {
+        let inf = Self::INF;
+        let vn = self.vn;
+        let mut cost_list = vec![vec![inf; vn]; vn];
+        for u in 0..vn {
+            for &(v, w) in &self.graph[u] {
+                cost_list[u][v] = w;
+            }
+        }
+        for i in 0..vn {
+            cost_list[i][i] = 0;
+        }
+        for k in 0..vn {
+            for i in 0..vn {
+                for j in 0..vn {
+                    cost_list[i][j] =
+                        std::cmp::min(cost_list[i][j], cost_list[i][k] + cost_list[k][j]);
+                }
+            }
+        }
+        cost_list
+            .into_iter()
+            .map(|v| self.optionalize(v))
+            .collect::<Vec<_>>()
+    }
+    fn optionalize(&self, v: Vec<i64>) -> Vec<Option<i64>> {
+        v.into_iter()
+            .map(|x| if x == Self::INF { None } else { Some(x) })
+            .collect::<Vec<_>>()
     }
 }
 
@@ -179,27 +244,22 @@ fn solve() {
         edges: [(Usize1, Usize1); n - 1]
     };
 
-    let lca = Lca::new(&edges, n, 0);
-    let mut d1 = 0;
-    let mut v1 = 0;
-    for u in 0..n {
-        let d = lca.depth(u);
-        if d > d1 {
-            v1 = u;
-            d1 = d;
-        }
-    }
-    let mut sum_d = 0;
-    let mut v2 = 0;
-    for u in 0..n {
-        let p = lca.query(u, v1);
-        let d = lca.depth(u) - lca.depth(p) + lca.depth(v1) - lca.depth(p);
-        if d > sum_d {
-            sum_d = d;
-            v2 = u;
-        }
-    }
-    println!("{} {}", v1 + 1, v2 + 1);
+    let graph = Graph::new(&edges, n);
+    let (_, u) = graph
+        .shortest_path(0)
+        .into_iter()
+        .enumerate()
+        .map(|(i, c)| (c.unwrap(), i))
+        .max()
+        .unwrap();
+    let (_, v) = graph
+        .shortest_path(u)
+        .into_iter()
+        .enumerate()
+        .map(|(i, c)| (c.unwrap(), i))
+        .max()
+        .unwrap();
+    println!("{} {}", u + 1, v + 1);
 }
 
 fn main() {
