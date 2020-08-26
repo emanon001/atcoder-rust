ead6467f18a64769e0694e1e589e988bda3f0c96
emanon001 <emanon001@gmail.com>
2020-08-26 17:39:21 +0900

    solve arc022/b

--- arc022/src/bin/b.rs
+++ arc022/src/bin/b.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n]
     };
+
+    let mut last = vec![-1; 10.pow(5) + 1];
+    let mut res = 1;
+    let mut l = 0;
+    for (i, a) in av.into_iter().enumerate() {
+        if l as isize <= last[a] {
+            l = last[a] as usize + 1;
+        }
+        last[a] = i as isize;
+        let size = i - l + 1;
+        if size > res {
+            res = size;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
