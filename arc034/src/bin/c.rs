4295fb752373fb5d595732af4538febf3a50bcc7
emanon001 <emanon001@gmail.com>
2020-08-28 21:09:18 +0900

    solve arc034/c

--- arc034/src/bin/c.rs
+++ arc034/src/bin/c.rs
@@ -8,9 +8,42 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn prime_factor(n: u64) -> std::collections::HashMap<u64, u64> {
+    if n < 2 {
+        return std::collections::HashMap::new();
+    }
+    let mut res = std::collections::HashMap::new();
+    let mut n = n;
+    let mut i = 2;
+    while i * i <= n {
+        while n % i == 0 {
+            let c = res.entry(i).or_insert(0);
+            *c += 1;
+            n /= i;
+        }
+        i += 1;
+    }
+    if n != 1 {
+        res.insert(n, 1);
+    }
+    res
+}
+
 fn solve() {
     input! {
+        a: u64, b: u64
     };
+
+    let mut table = HashMap::new();
+    for x in b + 1..=a {
+        for (p, c) in prime_factor(x) {
+            *table.entry(p).or_insert(0) += c;
+        }
+    }
+    let res = table
+        .values()
+        .fold(1, |acc, x| (acc * (x + 1)) % 1_000_000_007);
+    println!("{}", res);
 }
 
 fn main() {
