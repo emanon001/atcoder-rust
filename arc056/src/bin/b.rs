0cad9be66d6161c47369d1ddd16e943e6e443baa
emanon001 <emanon001@gmail.com>
2020-09-03 10:24:52 +0900

    solve arc056/b

--- arc056/src/bin/b.rs
+++ arc056/src/bin/b.rs
@@ -9,6 +9,36 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: usize, m: usize, s: Usize1,
+        edges: [(Usize1, Usize1); m]
+    };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    let mut heap = BinaryHeap::new();
+    heap.push((s, s));
+    let mut dp = vec![-1_isize; n];
+    dp[s] = s as isize;
+    while let Some((min_v, u)) = heap.pop() {
+        if dp[u] > min_v as isize {
+            continue;
+        }
+        for &v in &graph[u] {
+            let min_v = min_v.min(u);
+            if min_v as isize > dp[v] {
+                dp[v] = min_v as isize;
+                heap.push((min_v, v));
+            }
+        }
+    }
+    for u in 0..n {
+        if (u as isize) <= dp[u] {
+            println!("{}", u + 1);
+        }
+    }
 }
