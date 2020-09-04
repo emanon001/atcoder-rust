8912cb9a91d79d6c75aafd4b355f41c892d8c60d
emanon001 <emanon001@gmail.com>
2020-09-04 21:00:39 +0900

    solve arc039/a

--- arc039/src/bin/a.rs
+++ arc039/src/bin/a.rs
@@ -10,7 +10,49 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: isize, b: isize
     };
+
+    let av = a
+        .to_string()
+        .chars()
+        .map(|ch| ch.to_digit(10).unwrap() as isize)
+        .collect::<Vec<_>>();
+    let bv = b
+        .to_string()
+        .chars()
+        .map(|ch| ch.to_digit(10).unwrap() as isize)
+        .collect::<Vec<_>>();
+    let mut res = -1_isize << 30;
+    for i in 0..3 {
+        let a = av
+            .iter()
+            .enumerate()
+            .fold(0, |acc, (ai, &x)| acc * 10 + if ai == i { 9 } else { x });
+        let diff = a - b;
+        if diff > res {
+            res = diff;
+        }
+    }
+    for i in 0..3 {
+        let b = bv.iter().enumerate().fold(0, |acc, (bi, &x)| {
+            acc * 10
+                + if bi == i {
+                    if bi == 0 {
+                        1
+                    } else {
+                        0
+                    }
+                } else {
+                    x
+                }
+        });
+        let diff = a - b;
+        if diff > res {
+            res = diff;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
