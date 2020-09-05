1f2b02c87831a741a72589ab113dfd7e904639b0
emanon001 <emanon001@gmail.com>
2020-09-05 23:34:16 +0900

    solve abc017/a

--- abc017/src/bin/a.rs
+++ abc017/src/bin/a.rs
@@ -10,7 +10,13 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s1: i64, e1: i64,
+        s2: i64, e2: i64,
+        s3: i64, e3: i64,
     };
+
+    let res = s1 * e1 / 10 + s2 * e2 / 10 + s3 * e3 / 10;
+    println!("{}", res);
 }
 
 fn main() {
