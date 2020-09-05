3da7bc5b229e9db52050d1d9cf8d063edd2dc4e5
emanon001 <emanon001@gmail.com>
2020-09-06 04:23:46 +0900

    solve abc017/c

--- abc017/src/bin/c.rs
+++ abc017/src/bin/c.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        lrsv: [(Usize1, Usize1, i64); n]
     };
+
+    let mut sum = 0_i64;
+    let mut imos = vec![0; m + 1];
+    for (l, r, s) in lrsv {
+        sum += s;
+        imos[l] += s;
+        imos[r + 1] -= s;
+    }
+    for i in 1..m {
+        imos[i] = imos[i - 1] + imos[i];
+    }
+    let res = sum - imos[0..m].into_iter().min().unwrap();
+    println!("{}", res);
 }
 
 fn main() {
