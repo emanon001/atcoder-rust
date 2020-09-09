f0fe073830951bf301fbe7d22aec8fe2f45849d7
emanon001 <emanon001@gmail.com>
2020-09-09 09:17:23 +0900

    solve abc018/d

--- abc018/src/bin/d.rs
+++ abc018/src/bin/d.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, _: usize, p: usize, q: usize, r: usize,
+        xyzv: [(Usize1, Usize1, i64); r]
     };
+
+    let mut res = 0_i64;
+    for girls in (0..n).combinations(p) {
+        let girls = girls.into_iter().collect::<HashSet<_>>();
+        let mut table = HashMap::new();
+        for &(x, y, z) in &xyzv {
+            if girls.contains(&x) {
+                *table.entry(y).or_insert(0) += z;
+            }
+        }
+        let mut values = table.values().collect::<Vec<_>>();
+        values.sort_by_key(|&v| -v);
+        let sum: i64 = values.into_iter().take(q).sum();
+        if sum > res {
+            res = sum;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
