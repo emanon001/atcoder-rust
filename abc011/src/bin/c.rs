4ebf60c8dddd2d4eafffbfd85f5bce6ead1e60c3
emanon001 <emanon001@gmail.com>
2020-09-11 00:03:50 +0900

    solve abc011/c

--- abc011/src/bin/c.rs
+++ abc011/src/bin/c.rs
@@ -8,9 +8,38 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn dfs(i: usize, c: usize, ng_set: &HashSet<usize>, dp: &mut Vec<Option<usize>>) {
+    if ng_set.contains(&i) {
+        return;
+    }
+    if let Some(dc) = dp[i] {
+        if c >= dc {
+            return;
+        }
+    }
+    dp[i] = Some(c);
+    for m in (1..=3).rev() {
+        if i >= m {
+            dfs(i - m, c + 1, ng_set, dp);
+        }
+    }
+}
+
 fn solve() {
     input! {
+        n: usize,
+        ngv: [usize; 3]
     };
+
+    let ng_set = ngv.iter().copied().collect::<HashSet<_>>();
+    let mut dp = vec![None; n + 1];
+    dfs(n, 0, &ng_set, &mut dp);
+    let res = if dp[0].unwrap_or(1 << 30) <= 100 {
+        "YES"
+    } else {
+        "NO"
+    };
+    println!("{}", res);
 }
 
 fn main() {
