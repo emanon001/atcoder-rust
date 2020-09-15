89d0471e1741efd81c497b319616dc109633c67d
emanon001 <emanon001@gmail.com>
2020-09-15 18:56:23 +0900

    solve arc025/b

--- arc025/src/bin/b.rs
+++ arc025/src/bin/b.rs
@@ -10,7 +10,38 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [[i64; w]; h]
     };
+
+    let mut cusum = vec![vec![0; w + 1]; h + 1];
+    for i in 0..h {
+        for j in 0..w {
+            let mut p = grid[i][j];
+            if i % 2 == 0 {
+                p *= if j % 2 == 0 { 1 } else { -1 };
+            } else {
+                p *= if j % 2 == 0 { -1 } else { 1 };
+            };
+            let c = cusum[i + 1][j] + cusum[i][j + 1] - cusum[i][j] + p;
+            cusum[i + 1][j + 1] = c;
+        }
+    }
+    let mut res = 0;
+    for t in 0..h {
+        for l in 0..w {
+            for b in t..h {
+                for r in l..w {
+                    let p = cusum[b + 1][r + 1] - cusum[b + 1][l] - cusum[t][r + 1] + cusum[t][l];
+                    let c = (b - t + 1) * (r - l + 1);
+                    if p == 0 && c > res {
+                        res = c;
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
