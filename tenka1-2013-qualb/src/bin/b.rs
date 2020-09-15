06f9299c97b1fe639387116f8e021784fb4d0d2c
emanon001 <emanon001@gmail.com>
2020-09-15 15:54:18 +0900

    solve tenka1-2013-qualb/b

--- tenka1-2013-qualb/src/bin/b.rs
+++ tenka1-2013-qualb/src/bin/b.rs
@@ -10,7 +10,54 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize, l: i64,
     };
+
+    let mut stack = Vec::new();
+    let mut size = 0_i64;
+    for _ in 0..q {
+        input! { kind: String }
+        match kind.as_ref() {
+            "Push" => {
+                input! { n: i64, m: i64 }
+                if size + n > l {
+                    println!("FULL");
+                    return;
+                }
+                size += n;
+                stack.push((m, n));
+            }
+            "Pop" => {
+                input! { n: i64 }
+                if n > size {
+                    println!("EMPTY");
+                    return;
+                }
+                let mut rest = n;
+                while rest > 0 {
+                    let (x, y) = stack.pop().unwrap();
+                    if y > rest {
+                        stack.push((x, y - rest));
+                    }
+                    rest -= rest.min(y);
+                }
+                size -= n;
+            }
+            "Top" => {
+                if let Some((m, _)) = stack.last() {
+                    println!("{}", m);
+                } else {
+                    println!("EMPTY");
+                    return;
+                }
+            }
+            "Size" => {
+                println!("{}", size);
+            }
+            _ => unreachable!(),
+        }
+    }
+    println!("SAFE");
 }
 
 fn main() {
