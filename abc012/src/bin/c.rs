5a6ea8cc56ee9f95b1934a4019f202d81133d892
emanon001 <emanon001@gmail.com>
2020-09-16 18:12:30 +0900

    solve abc012/c

--- abc012/src/bin/c.rs
+++ abc012/src/bin/c.rs
@@ -1,6 +1,21 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: usize
+    };
+
+    let sum = 45 * 45;
+    let rest = sum - n;
+    let mut table = HashMap::new();
+    for x in 1..=9 {
+        for y in 1..=9 {
+            let z = x * y;
+            table.entry(z).or_insert(Vec::new()).push((x, y));
+        }
+    }
+    for (x, y) in table.get(&rest).unwrap() {
+        println!("{} x {}", x, y);
+    }
 }
