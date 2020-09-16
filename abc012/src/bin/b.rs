f53f5e91258cac11b1c2585b7f9b68d1fa106e1a
emanon001 <emanon001@gmail.com>
2020-09-16 18:04:03 +0900

    solve abc012/b

--- abc012/src/bin/b.rs
+++ abc012/src/bin/b.rs
@@ -1,6 +1,14 @@
 use proconio::input;
 
 fn main() {
-  input! {
-  };
+    input! {
+        mut n: usize,
+    };
+
+    let h = n / (60 * 60);
+    n = n % (60 * 60);
+    let m = n / 60;
+    n = n % 60;
+    let s = n;
+    println!("{0:02}:{1:02}:{2:02}", h, m, s);
 }
