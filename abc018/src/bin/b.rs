25cfe965ea6bf63fce3e0e064a76d395f4bb674a
emanon001 <emanon001@gmail.com>
2020-09-17 20:58:51 +0900

    solve abc018/b

--- abc018/src/bin/b.rs
+++ abc018/src/bin/b.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        mut s: Chars,
+        n: usize,
+        lrv: [(Usize1, Usize1); n]
     };
+
+    for (l, r) in lrv {
+        let front = s.iter().take(l).collect::<Vec<_>>();
+        let mid = s[l..=r].into_iter().rev().collect::<Vec<_>>();
+        let back = s.iter().skip(r + 1).collect::<Vec<_>>();
+        s = front
+            .into_iter()
+            .chain(mid)
+            .chain(back)
+            .copied()
+            .collect::<Vec<char>>();
+    }
+    println!("{}", s.into_iter().collect::<String>());
 }
 
 fn main() {
