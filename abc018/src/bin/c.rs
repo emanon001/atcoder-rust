1b2410b4a01196a54ceedade68d767b739b050c4
emanon001 <emanon001@gmail.com>
2020-09-18 16:04:07 +0900

    solve abc018/c

--- abc018/src/bin/c.rs
+++ abc018/src/bin/c.rs
@@ -10,7 +10,45 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        r: usize, c: usize, k: usize,
+        grid: [Chars; r]
     };
+
+    let mut cusum = vec![vec![0; c]; r];
+    for i in 0..r {
+        let mut count = 0;
+        for j in 0..c {
+            if grid[i][j] == 'x' {
+                count += 1;
+            }
+            cusum[i][j] = count;
+        }
+    }
+    let mut res = 0;
+    for i in 0..r {
+        for j in 0..c {
+            if i + 1 < k || i + k - 1 >= r || j + 1 < k || j + k - 1 >= c {
+                continue;
+            }
+            let mut count = 0;
+            for l in 0..k {
+                let ci = i + 1 - k + l;
+                count += cusum[ci][c - 1];
+                count -= if j - l == 0 { 0 } else { cusum[ci][j - l - 1] };
+                count -= cusum[ci][c - 1] - cusum[ci][j + l];
+            }
+            for l in 0..k - 1 {
+                let ci = i + k - 1 - l;
+                count += cusum[ci][c - 1];
+                count -= if j - l == 0 { 0 } else { cusum[ci][j - l - 1] };
+                count -= cusum[ci][c - 1] - cusum[ci][j + l];
+            }
+            if count == 0 {
+                res += 1;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
