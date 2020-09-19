6fa9c86f2422557866a90ec30b507537ef37a516
emanon001 <emanon001@gmail.com>
2020-09-19 23:04:01 +0900

    solve abc179/c

--- abc179/src/bin/c.rs
+++ abc179/src/bin/c.rs
@@ -10,7 +10,20 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize
     };
+
+    let mut res = 0;
+    for a in 1..=n {
+        let max_b = n / a;
+        for b in 1..=max_b {
+            if a * b >= n {
+                break;
+            }
+            res += 1;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
