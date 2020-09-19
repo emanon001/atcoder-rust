46bbddf743ba3fe672bc051f98bfac0bffc2e02e
emanon001 <emanon001@gmail.com>
2020-09-19 23:03:51 +0900

    solve abc179/b

--- abc179/src/bin/b.rs
+++ abc179/src/bin/b.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        dv: [(usize, usize); n]
     };
+
+    let mut c = 0;
+    for (d1, d2) in dv {
+        if d1 == d2 {
+            c += 1;
+        } else {
+            c = 0;
+        }
+        if c == 3 {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
