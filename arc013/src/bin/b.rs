197c501a6d3076395cd40a5c1d50a310404a3903
emanon001 <emanon001@gmail.com>
2020-09-20 05:14:03 +0900

    solve arc013/b

--- arc013/src/bin/b.rs
+++ arc013/src/bin/b.rs
@@ -10,7 +10,16 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        c: usize,
+        nmlv: [[usize; 3]; c]
     };
+
+    let (a, b, c) = nmlv.into_iter().fold((0, 0, 0), |acc, mut v| {
+        v.sort();
+        (acc.0.max(v[0]), acc.1.max(v[1]), acc.2.max(v[2]))
+    });
+    let res = a * b * c;
+    println!("{}", res);
 }
 
 fn main() {
