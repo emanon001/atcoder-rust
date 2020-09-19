9c59588686568948b570f6dc2ddb7d46e569584c
emanon001 <emanon001@gmail.com>
2020-09-19 23:03:45 +0900

    solve abc179/a

--- abc179/src/bin/a.rs
+++ abc179/src/bin/a.rs
@@ -10,7 +10,17 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let len = s.len();
+    if s[len - 1] == 's' {
+        let res = s.into_iter().chain(vec!['e', 's']).collect::<String>();
+        println!("{}", res);
+    } else {
+        let res = s.into_iter().chain(vec!['s']).collect::<String>();
+        println!("{}", res);
+    }
 }
 
 fn main() {
