fd4b456eb6d027e0ae4cafb9b9d8839ace9881e5
emanon001 <emanon001@gmail.com>
2020-09-20 01:16:17 +0900

    solve abc179/d

--- abc179/src/bin/d.rs
+++ abc179/src/bin/d.rs
@@ -8,9 +8,153 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct ModInt(u32);
+impl ModInt {
+    pub const MOD: u32 = 998244353;
+    pub fn inv(self) -> Self {
+        if self.0 == 0 {
+            panic!();
+        }
+        self.pow(Self::MOD - 2)
+    }
+    pub fn pow<T: num::Unsigned + num::PrimInt>(self, e: T) -> Self {
+        if e.is_zero() {
+            return Self::new(1);
+        }
+        let mut res = self.pow(e >> 1);
+        res *= res;
+        if e & T::one() == T::one() {
+            res *= self;
+        }
+        res
+    }
+    fn new(n: i64) -> Self {
+        let m = Self::MOD as i64;
+        let mut n = n % m;
+        if n.is_negative() {
+            n += m;
+        }
+        Self(n as u32)
+    }
+}
+macro_rules! impl_from {
+    ($ T : ty ) => {
+        impl From<$T> for ModInt {
+            fn from(n: $T) -> Self {
+                use std::convert::TryFrom;
+                Self::new(i64::try_from(n).unwrap())
+            }
+        }
+    };
+}
+impl_from!(i32);
+impl_from!(i64);
+impl_from!(isize);
+impl_from!(u32);
+impl_from!(u64);
+impl_from!(usize);
+macro_rules! impl_into {
+    ($ T : ty ) => {
+        impl Into<$T> for ModInt {
+            fn into(self) -> $T {
+                self.0 as $T
+            }
+        }
+    };
+}
+impl_into!(i32);
+impl_into!(i64);
+impl_into!(isize);
+impl_into!(u32);
+impl_into!(u64);
+impl_into!(usize);
+impl std::fmt::Display for ModInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl std::ops::Add for ModInt {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Self::new((self.0 + rhs.0) as i64)
+    }
+}
+impl std::ops::AddAssign for ModInt {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+impl std::ops::Div for ModInt {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self {
+        self * rhs.inv()
+    }
+}
+impl std::ops::DivAssign for ModInt {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+impl std::ops::Mul for ModInt {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        Self::new((self.0 as i64) * (rhs.0 as i64))
+    }
+}
+impl std::ops::MulAssign for ModInt {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+impl std::ops::Sub for ModInt {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        Self::new((self.0 as i64) - (rhs.0 as i64))
+    }
+}
+impl std::ops::SubAssign for ModInt {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+impl num::Zero for ModInt {
+    fn zero() -> Self {
+        Self::new(0)
+    }
+    fn is_zero(&self) -> bool {
+        *self == Self::zero()
+    }
+}
+impl num::One for ModInt {
+    fn one() -> Self {
+        Self::new(1)
+    }
+    fn is_one(&self) -> bool {
+        *self == Self::one()
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, k: usize,
+        lrv: [(usize, usize); k]
     };
+
+    let mut imos = vec![ModInt::zero(); n + 2];
+    imos[1] = ModInt::one();
+    for i in 1..n {
+        let cur = imos[i - 1] + imos[i];
+        imos[i] = cur;
+        for (l, r) in &lrv {
+            if i + l > n {
+                continue;
+            }
+            imos[i + l] += cur;
+            imos[(i + r + 1).min(n + 1)] -= cur;
+        }
+    }
+    println!("{}", imos[n]);
 }
 
 fn main() {
