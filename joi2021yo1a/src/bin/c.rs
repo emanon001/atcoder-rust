2a4131d75726c6d4609c03b7729c6810faea751e
emanon001 <emanon001@gmail.com>
2020-09-25 05:19:53 +0900

    solve joi2021yo1a/c

--- joi2021yo1a/src/bin/c.rs
+++ joi2021yo1a/src/bin/c.rs
@@ -10,7 +10,16 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [usize; n],
+        bv: [usize; m]
     };
+
+    let a_set = av.into_iter().collect::<BTreeSet<_>>();
+    let b_set = bv.into_iter().collect::<BTreeSet<_>>();
+    for x in a_set.intersection(&b_set) {
+        println!("{}", x);
+    }
 }
 
 fn main() {
