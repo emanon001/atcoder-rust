209bfc958d4f93dd24e7a5478805abccf0b059e9
emanon001 <emanon001@gmail.com>
2020-09-27 00:29:33 +0900

    solve abl/d

--- abl/src/bin/d.rs
+++ abl/src/bin/d.rs
@@ -8,9 +8,101 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait Monoid {
+    fn mempty() -> Self;
+    fn mappend(&self, other: &Self) -> Self;
+}
+pub struct SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    size: usize,
+    data: Vec<T>,
+}
+impl<T> SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    pub fn new(size: usize) -> Self {
+        let size = Self::normalize_data_size(size);
+        let data = vec![T::mempty(); size * 2 - 1];
+        Self { size, data }
+    }
+    pub fn from_slice(values: &[T]) -> Self {
+        let mut st = SegmentTree::new(values.len());
+        for (i, v) in values.into_iter().enumerate() {
+            st.data[i + st.size - 1] = v.clone();
+        }
+        if st.size < 2 {
+            return st;
+        }
+        for i in (0..=(st.size - 2)).rev() {
+            st.data[i] = st.data[i * 2 + 1].mappend(&st.data[i * 2 + 2]);
+        }
+        st
+    }
+    /// 0-origin
+    pub fn update(&mut self, i: usize, v: T) {
+        let mut i = i + self.size - 1;
+        self.data[i] = v;
+        while i > 0 {
+            i = (i - 1) / 2;
+            self.data[i] = self.data[i * 2 + 1].mappend(&self.data[i * 2 + 2]);
+        }
+    }
+    /// [a, b)
+    /// 0-origin
+    pub fn query(&self, a: usize, b: usize) -> T {
+        self.execute_query(a, b, 0, 0, self.size)
+    }
+    fn normalize_data_size(size: usize) -> usize {
+        let mut n = 1;
+        while n < size {
+            n *= 2;
+        }
+        n
+    }
+    fn execute_query(&self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
+        if r <= a || b <= l {
+            return T::mempty();
+        }
+        if a <= l && r <= b {
+            return self.data[i].clone();
+        }
+        let vl = self.execute_query(a, b, i * 2 + 1, l, (l + r) / 2);
+        let vr = self.execute_query(a, b, i * 2 + 2, (l + r) / 2, r);
+        vl.mappend(&vr)
+    }
+}
+
+impl Monoid for usize {
+    fn mempty() -> Self {
+        0
+    }
+    fn mappend(&self, other: &Self) -> Self {
+        *self.max(other)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, k: i64,
+        av: [i64; n]
     };
+
+    let max_a = 300000;
+    let mut st: SegmentTree<usize> = SegmentTree::new(max_a + 1);
+    let mut res = 0;
+    for a in av {
+        let l = (a - k).max(0) as usize;
+        let r = (a + k).min(max_a as i64) as usize;
+        let c = st.query(l, r + 1) + 1;
+        if c > res {
+            res = c;
+        }
+        st.update(a as usize, c);
+    }
+    println!("{}", res);
 }
 
 fn main() {
