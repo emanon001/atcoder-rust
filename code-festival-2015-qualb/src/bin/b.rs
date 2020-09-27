0f4f3bcc120cb7e6210af5605617c057917fde59
emanon001 <emanon001@gmail.com>
2020-09-27 13:04:03 +0900

    solve code-festival-2015-qualb/b

--- code-festival-2015-qualb/src/bin/b.rs
+++ code-festival-2015-qualb/src/bin/b.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [usize; n]
     };
+
+    let mut counts = vec![0; m + 1];
+    for a in av {
+        counts[a] += 1;
+    }
+    for a in 0..=m {
+        let c = counts[a];
+        if c * 2 > n {
+            println!("{}", a);
+            return;
+        }
+    }
+    println!("?");
 }
 
 fn main() {
