99579a38987a72c9bb5295fa01ba6a1e3ee46309
emanon001 <emanon001@gmail.com>
2020-09-27 13:23:30 +0900

    solve abc010/c

--- abc010/src/bin/c.rs
+++ abc010/src/bin/c.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        sx: i64, sy: i64, gx: i64, gy: i64, t: i64, v: i64,
+        n: usize,
+        vv: [(i64, i64); n]
     };
+
+    let max_d = t * v;
+    let is_ok = vv.into_iter().any(|(x, y)| {
+        let d1 = (((sx - x).abs().pow(2) + (sy - y).abs().pow(2)) as f64).sqrt();
+        let d2 = (((gx - x).abs().pow(2) + (gy - y).abs().pow(2)) as f64).sqrt();
+        (d1 + d2) <= max_d as f64
+    });
+    let res = if is_ok { "YES" } else { "NO" };
+    println!("{}", res);
 }
 
 fn main() {
