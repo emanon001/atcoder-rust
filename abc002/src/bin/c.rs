24cafb1ada560a557470fa0db064c08650456c18
emanon001 <emanon001@gmail.com>
2020-09-28 23:34:20 +0900

    solve abc002/c

--- abc002/src/bin/c.rs
+++ abc002/src/bin/c.rs
@@ -1,6 +1,12 @@
 use proconio::input;
 
 fn main() {
-  input! {
-  };
+    input! {
+        a: (f64, f64),
+        b: (f64, f64),
+        c: (f64, f64),
+    };
+
+    let res = (((b.0 - a.0) * (c.1 - a.1)) - ((b.1 - a.1) * (c.0 - a.0))).abs() / 2.0;
+    println!("{}", res);
 }
