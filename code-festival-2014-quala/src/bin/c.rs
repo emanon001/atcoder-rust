fbd3e711d7108ea1103024e3f13afe0209467926
emanon001 <emanon001@gmail.com>
2020-09-30 13:31:10 +0900

    solve code-festival-2014-quala/c

--- code-festival-2014-quala/src/bin/c.rs
+++ code-festival-2014-quala/src/bin/c.rs
@@ -8,9 +8,17 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn count(x: u64) -> u64 {
+    (x / 4) - (x / 100) + (x / 400)
+}
+
 fn solve() {
     input! {
+        a: u64, b: u64
     };
+
+    let res = count(b) - count(a - 1);
+    println!("{}", res);
 }
 
 fn main() {
