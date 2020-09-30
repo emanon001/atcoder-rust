75c59542f68fb178c6a38fdb160e6815d3c0496f
emanon001 <emanon001@gmail.com>
2020-09-30 13:25:56 +0900

    solve code-festival-2015-qualb/c

--- code-festival-2015-qualb/src/bin/c.rs
+++ code-festival-2015-qualb/src/bin/c.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        mut av: [i64; n],
+        mut bv: [i64; m],
     };
+
+    if m > n {
+        println!("NO");
+        return;
+    }
+
+    av.sort_by_key(|&a| -a);
+    bv.sort_by_key(|&b| -b);
+    let is_ok = av.into_iter().zip(bv).all(|(a, b)| a >= b);
+    let res = if is_ok { "YES" } else { "NO" };
+    println!("{}", res);
 }
 
 fn main() {
