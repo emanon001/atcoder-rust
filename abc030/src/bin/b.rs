0b6275b3bdcfa7a69307f4f676f720298a12fc2c
emanon001 <emanon001@gmail.com>
2020-10-02 23:04:04 +0900

    solve abc030/b

--- abc030/src/bin/b.rs
+++ abc030/src/bin/b.rs
@@ -10,7 +10,14 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize
     };
+
+    let n = n % 12;
+    let n_pos = 360_f64 / 720.0 * (n * 60 + m) as f64;
+    let m_pos = 360_f64 / 60.0 * m as f64;
+    let res = (n_pos - m_pos).abs().min(360.0 - (n_pos - m_pos).abs());
+    println!("{}", res);
 }
 
 fn main() {
