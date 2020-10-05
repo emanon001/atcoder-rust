441c9aa8cf2380c1ece4f4a53387ffde25e09fd7
emanon001 <emanon001@gmail.com>
2020-10-06 00:36:22 +0900

    solve past202004-open/e

--- past202004-open/src/bin/e.rs
+++ past202004-open/src/bin/e.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [Usize1; n]
     };
+
+    for i in 0..n {
+        let mut c = 0;
+        let mut x = i;
+        loop {
+            x = av[x];
+            c += 1;
+            if x == i {
+                break;
+            }
+        }
+        println!("{}", c);
+    }
 }
 
 fn main() {
