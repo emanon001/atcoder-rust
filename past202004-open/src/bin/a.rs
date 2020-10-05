70af79530ffe661cd24c2425390ad37af2e2ed57
emanon001 <emanon001@gmail.com>
2020-10-05 17:48:16 +0900

    solve past202004-open/a

--- past202004-open/src/bin/a.rs
+++ past202004-open/src/bin/a.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: String, t: String
     };
+
+    let s = s.chars().collect::<Vec<_>>();
+    let t = t.chars().collect::<Vec<_>>();
+    let res = match (s[0], t[0]) {
+        ('B', 'B') => {
+            (s[1].to_digit(10).unwrap() as isize - t[1].to_digit(10).unwrap() as isize).abs()
+        }
+        ('B', _) => {
+            (-(s[1].to_digit(10).unwrap() as isize) - t[0].to_digit(10).unwrap() as isize).abs() - 1
+        }
+        (_, 'B') => {
+            (s[0].to_digit(10).unwrap() as isize - -(t[1].to_digit(10).unwrap() as isize)).abs() - 1
+        }
+        _ => (s[0].to_digit(10).unwrap() as isize - t[0].to_digit(10).unwrap() as isize).abs(),
+    };
+    println!("{}", res);
 }
 
 fn main() {
