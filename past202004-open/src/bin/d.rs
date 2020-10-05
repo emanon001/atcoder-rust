4bcb04d815147be734b8889ab8f728c35861ce1f
emanon001 <emanon001@gmail.com>
2020-10-05 18:47:53 +0900

    solve past202004-open/d

--- past202004-open/src/bin/d.rs
+++ past202004-open/src/bin/d.rs
@@ -8,9 +8,54 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn make_ascii_lowercase_chars() -> Vec<char> {
+    let base = 0x61;
+    make_chars(base..base + 26)
+}
+pub fn make_ascii_uppercase_chars() -> Vec<char> {
+    let base = 0x41;
+    make_chars(base..base + 26)
+}
+fn make_chars(range: std::ops::Range<u8>) -> Vec<char> {
+    range.into_iter().map(char::from).collect::<Vec<_>>()
+}
+
+fn is_match(s: &[char], t: &[char]) -> bool {
+    if s.len() < t.len() {
+        return false;
+    }
+    (0..=s.len() - t.len()).any(|i| (0..t.len()).all(|j| s[i + j] == t[j] || t[j] == '.'))
+}
+
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let mut chars = make_ascii_lowercase_chars();
+    chars.push('.');
+    let mut res = 0;
+    for &a in &chars {
+        let mut t = vec![a];
+        if is_match(&s, &t) {
+            res += 1;
+        }
+        for &b in &chars {
+            t.push(b);
+            if is_match(&s, &t) {
+                res += 1;
+            }
+            for &c in &chars {
+                t.push(c);
+                if is_match(&s, &t) {
+                    res += 1;
+                }
+                t.pop();
+            }
+            t.pop();
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
