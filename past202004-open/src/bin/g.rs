ad747a585c34fa78c3e44b4722a73903ed390de3
emanon001 <emanon001@gmail.com>
2020-10-07 10:42:53 +0900

    solve past202004-open/g

--- past202004-open/src/bin/g.rs
+++ past202004-open/src/bin/g.rs
@@ -10,7 +10,42 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+
+    let mut que = VecDeque::new();
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    c: char, x: i64
+                };
+                que.push_back((c, x));
+            }
+            2 => {
+                input! {
+                    d: i64
+                };
+                let mut rest = d;
+                let mut deleted = HashMap::new();
+                while rest > 0 && !que.is_empty() {
+                    let (c, x) = que.pop_front().unwrap();
+                    let d_c = x.min(rest);
+                    *deleted.entry(c).or_insert(0_i64) += d_c;
+                    if x > rest {
+                        que.push_front((c, x - rest));
+                    }
+                    rest -= d_c;
+                }
+                let res = deleted.values().map(|&v| v * v).sum::<i64>();
+                println!("{}", res);
+            }
+            _ => unreachable!(),
+        };
+    }
 }
 
 fn main() {
