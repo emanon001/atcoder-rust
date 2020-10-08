50a381f8824d02646004c82a645088008f376fa3
emanon001 <emanon001@gmail.com>
2020-10-08 23:38:01 +0900

    solve past202004-open/i

--- past202004-open/src/bin/i.rs
+++ past202004-open/src/bin/i.rs
@@ -10,7 +10,31 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; 2.pow(n as u32)]
     };
+
+    let mut res = vec![0; av.len()];
+    let av = av.into_iter().enumerate().collect::<Vec<_>>();
+    let mut winners = av;
+    let mut c = 1;
+    while winners.len() >= 2 {
+        let mut new_winners = Vec::new();
+        for chunk in winners.chunks(2) {
+            let max = chunk.into_iter().max_by_key(|x| x.1).unwrap();
+            for (i, a) in chunk {
+                if i == &max.0 {
+                    new_winners.push((*i, *a));
+                }
+                res[*i] = c;
+            }
+        }
+        winners = new_winners;
+        c += 1;
+    }
+    for x in res {
+        println!("{}", x);
+    }
 }
 
 fn main() {
