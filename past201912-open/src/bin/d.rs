40737b4a1030ebb178e244d5cbe329d2a4b552fe
emanon001 <emanon001@gmail.com>
2020-10-09 22:26:45 +0900

    solve past201912-open/d

--- past201912-open/src/bin/d.rs
+++ past201912-open/src/bin/d.rs
@@ -9,6 +9,20 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: usize,
+        av: [Usize1; n]
+    };
+
+    let mut counts = vec![0; n];
+    for a in av {
+        counts[a] += 1;
+    }
+    if counts.iter().collect::<HashSet<_>>().len() == 1 {
+        println!("Correct");
+    } else {
+        let x = counts.iter().position(|&a| a == 0).unwrap() + 1;
+        let y = counts.iter().position(|&a| a == 2).unwrap() + 1;
+        println!("{} {}", y, x);
+    }
 }
