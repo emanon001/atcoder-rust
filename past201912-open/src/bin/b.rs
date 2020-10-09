1b3e8a7d1b2b657ffeb5c45932185525c2957f56
emanon001 <emanon001@gmail.com>
2020-10-09 22:18:21 +0900

    solve past201912-open/b

--- past201912-open/src/bin/b.rs
+++ past201912-open/src/bin/b.rs
@@ -9,6 +9,21 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: usize,
+        av: [i64; n]
+    };
+
+    let mut prev = av[0];
+    for a in av.into_iter().skip(1) {
+        let res = if a == prev {
+            "stay".to_string()
+        } else if a < prev {
+            format!("down {}", prev - a)
+        } else {
+            format!("up {}", a - prev)
+        };
+        println!("{}", res);
+        prev = a;
+    }
 }
