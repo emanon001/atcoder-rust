f726f9b96277917fdb8ad5824bdf97a46a85113a
emanon001 <emanon001@gmail.com>
2020-10-11 02:17:51 +0900

    solve hhkb2020/c

--- hhkb2020/src/bin/c.rs
+++ hhkb2020/src/bin/c.rs
@@ -10,7 +10,16 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        pv: [usize; n]
     };
+
+    let mut set = (0..=200_000).collect::<BTreeSet<_>>();
+    for p in pv {
+        set.remove(&p);
+        let res = set.iter().next().unwrap();
+        println!("{}", res);
+    }
 }
 
 fn main() {
