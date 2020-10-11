fe1344c983721292b7abab323398de0474c50476
emanon001 <emanon001@gmail.com>
2020-10-11 14:23:14 +0900

    solve past201912-open/f

--- past201912-open/src/bin/f.rs
+++ past201912-open/src/bin/f.rs
@@ -5,10 +5,21 @@ use num::*;
 use proconio::input;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use regex::Regex;
 #[allow(unused_imports)]
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        s: String
+    };
+
+    let re = Regex::new(r"[A-Z][a-z]*[A-Z]").unwrap();
+    let mut words = Vec::new();
+    for c in re.captures_iter(&s) {
+        words.push(c[0].to_string());
+    }
+    words.sort_by_key(|w| w.to_ascii_lowercase());
+    let res = words.join("");
+    println!("{}", res);
 }
