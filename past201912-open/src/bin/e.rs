97fd6a6083342d97350b5a2598cde55c289fd278
emanon001 <emanon001@gmail.com>
2020-10-11 14:05:03 +0900

    solve past201912-open/e

--- past201912-open/src/bin/e.rs
+++ past201912-open/src/bin/e.rs
@@ -9,6 +9,55 @@ use proconio::marker::*;
 use std::collections::*;
 
 fn main() {
-  input! {
-  };
+    input! {
+        n: usize, q: usize,
+    };
+
+    let mut following = vec![HashSet::new(); n];
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    a: Usize1, b: Usize1
+                };
+                following[a].insert(b);
+            }
+            2 => {
+                input! {
+                    a: Usize1
+                };
+                for b in 0..n {
+                    if following[b].contains(&a) {
+                        following[a].insert(b);
+                    }
+                }
+            }
+            3 => {
+                input! {
+                    a: Usize1
+                };
+                let mut users = HashSet::new();
+                for &x in &following[a] {
+                    for &b in &following[x] {
+                        if a != b {
+                            users.insert(b);
+                        }
+                    }
+                }
+                for b in users {
+                    following[a].insert(b);
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+    for i in 0..n {
+        let res = (0..n)
+            .map(|j| if following[i].contains(&j) { "Y" } else { "N" })
+            .join("");
+        println!("{}", res);
+    }
 }
