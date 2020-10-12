5e65e63a3533fbe30800f3d044ffa1813d63509d
emanon001 <emanon001@gmail.com>
2020-10-12 23:35:23 +0900

    solve past202005-open/b

--- past202005-open/src/bin/b.rs
+++ past202005-open/src/bin/b.rs
@@ -10,7 +10,37 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize, q: usize,
     };
+
+    let mut solves = vec![Vec::new(); n];
+    let mut solve_counts = vec![0; m];
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    n_i: Usize1
+                }
+                let mut res = 0;
+                for &i in &solves[n_i] {
+                    res += n - solve_counts[i];
+                }
+                println!("{}", res);
+            }
+            2 => {
+                input! {
+                    n_i: Usize1,
+                    m_i: Usize1,
+                }
+                solves[n_i].push(m_i);
+                solve_counts[m_i] += 1;
+            }
+            _ => unreachable!(),
+        };
+    }
 }
 
 fn main() {
