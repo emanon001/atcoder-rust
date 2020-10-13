6c432d07e3f1edd7c0a1499334d86b3af4043bd6
emanon001 <emanon001@gmail.com>
2020-10-13 14:52:33 +0900

    solve past202005-open/d

--- past202005-open/src/bin/d.rs
+++ past202005-open/src/bin/d.rs
@@ -8,9 +8,45 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn matched(i: usize, num: usize, grid: &[Vec<char>], numbers: &[Vec<char>]) -> bool {
+    for r in 0..5 {
+        for c in 0..4 {
+            if grid[r][i * 4 + c] != numbers[r][num * 4 + c] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
 fn solve() {
     input! {
+        n: usize,
+        grid: [Chars; 5]
     };
+
+    let numbers = r#"
+.###..#..###.###.#.#.###.###.###.###.###.
+.#.#.##....#...#.#.#.#...#.....#.#.#.#.#.
+.#.#..#..###.###.###.###.###...#.###.###.
+.#.#..#..#.....#...#...#.#.#...#.#.#...#.
+.###.###.###.###...#.###.###...#.###.###.
+    "#
+    .trim()
+    .split("\n")
+    .map(|row| row.chars().collect::<Vec<char>>())
+    .collect::<Vec<_>>();
+    let res = (0..n)
+        .map(|i| {
+            for num in 0..10 {
+                if matched(i, num, &grid, &numbers) {
+                    return num;
+                }
+            }
+            unreachable!();
+        })
+        .join("");
+    println!("{}", res);
 }
 
 fn main() {
