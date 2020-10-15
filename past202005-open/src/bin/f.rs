bcaac53c75bc13be0689928eb2ecc550cb703a02
emanon001 <emanon001@gmail.com>
2020-10-15 20:41:13 +0900

    solve past202005-open/f

--- past202005-open/src/bin/f.rs
+++ past202005-open/src/bin/f.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        grid: [Chars; n]
     };
+
+    let mut res = vec!['_'; n];
+    for i in 0..n / 2 {
+        let set1 = grid[i].iter().collect::<HashSet<_>>();
+        let set2 = grid[n - i - 1].iter().collect::<HashSet<_>>();
+        if let Some(&&ch) = set1.intersection(&set2).next() {
+            res[i] = ch;
+            res[n - i - 1] = ch;
+        } else {
+            println!("-1");
+            return;
+        }
+    }
+    if n % 2 == 1 {
+        let i = n / 2;
+        let ch = grid[i][0];
+        res[i] = ch;
+    }
+    println!("{}", res.into_iter().collect::<String>());
 }
 
 fn main() {
