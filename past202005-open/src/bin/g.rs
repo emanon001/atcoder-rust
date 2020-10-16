925c7f83f741a2bf1514c272eb70972ad3bde031
emanon001 <emanon001@gmail.com>
2020-10-16 11:27:31 +0900

    solve past202005-open/g

--- past202005-open/src/bin/g.rs
+++ past202005-open/src/bin/g.rs
@@ -8,9 +8,249 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Clone)]
+pub struct WeightedGraph {
+    graph: Vec<Vec<(usize, i64)>>,
+    vn: usize,
+}
+pub type WeightedEdge = (usize, usize, i64);
+impl WeightedGraph {
+    const INF: i64 = 1 << 60;
+    pub fn new(edges: &[WeightedEdge], vn: usize) -> Self {
+        let mut graph = vec![Vec::new(); vn];
+        for &(u, v, w) in edges {
+            graph[u].push((v, w));
+            graph[v].push((u, w));
+        }
+        Self { graph, vn }
+    }
+    pub fn new_directed(edges: &[WeightedEdge], vn: usize) -> Self {
+        let mut graph = vec![Vec::new(); vn];
+        for &(u, v, w) in edges {
+            graph[u].push((v, w));
+        }
+        Self { graph, vn }
+    }
+    pub fn add_directed_edge(&mut self, e: WeightedEdge) {
+        self.graph[e.0].push((e.1, e.2));
+    }
+    pub fn add_edge(&mut self, e: WeightedEdge) {
+        self.graph[e.0].push((e.1, e.2));
+        self.graph[e.1].push((e.0, e.2));
+    }
+    pub fn bellman_ford(&self, s: usize) -> Option<Vec<Option<i64>>> {
+        let vn = self.vn;
+        let inf = Self::INF;
+        let mut cost_list = vec![inf; vn];
+        cost_list[s] = 0;
+        for c in 0..vn {
+            for u in 0..vn {
+                for &(v, w) in &self.graph[u] {
+                    if cost_list[u] == inf {
+                        continue;
+                    }
+                    let new_cost = cost_list[u] + w;
+                    if new_cost < cost_list[v] {
+                        cost_list[v] = new_cost;
+                        if c == vn - 1 {
+                            return None;
+                        }
+                    }
+                }
+            }
+        }
+        Some(self.optionalize(cost_list))
+    }
+    pub fn prim(&self) -> i64 {
+        let mut used = std::collections::HashSet::new();
+        let mut heap = std::collections::BinaryHeap::new();
+        let mut res = 0_i64;
+        heap.push(std::cmp::Reverse((0_i64, 0)));
+        while let Some(std::cmp::Reverse((weight, u))) = heap.pop() {
+            if used.contains(&u) {
+                continue;
+            }
+            used.insert(u);
+            res += weight;
+            for &(v, w) in &self.graph[u] {
+                if used.contains(&v) {
+                    continue;
+                }
+                heap.push(std::cmp::Reverse((w, v)));
+            }
+        }
+        res
+    }
+    pub fn reachable_vertexes(&self, s: usize) -> std::collections::HashSet<usize> {
+        let mut visited = std::collections::HashSet::new();
+        let mut queue = std::collections::VecDeque::new();
+        visited.insert(s);
+        queue.push_back(s);
+        while let Some(u) = queue.pop_front() {
+            for &(v, _) in &self.graph[u] {
+                if visited.contains(&v) {
+                    continue;
+                }
+                visited.insert(v);
+                queue.push_back(v);
+            }
+        }
+        visited
+    }
+    pub fn rev(&self) -> WeightedGraph {
+        let mut edges = Vec::new();
+        for u in 0..self.vn {
+            for &(v, w) in &self.graph[u] {
+                edges.push((v, u, w));
+            }
+        }
+        Self::new_directed(&edges, self.vn)
+    }
+    pub fn shortest_path(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![Self::INF; self.vn];
+        let mut heap = std::collections::BinaryHeap::new();
+        cost_list[start] = 0;
+        heap.push(std::cmp::Reverse((0_i64, start)));
+        while let Some(std::cmp::Reverse((cost, u))) = heap.pop() {
+            if cost > cost_list[u] {
+                continue;
+            }
+            for &(v, w) in &self.graph[u] {
+                let new_cost = cost + w;
+                if new_cost < cost_list[v] {
+                    heap.push(std::cmp::Reverse((new_cost, v)));
+                    cost_list[v] = new_cost;
+                }
+            }
+        }
+        self.optionalize(cost_list)
+    }
+    pub fn shortest_path_1(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![None; self.vn];
+        let mut que = std::collections::VecDeque::new();
+        cost_list[start] = Some(0);
+        que.push_back(start);
+        while let Some(u) = que.pop_front() {
+            for &(v, w) in &self.graph[u] {
+                if w != 1 {
+                    panic!("weight is not 1");
+                }
+                if cost_list[v].is_some() {
+                    continue;
+                }
+                let new_cost = cost_list[u].unwrap() + 1;
+                cost_list[v] = Some(new_cost);
+                que.push_back(v);
+            }
+        }
+        cost_list
+    }
+    pub fn shortest_path_01(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![Self::INF; self.vn];
+        let mut que = std::collections::VecDeque::new();
+        cost_list[start] = 0;
+        que.push_front((start, 0_i64));
+        while let Some((u, cost)) = que.pop_front() {
+            if cost > cost_list[u] {
+                continue;
+            }
+            for &(v, w) in &self.graph[u] {
+                if w != 0 && w != 1 {
+                    panic!("weight is not 01");
+                }
+                let new_cost = cost + w;
+                if new_cost < cost_list[v] {
+                    cost_list[v] = new_cost;
+                    if w == 0 {
+                        que.push_front((v, new_cost));
+                    } else {
+                        que.push_back((v, new_cost));
+                    }
+                }
+            }
+        }
+        self.optionalize(cost_list)
+    }
+    pub fn warshall_floyd(&self) -> Vec<Vec<Option<i64>>> {
+        let inf = Self::INF;
+        let vn = self.vn;
+        let mut cost_list = vec![vec![inf; vn]; vn];
+        for u in 0..vn {
+            for &(v, w) in &self.graph[u] {
+                cost_list[u][v] = w;
+            }
+        }
+        for i in 0..vn {
+            cost_list[i][i] = 0;
+        }
+        for k in 0..vn {
+            for i in 0..vn {
+                for j in 0..vn {
+                    cost_list[i][j] =
+                        std::cmp::min(cost_list[i][j], cost_list[i][k] + cost_list[k][j]);
+                }
+            }
+        }
+        cost_list
+            .into_iter()
+            .map(|v| self.optionalize(v))
+            .collect::<Vec<_>>()
+    }
+    fn optionalize(&self, v: Vec<i64>) -> Vec<Option<i64>> {
+        v.into_iter()
+            .map(|x| if x == Self::INF { None } else { Some(x) })
+            .collect::<Vec<_>>()
+    }
+}
+
+const DIRECTIONS: [(isize, isize); 6] = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (0, -1)];
+
+pub type GridPos = (isize, isize);
+pub fn gen_grid_destinations(i: isize, j: isize, ng_set: &HashSet<GridPos>) -> Vec<GridPos> {
+    let mut dest = Vec::new();
+    for &(di, dj) in &DIRECTIONS {
+        let new_i = i + di;
+        let new_j = j + dj;
+        if new_i < -201 || new_i > 201 || new_j < -201 || new_j > 201 {
+            continue;
+        }
+        let pos = (new_i, new_j);
+        if ng_set.contains(&pos) {
+            continue;
+        }
+        dest.push(pos);
+    }
+    dest
+}
+
+fn vertex(pos: GridPos) -> usize {
+    let (i, j) = pos;
+    ((i + 201) * (201 + 201 + 1) + (j + 201)) as usize
+}
+
 fn solve() {
     input! {
+        n: usize, x: isize, y: isize,
+        xyv: [(isize, isize); n]
     };
+
+    let ng_set = xyv.into_iter().collect::<HashSet<_>>();
+    let mut edges = Vec::new();
+    for i in -201..=201 {
+        for j in -201..=201 {
+            let u = vertex((i, j));
+            for d in gen_grid_destinations(i, j, &ng_set) {
+                let v = vertex(d);
+                edges.push((u, v, 1));
+            }
+        }
+    }
+    let graph = WeightedGraph::new_directed(&edges, vertex((201, 201)) + 1);
+    let start = vertex((0, 0));
+    let dist = graph.shortest_path_1(start);
+    let goal = vertex((x, y));
+    let res = if let Some(w) = dist[goal] { w } else { -1 };
+    println!("{}", res);
 }
 
 fn main() {
