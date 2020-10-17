c8115f074fa8ef308a14df9edda198b225a9dd7a
emanon001 <emanon001@gmail.com>
2020-10-17 23:05:11 +0900

    solve abc180/e

--- abc180/src/bin/e.rs
+++ abc180/src/bin/e.rs
@@ -8,9 +8,41 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn f(state: usize, dp: &mut [Vec<Option<i64>>], u: usize, xyzv: &[(i64, i64, i64)]) -> i64 {
+    if let Some(res) = dp[state][u] {
+        return res;
+    }
+    if state == (1 << xyzv.len()) - 1 && u == 0 {
+        dp[state][u] = Some(0);
+        return 0;
+    }
+    let (cx, cy, cz) = xyzv[u];
+    let mut res = 1_i64 << 60;
+    for v in 0..xyzv.len() {
+        if state & (1 << v) == 1 {
+            continue;
+        }
+        let (x, y, z) = xyzv[v];
+        let new_state = state | (1 << v);
+        if new_state != state {
+            let cost =
+                ((x - cx).abs() + (y - cy).abs() + 0.max(cz - z)) + f(new_state, dp, v, xyzv);
+            res = res.min(cost);
+        }
+    }
+    dp[state][u] = Some(res);
+    res
+}
+
 fn solve() {
     input! {
+        n: usize,
+        xyzv: [(i64, i64, i64); n]
     };
+
+    let mut dp = vec![vec![None; n]; 1 << n];
+    let res = f(0, &mut dp, 0, &xyzv);
+    println!("{}", res);
 }
 
 fn main() {
