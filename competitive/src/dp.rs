d8afb49fcfeb89ef682b42bf6bfaed42026d0207
emanon001 <emanon001@gmail.com>
2020-10-18 10:15:10 +0900

    update `dp::*`

--- competitive/src/dp.rs
+++ competitive/src/dp.rs
@@ -7,7 +7,7 @@ pub fn bit_dp<C>(
     candidates: &[C],
     fin: usize,
     inf: i64,
-    f: fn(usize, &[C], usize) -> (usize, i64),
+    f: fn(usize, &C) -> (usize, i64),
 ) -> i64 {
     if let Some(res) = dp[state] {
         return res;
@@ -18,8 +18,8 @@ pub fn bit_dp<C>(
         return res;
     }
     let mut res = inf;
-    for i in 0..candidates.len() {
-        let (new_state, cost) = f(state, candidates, i);
+    for c in candidates {
+        let (new_state, cost) = f(state, c);
         if new_state != state {
             let cost = bit_dp(new_state, dp, candidates, fin, inf, f) + cost;
             res = res.min(cost);
@@ -30,15 +30,14 @@ pub fn bit_dp<C>(
 }
 
 #[snippet]
-pub fn traveling_salesman<C>(
+pub fn traveling_salesman(
     state: usize,
     u: usize,
     dp: &mut [Vec<Option<i64>>],
-    candidates: &[C],
+    graph: &[Vec<(usize, i64)>],
     start: usize,
     fin: usize,
     inf: i64,
-    f: fn(usize, &[C], usize, usize) -> (usize, i64),
 ) -> i64 {
     if let Some(res) = dp[state][u] {
         return res;
@@ -48,10 +47,10 @@ pub fn traveling_salesman<C>(
         return 0;
     }
     let mut res = inf;
-    for v in 0..candidates.len() {
-        let (new_state, cost) = f(state, candidates, u, v);
+    for &(v, cost) in &graph[u] {
+        let new_state = state | (1 << v);
         if new_state != state {
-            let cost = traveling_salesman(new_state, v, dp, candidates, start, fin, inf, f) + cost;
+            let cost = traveling_salesman(new_state, v, dp, graph, start, fin, inf) + cost;
             res = res.min(cost);
         }
     }
@@ -75,13 +74,13 @@ mod tests {
         let fin = (1 << 3) - 1;
         let inf = 1_i64 << 60;
         let mut dp = vec![None; fin + 1];
-        let cost = bit_dp(0, &mut dp, &candidates, fin, inf, |state, candidates, i| {
-            let (s, c) = &candidates[i];
+        let cost = bit_dp(0, &mut dp, &candidates, fin, inf, |state, c| {
+            let (s, cost) = c;
             let mut new_state = state;
             for (i, ch) in s.iter().enumerate() {
                 new_state |= if *ch == 'Y' { 1 << i } else { 0 };
             }
-            (new_state, *c)
+            (new_state, *cost)
         });
         assert_eq!(cost, 30);
     }
@@ -89,26 +88,24 @@ mod tests {
     #[test]
     fn test_traveling_salesman() {
         // ref. https://atcoder.jp/contests/abc180/tasks/abc180_e
-        let candidates: Vec<(i64, i64, i64)> = vec![(0, 0, 0), (1, 1, 1), (-1, -1, -1)];
-        let fin = (1 << 3) - 1;
-        let inf = 1_i64 << 60;
-        let mut dp = vec![vec![None; 3]; fin + 1];
-        let cost = traveling_salesman(
-            0,
-            0,
-            &mut dp,
-            &candidates,
-            0,
-            fin,
-            inf,
-            |state, candidates, u, v| {
-                let (ux, uy, uz) = candidates[u];
-                let (vx, vy, vz) = candidates[v];
-                let new_state = state | (1 << v);
+        let n = 3;
+        let vertexes: Vec<(i64, i64, i64)> = vec![(0, 0, 0), (1, 1, 1), (-1, -1, -1)];
+        let mut graph = vec![Vec::new(); n];
+        for u in 0..n {
+            for v in 0..n {
+                if u == v {
+                    continue;
+                }
+                let (ux, uy, uz) = vertexes[u];
+                let (vx, vy, vz) = vertexes[v];
                 let cost = (vx - ux).abs() + (vy - uy).abs() + 0.max(vz - uz);
-                (new_state, cost)
-            },
-        );
+                graph[u].push((v, cost));
+            }
+        }
+        let fin = (1 << n) - 1;
+        let inf = 1_i64 << 60;
+        let mut dp = vec![vec![None; n]; fin + 1];
+        let cost = traveling_salesman(0, 0, &mut dp, &graph, 0, fin, inf);
         assert_eq!(cost, 10);
     }
 }
