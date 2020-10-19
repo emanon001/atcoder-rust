de279be8e7a3d4c2ed34a80bfca94d7df0ca6293
emanon001 <emanon001@gmail.com>
2020-10-19 19:39:05 +0900

    solve past201912-open/k

--- past201912-open/src/bin/k.rs
+++ past201912-open/src/bin/k.rs
@@ -8,59 +8,49 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
-pub trait Monoid {
-    fn empty() -> Self;
-    fn append(&self, other: &Self) -> Self;
-}
-
 pub struct SegmentTree<T>
 where
-    T: Monoid + Clone + Copy,
+    T: Monoid + Clone,
 {
     size: usize,
     data: Vec<T>,
 }
-
 impl<T> SegmentTree<T>
 where
-    T: Monoid + Clone + Copy,
+    T: Monoid + Clone,
 {
     pub fn new(size: usize) -> Self {
         let size = Self::normalize_data_size(size);
-        let data = vec![T::empty(); size * 2 - 1];
+        let data = vec![T::mempty(); size * 2 - 1];
         Self { size, data }
     }
-
     pub fn from_slice(values: &[T]) -> Self {
         let mut st = SegmentTree::new(values.len());
         for (i, v) in values.into_iter().enumerate() {
-            st.data[i + st.size - 1] = *v;
+            st.data[i + st.size - 1] = v.clone();
         }
         if st.size < 2 {
             return st;
         }
         for i in (0..=(st.size - 2)).rev() {
-            st.data[i] = st.data[i * 2 + 1].append(&st.data[i * 2 + 2]);
+            st.data[i] = st.data[i * 2 + 1].mappend(&st.data[i * 2 + 2]);
         }
         st
     }
-
-    // 0-origin
+    /// 0-origin
     pub fn update(&mut self, i: usize, v: T) {
         let mut i = i + self.size - 1;
         self.data[i] = v;
         while i > 0 {
             i = (i - 1) / 2;
-            self.data[i] = self.data[i * 2 + 1].append(&self.data[i * 2 + 2]);
+            self.data[i] = self.data[i * 2 + 1].mappend(&self.data[i * 2 + 2]);
         }
     }
-
-    // [a, b)
-    // 0-origin
+    /// [a, b)
+    /// 0-origin
     pub fn query(&self, a: usize, b: usize) -> T {
         self.execute_query(a, b, 0, 0, self.size)
     }
-
     fn normalize_data_size(size: usize) -> usize {
         let mut n = 1;
         while n < size {
@@ -68,35 +58,35 @@ where
         }
         n
     }
-
     fn execute_query(&self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
         if r <= a || b <= l {
-            return T::empty();
+            return T::mempty();
         }
         if a <= l && r <= b {
-            return self.data[i];
+            return self.data[i].clone();
         }
         let vl = self.execute_query(a, b, i * 2 + 1, l, (l + r) / 2);
         let vr = self.execute_query(a, b, i * 2 + 2, (l + r) / 2, r);
-        vl.append(&vr)
+        vl.mappend(&vr)
     }
 }
-
+pub trait Monoid {
+    fn mempty() -> Self;
+    fn mappend(&self, other: &Self) -> Self;
+}
 #[derive(Debug, Copy, Clone)]
 pub struct LcaDepth {
     depth: usize,
     idx: usize,
 }
-
 impl Monoid for LcaDepth {
-    fn empty() -> Self {
+    fn mempty() -> Self {
         Self {
             depth: std::usize::MAX,
             idx: 0,
         }
     }
-
-    fn append(&self, other: &Self) -> Self {
+    fn mappend(&self, other: &Self) -> Self {
         if self.depth <= other.depth {
             *self
         } else {
@@ -104,7 +94,6 @@ impl Monoid for LcaDepth {
         }
     }
 }
-
 pub struct Lca {
     vs: Vec<usize>,
     vdepth: Vec<usize>,
@@ -112,11 +101,10 @@ pub struct Lca {
     vn: usize,
     st: SegmentTree<LcaDepth>,
 }
-
 impl Lca {
-    pub fn new(edges: &[(usize, usize)], vn: usize, root: usize) -> Self {
+    pub fn new(edges: Vec<(usize, usize)>, vn: usize, root: usize) -> Self {
         let mut graph = vec![Vec::new(); vn];
-        for &(u, v) in edges {
+        for (u, v) in edges {
             graph[u].push(v);
             graph[v].push(u);
         }
@@ -148,8 +136,7 @@ impl Lca {
             st,
         }
     }
-
-    // 0-origin
+    /// 0-origin
     pub fn depth(&self, u: usize) -> usize {
         if u >= self.vn {
             panic!("u >= self.vn");
@@ -157,7 +144,6 @@ impl Lca {
         let i = self.vidx[u];
         self.vdepth[i]
     }
-
     pub fn query(&self, u: usize, v: usize) -> usize {
         let ui = self.vidx[u];
         let vi = self.vidx[v];
@@ -166,7 +152,6 @@ impl Lca {
             .query(std::cmp::min(ui, vi), std::cmp::max(ui, vi) + 1);
         self.vs[idx]
     }
-
     fn traverse(
         u: usize,
         parent: usize,
@@ -194,30 +179,25 @@ impl Lca {
 
 fn main() {
     input! {
-      n: usize,
-      pv: [isize; n],
-      q: usize,
-      queries: [(Usize1, Usize1); q]
+        n: usize,
+        pv: [isize; n],
+        q: usize,
+        queries: [(Usize1, Usize1); q]
     };
 
     let mut root = 0;
     let mut edges = Vec::new();
-    for (u, v) in pv.into_iter().enumerate() {
-        if v == -1 {
+    for (u, p) in pv.into_iter().enumerate() {
+        if p == -1 {
             root = u;
             continue;
         }
-        let v = (v - 1) as usize;
-        edges.push((u, v));
+        let p = (p - 1) as usize;
+        edges.push((u, p));
     }
-    let lca = Lca::new(&edges, n, root);
-    for (u, v) in queries {
-        let i = lca.query(u, v);
-        let res = if lca.depth(i) == lca.depth(v) {
-            "Yes"
-        } else {
-            "No"
-        };
+    let lca = Lca::new(edges, n, root);
+    for (a, b) in queries {
+        let res = if lca.query(a, b) == b { "Yes" } else { "No" };
         println!("{}", res);
     }
 }
