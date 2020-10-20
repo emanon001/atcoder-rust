3807a76ef63ba4234ce0372dfc2ccd1ad716c1f4
emanon001 <emanon001@gmail.com>
2020-10-20 20:44:24 +0900

    solve past202004-open/j

--- past202004-open/src/bin/j.rs
+++ past202004-open/src/bin/j.rs
@@ -8,9 +8,58 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn parse_ascii_chars(i: usize, chars: &[char]) -> (usize, Vec<char>) {
+    let mut j = i;
+    let mut s = Vec::new();
+    while j < chars.len() && chars[j].is_ascii_alphabetic() {
+        s.push(chars[j]);
+        j += 1;
+    }
+    (j, s)
+}
+
+fn parse_parent(i: usize, chars: &[char]) -> (usize, Vec<char>) {
+    let mut j = i + 1;
+    let mut s = Vec::new();
+    while j < chars.len() && chars[j] != ')' {
+        let x = parse_expr(j, chars);
+        s.extend(x.1);
+        j = x.0;
+    }
+    j += 1;
+    let mut rs = s.clone();
+    rs.reverse();
+    s.append(&mut rs);
+    (j, s)
+}
+
+// "abc" | (<expr>[, <expr>, ...])
+fn parse_expr(i: usize, chars: &[char]) -> (usize, Vec<char>) {
+    if chars[i] == '(' {
+        parse_parent(i, chars)
+    } else {
+        parse_ascii_chars(i, chars)
+    }
+}
+
+fn parse(i: usize, chars: &[char]) -> (usize, Vec<char>) {
+    let mut j = i;
+    let mut s = Vec::new();
+    while j < chars.len() {
+        let mut expr = parse_expr(j, chars);
+        j = expr.0;
+        s.append(&mut expr.1);
+    }
+    (j, s)
+}
+
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let res = parse(0, &s).1.into_iter().join("");
+    println!("{}", res);
 }
 
 fn main() {
