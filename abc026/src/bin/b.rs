f6dfcc5ca3a14aab6555f524d2c8e3bdcc515562
emanon001 <emanon001@gmail.com>
2020-10-20 17:34:31 +0900

    solve abc026/b

--- abc026/src/bin/b.rs
+++ abc026/src/bin/b.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        mut rv: [i64; n]
     };
+
+    rv.sort_by_key(|r| -r);
+    let mut res = 0_f64;
+    for i in 0..n {
+        if i % 2 == 1 {
+            continue;
+        }
+        let r = rv[i];
+        let outer_area = std::f64::consts::PI * (r * r) as f64;
+        res += outer_area;
+        if i + 1 < n {
+            let ir = rv[i + 1];
+            let inner_area = std::f64::consts::PI * (ir * ir) as f64;
+            res -= inner_area;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
