66cfc819897bc6186e708f66cb3281c22d9558a1
emanon001 <emanon001@gmail.com>
2020-10-20 17:44:36 +0900

    solve abc026/c

--- abc026/src/bin/c.rs
+++ abc026/src/bin/c.rs
@@ -8,9 +8,33 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn f(u: usize, graph: &[Vec<usize>]) -> usize {
+    if graph[u].is_empty() {
+        return 1;
+    }
+    let mut max = 0;
+    let mut min = std::usize::MAX;
+    for &v in &graph[u] {
+        let x = f(v, graph);
+        max = max.max(x);
+        min = min.min(x);
+    }
+    max + min + 1
+}
+
 fn solve() {
     input! {
+        n: usize,
+        bv: [Usize1; n - 1]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for u in 1..n {
+        let p = bv[u - 1];
+        graph[p].push(u);
+    }
+    let res = f(0, &graph);
+    println!("{}", res);
 }
 
 fn main() {
