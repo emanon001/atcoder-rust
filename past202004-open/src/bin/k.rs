4aec90e6e012885d92f7c0507ddd26399af1c21c
emanon001 <emanon001@gmail.com>
2020-10-21 14:31:04 +0900

    solve past202004-open/k

--- past202004-open/src/bin/k.rs
+++ past202004-open/src/bin/k.rs
@@ -10,7 +10,43 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        s: Chars,
+        cv: [i64; n],
+        dv: [i64; n],
     };
+
+    let mut dp = vec![vec![1_i64 << 60; n + 1]; n + 1];
+    dp[0][0] = 0_i64;
+    for i in 0..n {
+        let ch = s[i];
+        for j in 0..n {
+            match ch {
+                '(' => {
+                    // not change
+                    dp[i + 1][j + 1] = dp[i + 1][j + 1].min(dp[i][j]);
+                    // change
+                    if j > 0 {
+                        dp[i + 1][j - 1] = dp[i + 1][j - 1].min(dp[i][j] + cv[i]);
+                    }
+                    // delete
+                    dp[i + 1][j] = dp[i + 1][j].min(dp[i][j] + dv[i]);
+                }
+                ')' => {
+                    // not change
+                    if j > 0 {
+                        dp[i + 1][j - 1] = dp[i + 1][j - 1].min(dp[i][j]);
+                    }
+                    // change
+                    dp[i + 1][j + 1] = dp[i + 1][j + 1].min(dp[i][j] + cv[i]);
+                    // delete
+                    dp[i + 1][j] = dp[i + 1][j].min(dp[i][j] + dv[i]);
+                }
+                _ => unreachable!(),
+            };
+        }
+    }
+    println!("{}", dp[n][0]);
 }
 
 fn main() {
