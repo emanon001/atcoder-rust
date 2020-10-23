dab4776ee6c6915889102e687067989db0b04448
emanon001 <emanon001@gmail.com>
2020-10-23 19:16:51 +0900

    solve past202005-open/j

--- past202005-open/src/bin/j.rs
+++ past202005-open/src/bin/j.rs
@@ -8,9 +8,47 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av:[i64; m]
     };
+
+    let mut sv = vec![0; n];
+    for a in av {
+        let i = bsearch(n as i64, -1, |x| {
+            let x = x as usize;
+            a > sv[x]
+        });
+        if let Some(i) = i {
+            sv[i as usize] = a;
+            println!("{}", i + 1);
+        } else {
+            println!("-1");
+        };
+    }
 }
 
 fn main() {
