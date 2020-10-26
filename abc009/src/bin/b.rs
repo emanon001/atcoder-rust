100675952fb1c92b44e03d314d5e19879d7b8ce8
emanon001 <emanon001@gmail.com>
2020-10-26 20:32:07 +0900

    solve abc009/b

--- abc009/src/bin/b.rs
+++ abc009/src/bin/b.rs
@@ -10,7 +10,15 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n]
     };
+
+    let set = av.into_iter().collect::<BTreeSet<_>>();
+    let mut iter = set.iter();
+    iter.next_back();
+    let res = iter.next_back().unwrap();
+    println!("{}", res);
 }
 
 fn main() {
