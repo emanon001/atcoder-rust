cda926038b651f9cd3238949c82e244cfe57a683
emanon001 <emanon001@gmail.com>
2020-10-28 21:07:44 +0900

    solve arc106/a

--- arc106/src/bin/a.rs
+++ arc106/src/bin/a.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: u128
     };
+
+    for a in 1..=50 {
+        for b in 1..=50 {
+            let v = 3.pow(a) + 5.pow(b);
+            if v > n {
+                break;
+            }
+            if v == n {
+                println!("{} {}", a, b);
+                return;
+            }
+        }
+    }
+    println!("-1");
 }
 
 fn main() {
