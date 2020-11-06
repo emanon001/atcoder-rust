e07ec42deef1488b040bee105a1e1903fc93ccab
emanon001 <emanon001@gmail.com>
2020-11-06 23:47:46 +0900

    solve numura2020/a

--- nomura2020/src/bin/a.rs
+++ nomura2020/src/bin/a.rs
@@ -10,7 +10,13 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h1: i64, m1: i64, h2: i64, m2: i64, k: i64
     };
+
+    let a = h1 * 60 + m1;
+    let b = h2 * 60 + m2;
+    let res = b - a - k;
+    println!("{}", res);
 }
 
 fn main() {
