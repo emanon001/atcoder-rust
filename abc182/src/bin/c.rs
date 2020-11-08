3acf9285071d20398c1b77baf9b1606a2b7350cd
emanon001 <emanon001@gmail.com>
2020-11-08 22:24:40 +0900

    solve abc182/c

--- abc182/src/bin/c.rs
+++ abc182/src/bin/c.rs
@@ -10,7 +10,34 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: Chars
     };
+
+    let n = n
+        .into_iter()
+        .map(|ch| ch.to_digit(10).unwrap() as u64)
+        .collect::<Vec<_>>();
+
+    let mut res = std::isize::MAX;
+    for bits in 0..(1 << n.len()) {
+        if bits == 0 {
+            continue;
+        }
+        let mut x = 0;
+        let mut c = 0;
+        for d in (0..n.len()).rev() {
+            if (bits >> d) & 1 == 1 {
+                x = x * 10 + n[n.len() - d - 1];
+            } else {
+                c += 1;
+            }
+        }
+        if x % 3 == 0 && c < res {
+            res = c;
+        }
+    }
+    let res = if res == std::isize::MAX { -1 } else { res };
+    println!("{}", res);
 }
 
 fn main() {
