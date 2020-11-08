b6d64a97d1fde75307fcf79e8a6e3c607800f86f
emanon001 <emanon001@gmail.com>
2020-11-08 22:24:44 +0900

    solve abc182/d

--- abc182/src/bin/d.rs
+++ abc182/src/bin/d.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let mut cusum = vec![0_i64; n + 5];
+    let mut cumax = vec![std::i64::MIN; n + 5];
+    for i in 1..=n {
+        cusum[i] = cusum[i - 1] + av[i - 1];
+        cumax[i] = cumax[i - 1].max(cusum[i]);
+    }
+    let mut res = 0_i64;
+    let mut cur = 0_i64;
+    for x in 1..=n {
+        let t = cur + cumax[x];
+        res = res.max(t);
+        cur += cusum[x];
+    }
+    println!("{}", res);
 }
 
 fn main() {
