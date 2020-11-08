2c43b2c4bb87cd3d22c20e63cb8c92be84c74220
emanon001 <emanon001@gmail.com>
2020-11-08 22:24:36 +0900

    solve abc182/b

--- abc182/src/bin/b.rs
+++ abc182/src/bin/b.rs
@@ -10,7 +10,25 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [u64; n]
     };
+
+    let mut max_c = 0;
+    let mut res = 0;
+    for k in 2..=1000 {
+        let mut c = 0;
+        for &a in &av {
+            if a % k == 0 {
+                c += 1;
+            }
+        }
+        if c >= max_c {
+            res = k;
+            max_c = c;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
