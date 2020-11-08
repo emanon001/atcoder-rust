4ced66768c84003bff9272fbbdc4ce4598e16a23
emanon001 <emanon001@gmail.com>
2020-11-08 22:24:49 +0900

    solve abc182/e

--- abc182/src/bin/e.rs
+++ abc182/src/bin/e.rs
@@ -10,7 +10,69 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize, n: usize, m: usize,
+        abv: [(Usize1, Usize1); n],
+        cdv: [(Usize1, Usize1); m],
     };
+
+    let hikari = abv.into_iter().collect::<HashSet<_>>();
+    let blocks = cdv.into_iter().collect::<HashSet<_>>();
+    let mut used = HashSet::new();
+    // 横方向
+    for i in 0..h {
+        let mut l = 0;
+        let mut ok = false;
+        for j in 0..w {
+            if blocks.contains(&(i, j)) {
+                if ok {
+                    for jj in l..j {
+                        used.insert((i, jj));
+                    }
+                }
+                ok = false;
+                l = j + 1;
+            } else {
+                if hikari.contains(&(i, j)) {
+                    ok = true;
+                }
+                if ok {
+                    if j == w - 1 {
+                        for jj in l..=j {
+                            used.insert((i, jj));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // 縦方向
+    for j in 0..w {
+        let mut t = 0;
+        let mut ok = false;
+        for i in 0..h {
+            if blocks.contains(&(i, j)) {
+                if ok {
+                    for ii in t..i {
+                        used.insert((ii, j));
+                    }
+                }
+                ok = false;
+                t = i + 1;
+            } else {
+                if hikari.contains(&(i, j)) {
+                    ok = true;
+                }
+                if ok {
+                    if i == h - 1 {
+                        for ii in t..=i {
+                            used.insert((ii, j));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", used.len());
 }
 
 fn main() {
