76d5b954b07206831389ba714e48eb9e75770f03
emanon001 <emanon001@gmail.com>
2020-11-16 01:14:30 +0900

    solve abc183/c

--- abc183/src/bin/c.rs
+++ abc183/src/bin/c.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: i64,
+        ttv: [[i64; n]; n]
     };
+
+    let res = (1..n)
+        .permutations(n - 1)
+        .filter(|v| {
+            let mut distance = 0;
+            let mut from = 0;
+            for &to in v {
+                distance += ttv[from][to];
+                from = to;
+            }
+            distance += ttv[from][0];
+            distance == k
+        })
+        .count();
+    println!("{}", res);
 }
 
 fn main() {
