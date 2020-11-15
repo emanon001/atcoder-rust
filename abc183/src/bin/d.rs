a3770e3f5b0fb227c5f7c1666e77faca2ff7bf02
emanon001 <emanon001@gmail.com>
2020-11-16 01:14:35 +0900

    solve abc183/d

--- abc183/src/bin/d.rs
+++ abc183/src/bin/d.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, w: i128,
+        stpv: [(usize, usize, i128); n]
     };
+
+    let mut imos = vec![0_i128; 10.pow(5) * 2 + 10];
+    for (s, t, p) in stpv {
+        imos[s] += p;
+        imos[t] -= p;
+    }
+    let mut cur = 0_i128;
+    for i in 0..=(10.pow(5) * 2) {
+        cur += imos[i];
+        if cur > w {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
 
 fn main() {
