dfa6d572927a742aeb5259f15343699fa045ac7d
emanon001 <emanon001@gmail.com>
2020-12-05 20:52:21 +0900

    solve arc109/{a-c}

--- arc109/src/bin/b.rs
+++ arc109/src/bin/b.rs
@@ -8,9 +8,39 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i128, ng: i128, pred: F) -> Option<i128>
+where
+    F: Fn(i128) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: i128
     };
+
+    let c = bsearch(0_i128, (n + 1) as i128, |x| {
+        let y = (1 + (1 + x)) * (x + 1) / 2;
+        y <= n + 1
+    });
+    let res = 1 + (n - (c.unwrap_or(0) + 1));
+    println!("{}", res);
 }
 
 fn main() {
--- arc109/src/bin/c.rs
+++ arc109/src/bin/c.rs
@@ -8,9 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn f(pos: usize, d: usize, n: usize, k: usize, s: &[char], dp: &mut [Vec<Option<char>>]) -> char {
+    if d == k {
+        return s[pos];
+    }
+    if let Some(res) = dp[pos][d] {
+        return res;
+    }
+    let a = f((pos * 2) % n, d + 1, n, k, s, dp);
+    let b = f((pos * 2 + 1) % n, d + 1, n, k, s, dp);
+    let res = match (a, b) {
+        ('R', 'S') => 'R',
+        ('S', 'R') => 'R',
+        ('P', 'R') => 'P',
+        ('R', 'P') => 'P',
+        ('S', 'P') => 'S',
+        ('P', 'S') => 'S',
+        _ => a,
+    };
+    dp[pos][d] = Some(res);
+    return res;
+}
+
 fn solve() {
     input! {
+        n: usize, k: usize,
+        s: Chars
     };
+
+    let mut dp = vec![vec![None; k + 1]; n];
+    let res = f(0, 0, n, k, &s, &mut dp);
+    println!("{}", res);
 }
 
 fn main() {
