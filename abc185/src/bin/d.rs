58c2481d82fbe4d944d613866cf0da279e580393
emanon001 <emanon001@gmail.com>
2020-12-13 21:57:48 +0900

    solve abc185/d

--- abc185/src/bin/d.rs
+++ abc185/src/bin/d.rs
@@ -10,7 +10,39 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        mut av: [usize; m]
     };
+
+    av.sort();
+    let mut list = Vec::new();
+    let mut min = std::usize::MAX;
+    let mut prev = 0;
+    for &a in &av {
+        let w = a - 1 - prev;
+        prev = a;
+        if w == 0 {
+            continue;
+        }
+        if w < min {
+            min = w;
+        }
+        list.push(w);
+    }
+    if prev < n {
+        let w = n - prev;
+        if w < min {
+            min = w;
+        }
+        list.push(w);
+    }
+    // println!("{}", min);
+    // println!("{:?}", list);
+    let mut res = 0;
+    for w in list {
+        res += (w + min - 1) / min;
+    }
+    println!("{}", res);
 }
 
 fn main() {
