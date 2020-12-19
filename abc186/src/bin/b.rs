0667144e90df172fde8a4df878c67101566350f3
emanon001 <emanon001@gmail.com>
2020-12-19 22:39:03 +0900

    solve abc186/b

--- abc186/src/bin/b.rs
+++ abc186/src/bin/b.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [[isize; w]; h]
     };
+
+    let mut min = 1 << 30;
+    for i in 0..h {
+        for j in 0..w {
+            let c = grid[i][j];
+            if c < min {
+                min = c;
+            }
+        }
+    }
+    let mut res = 0;
+    for i in 0..h {
+        for j in 0..w {
+            let c = grid[i][j];
+            res += c - min;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
