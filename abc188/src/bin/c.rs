ca8b51391b55d3be1c6d851edc2ae084db1eacbd
emanon001 <emanon001@gmail.com>
2021-01-10 21:46:34 +0900

    solve abc188/c

--- abc188/src/bin/c.rs
+++ abc188/src/bin/c.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; 2.pow(n as u32)]
     };
+    let mut v = (0..(2.pow(n as u32) as usize)).collect::<Vec<_>>();
+    while v.len() > 2 {
+        let mut newv = Vec::new();
+        for i in 0..v.len() / 2 {
+            if av[v[2 * i]] > av[v[2 * i + 1]] {
+                newv.push(v[2 * i]);
+            } else {
+                newv.push(v[2 * i + 1]);
+            }
+        }
+        v = newv;
+    }
+    let res = if av[v[0]] > av[v[1]] { v[1] } else { v[0] } + 1;
+    println!("{}", res);
 }
 
 fn main() {
