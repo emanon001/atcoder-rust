28cd4df8825040e5ddff8f3288dbc604abbabbe5
emanon001 <emanon001@gmail.com>
2021-01-10 20:48:18 +0900

    solve abc187/b

--- abc187/src/bin/b.rs
+++ abc187/src/bin/b.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        xyv: [(i64, i64); n]
     };
+
+    let mut res = 0;
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            let dy = xyv[j].1 - xyv[i].1;
+            let dx = xyv[j].0 - xyv[i].0;
+            if dy.abs() <= dx.abs() {
+                res += 1;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
