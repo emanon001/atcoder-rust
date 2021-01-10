23edb1431a89e97c14bd8b471b89809c4e608ad6
emanon001 <emanon001@gmail.com>
2021-01-10 20:39:12 +0900

    solve abc187/a

--- abc187/src/bin/a.rs
+++ abc187/src/bin/a.rs
@@ -10,7 +10,13 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: Chars, b: Chars
     };
+
+    let a: u32 = a.into_iter().map(|ch| ch.to_digit(10).unwrap()).sum();
+    let b: u32 = b.into_iter().map(|ch| ch.to_digit(10).unwrap()).sum();
+    let res = a.max(b);
+    println!("{}", res);
 }
 
 fn main() {
