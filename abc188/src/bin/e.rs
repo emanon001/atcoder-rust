174d76e7094d7f382f40eb4d1eef948d38648958
emanon001 <emanon001@gmail.com>
2021-01-13 22:06:08 +0900

    solve abc188/e

--- abc188/src/bin/e.rs
+++ abc188/src/bin/e.rs
@@ -8,9 +8,46 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn f(u: usize, graph: &[Vec<usize>], av: &[i64], dp: &mut Vec<Option<i64>>) -> i64 {
+    if let Some(res) = dp[u] {
+        return res;
+    }
+    let mut res = std::i64::MIN;
+    for &v in &graph[u] {
+        let d = av[v].max(f(v, graph, av, dp));
+        if d > res {
+            res = d;
+        }
+    }
+    dp[u] = Some(res);
+    return res;
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [i64; n],
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+    }
+    let mut dp = vec![None; n];
+    let mut res = std::i64::MIN;
+    for u in 0..n - 1 {
+        let a = av[u];
+        let b = f(u, &graph, &av, &mut dp);
+        if b == std::i64::MIN {
+            continue;
+        }
+        let c = b - a;
+        if c > res {
+            res = c;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
