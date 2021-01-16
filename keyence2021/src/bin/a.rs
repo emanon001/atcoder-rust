a01065532f293b5703046dbf671ebd82f006c2c0
emanon001 <emanon001@gmail.com>
2021-01-17 01:48:20 +0900

    solve keyence2021/a

--- keyence2021/src/bin/a.rs
+++ keyence2021/src/bin/a.rs
@@ -8,9 +8,32 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n],
+        bv: [i64; n],
     };
+
+    let mut max_a = 0;
+    let mut max = 0;
+    for i in 0..n {
+        chmax!(max_a, av[i]);
+        chmax!(max, max_a * bv[i]);
+        println!("{}", max);
+    }
 }
 
 fn main() {
