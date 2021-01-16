d3b62533a6b31ad292d460d485e27adc93ba3844
emanon001 <emanon001@gmail.com>
2021-01-17 02:04:56 +0900

    solve keyence2021/b

--- keyence2021/src/bin/b.rs
+++ keyence2021/src/bin/b.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        av: [i64; n]
     };
+
+    let mut map = HashMap::new();
+    for a in av {
+        *map.entry(a).or_insert(0) += 1;
+    }
+    let mut res = 0;
+    for i in 1..=k {
+        for x in 0..=n as i64 {
+            if map.get(&x).unwrap_or(&0) < &i {
+                res += x;
+                break;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
