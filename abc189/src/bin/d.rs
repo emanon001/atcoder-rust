66af32da37e104c597d74d0e4c1517be24971abd
emanon001 <emanon001@gmail.com>
2021-01-23 22:55:37 +0900

    solve abc189/d

--- abc189/src/bin/d.rs
+++ abc189/src/bin/d.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        sv: [String; n]
     };
+
+    let mut dp = vec![vec![0_i64; 2]; n + 1];
+    dp[0][0] = 1;
+    dp[0][1] = 1;
+    for i in 0..n {
+        if sv[i] == "AND" {
+            // x_i == 0 の場合
+            dp[i + 1][0] += dp[i][0];
+            dp[i + 1][0] += dp[i][1];
+            // x_i == 1 の場合
+            dp[i + 1][0] += dp[i][0];
+            dp[i + 1][1] += dp[i][1];
+        } else {
+            // OR
+            // x_i == 0 の場合
+            dp[i + 1][0] += dp[i][0];
+            dp[i + 1][1] += dp[i][1];
+            // x_i == 1 の場合
+            dp[i + 1][1] += dp[i][0];
+            dp[i + 1][1] += dp[i][1];
+        }
+    }
+    println!("{}", dp[n][1]);
 }
 
 fn main() {
