9c923b519fb58d8730c4e311115525e615916b97
emanon001 <emanon001@gmail.com>
2021-01-23 19:41:41 +0900

    solve abc187/c

--- abc187/src/bin/c.rs
+++ abc187/src/bin/c.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        sv: [String; n]
     };
+
+    let set = sv.into_iter().collect::<HashSet<_>>();
+    let mut map = HashMap::new();
+    for s in set {
+        let s = if s.starts_with('!') {
+            s[1..].to_string()
+        } else {
+            s
+        };
+        *map.entry(s).or_insert(0) += 1;
+    }
+    for (s, c) in map {
+        if c >= 2 {
+            println!("{}", s);
+            return;
+        }
+    }
+    println!("satisfiable");
 }
 
 fn main() {
