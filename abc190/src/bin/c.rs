c8c846c3dbea9fd6a2129fc8cddbf442f91be464
emanon001 <emanon001@gmail.com>
2021-02-06 20:51:05 +0900

    solve abc190/c

--- abc190/src/bin/c.rs
+++ abc190/src/bin/c.rs
@@ -10,7 +10,35 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        abv: [(Usize1, Usize1); m],
+        k: usize,
+        cdv: [(Usize1, Usize1); k],
     };
+
+    let mut res = 0;
+    for bits in 0..(1 << k) {
+        let mut sara = vec![0; n];
+        for i in 0..k {
+            let (c, d) = cdv[i];
+            if ((bits >> i) & 1) == 0 {
+                sara[c] += 1;
+            } else {
+                sara[d] += 1;
+            }
+        }
+        let mut count = 0;
+        for i in 0..m {
+            let (a, b) = abv[i];
+            if sara[a] >= 1 && sara[b] >= 1 {
+                count += 1;
+            }
+        }
+        if count > res {
+            res = count;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
