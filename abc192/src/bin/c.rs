3e14129505b7ab4f4223b1ccd6b44a4866e53ab2
emanon001 <emanon001@gmail.com>
2021-02-20 21:35:20 +0900

    solve abc192/c

--- abc192/src/bin/c.rs
+++ abc192/src/bin/c.rs
@@ -8,9 +8,28 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn f1(x: usize) -> usize {
+    let mut chars = x.to_string().chars().collect::<Vec<char>>();
+    chars.sort();
+    chars.reverse();
+    chars.into_iter().join("").parse().unwrap()
+}
+
+fn f2(x: usize) -> usize {
+    let mut chars = x.to_string().chars().collect::<Vec<char>>();
+    chars.sort();
+    chars.into_iter().join("").parse().unwrap()
+}
+
 fn solve() {
     input! {
+        n: usize, k: usize
     };
+    let mut x = n;
+    for _ in 0..k {
+        x = f1(x) - f2(x);
+    }
+    println!("{}", x);
 }
 
 fn main() {
