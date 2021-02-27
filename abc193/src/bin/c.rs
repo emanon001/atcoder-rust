ae1d4d6b4e70cafbb5568ac3a869ffbb77f0b2ad
emanon001 <emanon001@gmail.com>
2021-02-27 22:39:26 +0900

    solve abc193/c

--- abc193/src/bin/c.rs
+++ abc193/src/bin/c.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: i64
     };
+
+    let mut set = HashSet::new();
+    for a in 2..= ((n as f64).sqrt() as i64) {
+        let mut x = a * a;
+        while x <= n {
+            set.insert(x);
+            x *= a;
+        }
+    }
+    let res = n - set.len() as i64;
+    println!("{}", res);
 }
 
 fn main() {
