640edf8d623e9c05d85ffbb74b7d07c8c2e40f77
emanon001 <emanon001@gmail.com>
2021-03-14 02:05:47 +0900

    solve abc195/c

--- abc195/src/bin/c.rs
+++ abc195/src/bin/c.rs
@@ -10,7 +10,33 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: Chars
     };
+
+    let digits = n
+        .into_iter()
+        .map(|ch| ch.to_digit(10).unwrap() as u64)
+        .collect::<Vec<_>>();
+    let len = digits.len();
+    let mut res = 0_u64;
+    // same
+    if len > 3 {
+        let c = (len - 1) as u64 / 3;
+        res += c * (digits[1..].into_iter().fold(0_u64, |acc, x| acc * 10 + x) + 1);
+    }
+    // lower
+    for i in 0..len {
+        if len - i <= 3 {
+            continue;
+        }
+        let cur_d = digits[i] as usize;
+        let max_d = if i == 0 { if cur_d > 0 { cur_d - 1 } else { 0 } } else { 9 };
+        for _ in 1..=max_d {
+            let c = (len - i - 1) as u64 / 3;
+            res += c * 10_u64.pow((len - i - 1) as u32);
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
