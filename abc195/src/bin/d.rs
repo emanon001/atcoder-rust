9766fa5265a592869bee4602bfdd0944634164dd
emanon001 <emanon001@gmail.com>
2021-03-14 02:37:19 +0900

    solve abc195/d

--- abc195/src/bin/d.rs
+++ abc195/src/bin/d.rs
@@ -10,7 +10,38 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize, q: usize,
+        mut wvv: [(i64, i64); n],
+        xv: [i64; m],
+        queries: [(Usize1, Usize1); q]
     };
+
+    wvv.sort_by_key(|&(_, v)| -v);
+    for (l, r) in queries {
+        let mut boxes = Vec::new();
+        for i in 0..m {
+            if l <= i && i <= r {
+                continue;
+            }
+            boxes.push(i);
+        }
+        boxes.sort_by_key(|&i| xv[i]);
+        let mut used_boxes = vec![false; m];
+        let mut res = 0;
+        for &(w, v) in &wvv {
+            for &b in &boxes {
+                if used_boxes[b] {
+                    continue;
+                }
+                if xv[b] >= w {
+                    res += v;
+                    used_boxes[b] = true;
+                    break;
+                }
+            }
+        }
+        println!("{}", res);
+    }
 }
 
 fn main() {
