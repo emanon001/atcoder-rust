ff5e420442c78bc6e9928a9a3d4f75ced962c561
emanon001 <emanon001@gmail.com>
2021-03-17 22:13:06 +0900

    solve abc194/e

--- abc194/src/bin/e.rs
+++ abc194/src/bin/e.rs
@@ -8,9 +8,45 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [usize; n]
     };
+
+    let mut res = n;
+    let mut counts = vec![0; n];
+    let mut set = (0..=n).collect::<BTreeSet<_>>();
+    for i in 0..m {
+        let a = av[i];
+        counts[a] += 1;
+        set.remove(&a);
+    }
+    chmin!(res, *set.iter().next().unwrap());
+    for i in m..n {
+        let a = av[i];
+        counts[a] += 1;
+        set.remove(&a);
+        let b = av[i - m];
+        counts[b] -= 1;
+        if counts[b] == 0 {
+            set.insert(b);
+        }
+        chmin!(res, *set.iter().next().unwrap());
+    }
+    println!("{}", res);
 }
 
 fn main() {
