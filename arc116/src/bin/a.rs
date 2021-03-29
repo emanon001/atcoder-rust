eee85e458c252e641219e10f273be13ac534f4e9
emanon001 <emanon001@gmail.com>
2021-03-29 23:49:50 +0900

    solve arc116/a

--- arc116/src/bin/a.rs
+++ arc116/src/bin/a.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        t: usize,
+        cases: [u64; t]
     };
+
+    for case in cases {
+        let mut two_count = 0;
+        let mut a = case;
+        while a % 2 == 0 {
+            two_count += 1;
+            a /= 2;
+        }
+        let res = match two_count {
+            0 => "Odd",
+            1 => "Same",
+            _ => "Even"
+        };
+        println!("{}", res);
+    }
 }
 
 fn main() {
