a809d48195e44f38f00706b49ae352b5c146467b
emanon001 <emanon001@gmail.com>
2021-04-19 23:59:31 +0900

    solve abc194/c

--- abc194/src/bin/c.rs
+++ abc194/src/bin/c.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let mut counts = HashMap::new();
+    for a in av {
+        *counts.entry(a).or_insert(0_i64) += 1;
+    }
+
+    let max_a = 200;
+    let mut res = 0_i64;
+    for x in -max_a..max_a  {
+        for y in x + 1..=max_a {
+            let xc = counts.get(&x).unwrap_or(&0);
+            let yc = counts.get(&y).unwrap_or(&0);
+            let c = xc * yc;
+            res += (x - y).pow(2) * c;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
