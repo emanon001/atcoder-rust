ecec4987fb82a77462a1af834f6dee6086ca7961
emanon001 <emanon001@gmail.com>
2021-04-19 23:34:28 +0900

    solve abc194/a

--- abc194/src/bin/a.rs
+++ abc194/src/bin/a.rs
@@ -10,7 +10,20 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: usize, b: usize
     };
+
+    let c = a + b;
+    let res = if c >= 15 && b >= 8 {
+        1
+    } else if c >= 10 && b >= 3 {
+        2
+    } else if c >= 3 {
+        3
+    } else {
+        4
+    };
+    println!("{}", res);
 }
 
 fn main() {
