588a52d50ede91958938a33f7e3605aed57478eb
emanon001 <emanon001@gmail.com>
2021-04-25 00:35:53 +0900

    solve abc199/c

--- abc199/src/bin/c.rs
+++ abc199/src/bin/c.rs
@@ -8,9 +8,59 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn change(a: usize, b: usize, n: usize, s1: &mut[char], s2: &mut[char]) {
+    match (a > n, b > n) {
+        (false, false) => {
+            let ch = s1[a - 1];
+            s1[a - 1] = s1[b - 1];
+            s1[b - 1] = ch;
+        }
+        (false, true) => {
+            let ch = s1[a - 1];
+            s1[a - 1] = s2[b - n - 1];
+            s2[b - n - 1] = ch;
+        }
+        (true, true) => {
+            let ch = s2[a - n - 1];
+            s2[a - n - 1] = s2[b - n - 1];
+            s2[b - n - 1] = ch;
+        }
+        (true, false) => {
+            let ch = s2[a - n - 1];
+            s2[a - n - 1] = s1[b - 1];
+            s1[b - 1] = ch;
+        }
+    }
+}
+
 fn solve() {
     input! {
+        n: usize,
+        s: Chars,
+        q: usize,
+        queries: [(usize, usize, usize); q]
     };
+
+    let mut s1 = s.iter().take(n).copied().collect::<Vec<char>>();
+    let mut s2 = s.iter().skip(n).copied().collect::<Vec<char>>();
+    let mut swapped = false;
+    for (op, a, b) in queries {
+        if op == 1 {
+            if swapped {
+                change(a, b, n, &mut s2, &mut s1);
+            } else {
+                change(a, b, n, &mut s1, &mut s2);
+            }
+        } else {
+            swapped = !swapped;
+        }
+    }
+    let res = if swapped {
+        format!("{}{}", s2.iter().join(""), s1.iter().join(""))
+    } else {
+        format!("{}{}", s1.iter().join(""), s2.iter().join(""))
+    };
+    println!("{}", res);
 }
 
 fn main() {
