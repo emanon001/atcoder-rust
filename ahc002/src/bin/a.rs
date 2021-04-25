cd2514f73bbbf5f82f6847750cb1335cde90e3ee
emanon001 <emanon001@gmail.com>
2021-04-25 22:21:57 +0900

    random

--- ahc002/src/bin/a.rs
+++ ahc002/src/bin/a.rs
@@ -8,6 +8,7 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 use std::time::{Instant, Duration};
+use rand::seq::SliceRandom;
 
 fn dfs(
     pos: (usize, usize),
@@ -16,6 +17,7 @@ fn dfs(
     score: &mut i32,
     check_count: &mut u32,
     res: &mut (i32, Vec<(usize, usize, char)>),
+    dirs: &[(isize, isize, char)],
     tgrid: &[usize],
     pgrid: &[i32],
     now: Instant,
@@ -33,14 +35,8 @@ fn dfs(
         *check_count = 0;
     }
     *check_count += 1;
-    let dirs = vec![
-        (-1, 0, 'U'),
-        (0, 1, 'R'),
-        (1, 0, 'D'),
-        (0, -1, 'L')
-    ];
     let mut moved = false;
-    for (add_i, add_j, d) in &dirs {
+    for (add_i, add_j, d) in dirs {
         let new_i = pos.0 as isize + add_i;
         if new_i < 0 || new_i >= 50 {
             continue;
@@ -59,7 +55,7 @@ fn dfs(
         *score += pgrid[new_pos.0 * 50 + new_pos.1];
         path.push((new_i, new_j, *d));
         used[tgrid[new_i * 50 + new_j]] = true;
-        dfs(new_pos, path, used, score, check_count, res, tgrid, pgrid, now);
+        dfs(new_pos, path, used, score, check_count, res, dirs, tgrid, pgrid, now);
         *score -= pgrid[new_pos.0 * 50 + new_pos.1];
         path.pop();
         used[tgrid[new_i * 50 + new_j]] = false;
@@ -80,13 +76,21 @@ fn solve() {
     };
 
     let now = Instant::now();
+    let mut rng = rand::thread_rng();
+    let mut dirs = vec![
+        (-1, 0, 'U'),
+        (0, 1, 'R'),
+        (1, 0, 'D'),
+        (0, -1, 'L')
+    ];
+    dirs.shuffle(&mut rng);
     let mut path = Vec::new();
     let mut used = vec![false; 50 * 50];
     used[tgrid[si * 50 + sj]] = true;
     let mut check_count = 0;
     let mut score = pgrid[si * 50 + sj];
     let mut res1 = (0, vec![(si, sj, ' ')]);
-    dfs((si, sj), &mut path, &mut used, &mut score, &mut check_count, &mut res1, &tgrid, &pgrid, now);
+    dfs((si, sj), &mut path, &mut used, &mut score, &mut check_count, &mut res1, &dirs, &tgrid, &pgrid, now);
     let res = res1.1.iter().map(|(_, _, d)| *d).join("");
     println!("{}", res.trim());
 }
