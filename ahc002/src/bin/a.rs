820fd844b14a3672820e694fa1222a5728f1149e
emanon001 <emanon001@gmail.com>
2021-04-25 19:37:36 +0900

    隣接したマスの中で、最も得点の高いマスに移動

--- ahc002/src/bin/a.rs
+++ ahc002/src/bin/a.rs
@@ -19,9 +19,54 @@ fn score(path: &[(usize, usize)], grid: &[Vec<i32>]) -> i32 {
 fn solve() {
     input! {
         si: usize, sj: usize,
-        tgrid: [[i32; 50]; 50],
+        tgrid: [[usize; 50]; 50],
         pgrid: [[i32; 50]; 50],
     };
+
+    let mut res = Vec::new();
+    let mut used = vec![false; 50 * 50];
+    used[tgrid[si][sj]] = true;
+    let dirs = vec![
+        (-1, 0, 'U'),
+        (0, 1, 'R'),
+        (1, 0, 'D'),
+        (0, -1, 'L')
+    ];
+    let mut cur_pos = (si, sj);
+    loop {
+        let mut point = -1;
+        let mut dir = &'x';
+        let mut next_pos = None;
+        for (add_i, add_j, d) in &dirs {
+            let new_i = cur_pos.0 as isize + add_i;
+            if new_i < 0 || new_i >= 50 {
+                continue;
+            }
+            let new_j = cur_pos.1 as isize + add_j;
+            if new_j < 0 || new_j >= 50 {
+                continue;
+            }
+            let new_i = new_i as usize;
+            let new_j = new_j as usize;
+            if used[tgrid[new_i][new_j]] {
+                continue;
+            }
+            if pgrid[new_i][new_j] > point {
+                point = pgrid[new_i][new_j];
+                next_pos = Some((new_i, new_j));
+                dir = d;
+            }
+        }
+        if next_pos.is_none() {
+            break;
+        }
+        let npos = next_pos.unwrap();
+        used[tgrid[npos.0][npos.1]] = true;
+        res.push(dir);
+        cur_pos = npos;
+    }
+
+    println!("{}", res.iter().join(""));
 }
 
 fn main() {
