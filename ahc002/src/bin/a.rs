5ba2781bb454fe060dbd8bbe53cc8439d32f56d7
emanon001 <emanon001@gmail.com>
2021-04-25 21:43:01 +0900

    得点の高い順に移動を試す

--- ahc002/src/bin/a.rs
+++ ahc002/src/bin/a.rs
@@ -39,7 +39,8 @@ fn dfs(
         (1, 0, 'D'),
         (0, -1, 'L')
     ];
-    let mut moved = false;
+    // ポイントの高い順に試す
+    let mut cand = Vec::new();
     for (add_i, add_j, d) in &dirs {
         let new_i = pos.0 as isize + add_i;
         if new_i < 0 || new_i >= 50 {
@@ -54,7 +55,11 @@ fn dfs(
         if used[tgrid[new_i][new_j]] {
             continue;
         }
-        moved = true;
+        cand.push((new_i, new_j, d));
+    }
+    let moved = cand.len() > 0;
+    cand.sort_by_key(|(i, j, _)| -pgrid[*i][*j]);
+    for (new_i, new_j, d) in cand {
         let new_pos = (new_i, new_j);
         *score += pgrid[new_pos.0][new_pos.1];
         path.push((new_i, new_j, *d));
@@ -72,54 +77,6 @@ fn dfs(
     }
 }
 
-fn move_to_max_point_adjacent_cells(si: usize, sj: usize, used: &mut [bool], tgrid: &[Vec<usize>], pgrid: &[Vec<i32>]) -> (i32, Vec<char>) {
-    let mut score = 0_i32;
-    let mut path = Vec::new();
-    score += pgrid[si][sj];
-    used[tgrid[si][sj]] = true;
-    let dirs = vec![
-        (-1, 0, 'U'),
-        (0, 1, 'R'),
-        (1, 0, 'D'),
-        (0, -1, 'L')
-    ];
-    let mut cur_pos = (si, sj);
-    loop {
-        let mut point = -1;
-        let mut dir = &'x';
-        let mut next_pos = None;
-        for (add_i, add_j, d) in &dirs {
-            let new_i = cur_pos.0 as isize + add_i;
-            if new_i < 0 || new_i >= 50 {
-                continue;
-            }
-            let new_j = cur_pos.1 as isize + add_j;
-            if new_j < 0 || new_j >= 50 {
-                continue;
-            }
-            let new_i = new_i as usize;
-            let new_j = new_j as usize;
-            if used[tgrid[new_i][new_j]] {
-                continue;
-            }
-            if pgrid[new_i][new_j] > point {
-                point = pgrid[new_i][new_j];
-                next_pos = Some((new_i, new_j));
-                dir = d;
-            }
-        }
-        if next_pos.is_none() {
-            break;
-        }
-        let npos = next_pos.unwrap();
-        score += pgrid[npos.0][npos.1];
-        used[tgrid[npos.0][npos.1]] = true;
-        path.push(*dir);
-        cur_pos = npos;
-    }
-    (score, path)
-}
-
 fn solve() {
     input! {
         si: usize, sj: usize,
@@ -135,13 +92,7 @@ fn solve() {
     let mut score = pgrid[si][sj];
     let mut res1 = (0, vec![(si, sj, ' ')]);
     dfs((si, sj), &mut path, &mut used, &mut score, &mut check_count, &mut res1, &tgrid, &pgrid, now);
-    for &(i, j, _) in &res1.1 {
-        used[tgrid[i][j]] = true;
-    }
-    let last = res1.1.last().unwrap();
-    let res2 = move_to_max_point_adjacent_cells(last.0, last.1, &mut used, &tgrid, &pgrid);
-    let res1_str = res1.1.iter().map(|(_, _, d)| *d).join("");
-    let res = format!("{}{}", res1_str, res2.1.iter().join(""));
+    let res = res1.1.iter().map(|(_, _, d)| *d).join("");
     println!("{}", res.trim());
 }
 
