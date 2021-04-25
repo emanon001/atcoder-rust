f484f249465bf11dced3231323adcdf11b74f7ba
emanon001 <emanon001@gmail.com>
2021-04-25 19:16:29 +0900

    add score function

--- ahc002/src/bin/a.rs
+++ ahc002/src/bin/a.rs
@@ -8,8 +8,19 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn score(path: &[(usize, usize)], grid: &[Vec<i32>]) -> i32 {
+    let mut res = 0;
+    for &(i, j) in path {
+        res += grid[i][j];
+    }
+    res
+}
+
 fn solve() {
     input! {
+        si: usize, sj: usize,
+        tgrid: [[i32; 50]; 50],
+        pgrid: [[i32; 50]; 50],
     };
 }
 
