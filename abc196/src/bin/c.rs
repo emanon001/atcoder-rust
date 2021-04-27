2a54ab8c565dd47c5cf6cdce867c8a03182da8ab
emanon001 <emanon001@gmail.com>
2021-04-27 23:39:18 +0900

    solve abc196/c

--- abc196/src/bin/c.rs
+++ abc196/src/bin/c.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: i64
     };
+
+    let d = n.to_string().len();
+    let mut res = 0;
+    for x in 1..10.pow(d as u32 / 2) {
+        let s = x.to_string();
+        let x: i64 = format!("{}{}", s, s).parse::<i64>().unwrap();
+        if x <= n {
+            res += 1;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
