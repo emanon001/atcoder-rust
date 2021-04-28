24c2fd956b1e8783993008e70751b9b152e603e6
emanon001 <emanon001@gmail.com>
2021-04-28 23:51:46 +0900

    solve wupc2012/b

--- wupc2012/src/bin/b.rs
+++ wupc2012/src/bin/b.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        sv: [String; n]
     };
+
+    let mut cand = Vec::new();
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            cand.push(format!("{}{}", sv[i], sv[j]));
+            cand.push(format!("{}{}", sv[j], sv[i]));
+        }
+    }
+    cand.sort();
+    println!("{}", cand[0]);
 }
 
 fn main() {
