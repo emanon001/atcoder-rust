103dcb04588ebed96cbb545fa1dba172a0c89666
emanon001 <emanon001@gmail.com>
2021-04-29 00:17:53 +0900

    solve wupc2012/d

--- wupc2012/src/bin/d.rs
+++ wupc2012/src/bin/d.rs
@@ -8,9 +8,45 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
     };
+
+    let mut triangle = vec![vec![0_u32; n + 1]; n + 1];
+    for i in 0..n {
+        input! {
+            v: [u32; i + 1]
+        };
+        for j in 0..v.len() {
+            triangle[i][j] = v[j];
+        }
+    }
+    let mut dp = vec![vec![0_u32; n + 1]; n + 1];
+    dp[0][0] = triangle[0][0];
+    for i in 0..n - 1 {
+        for j in 0..n {
+            chmax!(dp[i + 1][j], dp[i][j] + triangle[i + 1][j]);
+            chmax!(dp[i + 1][j + 1], dp[i][j] + triangle[i + 1][j + 1]);
+        }
+    }
+    let mut res = 0_u32;
+    for i in 0..n {
+        chmax!(res, dp[n - 1][i]);
+    }
+    println!("{}", res);
 }
 
 fn main() {
