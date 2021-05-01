bd74a5f692d2bcb6e4f59e01acd8fe3db21f2c9b
emanon001 <emanon001@gmail.com>
2021-05-02 00:06:13 +0900

    solve zone2021/c

--- zone2021/src/bin/c.rs
+++ zone2021/src/bin/c.rs
@@ -8,42 +8,67 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
         n: usize,
-        abcdev: [(i64, i64, i64, i64, i64); n]
+        abcdev: [[i64; 5]; n]
     };
 
-    let mut v1 = abcdev.clone().into_iter().enumerate().collect::<Vec<_>>();
-    v1.sort_by_key(|&(i, (a, b, c, d, e))| a);
-    let mut v2 = abcdev.clone().into_iter().enumerate().collect::<Vec<_>>();
-    v2.sort_by_key(|&(i, (a, b, c, d, e))| b);
-    let mut v3 = abcdev.clone().into_iter().enumerate().collect::<Vec<_>>();
-    v3.sort_by_key(|&(i, (a, b, c, d, e))| c);
-    let mut v4 = abcdev.clone().into_iter().enumerate().collect::<Vec<_>>();
-    v4.sort_by_key(|&(i, (a, b, c, d, e))| d);
-    let mut v5 = abcdev.clone().into_iter().enumerate().collect::<Vec<_>>();
-    v5.sort_by_key(|&(i, (a, b, c, d, e))| e);
-
-    let mut res = 0;
-    for i in (0..n - 2).rev() {
-        for j in (i + 1..n - 1).rev() {
-            for k in (j + 1..n).rev() {
-                let x1 = abcdev[i].0.max(abcdev[j].0).max(abcdev[k].0);
-                let x2 = abcdev[i].1.max(abcdev[j].1).max(abcdev[k].1);
-                let x3 = abcdev[i].2.max(abcdev[j].2).max(abcdev[k].2);
-                let x4 = abcdev[i].3.max(abcdev[j].3).max(abcdev[k].3);
-                let x5 = abcdev[i].4.max(abcdev[j].4).max(abcdev[k].4);
-                let x = x1.min(x2).min(x3).min(x4).min(x5);
-                if x < res {
-                    break;
-                } else {
-                    res = x;
+    let res = bsearch(0, 10.pow(9) + 1, |x| {
+        let mut cand = HashSet::new();
+        for v in &abcdev {
+            let mut bits = 0;
+            for i in 0..5 {
+                bits |= (if v[i] >= x { 1 } else { 0 }) << i;
+            }
+            cand.insert(bits);
+        }
+        let is_ok = |bits: i64| -> bool {
+            (0..5).all(|i| (bits >> i) & 1 == 1)
+        };
+        let cand = cand.into_iter().collect::<Vec<_>>();
+        let len = cand.len();
+        for i in 0..len {
+            if is_ok(cand[i]) {
+                return true;
+            }
+            for j in i + 1..len {
+                if is_ok(cand[i] | cand[j]) {
+                    return true;
+                }
+                for k in j + 1..len {
+                    if is_ok(cand[i] | cand[j] | cand[k]) {
+                        return true;
+                    }
                 }
             }
         }
-    }
-    println!("{}", res);
+        false
+    });
+
+    println!("{}", res.unwrap());
 }
 
 fn main() {
