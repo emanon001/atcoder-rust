c6c070d5c6f21c41ef0d558927df3b1f4e5a338c
emanon001 <emanon001@gmail.com>
2021-05-02 01:32:34 +0900

    solve typical90/016

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -8,9 +8,43 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: i64,
+        a: i64, b: i64, c: i64
     };
+
+    let max_coins = 9999;
+    let mut res = i64::max_value();
+    for x in 0..=max_coins {
+        for y in 0..=max_coins - x {
+            if a * x + b * y > n {
+                break;
+            }
+            let rest = n - a * x - b * y;
+            if rest % c != 0 {
+                continue;
+            }
+            let count = x + y + rest / c;
+            if count > max_coins {
+                continue;
+            }
+            chmin!(res, count);
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
