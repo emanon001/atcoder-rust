09d2b36d0a23bb6f6e7d8d2f7a94cd331c0ff463
emanon001 <emanon001@gmail.com>
2021-05-04 04:01:45 +0900

    solve typical90/019

--- typical90/src/bin/019.rs
+++ typical90/src/bin/019.rs
@@ -8,9 +8,51 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
+const INF: i64 = 1_i64 << 60;
+
+fn dfs(l: usize, r: usize, dp: &mut [Vec<i64>], av: &[i64]) -> i64 {
+    // [l, r)
+    if dp[l][r] != -1 {
+        return dp[l][r];
+    }
+    if l == r {
+        return 0;
+    }
+    if l + 1 == r {
+        return INF;
+    }
+    let mut res = INF;
+    let x = dfs(l + 1, r - 1, dp, av) + (av[l] - av[r - 1]).abs();
+    chmin!(res, x);
+    for m in l + 1..r {
+        let x = dfs(l, m, dp, av) + dfs(m, r, dp, av);
+        chmin!(res, x);
+    }
+    dp[l][r] = res;
+    dp[l][r]
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; 2 * n]
     };
+
+    let mut dp = vec![vec![-1; 2 * n + 1]; 2 * n + 1];
+    let res = dfs(0, 2 * n, &mut dp, &av);
+    println!("{}", res);
 }
 
 fn main() {
