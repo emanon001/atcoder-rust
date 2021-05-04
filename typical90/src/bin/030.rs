6392a44aef42fd44c38cdfab0d314be80760da51
emanon001 <emanon001@gmail.com>
2021-05-04 10:12:56 +0900

    solve typical90/030

--- typical90/src/bin/030.rs
+++ typical90/src/bin/030.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        k: usize
     };
+
+    let mut counts = vec![0; n + 1];
+    for x in 2..=n {
+        if counts[x] > 0 {
+            continue;
+        }
+        let mut i = 1;
+        while x * i <= n {
+            counts[x * i] += 1;
+            i += 1;
+        }
+    }
+    let mut res = 0;
+    for i in 0..=n {
+        if counts[i] >= k {
+            res += 1;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
