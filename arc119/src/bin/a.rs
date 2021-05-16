efad40ab7931a4928daeb5fbf2ba9f41e43291e1
emanon001 <emanon001@gmail.com>
2021-05-16 22:49:03 +0900

    solve arc119/a

--- arc119/src/bin/a.rs
+++ arc119/src/bin/a.rs
@@ -8,9 +8,35 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: i128
     };
+
+    // let mut b = 0;
+    // while 2.pow(b + 1) <= n {
+    //     b += 1;
+    // }
+    let mut res = 1_i128 << 60;
+    for b in 0..=60 {
+        let a = n / 2.pow(b);
+        let c = n - a * 2.pow(b);
+        let x = a + b as i128 + c;
+        chmin!(res, x);
+    }
+    println!("{}", res);
 }
 
 fn main() {
