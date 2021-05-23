212f8456a9b9dafbc24efbcecbb65742ab255e02
emanon001 <emanon001@gmail.com>
2021-05-24 02:41:15 +0900

    ダイクストラ

--- ahc003/src/bin/a.rs
+++ ahc003/src/bin/a.rs
@@ -8,54 +8,88 @@ use proconio::marker::*;
 use std::collections::*;
 use whiteread::{parse_line};
 
+const H: usize = 30;
+const W: usize = 30;
+
 struct Solver {
+    graph: Vec<Vec<(usize, i64, char)>>
 }
 
 impl Solver {
     fn new() -> Self {
+        let n = H * W;
+        let mut graph = vec![Vec::new(); n];
+        let cost = 1;
+        for i in 0..H {
+            for j in 0..W {
+                let u = Self::vertex(i, j);
+                // ↑
+                if i > 0 {
+                    let v = Self::vertex(i - 1, j);
+                    graph[u].push((v, cost, 'U'));
+                    graph[v].push((u, cost, 'D'));
+                }
+                // ↓
+                if i + 1 < H {
+                    let v = Self::vertex(i + 1, j);
+                    graph[u].push((v, cost, 'D'));
+                    graph[v].push((u, cost, 'U'));
+                }
+                // ←
+                if j > 0 {
+                    let v = Self::vertex(i, j - 1);
+                    graph[u].push((v, cost, 'L'));
+                    graph[v].push((u, cost, 'R'));
+                }
+                // →
+                if j + 1 < W {
+                    let v = Self::vertex(i, j + 1);
+                    graph[u].push((v, cost, 'R'));
+                    graph[v].push((u, cost, 'L'));
+                }
+            }
+        }
         Self {
+            graph
         }
     }
 
     fn solve(&mut self) {
         for _ in 0..1000 {
-            let mut path = Vec::new();
             let (si, sj, ti, tj): (usize, usize, usize, usize) = parse_line().unwrap();
-            // 縦方向
-            if si <= ti {
-                // ↓ 
-                let mut i = si;
-                while i < ti {
-                    path.push('D');
-                    i += 1;
-                }
-            } else {
-                // ↑ 
-                let mut i = si;
-                while i > ti {
-                    path.push('U');
-                    i -= 1;
-                }
-            }
-            // 横方向
-            if sj <= tj {
-                // →
-                let mut j = sj;
-                while j < tj {
-                    path.push('R');
-                    j += 1;
-                }
-            } else {
-                // ←
-                let mut j = sj;
-                while j > tj {
-                    path.push('L');
-                    j -= 1;
-                }
-            }
-            println!("{}", path.into_iter().join(""));
+            let d = self.shortest_path(Self::vertex(si, sj));
+            println!("{}", d[Self::vertex(ti, tj)].iter().join(""));
             let _: i64 = parse_line().unwrap();
         }
+    }
+
+    fn shortest_path(&self, start: usize) -> Vec<Vec<char>> {
+        let mut cost_list = vec![1_i64 << 60; self.graph.len()];
+        let mut path_list = vec![vec![]; self.graph.len()];
+        let mut heap = std::collections::BinaryHeap::new();
+        cost_list[start] = 0;
+        path_list[start] = vec![];
+        heap.push(std::cmp::Reverse((0, start, vec![])));
+        while let Some(std::cmp::Reverse((cost, u, path))) = heap.pop() {
+            if cost > cost_list[u] {
+                continue;
+            }
+            for &(v, w, d) in &self.graph[u] {
+                let new_cost = cost + w;
+                if new_cost < cost_list[v] {
+                    let mut new_path= path.clone();
+                    new_path.push(d);
+                    path_list[v] = new_path.clone();
+                    cost_list[v] = new_cost;
+                    heap.push(std::cmp::Reverse((new_cost, v, new_path)));
+                }
+            }
+        }
+        path_list
+    }
+
+    fn vertex(i: usize, j: usize) -> usize {
+        i * W + j
     }
 }
 
