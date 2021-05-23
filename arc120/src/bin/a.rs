aca1146dcfc2e41d946fc4276b3a9d72a41c96d5
emanon001 <emanon001@gmail.com>
2021-05-23 22:11:07 +0900

    solve arc120/a

--- arc120/src/bin/a.rs
+++ arc120/src/bin/a.rs
@@ -10,7 +10,37 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let mut cusum = vec![0_i64; n];
+    let mut c = 0;
+    for i in 0..n {
+        c += av[i];
+        cusum[i] = c;
+    }
+    let mut cusum2 = vec![0_i64; n];
+    let mut c = 0;
+    for i in 0..n {
+        c += cusum[i];
+        cusum2[i] = c;
+    }
+    let mut max_v = vec![0_i64; n];
+    let mut c = 0_i64;
+    for i in 0..n {
+        if av[i] > c {
+            c = av[i];
+        }
+        max_v[i] = c;
+    }
+    for i in 0..n {
+        let mut sum = cusum2[i];
+        let max = max_v[i];
+        let x = max;
+        sum += x * (i + 1) as i64;
+        println!("{}", sum)
+    }
 }
 
 fn main() {
