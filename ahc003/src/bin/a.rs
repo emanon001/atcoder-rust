a868a4ae9371aad6ddc39dd3618d257bc18da78b
emanon001 <emanon001@gmail.com>
2021-05-25 04:15:19 +0900

    コスト確定処理を追加(変化なし) 87226090432

--- ahc003/src/bin/a.rs
+++ ahc003/src/bin/a.rs
@@ -17,6 +17,7 @@ struct Solver {
     graph: Vec<HashMap<usize, i64>>,
     dir: HashMap<(usize, usize), char>,
     history: Vec<(HashSet<(usize, usize)>, i64)>,
+    fixed: Vec<Vec<bool>>
 }
 
 impl Solver {
@@ -59,6 +60,7 @@ impl Solver {
             graph,
             dir,
             history: Vec::new(),
+            fixed: vec![vec![false; n]; n]
         }
     }
 
@@ -137,38 +139,107 @@ impl Solver {
     }
 
     fn update(&mut self, s: usize, path: &Path, cost: i64, i: usize) {
-        let mut u = s;
+        let mut updated = false;
+
+        // 与えられたコストから暫定のコストを計算する
         let mut path_set = HashSet::new();
-        let w = cost / path.len() as i64;
-        let ratio = 0.5 as f64 * (1000 - i) as f64 / 1000 as f64;
+        let mut not_fixed_cost = cost;
+        let mut not_fixed = vec![];
+        let mut u = s;
         for &v in path {
-            let new_w =  ((self.graph[u][&v] as f64 * (1 as f64 - ratio) + (w as f64 * ratio)) as i64).max(1);
-            self.graph[u].insert(v,  new_w);
+            if self.fixed[u][v] {
+                not_fixed_cost -= self.graph[u][&v];
+            } else {
+                not_fixed.push((u, v));
+            }
             path_set.insert((u, v));
-            u = v
+            u = v;
         }
 
+        if not_fixed.len() == 1 {
+            let (u, v) = not_fixed[0];
+            self.fixed[u][v] = true;
+            self.graph[u].insert(v, not_fixed_cost.max(1));
+            updated = true;
+        }
+
+        let ratio = 0.5 as f64 * (1000 - i) as f64 / 1000 as f64;
+        if not_fixed.len() > 0 {
+            let w = not_fixed_cost / not_fixed.len() as i64;
+            let mut u = s;
+            for &v in path {
+                if !self.fixed[u][v] {
+                    let new_w = ((self.graph[u][&v] as f64 * (1 as f64 - ratio) + (w as f64 * ratio)) as i64).max(1);
+                    self.graph[u].insert(v,  new_w);
+                }
+                u = v
+            }
+        }
+
+        if updated {
+            for (path2_set, cost2) in &self.history {
+                let mut fixed_cost = 0;
+                let mut not_fixed = vec![];
+                for &(u, v) in path2_set {
+                    if self.fixed[u][v] {
+                        fixed_cost += self.graph[u][&v];
+                    } else {
+                        not_fixed.push((u, v));
+                    }
+                }
+                if not_fixed.len() == 1 {
+                    // 過去の記録から辺のコストを確定する
+                    let (u, v) = not_fixed[0];
+                    self.fixed[u][v] = true;
+                    self.graph[u].insert(v, (cost2 - fixed_cost).max(1));
+                } else if not_fixed.len() > 1 {
+                    let w = (cost2 - fixed_cost) / not_fixed.len() as i64;
+                    for (u, v) in not_fixed {
+                        let new_w = ((self.graph[u][&v] as f64 * (1 as f64 - ratio) + (w as f64 * ratio)) as i64).max(1);
+                        self.graph[u].insert(v,  new_w);
+                    }
+                }
+            }
+        }
+
+        // 過去のパスについて、コストを再計算する
         for (path2_set, cost2) in &self.history {
             if path_set.is_subset(path2_set) {
-                let c = cost2 - cost;
+                // path_setがpath_set2に包含されている
+                let mut not_fixed_cost = cost2 - cost;
                 let len = path2_set.len() - path_set.len();
                 if len == 0 {
                     continue;
                 }
-                let w = c / len as i64;
+                let mut not_fixed = vec![];
                 for &(u, v) in path2_set.difference(&path_set) {
+                    if self.fixed[u][v] {
+                        not_fixed_cost -= self.graph[u][&v];
+                    } else {
+                        not_fixed.push((u, v));
+                    }
+                }
+                let w = not_fixed_cost / not_fixed.len() as i64;
+                for (u, v) in not_fixed {
                     let new_w =  ((self.graph[u][&v] as f64 * (1 as f64 - ratio) + (w as f64 * ratio)) as i64).max(1);
                     self.graph[u].insert(v,  new_w);
                 }
-            }
-            if path2_set.is_subset(&path_set) {
-                let c = cost - cost2;
+            } else if path2_set.is_subset(&path_set) {
+                let mut not_fixed_cost = cost - cost2;
                 let len = path_set.len() - path2_set.len();
                 if len == 0 {
                     continue;
                 }
-                let w = c / len as i64;
-                for &(u, v) in path_set.difference(&path2_set) {
+                let mut not_fixed = vec![];
+                for &(u, v) in path_set.difference(path2_set) {
+                    if self.fixed[u][v] {
+                        not_fixed_cost -= self.graph[u][&v];
+                    } else {
+                        not_fixed.push((u, v));
+                    }
+                }
+                let w = not_fixed_cost / not_fixed.len() as i64;
+                for (u, v) in not_fixed {
                     let new_w =  ((self.graph[u][&v] as f64 * (1 as f64 - ratio) + (w as f64 * ratio)) as i64).max(1);
                     self.graph[u].insert(v,  new_w);
                 }
@@ -176,6 +247,7 @@ impl Solver {
         }
 
         self.history.push((path_set, cost));
+
     }
 
     fn vertex(i: usize, j: usize) -> usize {
