48ecfee1712467777a833968d84ea3c764ccfc06
emanon001 <emanon001@gmail.com>
2021-06-06 00:05:37 +0900

    solve jsc2021/c

--- jsc2021/src/bin/c.rs
+++ jsc2021/src/bin/c.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: i64, b: i64
     };
+
+    let mut res = 1;
+    for x in 2..b {
+        let mut y = x;
+        let mut count = 0;
+        while y <= b {
+            if y >= a {
+                count += 1;
+            }
+            y += x;
+        }
+        if count >= 2 {
+            res = x;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
