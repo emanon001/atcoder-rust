ff754a9ede9ad4fae9bd05402ba63d6f28c6f266
emanon001 <emanon001@gmail.com>
2021-06-06 22:47:20 +0900

    solve abc204/d

--- abc204/src/bin/d.rs
+++ abc204/src/bin/d.rs
@@ -8,9 +8,44 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        mut tv: [usize; n]
     };
+
+    tv.sort();
+    let mut dp = vec![vec![1_usize << 30; 100000 + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 0..n {
+        let t = tv[i];
+        for j in 0..=100000 {
+            if j + t > 100000 {
+                continue;
+            }
+            // オーブン1を使用
+            chmin!(dp[i + 1][j + t], dp[i][j]);
+            // オーブン2を使用
+            chmin!(dp[i + 1][j], dp[i][j] + t);
+        }
+    }
+    let mut res = 1_usize << 30;
+    for j in 0..=100000 {
+        chmin!(res, j.max(dp[n][j]));
+    }
+    println!("{}", res);
 }
 
 fn main() {
