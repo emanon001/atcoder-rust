9284329ab44704c204f3c445a4776fcfd70e910b
emanon001 <emanon001@gmail.com>
2021-06-14 23:47:02 +0900

    solve typical90/046

--- typical90/src/bin/046.rs
+++ typical90/src/bin/046.rs
@@ -10,7 +10,33 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n],
+        bv: [usize; n],
+        cv: [usize; n],
     };
+
+    let mut ac = vec![0; 46];
+    for a in av {
+        ac[a % 46] += 1;
+    }
+    let mut bc = vec![0; 46];
+    for b in bv {
+        bc[b % 46] += 1;
+    }
+    let mut cc = vec![0; 46];
+    for c in cv {
+        cc[c % 46] += 1;
+    }
+
+    let mut res = 0_i64;
+    for a in 0..ac.len() {
+        for b in 0..bc.len() {
+            let c = (46 - ((a + b) % 46)) % 46;
+            res += ac[a] * bc[b] * cc[c];
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
