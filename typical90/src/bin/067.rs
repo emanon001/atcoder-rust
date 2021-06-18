25956d4e3571290bc98b7903f48135702b432ad2
emanon001 <emanon001@gmail.com>
2021-06-18 23:56:00 +0900

    solve typical90/067

--- typical90/src/bin/067.rs
+++ typical90/src/bin/067.rs
@@ -8,9 +8,34 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn calc(digits: Vec<u64>) -> Vec<u64> {
+    // 8 -> 10
+    let mut ten = digits.into_iter().fold(0_u64, |acc, x| acc * 8_u64 + x);
+    // 10 -> 9 (8 -> 5)
+    let mut res = VecDeque::new();
+    while ten > 0 {
+        let d = ten % 9;
+        res.push_front(if d == 8 { 5 } else { d });
+        ten /= 9;
+    }
+    if res.is_empty() {
+        vec![0]
+    } else {
+        res.into_iter().collect::<Vec<_>>()
+    }
+}
+
 fn solve() {
     input! {
+        n: Chars,
+        k: usize,
     };
+
+    let mut res = n.into_iter().map(|ch| ch.to_digit(10).unwrap() as u64).collect::<Vec<_>>();
+    for _ in 0..k {
+        res = calc(res);
+    }
+    println!("{}", res.into_iter().join(""));
 }
 
 fn main() {
