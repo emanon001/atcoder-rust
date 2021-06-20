b526d6854a2ab87551e47880168f635d7e7a40bb
emanon001 <emanon001@gmail.com>
2021-06-20 22:34:06 +0900

    solve past202104-open/e

--- past202104-open/src/bin/e.rs
+++ past202104-open/src/bin/e.rs
@@ -8,9 +8,106 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn pop_front(a: &mut VecDeque<usize>, pos: usize) -> Option<usize> {
+    if a.len() < pos {
+        return None;
+    }
+    let mut backup = Vec::new();
+    for _ in 0..pos - 1 {
+        let x = a.pop_front().unwrap();
+        backup.push(x);
+    }
+    let res = a.pop_front();
+    for x in backup.into_iter().rev() {
+        a.push_front(x);
+    }
+    res
+}
+
+fn pop_back(a: &mut VecDeque<usize>, pos: usize) -> Option<usize> {
+    if a.len() < pos {
+        return None;
+    }
+    let mut backup = Vec::new();
+    for _ in 0..pos - 1 {
+        let x = a.pop_back().unwrap();
+        backup.push(x);
+    }
+    let res = a.pop_back();
+    for x in backup.into_iter().rev() {
+        a.push_back(x);
+    }
+    res
+}
+
 fn solve() {
     input! {
+        n: usize,
+        s: Chars
     };
+
+    let mut a = VecDeque::new();
+    for i in 0..n {
+        let ch = s[i];
+        let i = i + 1;
+        match ch {
+            'L' => {
+                a.push_front(i);
+            }
+            'R' => {
+                a.push_back(i);
+            }
+            'A' => {
+                let res = if let Some(res) = pop_front(&mut a, 1) {
+                    res.to_string()
+                } else {
+                    "ERROR".to_string()
+                };
+                println!("{}", res);
+            }
+            'B' => {
+                let res = if let Some(res) = pop_front(&mut a, 2) {
+                    res.to_string()
+                } else {
+                    "ERROR".to_string()
+                };
+                println!("{}", res);
+            }
+            'C' => {
+                let res = if let Some(res) = pop_front(&mut a, 3) {
+                    res.to_string()
+                } else {
+                    "ERROR".to_string()
+                };
+                println!("{}", res);
+            }
+            'D' => {
+                let res = if let Some(res) = pop_back(&mut a, 1) {
+                    res.to_string()
+                } else {
+                    "ERROR".to_string()
+                };
+                println!("{}", res);
+            }
+            'E' => {
+                let res = if let Some(res) = pop_back(&mut a, 2) {
+                    res.to_string()
+                } else {
+                    "ERROR".to_string()
+                };
+                println!("{}", res);
+            }
+            'F' => {
+                let res = if let Some(res) = pop_back(&mut a, 3) {
+                    res.to_string()
+                } else {
+                    "ERROR".to_string()
+                };
+                println!("{}", res);
+            }
+            _ => unreachable!()
+        }
+    }
 }
 
 fn main() {
