7d6024277d94502400ece5e74cd6d681fb3266d1
emanon001 <emanon001@gmail.com>
2021-06-23 23:16:39 +0900

    solve typical90/061

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
+        txv: [(usize, usize); q]
     };
+
+    let mut vec = VecDeque::new();
+    for (t, v) in txv {
+        match t {
+            1 => vec.push_front(v),
+            2 => vec.push_back(v),
+            3 => println!("{}", vec[v - 1]),
+            _ => unreachable!()
+        }
+    }
 }
 
 fn main() {
