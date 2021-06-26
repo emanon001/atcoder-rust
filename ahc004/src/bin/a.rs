e7d6b6fa90e8d8de9d4a3ca47fdf5f0ee74ea642
emanon001 <emanon001@gmail.com>
2021-06-26 13:35:05 +0900

    左上から配置

--- ahc004/src/bin/a.rs
+++ ahc004/src/bin/a.rs
@@ -2,25 +2,63 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::input;
 #[allow(unused_imports)]
+use proconio::*;
 use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
+use rand::prelude::*;
+use std::time::{Instant, Duration};
 
-fn solve() {
-    input! {
-    };
+struct Solver {
+    n: usize,
+    m: usize,
+    sv: Vec<Vec<char>>,
+    res: Vec<Vec<char>>,
+    start_time: Instant,
+    rng: ThreadRng,
+}
+
+impl Solver {
+    fn new() -> Self {
+        let now = Instant::now();
+        input! {
+            n: usize, m: usize,
+            sv: [Chars; m]
+        };
+        let res = vec![vec!['.'; n]; n];
+        Self {
+            n,
+            m,
+            sv,
+            res,
+            start_time: now,
+            rng: rand::thread_rng(),
+        }
+    }
+
+    fn solve(&mut self) {
+        // 各行について左から右に配置していく
+        self.sv.sort_by_key(|s| s.len());
+        let mut s_i = 0;
+        for i in 0..self.n {
+            let mut j = 0;
+            while s_i < self.m && j + self.sv[s_i].len() < self.n {
+                let s = &self.sv[s_i];
+                for s_j in 0..s.len() {
+                    self.res[i][j + s_j] = s[s_j];
+                }
+                s_i += 1;
+                j += s.len();
+            }
+        }
+        for s in &self.res {
+            println!("{}", s.iter().join(""));
+        }
+    }
 }
 
 fn main() {
-    std::thread::Builder::new()
-        .name("big stack size".into())
-        .stack_size(256 * 1024 * 1024)
-        .spawn(|| {
-            solve();
-        })
-        .unwrap()
-        .join()
-        .unwrap();
+    let mut solver = Solver::new();
+    solver.solve();
 }
