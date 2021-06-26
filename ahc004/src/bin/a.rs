088f39d19e2512c33f144a1025dc5e6d09902542
emanon001 <emanon001@gmail.com>
2021-06-26 13:59:17 +0900

    使用済みの文字列を使用しない

--- ahc004/src/bin/a.rs
+++ ahc004/src/bin/a.rs
@@ -40,11 +40,30 @@ impl Solver {
     fn solve(&mut self) {
         // 各行について左から右に配置していく
         self.sv.sort_by_key(|s| s.len());
+        let mut used: HashSet<String> = HashSet::new();
         let mut s_i = 0;
         for i in 0..self.n {
             let mut j = 0;
             while s_i < self.m && j + self.sv[s_i].len() < self.n {
                 let s = &self.sv[s_i];
+                let st = s.iter().join("");
+                if used.contains(&st) {
+                    s_i += 1;
+                    continue;
+                }
+                let mut contains = false;
+                for us in &used {
+                    if us.contains(&st) {
+                        contains = true;
+                        break;
+                    }
+                }
+                if contains {
+                    used.insert(st);
+                    s_i += 1;
+                    continue;
+                }
+                used.insert(st);
                 for s_j in 0..s.len() {
                     self.res[i][j + s_j] = s[s_j];
                 }
