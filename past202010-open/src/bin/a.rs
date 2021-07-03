67f79bf486a5896b7a23967e5c65c0cf75031b93
emanon001 <emanon001@gmail.com>
2021-07-03 18:28:37 +0900

    solve past202010-open/a

--- past202010-open/src/bin/a.rs
+++ past202010-open/src/bin/a.rs
@@ -10,7 +10,16 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        v: [usize; 3]
     };
+
+    let mut v = vec![
+        (v[0], 'A'),
+        (v[1], 'B'),
+        (v[2], 'C'),
+    ];
+    v.sort();
+    println!("{}", v[1].1);
 }
 
 fn main() {
