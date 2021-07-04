885f61fcc0805807d791bed3b0a94092e4422798
emanon001 <emanon001@gmail.com>
2021-07-04 22:44:58 +0900

    solve abc208/c

--- abc208/src/bin/c.rs
+++ abc208/src/bin/c.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: u64,
+        mut av: [u64; n]
     };
+
+    let mut av = av.into_iter().enumerate().map(|(i, a)| (a, i)).collect::<Vec<_>>();
+    av.sort();
+
+    let mut res = vec![0_u64; n];
+    for i in 0..n {
+        let (_, pos) = av[i];
+        let mut x = k / n as u64;
+        let m = k % n as u64;
+        x += if i < m as usize { 1 } else { 0 };
+        res[pos] = x;
+    }
+    for x in res {
+        println!("{}", x);
+    }
 }
 
 fn main() {
