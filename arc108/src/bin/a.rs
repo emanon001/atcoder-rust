4ff7aa08140b5e1bef74c37f70d12dc4220f811f
emanon001 <emanon001@gmail.com>
2021-07-05 23:53:58 +0900

    solve arc108/a

--- arc108/src/bin/a.rs
+++ arc108/src/bin/a.rs
@@ -8,9 +8,35 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: u64) -> Vec<u64> {
+    let mut res = Vec::new();
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            res.push(x);
+            let y = n / x;
+            if y != x {
+                res.push(y);
+            }
+        }
+        x += 1;
+    }
+    res
+}
+
 fn solve() {
     input! {
+        s: u64, p: u64
     };
+
+    for x in divisors(p) {
+        let y = p / x;
+        if x + y == s {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
