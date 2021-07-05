c569d0fb6ff64528eb537c0ef31180b7599d3553
emanon001 <emanon001@gmail.com>
2021-07-05 20:27:33 +0900

    solve abc190/d

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -8,9 +8,48 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: u64) -> Vec<u64> {
+    let mut res = Vec::new();
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            res.push(x);
+            let y = n / x;
+            if y != x {
+                res.push(y);
+            }
+        }
+        x += 1;
+    }
+    res
+}
+
 fn solve() {
     input! {
+        n: u64
     };
+
+    if n == 1 {
+        println!("2");
+        return;
+    }
+
+    let divs = divisors(n);
+    let mut res = 0_u64;
+    if n % 2 == 0 {
+        for d in divs {
+            if d % 2 == 0 && (n / d) % 2 == 1 {
+                res += 2;
+            }
+        }
+    } else {
+        for d in divs {
+            if d % 2 == 1 && (n / d) % 2 == 1 {
+                res += 2;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
