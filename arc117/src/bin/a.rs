c137c4f8fe342a36a71b146da8ce5f6029efdce0
emanon001 <emanon001@gmail.com>
2021-07-07 22:48:59 +0900

    solve arc117/a

--- arc117/src/bin/a.rs
+++ arc117/src/bin/a.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: isize, b: isize
     };
+
+    let mut res = Vec::new();
+    for i in 1..=a {
+        res.push(i);
+    }
+    for i in 1..=b {
+        res.push(-i);
+    }
+    let mut diff = 0_isize;
+    for x in a.min(b) + 1..=a.max(b) {
+        diff += x;
+    }
+    if a > b {
+        res[(a + b - 1) as usize] -= diff;
+    } else {
+        res[(a - 1) as usize] += diff;
+    }
+    println!("{}", res.iter().join(" "));
 }
 
 fn main() {
