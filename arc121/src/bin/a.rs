d8197133a743b43a0aadd2f366d792e05d6a7691
emanon001 <emanon001@gmail.com>
2021-07-08 22:01:55 +0900

    solve arc121/a

--- arc121/src/bin/a.rs
+++ arc121/src/bin/a.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        xyv: [(i64, i64); n]
     };
+
+    let mut ixyv = xyv.iter().copied().enumerate().collect::<Vec<_>>();
+    let mut set = HashSet::new();
+    ixyv.sort_by_key(|(_, a)| a.0);
+    set.insert((ixyv[0].0, ixyv[n - 1].0));
+    set.insert((ixyv[0].0, ixyv[n - 2].0));
+    set.insert((ixyv[1].0, ixyv[n - 1].0));
+    ixyv.sort_by_key(|(_, a)| a.1);
+    set.insert((ixyv[0].0, ixyv[n - 1].0));
+    set.insert((ixyv[0].0, ixyv[n - 2].0));
+    set.insert((ixyv[1].0, ixyv[n - 1].0));
+    let mut v = Vec::new();
+    for (i, j) in set {
+        v.push((xyv[i].0 - xyv[j].0).abs().max((xyv[i].1 - xyv[j].1).abs()));
+    }
+    v.sort_by_key(|x| -x);
+    println!("{}", v[1]);
 }
 
 fn main() {
