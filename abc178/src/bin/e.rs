1fff66b2f220258bfcac64e9d1a7ce0d3eb47a1f
emanon001 <emanon001@gmail.com>
2021-07-08 23:48:37 +0900

    solve abc178/e

--- abc178/src/bin/e.rs
+++ abc178/src/bin/e.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        xyv: [(i64, i64); n]
     };
+
+    let mut points = Vec::new();
+    for (x, y) in xyv {
+        points.push((x + y, x - y));
+    }
+    points.sort_by_key(|p| p.0);
+    let mut res = (points[0].0 - points[n - 1].0).abs();
+    points.sort_by_key(|p| p.1);
+    res = res.max((points[0].1 - points[n - 1].1).abs());
+    println!("{}", res);
 }
 
 fn main() {
