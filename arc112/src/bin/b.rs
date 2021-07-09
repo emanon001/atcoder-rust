96320d319d6388e52c60126f28e1b7c3495e140b
emanon001 <emanon001@gmail.com>
2021-07-10 02:30:36 +0900

    solve arc112/b

--- arc112/src/bin/b.rs
+++ arc112/src/bin/b.rs
@@ -10,7 +10,33 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        b: i64, c: i64
     };
+
+    if b == 0 {
+        let res = c / 2 + (c - 2) / 2 + 1;
+        println!("{}", res);
+        return;
+    }
+
+    let count1 = c / 2 + (c - 2) / 2 + 1; // B基準
+    let count2 = (c - 1) / 2 + (c - 1) / 2 + 1; // -B基準
+    // x座標上で左と右の点を基準とした整数の数
+    let (left_b_count, right_b_count) = if b >= 0 {
+        (count2, count1)
+    } else {
+        (count1, count2)
+    };
+    let mut res = left_b_count + right_b_count;
+    let (left_b_r, right_b_l) = if b >= 0 {
+        (-b + (c - 1) / 2, b - c / 2)
+    } else {
+        (b + (c - 2) / 2, -b - (c - 1) / 2)
+    };
+    if left_b_r >= right_b_l {
+        res -= left_b_r - right_b_l + 1;
+    }
+    println!("{}", res);
 }
 
 fn main() {
