9a2edf7a37d31d877b4dab0db880de4bb9ce9c77
emanon001 <emanon001@gmail.com>
2021-07-12 00:05:46 +0900

    solve typical90/070

--- typical90/src/bin/070.rs
+++ typical90/src/bin/070.rs
@@ -8,9 +8,50 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        mut pv: [(i64, i64); n]
     };
+
+    // x
+    pv.sort_by_key(|p| p.0);
+    let mid = pv[n / 2];
+    let mut sum = 0;
+    for p in pv.iter().copied() {
+        sum += (mid.0 - p.0).abs();
+    }
+    // y
+    pv.sort_by_key(|p| p.1);
+    let mid = pv[n / 2];
+    for p in pv.iter().copied() {
+        sum += (mid.1 - p.1).abs();
+    }
+    println!("{}", sum);
 }
 
 fn main() {
