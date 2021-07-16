f4b8c1cac0a18d6e3989fa389e9a982d1570d635
emanon001 <emanon001@gmail.com>
2021-07-17 02:18:49 +0900

    solve typical90/078

--- typical90/src/bin/078.rs
+++ typical90/src/bin/078.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut res = 0;
+    for u in 0..n {
+        let is_ok = graph[u].clone().into_iter().filter(|&v| u > v).count() == 1;
+        if is_ok {
+            res += 1;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
