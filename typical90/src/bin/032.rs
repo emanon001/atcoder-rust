594d69d8d71c5a8f91b0adfed7d620741e195b32
emanon001 <emanon001@gmail.com>
2021-07-18 23:26:42 +0900

    solve typical90/032

--- typical90/src/bin/032.rs
+++ typical90/src/bin/032.rs
@@ -8,9 +8,43 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        time_table: [[i64; n]; n],
+        m: usize,
+        xyv: [(Usize1, Usize1); m]
     };
+
+    let ng_set = xyv.into_iter().collect::<HashSet<_>>();
+    let inf = 1_i64 << 60;
+    let mut res = inf;
+    'outer: for perm in (0..n).into_iter().permutations(n) {
+        let mut sum = 0;
+        for (&x, &y) in perm.iter().tuple_windows() {
+            if ng_set.contains(&(x.min(y), x.max(y))) {
+                continue 'outer;
+            }
+        }
+        for i in 0..n {
+            sum += time_table[perm[i]][i];
+        }
+        chmin!(res, sum);
+    }
+    let res = if res == inf { -1 } else { res };
+    println!("{}", res);
 }
 
 fn main() {
