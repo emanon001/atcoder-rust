eeb2b5b5b2b130a3cc7d9a568d2757053bfaffb5
emanon001 <emanon001@gmail.com>
2021-07-18 22:31:03 +0900

    solve typical90/002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -10,7 +10,39 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
     };
+
+    let mut res = Vec::new();
+    for bits in 0..1 << n {
+        let mut stack = Vec::new();
+        for i in 0..n {
+            let ch = if (bits >> i) & 1 == 0 {
+                '('
+            } else {
+                ')'
+            };
+            stack.push(ch);
+        }
+        let mut balance = 0;
+        for i in 0..n {
+            if stack[i] == '(' {
+                balance += 1;
+            } else {
+                balance -= 1;
+            }
+            if balance < 0 {
+                break;
+            }
+        }
+        if balance == 0 {
+            res.push(stack.iter().join(""));
+        }
+    }
+    res.sort();
+    for s in res {
+        println!("{}", s);
+    }
 }
 
 fn main() {
