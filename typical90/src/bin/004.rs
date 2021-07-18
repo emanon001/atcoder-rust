33b67a36ecee40ce0148ffbd106c3cd6e35f2747
emanon001 <emanon001@gmail.com>
2021-07-18 22:43:34 +0900

    solve typical90/004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -10,7 +10,34 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [[i64; w]; h]
     };
+
+    let mut vertical_cusum = vec![0_i64; w];
+    for j in 0..w {
+        let mut sum = 0_i64;
+        for i in 0..h {
+            sum += grid[i][j];
+        }
+        vertical_cusum[j] = sum;
+    }
+    let mut horizontal_cusum = vec![0_i64; h];
+    for i in 0..h {
+        let mut sum = 0_i64;
+        for j in 0..w {
+            sum += grid[i][j];
+        }
+        horizontal_cusum[i] = sum;
+    }
+    for i in 0..h {
+        let mut res = Vec::new();
+        for j in 0..w {
+            let x = vertical_cusum[j] + horizontal_cusum[i] - grid[i][j];
+            res.push(x);
+        }
+        println!("{}", res.iter().join(" "));
+    }
 }
 
 fn main() {
