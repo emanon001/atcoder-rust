bf12cd7a1fd623802015c38fc1dcdc4764d9d93a
emanon001 <emanon001@gmail.com>
2021-07-18 22:16:23 +0900

    solve abc210/c

--- abc210/src/bin/c.rs
+++ abc210/src/bin/c.rs
@@ -8,9 +8,43 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize, k: usize,
+        cv: [i64; n]
     };
+
+    let mut map = HashMap::new();
+    for i in 0..k {
+        *map.entry(cv[i]).or_insert(0) += 1;
+    }
+    let mut res = map.len();
+    for i in k..n {
+        *map.entry(cv[i]).or_insert(0) += 1;
+        let x = cv[i - k];
+        if map.get(&x).is_some() {
+            let count = *map.get(&x).unwrap();
+            if count == 1 {
+                map.remove(&x);
+            } else {
+                map.insert(x, count - 1);
+            }
+        }
+        chmax!(res, map.len());
+    }
+    println!("{}", res);
 }
 
 fn main() {
