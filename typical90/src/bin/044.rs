9429c569c667ca58289a016eed369a6e5a7d77a6
emanon001 <emanon001@gmail.com>
2021-07-19 21:07:41 +0900

    solve typical90/044

--- typical90/src/bin/044.rs
+++ typical90/src/bin/044.rs
@@ -10,7 +10,42 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
+        mut av: [i64; n]
     };
+
+    let mut shift = 0_usize;
+    for _ in 0..q {
+        input! {
+            t: usize
+        };
+        match t {
+            1 => {
+                input! {
+                    x: Usize1, y: Usize1
+                };
+                let i = (n + x - shift) % n;
+                let j = (n + y - shift) % n;
+                let t = av[i];
+                av[i] = av[j];
+                av[j] = t;
+            }
+            2 => {
+                input! {
+                    _: usize, _: usize
+                };
+                shift = (shift + 1) % n;
+            }
+            3 => {
+                input! {
+                    x: Usize1, _: usize
+                };
+                let i = (n + x - shift) % n;
+                println!("{}", av[i]);
+            }
+            _ => unreachable!()
+        };
+    }
 }
 
 fn main() {
