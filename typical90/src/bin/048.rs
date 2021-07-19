69aa9ce294188f0b00a07931dbc1e48c736c4af2
emanon001 <emanon001@gmail.com>
2021-07-19 21:32:59 +0900

    solve typical90/048

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -10,7 +10,18 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        points: [(i64, i64); n]
     };
+
+    let mut v = Vec::new();
+    for (a, b) in points {
+        v.push(a - b);
+        v.push(b);
+    }
+    v.sort_by_key(|x| -x);
+    let res = v.iter().take(k).sum::<i64>();
+    println!("{}", res);
 }
 
 fn main() {
