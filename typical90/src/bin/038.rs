4924d947c83bef6ea122dd689f1e078ae774ab36
emanon001 <emanon001@gmail.com>
2021-07-19 21:20:54 +0900

    solve typical90/038

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -10,7 +10,17 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: u128, b: u128
     };
+
+    // answer1: 
+    // let n = a.lcm(&b);
+    // let res = if n > 10.pow(18) { "Large".to_string() } else { n.to_string() };
+
+    let gcd = a.gcd(&b);
+    let c = b / gcd;
+    let res = if c > 10.pow(18) / a { "Large".to_string() } else { (a * c).to_string() };
+    println!("{}", res);
 }
 
 fn main() {
