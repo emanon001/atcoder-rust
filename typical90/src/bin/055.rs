f05b1685db43ab91ec336cfae6ef7624019108fa
emanon001 <emanon001@gmail.com>
2021-07-20 22:28:48 +0900

    solve typical90/055

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, p: u64, q: u64,
+        av: [u64; n]
     };
+
+    let mut res = 0;
+    for a in 0..n {
+        for b in a + 1..n {
+            for c in b + 1..n {
+                for d in c + 1..n {
+                    for e in d + 1..n {
+                        let x = [av[a], av[b], av[c], av[d], av[e]].iter().fold(1, |acc, x| (acc * x) % p);
+                        if x == q {
+                            res += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
