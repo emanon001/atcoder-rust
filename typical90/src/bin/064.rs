546ddabb59b51f795309ede15793db06cef3c192
emanon001 <emanon001@gmail.com>
2021-07-20 23:27:54 +0900

    solve typical90/064

--- typical90/src/bin/064.rs
+++ typical90/src/bin/064.rs
@@ -10,7 +10,40 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
+        av: [i64; n],
+        queries: [(Usize1, Usize1, i64); q]
     };
+
+    let mut diff = vec![0; n - 1];
+    let mut res = 0_i64;
+    for i in 0..n - 1 {
+        diff[i] = av[i] - av[i + 1];
+        res += diff[i].abs();
+    }
+    for (l, r, v) in queries {
+        if l > 0 {
+            let x = diff[l - 1];
+            diff[l - 1] -= v;
+            let y = diff[l - 1];
+            if y.abs() > x.abs() {
+                res += y.abs() - x.abs();
+            } else {
+                res -= x.abs() - y.abs();
+            }
+        }
+        if r < n - 1 {
+            let x = diff[r];
+            diff[r] += v;
+            let y = diff[r];
+            if y.abs() > x.abs() {
+                res += y.abs() - x.abs();
+            } else {
+                res -= x.abs() - y.abs();
+            }
+        }
+        println!("{}", res);
+    }
 }
 
 fn main() {
