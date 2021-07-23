67e5a204c212f6e6f03a02143ceb07bfe331dd99
emanon001 <emanon001@gmail.com>
2021-07-24 01:19:31 +0900

    solve typical90/079

--- typical90/src/bin/079.rs
+++ typical90/src/bin/079.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        mut a_grid: [[i64; w]; h],
+        b_grid: [[i64; w]; h],
     };
+
+    let mut res = 0;
+    for i in 0..=h - 2 {
+        for j in 0..=w - 2 {
+            let diff = b_grid[i][j] - a_grid[i][j];
+            a_grid[i][j] += diff;
+            a_grid[i][j + 1] += diff;
+            a_grid[i + 1][j] += diff;
+            a_grid[i + 1][j + 1] += diff;
+            res += diff.abs();
+        }
+    }
+    for i in 0..h {
+        for j in 0..w {
+            if a_grid[i][j] != b_grid[i][j] {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
+    println!("{}", res);
 }
 
 fn main() {
