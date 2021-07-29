9ac9071fc98bf45d1d30c29e25cf76d5b15365eb
emanon001 <emanon001@gmail.com>
2021-07-29 23:16:50 +0900

    solve typical90/034

--- typical90/src/bin/034.rs
+++ typical90/src/bin/034.rs
@@ -8,9 +8,43 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize, k: usize,
+        av: [i64; n]
     };
+
+    let mut l = 0;
+    let mut map = HashMap::new();
+    let mut res = 0;
+    for r in 0..n {
+        *map.entry(av[r]).or_insert(0) += 1;
+        if map.len() <= k {
+            chmax!(res, r - l + 1);
+        } else {
+            while map.len() > k {
+                let v = av[l];
+                *map.get_mut(&v).unwrap() -= 1;
+                if map[&v] == 0 {
+                    map.remove(&v);
+                }
+                l += 1;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
