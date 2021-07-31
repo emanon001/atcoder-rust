59837df8a229b0fa9528d70e7d6afdf25a45f600
emanon001 <emanon001@gmail.com>
2021-07-31 22:52:30 +0900

    solve abc212/b

--- abc212/src/bin/b.rs
+++ abc212/src/bin/b.rs
@@ -10,7 +10,17 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let s = s.into_iter().map(|ch| ch.to_digit(10).unwrap()).collect::<Vec<_>>();
+    if s[0] == s[1] && s[1] == s[2] && s[2] == s[3] {
+        println!("Weak");
+        return;
+    }
+    let is_strong = (s[0] + 1) % 10 != s[1] || (s[1] + 1) % 10 != s[2] || (s[2] + 1) % 10 != s[3];
+    let res = if is_strong { "Strong" } else { "Weak" };
+    println!("{}", res);
 }
 
 fn main() {
