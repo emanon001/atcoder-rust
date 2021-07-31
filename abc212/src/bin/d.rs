154bf8b68c17d44496d566d01dfb22ce61e992ea
emanon001 <emanon001@gmail.com>
2021-07-31 22:52:39 +0900

    solve abc212/d

--- abc212/src/bin/d.rs
+++ abc212/src/bin/d.rs
@@ -10,7 +10,37 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+
+    let mut heap = BinaryHeap::new();
+    let mut add = 0_i64;
+    for _ in 0..q {
+        input! {
+            kind: usize,
+        };
+        match kind {
+            1 => {
+                input! {
+                    x: i64
+                };
+                let x = x - add;
+                heap.push(std::cmp::Reverse(x));
+            }
+            2 => {
+                input! {
+                    x: i64
+                };
+                add += x;
+            }
+            3 => {
+                let std::cmp::Reverse(x) = heap.pop().unwrap();
+                let res = x + add;
+                println!("{}", res);
+            }
+            _ => unreachable!()
+        }
+    }
 }
 
 fn main() {
