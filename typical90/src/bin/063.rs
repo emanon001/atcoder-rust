13564ecb2ff499b29b05de06a24f03bd58074ac3
emanon001 <emanon001@gmail.com>
2021-08-02 22:37:22 +0900

    solve typical90/063

--- typical90/src/bin/063.rs
+++ typical90/src/bin/063.rs
@@ -8,9 +8,46 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [[usize; w]; h]
     };
+
+    let mut res = 0;
+    for bits in 1..1 << h {
+        let mut map = HashMap::new();
+        let mut score = 0;
+        for j in 0..w {
+            let mut hc = 0;
+            let mut set = HashSet::new();
+            for i in 0..h {
+                if (bits >> i) & 1 == 1 {
+                    hc += 1;
+                    set.insert(grid[i][j]);
+                }
+            }
+            if set.len() == 1 {
+                let key = *set.iter().next().unwrap();
+                *map.entry(key).or_insert(0) += hc;
+                chmax!(score, map[&key]);
+            }
+        }
+        chmax!(res, score);
+    }
+    println!("{}", res);
 }
 
 fn main() {
