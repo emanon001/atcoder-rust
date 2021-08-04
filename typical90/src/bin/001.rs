8a2757e414a993d1119ee061e9ad3b4983ba86e9
emanon001 <emanon001@gmail.com>
2021-08-04 23:59:35 +0900

    solve typical90/001

--- typical90/src/bin/001.rs
+++ typical90/src/bin/001.rs
@@ -8,9 +8,49 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, l: i64,
+        k: i64,
+        mut av: [i64; n]
     };
+    av.push(l);
+
+    let res = bsearch(0_i64, 10_i64.pow(9) + 1, |x| {
+        let mut count = 0;
+        let mut l = 0;
+        for &a in &av {
+            let r = a;
+            if r - l >= x {
+                l = r;
+                count += 1;
+            }
+        }
+        count > k
+    }).unwrap();
+    println!("{}", res);
 }
 
 fn main() {
