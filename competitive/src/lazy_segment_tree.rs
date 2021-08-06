654f66a2953435bd1566b870b051af80c38d9346
emanon001 <emanon001@gmail.com>
2021-08-06 23:18:38 +0900

    wip LazySegmentTree

--- competitive/src/lazy_segment_tree.rs
+++ competitive/src/lazy_segment_tree.rs
@@ -1,7 +1,7 @@
 use cargo_snippet::snippet;
 
 #[snippet("lazy_segment_tree")]
-pub trait Monoid {
+pub trait Foo {
     fn mempty() -> Self;
     fn mappend(&self, other: &Self) -> Self;
 }
@@ -10,36 +10,34 @@ pub trait Monoid {
 #[snippet(include = "monoid")]
 pub struct LazySegmentTree<T>
 where
-    T: Monoid + Clone,
+    T: Foo + Clone,
 {
     size: usize,
     data: Vec<T>,
+    lazy: Vec<Option<T>>,
 }
 
 #[snippet("lazy_segment_tree")]
 impl<T> LazySegmentTree<T>
 where
-    T: Monoid + Clone,
+    T: Foo + Clone,
 {
     pub fn new(size: usize) -> Self {
         let size = Self::normalize_data_size(size);
         let data = vec![T::mempty(); size * 2 - 1];
-        Self { size, data }
-    }
-
-    /// 0-origin
-    pub fn update(&mut self, i: usize, v: T) {
-        let mut i = i + self.size - 1;
-        self.data[i] = v;
-        while i > 0 {
-            i = (i - 1) / 2;
-            self.data[i] = self.data[i * 2 + 1].mappend(&self.data[i * 2 + 2]);
-        }
+        let lazy = vec![None; size * 2 - 1];
+        Self { size, data, lazy }
     }
 
     /// [a, b)
     /// 0-origin
-    pub fn query(&self, a: usize, b: usize) -> T {
+    pub fn update(&mut self, a: usize, b: usize, v: T) {
+        self.execute_update(a, b, v, 0, 0, self.size)
+    }
+
+    /// [a, b)
+    /// 0-origin
+    pub fn query(&mut self, a: usize, b: usize) -> T {
         self.execute_query(a, b, 0, 0, self.size)
     }
 
@@ -51,7 +49,23 @@ where
         n
     }
 
-    fn execute_query(&self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
+    fn execute_update(&mut self, a: usize, b: usize, v: T, i: usize, l: usize, r: usize) {
+        self.eval(i);
+        if r <= a || b <= l {
+            return;
+        }
+        if a <= l && r <= b {
+            self.lazy[i] = Some(v.clone());
+            self.eval(i);
+        } else {
+            self.execute_update(a, b, v.clone(), i * 2 + 1, l, (l + r) / 2);
+            self.execute_update(a, b, v.clone(), i * 2 + 2, (l + r) / 2, r);
+            self.data[i] = self.data[i * 2 + 1].clone().mappend(&self.data[i * 2 + 2]);
+        }
+    }
+
+    fn execute_query(&mut self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
+        self.eval(i);
         if r <= a || b <= l {
             return T::mempty();
         }
@@ -62,12 +76,25 @@ where
         let vr = self.execute_query(a, b, i * 2 + 2, (l + r) / 2, r);
         vl.mappend(&vr)
     }
+
+    fn eval(&mut self, i: usize) {
+        if self.lazy[i].is_none() {
+            return;
+        }
+        let is_leaf = (i + 1) * 2 >= self.size;
+        if !is_leaf {
+            self.lazy[i * 2 + 1] = self.lazy[i].clone();
+            self.lazy[i * 2 + 2] = self.lazy[i].clone();
+        }
+        self.data[i] = self.lazy[i].clone().unwrap();
+        self.lazy[i] = None;
+    }
 }
 
 #[snippet("lazy_segment_tree")]
 impl<T> From<&[T]> for LazySegmentTree<T>
 where
-    T: Monoid + Clone,
+    T: Foo + Clone,
 {
     fn from(values: &[T]) -> Self {
         let mut st = LazySegmentTree::new(values.len());
@@ -87,7 +114,7 @@ where
 #[snippet("lazy_segment_tree")]
 impl<T> From<Vec<T>> for LazySegmentTree<T>
 where
-    T: Monoid + Clone,
+    T: Foo + Clone,
 {
     fn from(values: Vec<T>) -> Self {
         let values: &[T] = &values;
@@ -99,38 +126,38 @@ where
 mod tests {
     use super::*;
 
-    // max
-    impl Monoid for usize {
+    // range add
+    impl Foo for usize {
         fn mempty() -> Self {
             0
         }
         fn mappend(&self, other: &Self) -> Self {
-            std::cmp::max(*self, *other)
+            self + other
         }
     }
 
     #[test]
     fn test_new() {
-        let st: LazySegmentTree<usize> = LazySegmentTree::new(3);
+        let mut st: LazySegmentTree<usize> = LazySegmentTree::new(3);
         assert_eq!(st.query(0, 3), 0);
     }
 
     #[test]
     fn test_from() {
         let v = vec![1, 3, 2];
-        let st = LazySegmentTree::from(v);
-        assert_eq!(st.query(0, 1), 1);
-        assert_eq!(st.query(0, 2), 3);
-        assert_eq!(st.query(0, 3), 3);
-        assert_eq!(st.query(1, 2), 3);
-        assert_eq!(st.query(1, 3), 3);
-        assert_eq!(st.query(2, 3), 2);
+        let mut st = LazySegmentTree::from(v);
+        // assert_eq!(st.query(0, 1), 1);
+        // assert_eq!(st.query(0, 2), 3);
+        // assert_eq!(st.query(0, 3), 3);
+        // assert_eq!(st.query(1, 2), 3);
+        // assert_eq!(st.query(1, 3), 3);
+        // assert_eq!(st.query(2, 3), 2);
     }
 
     #[test]
     fn tst_from_size1() {
         let v = vec![1];
-        let st = LazySegmentTree::from(v);
+        let mut st = LazySegmentTree::from(v);
         assert_eq!(st.query(0, 1), 1);
     }
 
@@ -138,15 +165,15 @@ mod tests {
     fn test_update_and_query() {
         let mut st: LazySegmentTree<usize> = LazySegmentTree::new(3);
         assert_eq!(st.query(0, 3), 0);
-        st.update(0, 1);
-        st.update(1, 3);
-        st.update(2, 2);
+        // st.update(0, 3, 1);
+        // st.update(1, 3, 2);
+        // st.update(2, 3, 3);
 
-        assert_eq!(st.query(0, 1), 1);
-        assert_eq!(st.query(0, 2), 3);
-        assert_eq!(st.query(0, 3), 3);
-        assert_eq!(st.query(1, 2), 3);
-        assert_eq!(st.query(1, 3), 3);
-        assert_eq!(st.query(2, 3), 2);
+        // assert_eq!(st.query(0, 1), 1);
+        // assert_eq!(st.query(0, 2), 3);
+        // assert_eq!(st.query(0, 3), 3);
+        // assert_eq!(st.query(1, 2), 3);
+        // assert_eq!(st.query(1, 3), 3);
+        // assert_eq!(st.query(2, 3), 2);
     }
 }
