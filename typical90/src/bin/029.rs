8ea9a626d4cc9ea23319ce2c371fa77cdeef2e8b
emanon001 <emanon001@gmail.com>
2021-08-07 03:23:57 +0900

    update typical90/029

--- typical90/src/bin/029.rs
+++ typical90/src/bin/029.rs
@@ -8,38 +8,39 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
-pub trait Monoid {
-    fn mempty() -> Self;
-    fn mappend(&self, other: &Self) -> Self;
-}
-pub trait MapMonoid<F>: Monoid {
-    fn mmapping(&self, f: &F) -> Self;
-    fn mfempty() -> F;
-    fn mfappend(a: &F, b: &F) -> F;
-}
-pub struct LazySegmentTree<T, F>
+pub trait LazySegmentTreeItem<U>: Clone
 where
-    T: MapMonoid<F> + Clone,
-    F: Clone,
+    U: Clone,
+{
+    fn empty_item() -> Self;
+    fn append_item(&self, other: &Self) -> Self;
+    fn update_item(&self, v: &U) -> Self;
+    fn empty_update_value() -> U;
+    fn append_update_value(a: &U, b: &U) -> U;
+}
+pub struct LazySegmentTree<T, U>
+where
+    T: LazySegmentTreeItem<U>,
+    U: Clone,
 {
     size: usize,
     data: Vec<T>,
-    lazy: Vec<F>,
+    lazy: Vec<U>,
 }
-impl<T, F> LazySegmentTree<T, F>
+impl<T, U> LazySegmentTree<T, U>
 where
-    T: MapMonoid<F> + Clone,
-    F: Clone,
+    T: LazySegmentTreeItem<U> + Clone,
+    U: Clone,
 {
     pub fn new(size: usize) -> Self {
         let size = Self::normalize_data_size(size);
-        let data = vec![T::mempty(); size * 2 - 1];
-        let lazy = vec![T::mfempty(); size * 2 - 1];
+        let data = vec![T::empty_item(); size * 2 - 1];
+        let lazy = vec![T::empty_update_value(); size * 2 - 1];
         Self { size, data, lazy }
     }
     /// [a, b)
     /// 0-origin
-    pub fn update(&mut self, a: usize, b: usize, v: F) {
+    pub fn update(&mut self, a: usize, b: usize, v: U) {
         self.execute_update(a, b, &v, 0, 0, self.size);
     }
     /// [a, b)
@@ -54,45 +55,45 @@ where
         }
         n
     }
-    fn execute_update(&mut self, a: usize, b: usize, v: &F, i: usize, l: usize, r: usize) {
+    fn execute_update(&mut self, a: usize, b: usize, v: &U, i: usize, l: usize, r: usize) {
         self.eval(i);
         if r <= a || b <= l {
             return;
         }
         if a <= l && r <= b {
-            self.lazy[i] = T::mfappend(&self.lazy[i], &v);
+            self.lazy[i] = T::append_update_value(&self.lazy[i], &v);
             self.eval(i);
         } else {
             self.execute_update(a, b, v, i * 2 + 1, l, (l + r) / 2);
             self.execute_update(a, b, v, i * 2 + 2, (l + r) / 2, r);
-            self.data[i] = self.data[i * 2 + 1].mappend(&self.data[i * 2 + 2]);
+            self.data[i] = self.data[i * 2 + 1].append_item(&self.data[i * 2 + 2]);
         }
     }
     fn execute_query(&mut self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
         self.eval(i);
         if r <= a || b <= l {
-            return T::mempty();
+            return T::empty_item();
         }
         if a <= l && r <= b {
             self.data[i].clone()
         } else {
             let vl = self.execute_query(a, b, i * 2 + 1, l, (l + r) / 2);
             let vr = self.execute_query(a, b, i * 2 + 2, (l + r) / 2, r);
-            vl.mappend(&vr)
+            vl.append_item(&vr)
         }
     }
     fn eval(&mut self, i: usize) {
         if i < self.size - 1 {
-            self.lazy[i * 2 + 1] = T::mfappend(&self.lazy[i * 2 + 1], &self.lazy[i]);
-            self.lazy[i * 2 + 2] = T::mfappend(&self.lazy[i * 2 + 2], &self.lazy[i]);
+            self.lazy[i * 2 + 1] = T::append_update_value(&self.lazy[i * 2 + 1], &self.lazy[i]);
+            self.lazy[i * 2 + 2] = T::append_update_value(&self.lazy[i * 2 + 2], &self.lazy[i]);
         }
-        self.data[i] = self.data[i].mmapping(&self.lazy[i]);
-        self.lazy[i] = T::mfempty();
+        self.data[i] = self.data[i].update_item(&self.lazy[i]);
+        self.lazy[i] = T::empty_update_value();
     }
 }
 impl<T, F> From<&[T]> for LazySegmentTree<T, F>
 where
-    T: MapMonoid<F> + Clone,
+    T: LazySegmentTreeItem<F> + Clone,
     F: Clone,
 {
     fn from(values: &[T]) -> Self {
@@ -104,14 +105,14 @@ where
             return st;
         }
         for i in (0..=(st.size - 2)).rev() {
-            st.data[i] = st.data[i * 2 + 1].mappend(&st.data[i * 2 + 2]);
+            st.data[i] = st.data[i * 2 + 1].append_item(&st.data[i * 2 + 2]);
         }
         st
     }
 }
 impl<T, F> From<Vec<T>> for LazySegmentTree<T, F>
 where
-    T: MapMonoid<F> + Clone,
+    T: LazySegmentTreeItem<F> + Clone,
     F: Clone,
 {
     fn from(values: Vec<T>) -> Self {
@@ -132,23 +133,24 @@ macro_rules! chmax {
     };
 }
 
-impl Monoid for usize {
-    fn mempty() -> Self {
+impl LazySegmentTreeItem<usize> for usize {
+    fn empty_item() -> Self {
         0
     }
-    fn mappend(&self, other: &Self) -> Self {
+
+    fn append_item(&self, other: &Self) -> Self {
         *self.max(other)
     }
-}
 
-impl MapMonoid<usize> for usize {
-    fn mmapping(&self, f: &usize) -> usize {
-        *self.max(f)
+    fn update_item(&self, v: &usize) -> Self {
+        *self.max(v)
     }
-    fn mfempty() -> usize {
+
+    fn empty_update_value() -> usize {
         0
     }
-    fn mfappend(a: &usize, b: &usize) -> usize {
+
+    fn append_update_value(a: &usize, b: &usize) -> usize {
         *a.max(b)
     }
 }
