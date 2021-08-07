9a9d5308bc8285d309b8f9ab3e2a776fbc05563f
emanon001 <emanon001@gmail.com>
2021-08-07 23:43:24 +0900

    solve typical90/036

--- typical90/src/bin/036.rs
+++ typical90/src/bin/036.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
+        points: [(i64, i64); n],
+        queries: [Usize1; q]
     };
+
+    let points = points.into_iter().map(|(x, y)| (x - y, x + y)).collect::<Vec<_>>();
+    let mut xv = Vec::new();
+    let mut yv = Vec::new();
+    for &(x, y) in &points {
+        xv.push(x);
+        yv.push(y);
+    }
+    xv.sort();
+    yv.sort();
+    let x_min = xv[0];
+    let x_max = xv[n - 1];
+    let y_min = yv[0];
+    let y_max = yv[n - 1];
+    for i in queries {
+        let (x, y) = points[i];
+        let res = std::cmp::max(
+            std::cmp::max((x - x_min).abs(), (x - x_max).abs()),
+            std::cmp::max((y - y_min).abs(), (y - y_max).abs()),
+        );
+        println!("{}", res);
+    }
 }
 
 fn main() {
