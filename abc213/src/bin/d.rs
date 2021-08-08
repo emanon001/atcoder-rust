6d36311f20c7de8b36d101b2f8363b8067302d3d
emanon001 <emanon001@gmail.com>
2021-08-09 02:29:26 +0900

    solve abc213/d

--- abc213/src/bin/d.rs
+++ abc213/src/bin/d.rs
@@ -10,7 +10,36 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        edges: [(Usize1, Usize1); n - 1]
     };
+
+    let mut graph = vec![BTreeSet::new(); n];
+    for (u, v) in edges {
+        graph[u].insert(v);
+        graph[v].insert(u);
+    }
+    let mut first = vec![n; n];
+    let mut res = Vec::new();
+    let mut cur = 0;
+    loop {
+        res.push(cur + 1);
+        if graph[cur].is_empty() {
+            if cur == 0 {
+                break;
+            }
+            cur = first[cur];
+        } else {
+            let u = *graph[cur].iter().next().unwrap();
+            graph[cur].remove(&u);
+            graph[u].remove(&cur);
+            if first[u] == n {
+                first[u] = cur;
+            }
+            cur = u;
+        }
+    }
+    println!("{}", res.iter().join(" "));
 }
 
 fn main() {
