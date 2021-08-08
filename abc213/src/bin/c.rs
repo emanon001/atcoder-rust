195f841886ab598c812dc8dc876b726e329fa2ce
emanon001 <emanon001@gmail.com>
2021-08-09 02:29:15 +0900

    solve abc213/c

--- abc213/src/bin/c.rs
+++ abc213/src/bin/c.rs
@@ -8,9 +8,53 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        _h: usize, _w: usize, n: usize,
+        abv: [(usize, usize); n]
     };
+
+    let mut av = HashSet::new();
+    let mut bv = HashSet::new();
+    for &(a, b) in &abv {
+        av.insert(a);
+        bv.insert(b);
+    }
+    let mut av = av.into_iter().collect::<Vec<_>>();
+    let mut bv = bv.into_iter().collect::<Vec<_>>();
+    av.sort();
+    bv.sort();
+    for (a, b) in abv {
+        let a = bsearch(-1 as i64, av.len() as i64, |x| {
+            av[x as usize] <= a
+        }).unwrap_or(0) + 1;
+        let b = bsearch(-1 as i64, bv.len() as i64, |x| {
+            bv[x as usize] <= b
+        }).unwrap_or(0) + 1;
+        println!("{} {}", a, b);
+    }
 }
 
 fn main() {
