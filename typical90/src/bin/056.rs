557cc1c272fbb0bd01b48c03df1ec8ad40ae1a48
emanon001 <emanon001@gmail.com>
2021-08-10 05:58:44 +0900

    solve typical90/056

--- typical90/src/bin/056.rs
+++ typical90/src/bin/056.rs
@@ -10,7 +10,41 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, s: usize,
+        abv: [(usize, usize); n]
     };
+
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+    for i in 0..n {
+        let (a, b) = abv[i];
+        for j in 0..s {
+            if j + a <= s {
+                dp[i + 1][j + a] = dp[i + 1][j + a] || dp[i][j];
+            }
+            if j + b <= s {
+                dp[i + 1][j + b] = dp[i + 1][j + b] || dp[i][j];
+            }
+        }
+    }
+    if !dp[n][s] {
+        println!("Impossible");
+        return;
+    }
+
+    let mut res = VecDeque::new();
+    let mut cur = s;
+    for i in (0..n).rev() {
+        let (a, b) = abv[i];
+        if a <= cur && dp[i][cur - a] {
+            res.push_front('A');
+            cur = cur - a;
+        } else {
+            res.push_front('B');
+            cur = cur - b;
+        }
+    }
+    println!("{}", res.iter().join(""));
 }
 
 fn main() {
