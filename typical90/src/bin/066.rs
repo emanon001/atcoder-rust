697ccfcfcb17948213d3b7eed2f088ed3407d9ca
emanon001 <emanon001@gmail.com>
2021-08-14 06:35:50 +0900

    solve typical90/066

--- typical90/src/bin/066.rs
+++ typical90/src/bin/066.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        lrv: [(u64, u64); n]
     };
+
+    let mut res = 0_f64;
+    for i in 0..n {
+        let (l1, r1) = lrv[i];
+        for j in i + 1..n {
+            let (l2, r2) = lrv[j];
+            let all_c = (r1 - l1 + 1) * (r2 - l2 + 1);
+            let mut c = 0_u64;
+            for v1 in l1..=r1 {
+                for v2 in l2..=r2 {
+                    if v1 > v2 {
+                        c += 1;
+                    }
+                }
+            }
+            res += c as f64 / all_c as f64;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
