52adbb0a722e6b1fe4b34045c5c7acd8001f9039
emanon001 <emanon001@gmail.com>
2021-08-14 23:25:30 +0900

    solve abc214/b

--- abc214/src/bin/b.rs
+++ abc214/src/bin/b.rs
@@ -10,7 +10,20 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: u64, t: u64
     };
+
+    let mut res = 0;
+    for a in 0..=100 {
+        for b in 0..=100 {
+            for c in 0..=100 {
+                if a + b + c <= s && a * b * c <= t {
+                    res += 1;
+                }
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
