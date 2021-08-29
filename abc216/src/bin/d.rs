f6f587aa24dcfea764ca257e8aa1859bdef2aa17
emanon001 <emanon001@gmail.com>
2021-08-29 23:58:17 +0900

    solve abc216/d

--- abc216/src/bin/d.rs
+++ abc216/src/bin/d.rs
@@ -10,7 +10,52 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
     };
+
+    let mut kv = vec![vec![]; m];
+    for i in 0..m {
+        input! {
+            k: usize,
+            v: [i64; k]
+        };
+        kv[i] = v;
+    }
+    let mut ball_set = HashSet::new();
+    let mut v_to_i = vec![n; n + 1];
+    let mut pos = vec![0; m];
+    let mut next = (0..m).collect::<Vec<_>>();
+    let mut pop_count = 0;
+    while pop_count < 2 * n {
+        let mut new_next = vec![];
+        let mut poped = false;
+        for &i in &next {
+            let v = kv[i][pos[i]];
+            if ball_set.contains(&v) {
+                poped = true;
+                pos[i] += 1;
+                if pos[i] < kv[i].len() {
+                    new_next.push(i);
+                }
+                let j = v_to_i[v as usize];
+                pos[j] += 1;
+                if pos[j] < kv[j].len() {
+                    new_next.push(j);
+                }
+                pop_count += 2;
+            } else {
+                let v = kv[i][pos[i]];
+                v_to_i[v as usize] = i;
+                ball_set.insert(v);
+            }
+        }
+        if !poped {
+            println!("No");
+            return;
+        }
+        next = new_next;
+    }
+    println!("Yes");
 }
 
 fn main() {
