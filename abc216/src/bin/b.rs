be82eb3c6824be7ecdfcb7ac7e8d8934c54021b9
emanon001 <emanon001@gmail.com>
2021-08-29 22:45:18 +0900

    solve abc216/b

--- abc216/src/bin/b.rs
+++ abc216/src/bin/b.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        stv: [(String, String); n]
     };
+
+    for i in 0..n {
+        for j in i + 1..n {
+            if stv[i].0 == stv[j].0 && stv[i].1 == stv[j].1 {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
 
 fn main() {
