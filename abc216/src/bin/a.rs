69e92e324f994a9c9333f510ea5af43ba6613178
emanon001 <emanon001@gmail.com>
2021-08-29 22:45:14 +0900

    solve abc216/a

--- abc216/src/bin/a.rs
+++ abc216/src/bin/a.rs
@@ -10,7 +10,20 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: String
     };
+
+    let mut v = s.split(".");
+    let x: i32 = v.next().unwrap().to_string().parse().unwrap();
+    let y: i32 = v.next().unwrap().to_string().parse().unwrap();
+    let res = if 0 <= y && y <= 2 {
+        format!("{}-", x)
+    } else if 3 <= y && y <= 6 {
+        format!("{}", x)
+    } else {
+        format!("{}+", x)
+    };
+    println!("{}", res);
 }
 
 fn main() {
