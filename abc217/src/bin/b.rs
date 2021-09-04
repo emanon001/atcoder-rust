556a119f2291d5689f8f7a767e99aa4408751080
emanon001 <emanon001@gmail.com>
2021-09-04 23:08:20 +0900

    solve abc217/b

--- abc217/src/bin/b.rs
+++ abc217/src/bin/b.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s1: String,
+        s2: String,
+        s3: String
     };
+
+    let mut set = HashSet::new();
+    set.insert("ABC".to_string());
+    set.insert("ARC".to_string());
+    set.insert("AGC".to_string());
+    set.insert("AHC".to_string());
+    set.remove(&s1);
+    set.remove(&s2);
+    set.remove(&s3);
+    for res in set {
+        println!("{}", res);
+    }
 }
 
 fn main() {
