34fcfff9c461a7483dee4460d756d4f805b5d454
emanon001 <emanon001@gmail.com>
2021-09-04 23:09:00 +0900

    solve abc217/e

--- abc217/src/bin/e.rs
+++ abc217/src/bin/e.rs
@@ -10,7 +10,41 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+
+    let mut front_map: BTreeMap<usize, usize> = BTreeMap::new();
+    let mut back = VecDeque::new();
+    for _ in 0..q {
+        input! { kind: usize };
+        match kind {
+            1 => {
+                input! { x: usize };
+                back.push_back(x);
+            }
+            2 => {
+                if !front_map.is_empty() {
+                    let (&v, &c) = front_map.iter().next().unwrap();
+                    println!("{}", v);
+                    let next_c = c - 1;
+                    front_map.insert(v, next_c);
+                    if next_c == 0 {
+                        front_map.remove(&v);
+                    }
+                } else {
+                    let v = back.pop_front().unwrap();
+                    println!("{}", v);
+                }
+            }
+            3 => {
+                for &v in &back {
+                    *front_map.entry(v).or_insert(0) += 1;
+                }
+                back = VecDeque::new();
+            }
+            _ => unreachable!()
+        }
+    }
 }
 
 fn main() {
