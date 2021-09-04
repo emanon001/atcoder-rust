813f1da14521b2def7b6267f83d893dbad14533b
emanon001 <emanon001@gmail.com>
2021-09-04 23:08:35 +0900

    solve abc217/d

--- abc217/src/bin/d.rs
+++ abc217/src/bin/d.rs
@@ -10,7 +10,25 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        l: usize, q: usize,
+        cxv: [(usize, usize); q]
     };
+
+    let mut set: BTreeSet<usize> = BTreeSet::new();
+    for (c, x) in cxv {
+        match c {
+            1 => {
+                set.insert(x);
+            }
+            2 => {
+                let a = set.range(..x).next_back().unwrap_or(&0);
+                let b = set.range(x..).next().unwrap_or(&l);
+                let res = b - a;
+                println!("{}", res);
+            }
+            _ => unreachable!()
+        }
+    }
 }
 
 fn main() {
