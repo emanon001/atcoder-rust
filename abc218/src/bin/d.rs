5481ba7e3126584cad588e981610956a67b394f8
emanon001 <emanon001@gmail.com>
2021-09-11 22:41:56 +0900

    solve abc218/d

--- abc218/src/bin/d.rs
+++ abc218/src/bin/d.rs
@@ -10,7 +10,30 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        points: [(usize, usize); n]
     };
+
+    let mut set = HashSet::new();
+    for &p in &points {
+        set.insert(p);
+    }
+    let mut res = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            let p1 = points[i];
+            let p2 = points[j];
+            if p1.0 == p2.0 || p1.1 == p2.1 {
+                continue;
+            }
+            let p3 = (p1.0, p2.1);
+            let p4 = (p2.0, p1.1);
+            if set.contains(&p3) && set.contains(&p4) {
+                res += 1;
+            }
+        }
+    }
+    println!("{}", res / 2);
 }
 
 fn main() {
