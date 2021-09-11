faccd13851791c0893fbaed4fc13fb9cd089185b
emanon001 <emanon001@gmail.com>
2021-09-11 22:42:30 +0900

    wip abc218/f

--- abc218/src/bin/e.rs
+++ abc218/src/bin/e.rs
@@ -8,9 +8,164 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Clone)]
+pub struct Graph<Cost>
+where
+    Cost: PartialOrd + Ord + Copy + num::traits::NumAssign,
+{
+    graph: Vec<Vec<(usize, Cost)>>,
+    vc: usize,
+    inf: Cost,
+}
+#[derive(Clone)]
+pub struct Edge<Cost>
+where
+    Cost: PartialOrd + Ord + Copy + num::traits::NumAssign,
+{
+    from: usize,
+    to: usize,
+    cost: Cost,
+}
+impl<Cost> From<(usize, usize, Cost)> for Edge<Cost>
+where
+    Cost: PartialOrd + Ord + Copy + num::traits::NumAssign,
+{
+    fn from(e: (usize, usize, Cost)) -> Edge<Cost> {
+        Edge {
+            from: e.0,
+            to: e.1,
+            cost: e.2,
+        }
+    }
+}
+impl<Cost> From<(usize, usize)> for Edge<Cost>
+where
+    Cost: PartialOrd + Ord + Copy + num::traits::NumAssign,
+{
+    fn from(e: (usize, usize)) -> Edge<Cost> {
+        Edge {
+            from: e.0,
+            to: e.1,
+            cost: Cost::one(),
+        }
+    }
+}
+impl<Cost> Graph<Cost>
+where
+    Cost: PartialOrd + Ord + Copy + num::traits::NumAssign,
+{
+    pub fn new_undirected(edges: Vec<impl Into<Edge<Cost>>>, vc: usize, inf: Cost) -> Self {
+        let mut graph = vec![Vec::new(); vc];
+        for e in edges {
+            let e = e.into();
+            graph[e.from].push((e.to, e.cost));
+            graph[e.to].push((e.from, e.cost));
+        }
+        Self { graph, vc, inf }
+    }
+    pub fn new_directed(edges: Vec<impl Into<Edge<Cost>>>, vc: usize, inf: Cost) -> Self {
+        let mut graph = vec![Vec::new(); vc];
+        for e in edges {
+            let e = e.into();
+            graph[e.from].push((e.to, e.cost));
+        }
+        Self { graph, vc, inf }
+    }
+    pub fn new_empty(vc: usize, inf: Cost) -> Self {
+        let graph = vec![Vec::new(); vc];
+        Self { graph, vc, inf }
+    }
+    pub fn add_undirected_edge(&mut self, e: impl Into<Edge<Cost>>) {
+        let e = e.into();
+        self.graph[e.from].push((e.to, e.cost));
+        self.graph[e.to].push((e.from, e.cost));
+    }
+    pub fn add_directed_edge(&mut self, e: impl Into<Edge<Cost>>) {
+        let e = e.into();
+        self.graph[e.from].push((e.to, e.cost));
+    }
+    pub fn rev(&self) -> Graph<Cost> {
+        let mut edges: Vec<Edge<Cost>> = Vec::new();
+        for u in 0..self.vc {
+            for &(v, w) in &self.graph[u] {
+                edges.push((v, u, w).into());
+            }
+        }
+        Self::new_directed(edges, self.vc, self.inf)
+    }
+    pub fn reachable_vertexes(&self, s: usize) -> std::collections::HashSet<usize> {
+        let mut visited = std::collections::HashSet::new();
+        let mut queue = std::collections::VecDeque::new();
+        visited.insert(s);
+        queue.push_back(s);
+        while let Some(u) = queue.pop_front() {
+            for &(v, _) in &self.graph[u] {
+                if visited.contains(&v) {
+                    continue;
+                }
+                visited.insert(v);
+                queue.push_back(v);
+            }
+        }
+        visited
+    }
+    pub fn edges<'a>(&self, u: usize) -> &Vec<(usize, Cost)> {
+        &self.graph[u]
+    }
+    pub fn vertex_count(&self) -> usize {
+        self.vc
+    }
+}
+
+impl<Cost> Graph<Cost>
+where
+    Cost: PartialOrd + Ord + Copy + num::traits::NumAssign,
+{
+    pub fn prim(&self) -> (Cost, Cost) {
+        let mut used = std::collections::HashSet::new();
+        let mut heap = std::collections::BinaryHeap::new();
+        let mut res1 = Cost::zero();
+        let mut res2 = Cost::zero();
+        heap.push(std::cmp::Reverse((Cost::zero(), 0)));
+        while let Some(std::cmp::Reverse((weight, u))) = heap.pop() {
+            if used.contains(&u) {
+                continue;
+            }
+            used.insert(u);
+            if weight >= Cost::zero() {
+                res1 += weight;
+            } else {
+                res2 += weight;
+            }
+            for &(v, w) in &self.graph[u] {
+                if used.contains(&v) {
+                    continue;
+                }
+                heap.push(std::cmp::Reverse((w, v)));
+            }
+        }
+        (res1, res2)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1, i64); m]
     };
+
+    let mut add_sum = 0_i64;
+    for e in &edges {
+        if e.2 >= 0 {
+            add_sum += e.2;
+        }
+    }
+
+    let inf = 1_i64 << 60;
+    let graph = Graph::new_undirected(edges, n, inf);
+    let (add, _) = graph.prim();
+    let res = add_sum - add;
+    println!("{}", res)
 }
 
 fn main() {
--- abc218/src/bin/f.rs
+++ abc218/src/bin/f.rs
@@ -8,9 +8,78 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Clone)]
+pub struct Graph {
+    graph: Vec<HashSet<(usize, i64)>>,
+    vc: usize,
+    inf: i64,
+}
+#[derive(Clone)]
+pub struct Edge {
+    from: usize,
+    to: usize,
+    cost: i64,
+}
+impl From<(usize, usize)> for Edge {
+    fn from(e: (usize, usize)) -> Edge {
+        Edge {
+            from: e.0,
+            to: e.1,
+            cost: 0,
+        }
+    }
+}
+impl Graph {
+    pub fn new_directed(edges: Vec<impl Into<Edge>>, vc: usize, inf: i64) -> Self {
+        let mut graph = vec![HashSet::new(); vc];
+        for e in edges {
+            let e = e.into();
+            graph[e.from].insert((e.to, e.cost));
+        }
+        Self { graph, vc, inf }
+    }
+    pub fn add_directed_edge(&mut self, e: impl Into<Edge>) {
+        let e = e.into();
+        self.graph[e.from].insert((e.to, e.cost));
+    }
+    pub fn remove_directed_edge(&mut self, e: impl Into<Edge>) {
+        let e = e.into();
+        self.graph[e.from].remove(&(e.to, e.cost));
+    }
+}
+
+impl Graph {
+    pub fn shortest_path_1(&self, start: usize) -> Vec<Option<i64>> {
+        let mut cost_list = vec![None; self.vc];
+        let mut que = std::collections::VecDeque::new();
+        cost_list[start] = Some(0);
+        que.push_back(start);
+        while let Some(u) = que.pop_front() {
+            for (v, _) in &self.graph[u] {
+                if cost_list[*v].is_some() {
+                    continue;
+                }
+                let new_cost = cost_list[u].unwrap() + 1;
+                cost_list[*v] = Some(new_cost);
+                que.push_back(*v);
+            }
+        }
+        cost_list
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+    let mut graph = Graph::new_directed(edges.clone(), n, 1_i64 << 60);
+    for e in edges {
+        graph.remove_directed_edge(e);
+        let res = graph.shortest_path_1(0)[n - 1].unwrap_or(-1);
+        println!("{}", res);
+        graph.add_directed_edge(e);
+    }
 }
 
 fn main() {
