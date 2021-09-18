369b6666f5968ee70620e88b284dd7d2f21dde1c
emanon001 <emanon001@gmail.com>
2021-09-18 22:40:52 +0900

    solve abc219/a

--- abc219/src/bin/a.rs
+++ abc219/src/bin/a.rs
@@ -10,6 +10,17 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        x: usize
+    };
+
+    if x < 40 {
+        println!("{}", 40 - x);
+    } else if x < 70 {
+        println!("{}", 70 - x);
+    } else if x < 90 {
+        println!("{}", 90 - x);
+    } else {
+        println!("expert");
     };
 }
 
