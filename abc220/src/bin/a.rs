7cdb9470831ab48c9c277836278193d777efeaf0
emanon001 <emanon001@gmail.com>
2021-10-02 00:50:00 +0900

    solve abc220/a

--- abc220/src/bin/a.rs
+++ abc220/src/bin/a.rs
@@ -10,7 +10,16 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: usize, b: usize, c: usize,
     };
+
+    for x in a..=b {
+        if x % c == 0 {
+            println!("{}", x);
+            return;
+        }
+    }
+    println!("-1");
 }
 
 fn main() {
