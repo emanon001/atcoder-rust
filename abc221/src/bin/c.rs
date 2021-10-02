fc65266e62cc0ebe1a0da3cb21c3394dbb3b030f
emanon001 <emanon001@gmail.com>
2021-10-03 00:32:09 +0900

    solve abc221/c

--- abc221/src/bin/c.rs
+++ abc221/src/bin/c.rs
@@ -8,9 +8,39 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: Chars
     };
+
+    let len = n.len();
+    let mut res = 0_u64;
+    for nv in n.into_iter().permutations(len) {
+        for i in 1..len {
+            let s1 = nv[..i].iter().collect::<String>();
+            let s2 = nv[i..].iter().collect::<String>();
+            if s1.starts_with("0") || s2.starts_with("0") {
+                continue;
+            }
+            let a: u64 = s1.parse().unwrap();
+            let b: u64 = s2.parse().unwrap();
+            let c = a * b;
+            chmax!(res, c);
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
