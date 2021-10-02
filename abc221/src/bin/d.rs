4f50b8511eed8174bf7c0a0f05c81a4af524630e
emanon001 <emanon001@gmail.com>
2021-10-03 00:32:14 +0900

    solve abc221/d

--- abc221/src/bin/d.rs
+++ abc221/src/bin/d.rs
@@ -8,9 +8,60 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn compress_zahyo<T: Ord + std::hash::Hash>(
+    zahyo: &[T],
+) -> std::collections::HashMap<&T, usize> {
+    let mut set = std::collections::BTreeSet::new();
+    for x in zahyo {
+        set.insert(x);
+    }
+    let mut map = std::collections::HashMap::new();
+    for (i, x) in set.into_iter().enumerate() {
+        map.insert(x, i);
+    }
+    map
+}
+
+#[derive(Debug)]
+enum Event {
+    Begin(usize),
+    End(usize),
+}
+
 fn solve() {
     input! {
+        n: usize,
+        abv: [(usize, usize); n]
     };
+
+    let mut events = Vec::new();
+    for (a, b) in abv {
+        events.push(Event::Begin(a));
+        events.push(Event::End(a + b));
+    }
+    events.sort_by_key(|e| match e {
+        &Event::Begin(x) => x,
+        &Event::End(x) => x,
+    });
+
+    let mut res = vec![0_usize; n + 1];
+    let mut cur = 0_usize;
+    let mut k = 0_usize;
+    for e in events {
+        match e {
+            Event::Begin(x) => {
+                res[k] += x - cur;
+                cur = x;
+                k += 1;
+            }
+            Event::End(x) => {
+                res[k] += x - cur;
+                cur = x;
+                k -= 1;
+            }
+        }
+    }
+    println!("{}", res[1..].into_iter().join(" "));
 }
 
 fn main() {
