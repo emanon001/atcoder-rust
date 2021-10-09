8b67492033ad0351a601985905c6829ec11b43fa
emanon001 <emanon001@gmail.com>
2021-10-09 23:04:33 +0900

    solve abc222/d

--- abc222/src/bin/d.rs
+++ abc222/src/bin/d.rs
@@ -8,9 +8,220 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct ModInt(u32);
+impl ModInt {
+    pub const MOD: u32 = 998244353;
+    pub fn inv(self) -> Self {
+        if self.0 == 0 {
+            panic!();
+        }
+        self.pow(Self::MOD - 2)
+    }
+    pub fn pow<T: num::Unsigned + num::PrimInt>(self, e: T) -> Self {
+        if e.is_zero() {
+            return Self::new(1);
+        }
+        let mut res = self.pow(e >> 1);
+        res *= res;
+        if e & T::one() == T::one() {
+            res *= self;
+        }
+        res
+    }
+    fn new(n: i64) -> Self {
+        let m = Self::MOD as i64;
+        let mut n = n % m;
+        if n.is_negative() {
+            n += m;
+        }
+        Self(n as u32)
+    }
+}
+macro_rules! impl_from {
+    ($ T : ty ) => {
+        impl From<$T> for ModInt {
+            fn from(n: $T) -> Self {
+                use std::convert::TryFrom;
+                Self::new(i64::try_from(n).unwrap())
+            }
+        }
+    };
+}
+impl_from!(i32);
+impl_from!(i64);
+impl_from!(isize);
+impl_from!(u32);
+impl_from!(u64);
+impl_from!(usize);
+macro_rules! impl_into {
+    ($ T : ty ) => {
+        impl Into<$T> for ModInt {
+            fn into(self) -> $T {
+                self.0 as $T
+            }
+        }
+    };
+}
+impl_into!(i32);
+impl_into!(i64);
+impl_into!(isize);
+impl_into!(u32);
+impl_into!(u64);
+impl_into!(usize);
+impl std::fmt::Display for ModInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl std::ops::Add for ModInt {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Self::new((self.0 + rhs.0) as i64)
+    }
+}
+impl std::ops::AddAssign for ModInt {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+impl std::ops::Div for ModInt {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self {
+        self * rhs.inv()
+    }
+}
+impl std::ops::DivAssign for ModInt {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+impl std::ops::Mul for ModInt {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        Self::new((self.0 as i64) * (rhs.0 as i64))
+    }
+}
+impl std::ops::MulAssign for ModInt {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+impl std::ops::Sub for ModInt {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        Self::new((self.0 as i64) - (rhs.0 as i64))
+    }
+}
+impl std::ops::SubAssign for ModInt {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+impl num::Zero for ModInt {
+    fn zero() -> Self {
+        Self::new(0)
+    }
+    fn is_zero(&self) -> bool {
+        *self == Self::zero()
+    }
+}
+impl num::One for ModInt {
+    fn one() -> Self {
+        Self::new(1)
+    }
+    fn is_one(&self) -> bool {
+        *self == Self::one()
+    }
+}
+
+/// `T` is numeric only
+pub struct Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    n: usize,
+    data: Vec<T>,
+}
+/// 0-origin
+/// [0, n)
+impl<T> Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    pub fn new(n: usize) -> Self {
+        Self {
+            n,
+            data: vec![T::zero(); n + 1],
+        }
+    }
+    /// 0-origin
+    pub fn add(&mut self, i: usize, x: T) {
+        if i >= self.n {
+            panic!();
+        }
+        let mut i = i + 1;
+        while i <= self.n {
+            self.data[i] += x.clone();
+            i += ((i as isize) & -(i as isize)) as usize;
+        }
+    }
+    /// [0, i)
+    pub fn sum(&self, i: usize) -> T {
+        if i > self.n {
+            panic!();
+        }
+        let mut i = i;
+        let mut res = T::zero();
+        while i > 0 {
+            res += self.data[i].clone();
+            i -= ((i as isize) & -(i as isize)) as usize;
+        }
+        res
+    }
+    /// [i, j)
+    pub fn range_sum(&self, i: usize, j: usize) -> T {
+        if i > self.n || j > self.n {
+            panic!();
+        }
+        if i >= j {
+            return T::zero();
+        }
+        self.sum(j) - self.sum(i)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n],
+        bv: [usize; n],
     };
+
+    let mut dp = vec![vec![ModInt::zero(); 3000 + 1]; n + 1];
+    let mut bit = Bit::new(3000 + 1);
+    for i in 0..n {
+        let a = av[i];
+        let b = bv[i];
+        let min = a.min(b);
+        let max = a.max(b);
+        for j in min..=max {
+            let new_v = dp[i + 1][j]
+                + if i == 0 {
+                    ModInt::one()
+                } else {
+                    bit.sum(j + 1)
+                };
+            dp[i + 1][j] = new_v;
+        }
+        let mut new_bit = Bit::new(3000 + 1);
+        for j in 0..3000 + 1 {
+            new_bit.add(j, dp[i + 1][j]);
+        }
+        bit = new_bit;
+    }
+    let res = bit.sum(3000 + 1);
+    println!("{}", res);
 }
 
 fn main() {
