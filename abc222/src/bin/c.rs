f19e33b7f761801de00a3f865e61a5a5d27fa77e
emanon001 <emanon001@gmail.com>
2021-10-09 23:04:28 +0900

    solve abc222/c

--- abc222/src/bin/c.rs
+++ abc222/src/bin/c.rs
@@ -10,7 +10,58 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        table: [Chars; 2 * n]
     };
+
+    let mut v = (0..n * 2).map(|i| (0, i)).collect::<Vec<_>>();
+    for j in 0..m {
+        let mut new_v = Vec::new();
+        for p in 0..n {
+            let (aw, ai) = v[p * 2];
+            let (bw, bi) = v[p * 2 + 1];
+            match (table[ai][j], table[bi][j]) {
+                ('G', 'C') => {
+                    new_v.push((aw + 1, ai));
+                    new_v.push((bw, bi));
+                }
+                ('G', 'P') => {
+                    new_v.push((aw, ai));
+                    new_v.push((bw + 1, bi));
+                }
+                ('C', 'P') => {
+                    new_v.push((aw + 1, ai));
+                    new_v.push((bw, bi));
+                }
+                ('C', 'G') => {
+                    new_v.push((aw, ai));
+                    new_v.push((bw + 1, bi));
+                }
+                ('P', 'G') => {
+                    new_v.push((aw + 1, ai));
+                    new_v.push((bw, bi));
+                }
+                ('P', 'C') => {
+                    new_v.push((aw, ai));
+                    new_v.push((bw + 1, bi));
+                }
+                _ => {
+                    new_v.push((aw, ai));
+                    new_v.push((bw, bi));
+                }
+            }
+        }
+        v = new_v;
+        v.sort_by_key(|&(w, i)| (-w, i));
+    }
+    let mut res = vec![0; n * 2];
+    for j in 0..n * 2 {
+        let (_, i) = v[j];
+        res[j] = i + 1;
+    }
+    for x in res {
+        println!("{}", x);
+    }
 }
 
 fn main() {
