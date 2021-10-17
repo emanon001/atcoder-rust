6b9b458a506883e1f6772dcb4bd144c2d685311d
emanon001 <emanon001@gmail.com>
2021-10-17 23:12:57 +0900

    solve abc223/d

--- abc223/src/bin/d.rs
+++ abc223/src/bin/d.rs
@@ -10,7 +10,37 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    let mut indegree = vec![0; n];
+    for (u, v) in edges {
+        indegree[v] += 1;
+        graph[u].push(v);
+    }
+    let mut heap = BinaryHeap::new();
+    for i in 0..n {
+        if indegree[i] == 0 {
+            heap.push(std::cmp::Reverse(i));
+        }
+    }
+    let mut res = Vec::new();
+    while let Some(std::cmp::Reverse(u)) = heap.pop() {
+        for &v in &graph[u] {
+            indegree[v] -= 1;
+            if indegree[v] == 0 {
+                heap.push(std::cmp::Reverse(v));
+            }
+        }
+        res.push(u + 1);
+    }
+    if res.len() < n {
+        println!("-1");
+        return;
+    }
+    println!("{}", res.into_iter().join(" "));
 }
 
 fn main() {
