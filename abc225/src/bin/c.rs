67383db56302d50a12dcf4a718321f0e85456e1f
emanon001 <emanon001@gmail.com>
2021-10-30 23:15:10 +0900

    solve abc225/c

--- abc225/src/bin/c.rs
+++ abc225/src/bin/c.rs
@@ -10,7 +10,43 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        grid: [[i64; m]; n]
     };
+
+    // 枠外
+    for i in 0..n {
+        let mut pos = grid[i][0] % 7;
+        if pos == 0 {
+            pos = 7;
+        }
+        if pos + m as i64 - 1 > 7 {
+            println!("No");
+            return;
+        }
+    }
+
+    // 先頭の位置
+    let mut pos_set = HashSet::new();
+    for i in 0..n {
+        let pos = grid[i][0] % 7;
+        pos_set.insert(pos);
+    }
+    if pos_set.len() > 1 {
+        println!("No");
+        return;
+    }
+
+    // 連続しているか
+    for i in 0..n {
+        for j in 1..m {
+            if grid[i][j] != grid[i][j - 1] + 1 {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
 }
 
 fn main() {
