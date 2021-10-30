b522ff6e2df5ba3e2199ac2c6495079e9a102428
emanon001 <emanon001@gmail.com>
2021-10-30 23:15:15 +0900

    solve abc225/d

--- abc225/src/bin/d.rs
+++ abc225/src/bin/d.rs
@@ -10,7 +10,52 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
     };
+
+    let mut next_pos = vec![-1; n];
+    let mut prev_pos = vec![-1; n];
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    x: Usize1,
+                    y: Usize1,
+                };
+                next_pos[x] = y as isize;
+                prev_pos[y] = x as isize;
+            }
+            2 => {
+                input! {
+                    x: Usize1,
+                    y: Usize1,
+                };
+                next_pos[x] = -1;
+                prev_pos[y] = -1;
+            }
+            3 => {
+                input! {
+                    x: Usize1,
+                };
+                let mut pos = x;
+                while prev_pos[pos] != -1 {
+                    pos = prev_pos[pos] as usize;
+                }
+                let mut res = Vec::new();
+                res.push(pos + 1);
+                while next_pos[pos] != -1 {
+                    pos = next_pos[pos] as usize;
+                    res.push(pos + 1);
+                }
+                let len = res.len();
+                println!("{} {}", len, res.into_iter().join(" "));
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
