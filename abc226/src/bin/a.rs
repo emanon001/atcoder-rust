4ad885cd3d36889afc05d678cba030031a3d4f59
emanon001 <emanon001@gmail.com>
2021-11-07 22:54:42 +0900

    solve abc226/a

--- abc226/src/bin/a.rs
+++ abc226/src/bin/a.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        x: String
     };
+
+    let v = x.split(".").collect::<Vec<_>>();
+    let a = v[0];
+    let b = v[1]
+        .chars()
+        .into_iter()
+        .next()
+        .unwrap()
+        .to_digit(10)
+        .unwrap();
+    if b >= 5 {
+        let res = a.parse::<usize>().unwrap() + 1;
+        println!("{}", res);
+    } else {
+        println!("{}", a);
+    }
 }
 
 fn main() {
