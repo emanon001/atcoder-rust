0dce8583496e8a960077b675630a54ade6d48a01
emanon001 <emanon001@gmail.com>
2021-11-07 22:55:11 +0900

    solve abc226/d

--- abc226/src/bin/d.rs
+++ abc226/src/bin/d.rs
@@ -10,7 +10,34 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        points: [(i64, i64); n]
     };
+
+    let mut set = HashSet::new();
+    for i in 0..n {
+        for j in i + 1..n {
+            let (x1, y1) = points[i];
+            let (x2, y2) = points[j];
+            let dx = x2 - x1;
+            let dy = y2 - y1;
+            let gcd = dx.gcd(&dy);
+            let d = (dx / gcd, dy / gcd);
+            set.insert(d);
+        }
+    }
+    for i in (0..n).rev() {
+        for j in (0..i).rev() {
+            let (x1, y1) = points[i];
+            let (x2, y2) = points[j];
+            let dx = x2 - x1;
+            let dy = y2 - y1;
+            let gcd = dx.gcd(&dy);
+            let d = (dx / gcd, dy / gcd);
+            set.insert(d);
+        }
+    }
+    println!("{}", set.len());
 }
 
 fn main() {
