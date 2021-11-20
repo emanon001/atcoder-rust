754b3e9527242ab6727e89fcc059d7eb1301b1c4
emanon001 <emanon001@gmail.com>
2021-11-20 22:52:53 +0900

    solve abc228/d

--- abc228/src/bin/d.rs
+++ abc228/src/bin/d.rs
@@ -10,7 +10,34 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
+        mut queries: [(usize, i64); q]
     };
+    let n = 2.pow(20);
+    let mut v = vec![-1; n];
+    let mut set = (0..n).collect::<BTreeSet<_>>();
+    for (t, x) in queries {
+        match t {
+            1 => {
+                let pos = x as usize % n;
+                if set.range(pos..).next().is_some() {
+                    let pos = *set.range(pos..).next().unwrap();
+                    v[pos] = x;
+                    set.remove(&pos);
+                } else if set.range(0..pos).next().is_some() {
+                    let pos = *set.range(0..pos).next().unwrap();
+                    v[pos] = x;
+                    set.remove(&pos);
+                }
+            }
+            2 => {
+                let pos = x as usize % n;
+                let res = v[pos];
+                println!("{}", res);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
