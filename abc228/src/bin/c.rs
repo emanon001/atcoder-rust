fe7ae3327568212de0abdd8dc312c9234e158ecf
emanon001 <emanon001@gmail.com>
2021-11-20 22:52:48 +0900

    solve abc228/c

--- abc228/src/bin/c.rs
+++ abc228/src/bin/c.rs
@@ -10,7 +10,25 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: i64,
+        pv: [[usize; 3]; n]
     };
+
+    let mut points = vec![0_i64; 900 + 1];
+    for i in 0..n {
+        let p = pv[i][0] + pv[i][1] + pv[i][2];
+        points[p] += 1;
+    }
+    for i in 0..n {
+        let p = pv[i][0] + pv[i][1] + pv[i][2];
+        let mut sum = 0;
+        for j in (p + 300 + 1)..=900 {
+            sum += points[j];
+        }
+        sum += 1;
+        let res = if sum <= k { "Yes" } else { "No" };
+        println!("{}", res);
+    }
 }
 
 fn main() {
