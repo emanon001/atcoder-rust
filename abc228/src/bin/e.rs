b373c70452598ec528e3ca25dc7114d9d58ec813
emanon001 <emanon001@gmail.com>
2021-11-29 00:43:50 +0900

    solve abc228/e

--- abc228/src/bin/e.rs
+++ abc228/src/bin/e.rs
@@ -8,9 +8,33 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn mod_pow(n: u64, e: u64, _mod: u64) -> u64 {
+    if e == 0 {
+        return 1;
+    }
+    let n = n % _mod;
+    let mut res = mod_pow(n, e >> 1, _mod);
+    res = (res * res) % _mod;
+    if e & 1 == 1 {
+        res = (res * n) % _mod;
+    }
+    res
+}
+
 fn solve() {
     input! {
+        n: u64, k: u64, m: u64,
     };
+
+    let _mod = 998244353_u64;
+    if m % _mod == 0 {
+        println!("0");
+        return;
+    }
+
+    let a = mod_pow(k, n, _mod - 1);
+    let b = mod_pow(m, a, _mod);
+    println!("{}", b);
 }
 
 fn main() {
