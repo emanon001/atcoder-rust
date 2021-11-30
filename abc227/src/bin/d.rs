34325cba4ac02a743502471074abac50e7f531bb
emanon001 <emanon001@gmail.com>
2021-11-30 23:54:50 +0900

    solve abc227/d

--- abc227/src/bin/d.rs
+++ abc227/src/bin/d.rs
@@ -8,9 +8,44 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, k: i128,
+        av: [i128; n]
     };
+
+    let res = bsearch(-1, 10_i64.pow(18), |x| {
+        let x = x as i128;
+        let mut sum = 0_i128;
+        for i in 0..n {
+            sum += av[i].min(x);
+        }
+        sum >= x * k
+    })
+    .unwrap_or(0);
+    println!("{}", res);
 }
 
 fn main() {
