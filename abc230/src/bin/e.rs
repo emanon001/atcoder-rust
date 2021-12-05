67c17b786ee84ce82818b0482773cd5306d041fc
emanon001 <emanon001@gmail.com>
2021-12-05 21:41:28 +0900

    solve abc230/e

--- abc230/src/bin/e.rs
+++ abc230/src/bin/e.rs
@@ -8,9 +8,42 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: i64
     };
+
+    let mut cur = 1; // cur
+    let mut res = 0;
+    while cur <= n {
+        let x = n / cur;
+        let pos = bsearch(cur - 1, n + 1, |_x| n / _x >= x).unwrap();
+        res += (pos - cur + 1) * x;
+        cur = pos + 1;
+    }
+    println!("{}", res);
 }
 
 fn main() {
