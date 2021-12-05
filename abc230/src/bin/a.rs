979414d709406d70a62bcb40cfc49ee213faddf3
emanon001 <emanon001@gmail.com>
2021-12-05 21:40:42 +0900

    solve abc230/a

--- abc230/src/bin/a.rs
+++ abc230/src/bin/a.rs
@@ -10,7 +10,11 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
     };
+
+    let x = if n >= 42 { n + 1 } else { n };
+    println!("AGC{0: >03}", x);
 }
 
 fn main() {
