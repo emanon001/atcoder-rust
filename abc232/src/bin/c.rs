6ac57663c89b0bd62dd04444391769f6d6c6c4ff
emanon001 <emanon001@gmail.com>
2022-01-09 22:42:15 +0900

    solve abc232/c

--- abc232/src/bin/c.rs
+++ abc232/src/bin/c.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges_t: [(Usize1, Usize1); m],
+        edges_a: [(Usize1, Usize1); m],
     };
+
+    let mut graph_t = vec![HashSet::new(); n];
+    for (u, v) in edges_t {
+        graph_t[u].insert(v);
+        graph_t[v].insert(u);
+    }
+
+    for perm in (0..n).permutations(n) {
+        let mut graph_a = vec![HashSet::new(); n];
+        for &(u, v) in &edges_a {
+            graph_a[perm[u]].insert(perm[v]);
+            graph_a[perm[v]].insert(perm[u]);
+        }
+        if graph_t == graph_a {
+            return println!("Yes");
+        }
+    }
+    println!("No");
 }
 
 fn main() {
