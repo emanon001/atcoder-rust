ab7f3d0c57bc18a6f57052e95400f4b25ec9cb81
emanon001 <emanon001@gmail.com>
2022-01-10 04:09:58 +0900

    solve abc233/d

--- abc233/src/bin/d.rs
+++ abc233/src/bin/d.rs
@@ -8,9 +8,57 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, k: i64,
+        av: [i64; n],
     };
+
+    let mut cusum: HashMap<i64, Vec<usize>> = HashMap::new();
+    let mut sum = 0_i64;
+    for i in 0..n {
+        sum += av[i];
+        cusum.entry(sum).or_insert(Vec::new()).push(i);
+    }
+    let mut sum = 0_i64;
+    let mut res = 0_i64;
+    for i in 0..n {
+        let diff = k + sum;
+        let pos = cusum.get(&diff);
+        if let Some(pos) = pos {
+            let j = bsearch(pos.len() as i64, -1, |x| {
+                let x = x as usize;
+                pos[x] >= i
+            });
+            if let Some(j) = j {
+                res += pos.len() as i64 - j;
+            }
+        }
+        sum += av[i];
+    }
+    println!("{}", res);
 }
 
 fn main() {
