88ac97ef3a11cf8124c5073b9972bf1f6c952482
emanon001 <emanon001@gmail.com>
2022-01-09 22:42:00 +0900

    solve abc232/a

--- abc232/src/bin/a.rs
+++ abc232/src/bin/a.rs
@@ -10,7 +10,13 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars,
     };
+
+    let a = s[0].to_digit(10).unwrap();
+    let b = s[2].to_digit(10).unwrap();
+    let res = a * b;
+    println!("{}", res);
 }
 
 fn main() {
