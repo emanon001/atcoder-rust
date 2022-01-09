fdbc128e8227b5f3bc9f57ed9096c65adf366cbd
emanon001 <emanon001@gmail.com>
2022-01-09 22:42:42 +0900

    solve abc232/d

--- abc232/src/bin/d.rs
+++ abc232/src/bin/d.rs
@@ -8,9 +8,54 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
+fn max_step(
+    i: usize,
+    j: usize,
+    h: usize,
+    w: usize,
+    grid: &[Vec<char>],
+    dp: &mut Vec<Vec<Option<usize>>>,
+) -> usize {
+    if let Some(res) = dp[i][j] {
+        return res;
+    }
+    if grid[i][j] == '#' {
+        dp[i][j] = Some(0);
+        return 0;
+    }
+    let mut res = 0;
+    if i + 1 < h {
+        chmax!(res, max_step(i + 1, j, h, w, grid, dp));
+    }
+    if j + 1 < w {
+        chmax!(res, max_step(i, j + 1, h, w, grid, dp));
+    }
+    res += 1;
+    dp[i][j] = Some(res);
+    res
+}
+
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [Chars; h],
     };
+
+    let mut dp = vec![vec![None; w]; h];
+    let res = max_step(0, 0, h, w, &grid, &mut dp);
+    println!("{}", res);
 }
 
 fn main() {
