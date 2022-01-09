03e8a20fc9f699dd9b4bb4f8bcddee0342173077
emanon001 <emanon001@gmail.com>
2022-01-10 04:10:02 +0900

    solve abc233/e

--- abc233/src/bin/e.rs
+++ abc233/src/bin/e.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        x: Chars
     };
+
+    let xv = x
+        .into_iter()
+        .map(|ch| ch.to_digit(10).unwrap() as i64)
+        .collect::<Vec<_>>();
+    let mut cusum = vec![0; xv.len() + 1];
+    let mut cur = 0_i64;
+    for i in 0..xv.len() {
+        cur += xv[i];
+        cusum[i] = cur;
+    }
+    let mut res = Vec::new();
+    let mut add = 0_i64;
+    for i in (0..xv.len()).rev() {
+        let a = cusum[i] + add;
+        let b = a % 10;
+        add = a / 10;
+        res.push(b);
+    }
+    if add > 0 {
+        res.push(add);
+    }
+    res.reverse();
+    println!("{}", res.into_iter().join(""));
 }
 
 fn main() {
