d491b0bb371324f8cf0c351df4be561b38c069cf
emanon001 <emanon001@gmail.com>
2022-01-25 00:12:24 +0900

    solve past202112-open/b

--- past202112-open/src/bin/b.rs
+++ past202112-open/src/bin/b.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        abv: [(i64, i64); n]
     };
+
+    let mut counts = vec![0; 6];
+    for (a, b) in abv {
+        let mut rest = b - a;
+        let coins = vec![500, 100, 50, 10, 5, 1];
+        for (i, coin) in coins.iter().enumerate() {
+            counts[i] += rest / coin;
+            rest = rest % coin;
+        }
+    }
+    let res = counts[2] + counts[4];
+    println!("{}", res);
 }
 
 fn main() {
