f4ac053037f4727dbad6d85766109b2264b70a37
emanon001 <emanon001@gmail.com>
2022-02-16 01:36:23 +0900

    一番下のセルの通行禁止をランダムで判定

--- ahc008/src/bin/a.rs
+++ ahc008/src/bin/a.rs
@@ -4,6 +4,7 @@ use itertools::Itertools;
 use num::*;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use rand::prelude::*;
 #[allow(unused_imports)]
 use std::collections::*;
 use whiteread::parse_line;
@@ -14,17 +15,20 @@ pub fn read_line() -> String {
     s.trim().to_string()
 }
 
+#[derive(Clone, PartialEq, Eq, Debug)]
 struct Human {
     i: usize,
     j: usize,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug)]
 struct Pet {
     i: usize,
     j: usize,
     t: PetType,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug)]
 enum PetType {
     Ushi,
     Buta,
@@ -36,7 +40,7 @@ enum PetType {
 #[derive(Clone, PartialEq, Eq, Debug)]
 enum CellType {
     None,
-    Stop,
+    Blocked,
     HumanOrPet(usize, usize),
 }
 
@@ -61,9 +65,11 @@ struct Solver {
     all_turn: usize,
     cur_turn: usize,
     done_move_to_initial_positon: bool,
+    done_move_and_block_cells: bool,
     h_size: usize,
     v_size: usize,
     cells: Vec<Vec<CellType>>,
+    rng: ThreadRng,
 }
 
 impl Solver {
@@ -93,9 +99,11 @@ impl Solver {
             all_turn: 300,
             cur_turn: 1,
             done_move_to_initial_positon: false,
+            done_move_and_block_cells: false,
             h_size: 30,
             v_size: 30,
             cells,
+            rng: rand::thread_rng(),
         }
     }
 
@@ -103,7 +111,13 @@ impl Solver {
         for turn in 1..=self.all_turn {
             self.cur_turn = turn;
 
-            let human_actions = self.move_humans();
+            let human_actions = if !self.done_move_to_initial_positon {
+                self.move_to_initial_position()
+            } else if !self.done_move_and_block_cells {
+                self.move_and_block_cells()
+            } else {
+                self.random_actions()
+            };
             println!("{}", human_actions.iter().join(""));
             let pet_actions: Vec<Vec<char>> = read_line()
                 .split(" ")
@@ -111,25 +125,6 @@ impl Solver {
                 .collect();
             self.move_pets_by_actions(&pet_actions);
         }
-    }
-
-    fn move_humans(&mut self) -> Vec<char> {
-        if !self.done_move_to_initial_positon {
-            return self.move_to_initial_position();
-        }
-
-        let mut actions = Vec::new();
-        for i in 0..self.m {
-            let human = &self.humans[i];
-            let act = match self.cells[human.i][human.j - 1] {
-                CellType::Stop if human.i < self.v_size - 1 => 'D',
-                CellType::None if self.can_stop_cell(human.i, human.j - 1) => 'l',
-                _ => '.',
-            };
-            actions.push(act);
-        }
-        self.move_humans_by_actions(&actions);
-        actions
     }
 
     fn move_to_initial_position(&mut self) -> Vec<char> {
@@ -161,44 +156,106 @@ impl Solver {
         actions
     }
 
-    fn move_humans_by_actions(&mut self, action: &Vec<char>) {
+    fn move_and_block_cells(&mut self) -> Vec<char> {
+        let mut actions = Vec::new();
         for i in 0..self.m {
-            let human = &mut self.humans[i];
-            let mut new_i = human.i;
-            let mut new_j = human.j;
-            match action[i] {
-                'U' => new_i -= 1,
-                'D' => new_i += 1,
-                'L' => new_j -= 1,
-                'R' => new_j += 1,
-                'u' => self.cells[human.i - 1][human.j] = CellType::Stop,
-                'd' => self.cells[human.i + 1][human.j] = CellType::Stop,
-                'l' => self.cells[human.i][human.j - 1] = CellType::Stop,
-                'r' => self.cells[human.i][human.j + 1] = CellType::Stop,
-                _ => {}
-            }
-
-            // セルの状態更新
-            if let CellType::HumanOrPet(c, c2) = self.cells[human.i][human.j] {
-                // 移動元
-                if c == 1 && c2 == 0 {
-                    self.cells[human.i][human.j] = CellType::None;
-                } else {
-                    self.cells[human.i][human.j] = CellType::HumanOrPet(c - 1, c2);
+            let human = &self.humans[i];
+            let act = match self.cells[human.i][human.j - 1] {
+                CellType::Blocked if human.i < self.v_size - 1 => 'D',
+                CellType::None
+                    if human.i < self.v_size - 1 && self.can_block_cell(human.i, human.j - 1) =>
+                {
+                    'l'
                 }
-                // 移動先
-                if let CellType::HumanOrPet(c, c2) = self.cells[new_i][new_j] {
-                    self.cells[new_i][new_j] = CellType::HumanOrPet(c + 1, c2);
-                } else {
-                    self.cells[new_i][new_j] = CellType::HumanOrPet(1, 0);
-                }
-            } else {
-                unreachable!();
-            }
-
-            human.i = new_i;
-            human.j = new_j;
+                _ => '.',
+            };
+            actions.push(act);
         }
+        self.move_humans_by_actions(&actions);
+        let mut done = true;
+        for i in 0..self.m {
+            let h = &self.humans[i];
+            if h.i < self.v_size - 1 {
+                done = false;
+            }
+        }
+        if done {
+            self.done_move_and_block_cells = true;
+        }
+        actions
+    }
+
+    fn random_actions(&mut self) -> Vec<char> {
+        let mut best_actions = vec!['.'; self.m];
+        let mut best_score = 0_f64;
+        // let action_list = vec!['U', 'D', 'L', 'R', 'u', 'd', 'l', 'r', '.'];
+        let action_list = vec!['l', '.'];
+        for _ in 0..50 {
+            let mut actions = Vec::new();
+            for _ in 0..self.m {
+                actions.push(action_list[self.rng.gen::<usize>() % action_list.len()]);
+            }
+            let (score, bk_humans, bk_cells) = self.calc_score(&actions);
+            if score > best_score {
+                best_score = score;
+                best_actions = actions;
+            }
+            self.humans = bk_humans;
+            self.cells = bk_cells;
+        }
+        self.move_humans_by_actions(&best_actions)
+    }
+
+    fn move_humans_by_actions(&mut self, actions: &Vec<char>) -> Vec<char> {
+        let mut fixed_actions = Vec::new();
+        for i in 0..self.m {
+            let act = if self.can_action(&self.humans[i], actions[i]) {
+                actions[i]
+            } else {
+                '.'
+            };
+            fixed_actions.push(act);
+            self.move_human_by_action(i, act);
+        }
+        fixed_actions
+    }
+
+    fn move_human_by_action(&mut self, h_i: usize, action: char) {
+        let human = &mut self.humans[h_i];
+        let mut new_i = human.i;
+        let mut new_j = human.j;
+        match action {
+            'U' => new_i -= 1,
+            'D' => new_i += 1,
+            'L' => new_j -= 1,
+            'R' => new_j += 1,
+            'u' => self.cells[human.i - 1][human.j] = CellType::Blocked,
+            'd' => self.cells[human.i + 1][human.j] = CellType::Blocked,
+            'l' => self.cells[human.i][human.j - 1] = CellType::Blocked,
+            'r' => self.cells[human.i][human.j + 1] = CellType::Blocked,
+            _ => {}
+        }
+
+        // セルの状態更新
+        if let CellType::HumanOrPet(c, c2) = self.cells[human.i][human.j] {
+            // 移動元
+            if c == 1 && c2 == 0 {
+                self.cells[human.i][human.j] = CellType::None;
+            } else {
+                self.cells[human.i][human.j] = CellType::HumanOrPet(c - 1, c2);
+            }
+            // 移動先
+            if let CellType::HumanOrPet(c, c2) = self.cells[new_i][new_j] {
+                self.cells[new_i][new_j] = CellType::HumanOrPet(c + 1, c2);
+            } else {
+                self.cells[new_i][new_j] = CellType::HumanOrPet(1, 0);
+            }
+        } else {
+            unreachable!();
+        }
+
+        human.i = new_i;
+        human.j = new_j;
     }
 
     fn move_pets_by_actions(&mut self, actions: &Vec<Vec<char>>) {
@@ -238,16 +295,37 @@ impl Solver {
         }
     }
 
-    fn can_stop_cell(&self, i: usize, j: usize) -> bool {
+    fn can_action(&self, h: &Human, act: char) -> bool {
+        match act {
+            'U' => h.i > 0 && self.can_move(h.i - 1, h.j),
+            'D' => h.i < self.v_size - 1 && self.can_move(h.i + 1, h.j),
+            'L' => h.j > 0 && self.can_move(h.i, h.j - 1),
+            'R' => h.j < self.h_size - 1 && self.can_move(h.i, h.j + 1),
+            'u' => h.i > 0 && self.can_block_cell(h.i - 1, h.j),
+            'd' => h.i < self.v_size - 1 && self.can_block_cell(h.i + 1, h.j),
+            'l' => h.j > 0 && self.can_block_cell(h.i, h.j - 1),
+            'r' => h.j < self.h_size - 1 && self.can_block_cell(h.i, h.j + 1),
+            _ => false,
+        }
+    }
+
+    fn can_move(&self, i: usize, j: usize) -> bool {
+        match self.cells[i][j] {
+            CellType::None | CellType::HumanOrPet(_, _) => true,
+            _ => false,
+        }
+    }
+
+    fn can_block_cell(&self, i: usize, j: usize) -> bool {
         let cell = &self.cells[i][j];
-        if cell != &CellType::None && cell != &CellType::Stop {
+        if cell != &CellType::None && cell != &CellType::Blocked {
             return false;
         }
         // U
         if i > 0 {
             let cell = &self.cells[i - 1][j];
             let ok = match cell {
-                CellType::None | CellType::Stop => true,
+                CellType::None | CellType::Blocked => true,
                 CellType::HumanOrPet(_, c) => c == &0,
             };
             if !ok {
@@ -258,7 +336,7 @@ impl Solver {
         if i < self.v_size - 1 {
             let cell = &self.cells[i + 1][j];
             let ok = match cell {
-                CellType::None | CellType::Stop => true,
+                CellType::None | CellType::Blocked => true,
                 CellType::HumanOrPet(_, c) => c == &0,
             };
             if !ok {
@@ -269,7 +347,7 @@ impl Solver {
         if j > 0 {
             let cell = &self.cells[i][j - 1];
             let ok = match cell {
-                CellType::None | CellType::Stop => true,
+                CellType::None | CellType::Blocked => true,
                 CellType::HumanOrPet(_, c) => c == &0,
             };
             if !ok {
@@ -280,7 +358,7 @@ impl Solver {
         if j < self.h_size - 1 {
             let cell = &self.cells[i][j + 1];
             let ok = match cell {
-                CellType::None | CellType::Stop => true,
+                CellType::None | CellType::Blocked => true,
                 CellType::HumanOrPet(_, c) => c == &0,
             };
             if !ok {
@@ -288,6 +366,79 @@ impl Solver {
             }
         }
         true
+    }
+
+    fn calc_score(&mut self, actions: &Vec<char>) -> (f64, Vec<Human>, Vec<Vec<CellType>>) {
+        let bk_humans = self.humans.clone();
+        let bk_cells = self.cells.clone();
+        for i in 0..actions.len() {
+            let act = actions[i];
+            let act = if self.can_action(&self.humans[i], act) {
+                act
+            } else {
+                '.'
+            };
+            self.move_human_by_action(i, act);
+        }
+
+        let mut visited = HashSet::new();
+        let mut score = 0_f64;
+        for h in &self.humans {
+            if visited.contains(&(h.i, h.j)) {
+                continue;
+            }
+            let mut cell_count = 0_usize;
+            let mut human_count = 0_usize;
+            let mut pet_count = 0_usize;
+            let mut que = VecDeque::new();
+            que.push_back((h.i, h.j));
+            visited.insert((h.i, h.j));
+            while let Some((i, j)) = que.pop_front() {
+                // 人間と動物の数を加算
+                cell_count += 1;
+                match self.cells[i][j] {
+                    CellType::HumanOrPet(hc, pc) => {
+                        human_count += hc;
+                        pet_count += pc;
+                    }
+                    _ => {}
+                }
+
+                // 上下左右に移動
+                let dirs = vec![(-1, 0), (1, 0), (0, -1), (0, 1)];
+                for (di, dj) in dirs {
+                    let new_i = i as isize + di;
+                    let new_j = j as isize + dj;
+                    if new_i < 0
+                        || new_i >= self.v_size as isize
+                        || new_j < 0
+                        || new_j >= self.h_size as isize
+                    {
+                        continue;
+                    }
+                    let new_i = new_i as usize;
+                    let new_j = new_j as usize;
+                    if visited.contains(&(new_i, new_j)) {
+                        continue;
+                    }
+                    visited.insert((new_i, new_j));
+                    match self.cells[new_i][new_j] {
+                        CellType::None => {
+                            que.push_back((new_i, new_j));
+                        }
+                        CellType::HumanOrPet(_, _) => {
+                            que.push_back((new_i, new_j));
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            score += (cell_count as f64 / (self.h_size * self.v_size) as f64)
+                * 2_f64.powi(-(pet_count as i32))
+                * human_count as f64;
+        }
+        score = 10.pow(8) as f64 * (1.0 as f64 / self.m as f64) * score;
+        (score, bk_humans, bk_cells)
     }
 }
 
