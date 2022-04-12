2f6a480bf951889020d8edd03057dd5bdeb6d948
emanon001 <emanon001@gmail.com>
2022-04-13 04:08:04 +0900

    solve past202112-open/f

--- past202112-open/src/bin/f.rs
+++ past202112-open/src/bin/f.rs
@@ -10,7 +10,48 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: Usize1, b: Usize1,
+        s: [Chars; 3],
     };
+
+    let mut visited = vec![vec![false; 9]; 9];
+    let mut que = VecDeque::new();
+    que.push_back((a, b));
+    visited[a][b] = true;
+    while let Some((x, y)) = que.pop_front() {
+        for i in 0..3 {
+            for j in 0..3 {
+                if s[i][j] == '.' {
+                    continue;
+                }
+
+                let new_x = x as isize + i as isize + 1 - 2;
+                let new_y = y as isize + j as isize + 1 - 2;
+                if new_x < 0 || new_x >= 9 {
+                    continue;
+                }
+                if new_y < 0 || new_y >= 9 {
+                    continue;
+                }
+                let new_x = new_x as usize;
+                let new_y = new_y as usize;
+                if visited[new_x][new_y] {
+                    continue;
+                }
+                visited[new_x][new_y] = true;
+                que.push_back((new_x, new_y));
+            }
+        }
+    }
+    let mut res = 0;
+    for i in 0..9 {
+        for j in 0..9 {
+            if visited[i][j] {
+                res += 1;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
