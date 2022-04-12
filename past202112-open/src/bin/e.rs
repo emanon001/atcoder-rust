bc277984a65232899804c8f7620bc507c1064e7f
emanon001 <emanon001@gmail.com>
2022-04-13 03:56:53 +0900

    solve past202112-open/e

--- past202112-open/src/bin/e.rs
+++ past202112-open/src/bin/e.rs
@@ -10,7 +10,35 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        nv: Chars,
     };
+
+    let nv = nv
+        .into_iter()
+        .map(|ch| ch.to_digit(10).unwrap() as i32)
+        .collect::<Vec<_>>();
+    let mut res = 0_i32;
+    let mut prev_n = -1;
+    let mut prev_hand = "none";
+    for (i, n) in nv.into_iter().enumerate() {
+        let hand = if [1, 2, 3, 4, 5].contains(&n) {
+            "left"
+        } else {
+            "right"
+        };
+        res += if i == 0 {
+            500
+        } else if n == prev_n {
+            301
+        } else if hand == prev_hand {
+            210
+        } else {
+            100
+        };
+        prev_n = n;
+        prev_hand = hand;
+    }
+    println!("{}", res);
 }
 
 fn main() {
