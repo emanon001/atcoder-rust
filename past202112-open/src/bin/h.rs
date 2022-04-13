c92c708c4b060327f2d41c3a4efba2fe02ac2251
emanon001 <emanon001@gmail.com>
2022-04-14 04:17:18 +0900

    solve past202112-open/h

--- past202112-open/src/bin/h.rs
+++ past202112-open/src/bin/h.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars,
+        t: Chars,
     };
+
+    let mut dp = vec![vec![0_u32; t.len() + 1]; s.len() + 1];
+    for i in 0..s.len() {
+        for j in 0..t.len() {
+            if s[i] != t[j] {
+                dp[i + 1][j + 1] = dp[i][j] + 1;
+            } else {
+                dp[i + 1][j + 1] = u32::max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+    }
+    println!("{}", dp[s.len()][t.len()]);
 }
 
 fn main() {
