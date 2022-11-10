81d85758f37ac6bc531a50d71e1b333c840e39c7
emanon001 <emanon001@gmail.com>
2022-11-10 23:23:28 +0900

    solve tessoku-book/b06

--- tessoku-book/src/bin/b06.rs
+++ tessoku-book/src/bin/b06.rs
@@ -10,7 +10,31 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n],
+        q: usize,
+        lrv: [(usize, usize); q]
     };
+
+    let mut cusum1 = vec![0; n + 1];
+    let mut cusum2 = vec![0; n + 1];
+    for i in 0..n {
+        cusum1[i + 1] = cusum1[i] + if av[i] == 1 { 1 } else { 0 };
+        cusum2[i + 1] = cusum2[i] + if av[i] == 1 { 0 } else { 1 };
+    }
+
+    for (l, r) in lrv {
+        let count1 = cusum1[r] - cusum1[l - 1];
+        let count2 = cusum2[r] - cusum2[l - 1];
+        let res = if count1 > count2 {
+            "win"
+        } else if count1 < count2 {
+            "lose"
+        } else {
+            "draw"
+        };
+        println!("{}", res);
+    }
 }
 
 fn main() {
