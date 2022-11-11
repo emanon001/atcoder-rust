9b4f96990285db373b7b3c4bd300790c92db649d
emanon001 <emanon001@gmail.com>
2022-11-11 23:24:35 +0900

    solve tessoku-book/a07

--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        d: usize,
+        n: usize,
+        lrv: [(Usize1, Usize1); n]
     };
+
+    let mut cusum = vec![0_i32; d + 1];
+    for (l, r) in lrv {
+        cusum[l] += 1;
+        cusum[r + 1] -= 1;
+    }
+    for i in 0..d {
+        cusum[i + 1] += cusum[i]
+    }
+    for c in cusum.iter().take(d) {
+        println!("{}", c);
+    }
 }
 
 fn main() {
