568afe3c57b48e5ff8f4e296aa8c7778cda49a94
emanon001 <emanon001@gmail.com>
2022-11-12 22:16:17 +0900

    solve tessoku-book/a09

--- tessoku-book/src/bin/a09.rs
+++ tessoku-book/src/bin/a09.rs
@@ -10,7 +10,31 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize, n: usize,
+        rectangles: [(usize, usize, usize, usize); n]
     };
+
+    let mut cusum = vec![vec![0_i32; w + 2]; h + 2];
+    for (y1, x1, y2, x2) in rectangles {
+        cusum[y1][x1] += 1;
+        cusum[y1][x2 + 1] -= 1;
+        cusum[y2 + 1][x1] -= 1;
+        cusum[y2 + 1][x2 + 1] += 1;
+    }
+    for i in 0..h + 2 {
+        for j in 0..w + 1 {
+            cusum[i][j + 1] += cusum[i][j];
+        }
+    }
+    for j in 0..w + 2 {
+        for i in 0..h + 1 {
+            cusum[i + 1][j] += cusum[i][j];
+        }
+    }
+
+    for row in cusum.into_iter().skip(1).take(h) {
+        println!("{}", row.iter().skip(1).take(w).join(" "));
+    }
 }
 
 fn main() {
