9ccb9ff07f80d95131c8d5dddb9b00d8bcc8a28b
emanon001 <emanon001@gmail.com>
2022-11-13 01:03:12 +0900

    tessoku-book/b09

--- tessoku-book/src/bin/b09.rs
+++ tessoku-book/src/bin/b09.rs
@@ -10,7 +10,46 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        rectangles: [(usize, usize, usize, usize); n]
     };
+
+    let size = 1500;
+    let mut cusum = vec![vec![0_i32; size + 2]; size + 2];
+
+    for (a, b, c, d) in rectangles {
+        let x1 = a + 1;
+        let y1 = b + 1;
+        let x2 = c;
+        let y2 = d;
+        cusum[y1][x1] += 1;
+        cusum[y1][x2 + 1] -= 1;
+        cusum[y2 + 1][x1] -= 1;
+        cusum[y2 + 1][x2 + 1] += 1;
+    }
+
+    for i in 1..=size {
+        for j in 1..=size {
+            cusum[i][j] += cusum[i][j - 1];
+        }
+    }
+    for j in 1..=size {
+        for i in 1..=size {
+            cusum[i][j] += cusum[i - 1][j];
+        }
+    }
+
+    let mut res = 0_i32;
+    for i in 1..=size {
+        for j in 1..=size {
+            let c = cusum[i][j];
+            if c > 0 {
+                res += 1;
+            }
+        }
+    }
+
+    println!("{}", res);
 }
 
 fn main() {
