827b4afe25e4f2bb91b97a119ebe4b075dcb70c3
emanon001 <emanon001@gmail.com>
2022-11-12 21:45:20 +0900

    solve tessoku-book/b08

--- tessoku-book/src/bin/b08.rs
+++ tessoku-book/src/bin/b08.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        points: [(usize, usize); n],
+        q: usize,
+        rectangles: [(usize, usize, usize, usize); q]
     };
+
+    let size = 1500;
+    let mut cusum = vec![vec![0; size + 1]; size + 1];
+    for (x, y) in points {
+        cusum[y][x] += 1;
+    }
+    for i in 0..size + 1 {
+        for j in 0..size {
+            cusum[i][j + 1] += cusum[i][j];
+        }
+    }
+    for j in 0..size + 1 {
+        for i in 0..size {
+            cusum[i + 1][j] += cusum[i][j];
+        }
+    }
+
+    for (a, b, c, d) in rectangles {
+        let res = cusum[d][c] - cusum[d][a - 1] - cusum[b - 1][c] + cusum[b - 1][a - 1];
+        println!("{}", res);
+    }
 }
 
 fn main() {
