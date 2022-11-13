17f7f281f7f3ae3c94c68420e33dea970908d363
emanon001 <emanon001@gmail.com>
2022-11-13 20:36:29 +0900

    tessoku-book/a10

--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -8,9 +8,41 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i32; n],
+        d: usize,
+        lrv: [(usize, usize); d]
     };
+
+    let mut cumax1 = vec![0; n + 1];
+    for i in 0..n {
+        chmax!(cumax1[i + 1], cumax1[i]);
+        chmax!(cumax1[i + 1], av[i]);
+    }
+    let mut cumax2 = vec![0; n + 1];
+    for i in 0..n {
+        chmax!(cumax2[i + 1], cumax2[i]);
+        chmax!(cumax2[i + 1], av[n - i - 1]);
+    }
+
+    for (l, r) in lrv {
+        let res = (cumax1[l - 1]).max(cumax2[n - r]);
+        println!("{}", res);
+    }
 }
 
 fn main() {
