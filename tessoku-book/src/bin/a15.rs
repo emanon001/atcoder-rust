3f5e9e0ecf220e081be0d1482435baa4da29b8a6
emanon001 <emanon001@gmail.com>
2022-11-19 22:47:51 +0900

    tessoku-book/a15

--- tessoku-book/src/bin/a15.rs
+++ tessoku-book/src/bin/a15.rs
@@ -8,9 +8,29 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn compress_zahyo<T: Ord + std::hash::Hash>(
+    zahyo: &[T],
+) -> std::collections::HashMap<&T, usize> {
+    let mut set = std::collections::BTreeSet::new();
+    for x in zahyo {
+        set.insert(x);
+    }
+    let mut map = std::collections::HashMap::new();
+    for (i, x) in set.into_iter().enumerate() {
+        map.insert(x, i);
+    }
+    map
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let zahyo = compress_zahyo(&av);
+    let res = av.iter().map(|a| zahyo.get(&a).unwrap() + 1).join(" ");
+    println!("{}", res);
 }
 
 fn main() {
