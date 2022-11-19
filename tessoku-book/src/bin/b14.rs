4ba3df9a4888d349b4d958acc302c69aae4adb98
emanon001 <emanon001@gmail.com>
2022-11-19 22:38:29 +0900

    tessoku-book/b14

--- tessoku-book/src/bin/b14.rs
+++ tessoku-book/src/bin/b14.rs
@@ -10,7 +10,39 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: i64,
+        av: [i64; n]
     };
+
+    let m = n / 2;
+    let mut set1 = HashSet::new();
+    for bits in 0..1 << m {
+        let mut sum = 0_i64;
+        for i in 0..m {
+            if (bits >> i) & 1 == 1 {
+                sum += av[i];
+            }
+        }
+        set1.insert(sum);
+    }
+    let mut set2 = HashSet::new();
+    for bits in 0..1 << (n - m) {
+        let mut sum = 0_i64;
+        for i in 0..(n - m) {
+            if (bits >> i) & 1 == 1 {
+                sum += av[m + i];
+            }
+        }
+        set2.insert(sum);
+    }
+    for a in set1 {
+        let b = k - a;
+        if set2.contains(&b) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
