aa0127ba5093d549b8e0afd08d9db161654a3f16
emanon001 <emanon001@gmail.com>
2022-11-19 23:28:16 +0900

    tessoku-book/a17

--- tessoku-book/src/bin/a17.rs
+++ tessoku-book/src/bin/a17.rs
@@ -8,9 +8,49 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n - 1],
+        bv: [i64; n - 2],
     };
+
+    let mut dp = vec![1_i64 << 60; n];
+    let mut prev = vec![n; n];
+    dp[0] = 0;
+    for i in 0..n {
+        if i + 1 < n {
+            if chmin!(dp[i + 1], dp[i] + av[i]) {
+                prev[i + 1] = i;
+            }
+        }
+        if i + 2 < n {
+            if chmin!(dp[i + 2], dp[i] + bv[i]) {
+                prev[i + 2] = i;
+            }
+        }
+    }
+    let mut pos = n - 1;
+    let mut path = vec![pos + 1];
+    while pos != 0 {
+        pos = prev[pos];
+        path.push(pos + 1);
+    }
+    path.reverse();
+    println!("{}", path.len());
+    println!("{}", path.iter().join(" "));
 }
 
 fn main() {
