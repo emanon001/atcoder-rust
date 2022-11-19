504373e18ddadc5b9af4d3b2a65ab796edfa3190
emanon001 <emanon001@gmail.com>
2022-11-19 23:19:20 +0900

    tessoku-book/b16

--- tessoku-book/src/bin/b16.rs
+++ tessoku-book/src/bin/b16.rs
@@ -8,9 +8,35 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        hv: [i64; n]
     };
+
+    let mut dp = vec![1_i64 << 60; n];
+    dp[0] = 0;
+    for i in 0..n {
+        if i + 1 < n {
+            chmin!(dp[i + 1], dp[i] + (hv[i] - hv[i + 1]).abs());
+        }
+        if i + 2 < n {
+            chmin!(dp[i + 2], dp[i] + (hv[i] - hv[i + 2]).abs());
+        }
+    }
+    println!("{}", dp[n - 1]);
 }
 
 fn main() {
