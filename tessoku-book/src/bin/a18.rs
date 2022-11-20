0d107e5bd0b422855b7c930118e3a6576fc86563
emanon001 <emanon001@gmail.com>
2022-11-20 23:10:41 +0900

    tessoku-book/a18

--- tessoku-book/src/bin/a18.rs
+++ tessoku-book/src/bin/a18.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, s: usize,
+        av: [usize; n]
     };
+
+    let max = 10000;
+    let mut dp = vec![vec![false; max + 1]; n + 1];
+    dp[0][0] = true;
+    for i in 0..n {
+        let a = av[i];
+        for j in 0..=max {
+            dp[i + 1][j] |= dp[i][j];
+            if j + a <= max {
+                dp[i + 1][j + a] |= dp[i][j];
+            }
+        }
+    }
+    let res = if dp[n][s] { "Yes" } else { "No" };
+    println!("{}", res);
 }
 
 fn main() {
