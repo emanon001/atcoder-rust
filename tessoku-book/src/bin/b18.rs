a18846308144c461c7782a556ab8e913f30c720e
emanon001 <emanon001@gmail.com>
2022-11-20 23:48:58 +0900

    tessoku-book/b18

--- tessoku-book/src/bin/b18.rs
+++ tessoku-book/src/bin/b18.rs
@@ -10,7 +10,40 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, s: usize,
+        av: [usize; n]
     };
+
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+    let mut from = vec![None; s + 1];
+    for i in 0..n {
+        let a = av[i];
+        for j in 0..=s {
+            dp[i + 1][j] |= dp[i][j];
+        }
+        for j in 0..=s {
+            if j + a <= s && dp[i][j] && !dp[i + 1][j + a] {
+                from[j + a] = i.into();
+                dp[i + 1][j + a] = true;
+            }
+        }
+    }
+
+    if !dp[n][s] {
+        println!("-1");
+        return;
+    }
+
+    let mut res = VecDeque::new();
+    let mut sum = s;
+    while sum > 0 {
+        let i = from[sum].unwrap();
+        res.push_front(i + 1); // 1-origin
+        sum -= av[i];
+    }
+    println!("{}", res.len());
+    println!("{}", res.iter().join(" "));
 }
 
 fn main() {
