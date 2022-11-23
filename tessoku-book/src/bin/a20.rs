7411694b9ff96d77abfd045d2a8933407bd9d308
emanon001 <emanon001@gmail.com>
2022-11-23 21:40:14 +0900

    tessoku-book/a20

--- tessoku-book/src/bin/a20.rs
+++ tessoku-book/src/bin/a20.rs
@@ -8,9 +8,54 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
+fn dfs(i: usize, j: usize, s: &[char], t: &[char], dp: &mut Vec<Vec<Option<usize>>>) -> usize {
+    if i == 0 || j == 0 {
+        let res = 0;
+        dp[i][j] = res.into();
+        return res;
+    }
+
+    if let Some(res) = dp[i][j] {
+        return res;
+    }
+
+    let res = if s[i - 1] == t[j - 1] {
+        dfs(i - 1, j - 1, s, t, dp) + 1
+    } else {
+        let mut res = 0;
+        if i > 0 {
+            chmax!(res, dfs(i - 1, j, s, t, dp));
+        }
+        if j > 0 {
+            chmax!(res, dfs(i, j - 1, s, t, dp));
+        }
+        res
+    };
+    dp[i][j] = res.into();
+    res
+}
+
 fn solve() {
     input! {
+        s: Chars,
+        t: Chars,
     };
+
+    let mut dp = vec![vec![None; t.len() + 1]; s.len() + 1];
+    let res = dfs(s.len(), t.len(), &s, &t, &mut dp);
+    println!("{}", res);
 }
 
 fn main() {
