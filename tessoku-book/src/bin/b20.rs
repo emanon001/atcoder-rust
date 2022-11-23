87bd57d4b4b5b0e3551186cf48a9e41948096c73
emanon001 <emanon001@gmail.com>
2022-11-23 23:54:58 +0900

    tessoku-book/b20

--- tessoku-book/src/bin/b20.rs
+++ tessoku-book/src/bin/b20.rs
@@ -8,9 +8,53 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
+fn dfs(si: usize, ti: usize, s: &[char], t: &[char], dp: &mut Vec<Vec<Option<usize>>>) -> usize {
+    if let Some(res) = dp[si][ti] {
+        return res;
+    }
+
+    // dp[si][ti] = si と ti が対応する場合の最小コスト
+    if si == 0 || ti == 0 {
+        let res = (si as isize - ti as isize).abs() as usize;
+        dp[si][ti] = res.into();
+        return res;
+    }
+
+    let mut res = 1 << 30;
+    if s[si - 1] == t[ti - 1] {
+        chmin!(res, dfs(si - 1, ti - 1, s, t, dp));
+    }
+    // S[i]を削除
+    chmin!(res, dfs(si - 1, ti, s, t, dp) + 1);
+    // S[i]の文字を変更
+    chmin!(res, dfs(si - 1, ti - 1, s, t, dp) + 1);
+    // S[i]の後ろに文字を追加
+    chmin!(res, dfs(si, ti - 1, s, t, dp) + 1);
+    dp[si][ti] = res.into();
+    res
+}
+
 fn solve() {
     input! {
+        s: Chars,
+        t: Chars,
     };
+
+    let mut dp = vec![vec![None; t.len() + 1]; s.len() + 1];
+    let res = dfs(s.len(), t.len(), &s, &t, &mut dp);
+    println!("{}", res);
 }
 
 fn main() {
