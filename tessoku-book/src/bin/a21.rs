3b6f0a6b3744d31767d79fb70dd01dbafe6dffe1
emanon001 <emanon001@gmail.com>
2022-11-24 23:20:44 +0900

    tessoku-book/a21

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -8,9 +8,54 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
+fn dfs(l: usize, r: usize, pav: &[(usize, i64)], dp: &mut Vec<Vec<Option<i64>>>) -> i64 {
+    if l >= r {
+        let res = 0;
+        dp[l][r] = res.into();
+        return res;
+    }
+
+    if let Some(res) = dp[l][r] {
+        return res;
+    }
+
+    let mut res = -(1 << 30);
+
+    // 左端を取り除いた時
+    let (p, a) = pav[l];
+    let plus_score = if (l..r).contains(&p) { a } else { 0 };
+    chmax!(res, dfs(l + 1, r, pav, dp) + plus_score);
+
+    // 右端を取り除いた時
+    let (p, a) = pav[r - 1]; // [l, r)
+    let plus_score = if (l..r).contains(&p) { a } else { 0 };
+    chmax!(res, dfs(l, r - 1, pav, dp) + plus_score);
+
+    dp[l][r] = res.into();
+    res
+}
+
 fn solve() {
     input! {
+        n: usize,
+        pav: [(Usize1, i64); n]
     };
+
+    let mut dp = vec![vec![None; n + 1]; n + 1];
+    let res = dfs(0, n, &pav, &mut dp);
+    println!("{}", res);
 }
 
 fn main() {
