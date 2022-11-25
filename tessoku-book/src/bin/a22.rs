8ed866a2bb172b20933da4150d14b6df70326bb0
emanon001 <emanon001@gmail.com>
2022-11-25 22:57:17 +0900

    tessoku-book/a22

--- tessoku-book/src/bin/a22.rs
+++ tessoku-book/src/bin/a22.rs
@@ -8,9 +8,32 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [Usize1; n - 1],
+        bv: [Usize1; n - 1],
     };
+
+    let mut dp = vec![-(1 << 30); n];
+    dp[0] = 0;
+    for i in 0..n - 1 {
+        chmax!(dp[av[i]], dp[i] + 100);
+        chmax!(dp[bv[i]], dp[i] + 150);
+    }
+    println!("{}", dp[n - 1]);
 }
 
 fn main() {
