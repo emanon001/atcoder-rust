33735aa682e53056165f1799748420065d90a4b9
emanon001 <emanon001@gmail.com>
2022-11-25 23:17:57 +0900

    tessoku-book/a23

--- tessoku-book/src/bin/a23.rs
+++ tessoku-book/src/bin/a23.rs
@@ -8,9 +8,51 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bit_dp<C>(
+    state: usize,
+    dp: &mut [Option<i64>],
+    candidates: &[C],
+    fin: usize,
+    inf: i64,
+    f: fn(usize, &C) -> (usize, i64),
+) -> i64 {
+    if let Some(res) = dp[state] {
+        return res;
+    }
+    if state == fin {
+        let res = 0;
+        dp[state] = Some(res);
+        return res;
+    }
+    let mut res = inf;
+    for c in candidates {
+        let (new_state, cost) = f(state, c);
+        if new_state != state {
+            let cost = bit_dp(new_state, dp, candidates, fin, inf, f) + cost;
+            res = res.min(cost);
+        }
+    }
+    dp[state] = Some(res);
+    res
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        coupon_v: [[usize; n]; m]
     };
+
+    let inf = 1 << 30;
+    let mut dp = vec![None; 1 << n];
+    let res = bit_dp(0, &mut dp, &coupon_v, (1 << n) - 1, inf, |state, c| {
+        let mut state = state;
+        for (i, x) in c.iter().enumerate() {
+            state |= x << i;
+        }
+        (state, 1)
+    });
+    let res = if res == inf { -1 } else { res };
+    println!("{}", res);
 }
 
 fn main() {
