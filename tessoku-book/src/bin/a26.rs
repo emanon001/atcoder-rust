c031d3a83add9b151f9e1ec4eb5f660b6df557d7
emanon001 <emanon001@gmail.com>
2022-11-27 08:56:11 +0900

    tessoku-book/a26

--- tessoku-book/src/bin/a26.rs
+++ tessoku-book/src/bin/a26.rs
@@ -8,9 +8,38 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn primes(n: usize) -> Vec<usize> {
+    if n < 2 {
+        return Vec::new();
+    }
+    let mut is_prime = vec![true; n + 1];
+    is_prime[0] = false;
+    is_prime[1] = false;
+    let mut res = Vec::new();
+    for i in 2..=n {
+        if is_prime[i] {
+            res.push(i);
+            let mut j = 2 * i;
+            while j <= n {
+                is_prime[j] = false;
+                j += i;
+            }
+        }
+    }
+    res
+}
+
 fn solve() {
     input! {
+        q: usize,
+        xv: [usize; q]
     };
+
+    let prime_set = primes(300000).into_iter().collect::<HashSet<_>>();
+    for x in xv {
+        let res = if prime_set.contains(&x) { "Yes" } else { "No" };
+        println!("{}", res);
+    }
 }
 
 fn main() {
