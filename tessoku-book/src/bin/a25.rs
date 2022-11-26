5924b44f4b83e8c32ef281ee348841f964b75ca0
emanon001 <emanon001@gmail.com>
2022-11-27 03:59:46 +0900

    tessoku-book/a25

--- tessoku-book/src/bin/a25.rs
+++ tessoku-book/src/bin/a25.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [Chars; h]
     };
+
+    let mut dp = vec![vec![0_u64; w]; h];
+    dp[0][0] = 1;
+    for i in 0..h {
+        for j in 0..w {
+            if grid[i][j] == '#' {
+                continue;
+            }
+            if i + 1 < h && grid[i + 1][j] == '.' {
+                dp[i + 1][j] += dp[i][j];
+            }
+            if j + 1 < w && grid[i][j + 1] == '.' {
+                dp[i][j + 1] += dp[i][j];
+            }
+        }
+    }
+    let res = dp[h - 1][w - 1];
+    println!("{}", res);
 }
 
 fn main() {
