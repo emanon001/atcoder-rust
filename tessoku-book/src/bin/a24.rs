0db7f9d8367f643f8e682fef1238ab18f02d246f
emanon001 <emanon001@gmail.com>
2022-11-26 23:17:03 +0900

    tessoku-book/a24

--- tessoku-book/src/bin/a24.rs
+++ tessoku-book/src/bin/a24.rs
@@ -8,9 +8,47 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n]
     };
+
+    let inf = 1 << 30;
+    let mut dp = vec![inf; n];
+    for &a in &av {
+        let pos = bsearch(-1, n as i64, |x| {
+            let i = x as usize;
+            dp[i] < a
+        })
+        .map(|i| i + 1)
+        .unwrap_or(0) as usize;
+        dp[pos] = a;
+    }
+    let res = dp.into_iter().take_while(|x| x != &inf).count();
+    println!("{}", res);
 }
 
 fn main() {
