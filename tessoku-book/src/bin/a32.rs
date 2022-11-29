a9e83ef4520f909de1a1164eb147d60abd9c63a6
emanon001 <emanon001@gmail.com>
2022-11-29 23:58:16 +0900

    tessoku-book/a32

--- tessoku-book/src/bin/a32.rs
+++ tessoku-book/src/bin/a32.rs
@@ -8,9 +8,31 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn rec(turn: usize, c: usize, a: usize, b: usize, dp: &mut [Vec<Option<bool>>]) -> bool {
+    if let Some(is_win) = dp[turn][c] {
+        return is_win;
+    }
+
+    let mut is_win = false;
+    if c >= a {
+        is_win = is_win || !rec((turn + 1) % 2, c - a, a, b, dp);
+    }
+    if c >= b {
+        is_win = is_win || !rec((turn + 1) % 2, c - b, a, b, dp);
+    }
+    dp[turn][c] = is_win.into();
+    is_win
+}
+
 fn solve() {
     input! {
+        n: usize, a: usize, b: usize,
     };
+
+    let mut dp = vec![vec![None; n + 1]; 2];
+    let is_win = rec(0, n, a, b, &mut dp);
+    let res = if is_win { "First" } else { "Second" };
+    println!("{}", res);
 }
 
 fn main() {
