fa8505860b90d960a178acf21d84861ee1b0fc5d
emanon001 <emanon001@gmail.com>
2022-12-11 23:46:33 +0900

    tessoku-book/b34

--- tessoku-book/src/bin/b34.rs
+++ tessoku-book/src/bin/b34.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, x: usize, y: usize,
+        av: [usize; n]
     };
+
+    let max = 5;
+    let mut dp = vec![0; max];
+    let moves = vec![x, y];
+    for i in 0..max {
+        let mut transit = vec![false; moves.len() + 1];
+        for &m in &moves {
+            if i >= m {
+                transit[dp[i - m]] = true;
+            }
+        }
+        let (g, _) = transit.iter().find_position(|&x| !x).unwrap();
+        dp[i] = g;
+    }
+    let xor = av.into_iter().fold(0, |acc, a| acc ^ dp[a % max]);
+    let res = if xor == 0 { "Second" } else { "First" };
+    println!("{}", res);
 }
 
 fn main() {
