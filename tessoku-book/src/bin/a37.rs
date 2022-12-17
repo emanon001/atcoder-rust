368ff54299eaa9064ccf1453364fd130519b34f4
emanon001 <emanon001@gmail.com>
2022-12-17 23:38:35 +0900

    tessoku-book/a37

--- tessoku-book/src/bin/a37.rs
+++ tessoku-book/src/bin/a37.rs
@@ -10,7 +10,15 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize, b: i64,
+        av: [i64; n],
+        cv: [i64; m],
     };
+
+    let res = (av.into_iter().sum::<i64>() * m as i64)
+        + (b * (n * m) as i64)
+        + (cv.into_iter().sum::<i64>() * n as i64);
+    println!("{}", res);
 }
 
 fn main() {
