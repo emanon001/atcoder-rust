002602a0e1d6bfa6f4a86f368a5d7aad656ec8a0
emanon001 <emanon001@gmail.com>
2022-12-18 09:11:00 +0900

    tessoku-book/a38

--- tessoku-book/src/bin/a38.rs
+++ tessoku-book/src/bin/a38.rs
@@ -8,9 +8,32 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {
+        if $min > $v {
+            $min = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        d: usize, n: usize,
+        lrhv: [(Usize1, Usize1, usize); n]
     };
+
+    let mut dp = vec![24; d];
+    for (l, r, h) in lrhv {
+        for i in l..=r {
+            chmin!(dp[i], h);
+        }
+    }
+    let res = dp.into_iter().sum::<usize>();
+    println!("{}", res);
 }
 
 fn main() {
