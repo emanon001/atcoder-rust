7a754557646a4500eacbac33773039cf9b7dce47
emanon001 <emanon001@gmail.com>
2022-12-22 04:57:11 +0900

    tessoku-book/b40

--- tessoku-book/src/bin/b40.rs
+++ tessoku-book/src/bin/b40.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [u64; n]
     };
+
+    let mut map = HashMap::new();
+    for &a in &av {
+        *map.entry(a % 100).or_insert(0_u64) += 1;
+    }
+    let mut res = 0_u64;
+    for &a in &av {
+        let x = a % 100;
+        *map.entry(x).or_insert(0_u64) -= 1;
+        res += *map.get(&((100 - x) % 100)).unwrap_or(&0);
+    }
+    println!("{}", res);
 }
 
 fn main() {
