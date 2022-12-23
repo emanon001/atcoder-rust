ed22608d251561b88e74463d8e210658eb3f158a
emanon001 <emanon001@gmail.com>
2022-12-23 23:28:38 +0900

    tessoku-book/b41

--- tessoku-book/src/bin/b41.rs
+++ tessoku-book/src/bin/b41.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        x: i64, y: i64
     };
+
+    let mut res = VecDeque::new();
+    let mut tx = x;
+    let mut ty = y;
+    while tx != 1 || ty != 1 {
+        res.push_front((tx, ty));
+        if tx > ty {
+            tx -= ty;
+        } else {
+            ty -= tx;
+        }
+    }
+    println!("{}", res.len());
+    println!(
+        "{}",
+        res.into_iter()
+            .map(|(x, y)| format!("{} {}", x, y))
+            .join("\n")
+    );
 }
 
 fn main() {
