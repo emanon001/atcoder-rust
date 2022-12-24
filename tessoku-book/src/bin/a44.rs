31894abb9dd3d27fb268e0a519a77cf4d2007ff4
emanon001 <emanon001@gmail.com>
2022-12-25 04:13:39 +0900

    tessoku-book/a44

--- tessoku-book/src/bin/a44.rs
+++ tessoku-book/src/bin/a44.rs
@@ -10,7 +10,43 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
     };
+
+    let mut vec = (1..=n).into_iter().collect::<Vec<_>>();
+    let mut reversed = false;
+    let to_i = |x: usize, reversed: bool| -> usize {
+        if reversed {
+            n - x - 1
+        } else {
+            x
+        }
+    };
+    for _ in 0..q {
+        input! {
+            kind: usize,
+        }
+        match kind {
+            1 => {
+                input! {
+                    x: Usize1, y: usize,
+                };
+                let i = to_i(x, reversed);
+                vec[i] = y;
+            }
+            2 => {
+                reversed = !reversed;
+            }
+            3 => {
+                input! {
+                    x: Usize1,
+                };
+                let i = to_i(x, reversed);
+                println!("{}", vec[i]);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
