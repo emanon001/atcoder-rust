91d60de4c9f0dd9f98f3dbff1b9c481835b73b7c
emanon001 <emanon001@gmail.com>
2022-12-25 04:39:31 +0900

    tessoku-book/b44

--- tessoku-book/src/bin/b44.rs
+++ tessoku-book/src/bin/b44.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        grid: [[usize; n]; n],
+        q: usize
     };
+
+    let mut row_pos = (0..n).collect::<Vec<_>>();
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    x: Usize1, y: Usize1
+                };
+                row_pos.swap(x, y);
+            }
+            2 => {
+                input! {
+                    x: Usize1, y: Usize1
+                };
+                println!("{}", grid[row_pos[x]][y]);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
