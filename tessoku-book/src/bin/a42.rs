49459d66bb5d6a3c6c9924c0e59bbee67955c5ec
emanon001 <emanon001@gmail.com>
2022-12-24 22:50:31 +0900

    tessoku-book/a42

--- tessoku-book/src/bin/a42.rs
+++ tessoku-book/src/bin/a42.rs
@@ -8,9 +8,39 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {
+        if $max < $v {
+            $max = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn solve() {
     input! {
+        n: usize, k: i64,
+        abv: [(i64, i64); n]
     };
+
+    let mut res = 0;
+    for a_min in 1..100 {
+        let a_max = a_min + k;
+        for b_min in 1..100 {
+            let b_max = b_min + k;
+            let mut count = 0;
+            for (a, b) in &abv {
+                if (a_min..=a_max).contains(a) && (b_min..=b_max).contains(b) {
+                    count += 1;
+                }
+            }
+            chmax!(res, count);
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
