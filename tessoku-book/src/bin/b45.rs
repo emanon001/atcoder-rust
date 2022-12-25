68b9ea2aa21fdbe1e12b029d1a676d5fb252d2a8
emanon001 <emanon001@gmail.com>
2022-12-26 00:01:39 +0900

    tessoku-book/b45

--- tessoku-book/src/bin/b45.rs
+++ tessoku-book/src/bin/b45.rs
@@ -10,7 +10,11 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: i64, b: i64, c: i64
     };
+
+    let res = if a + b + c == 0 { "Yes" } else { "No" };
+    println!("{}", res);
 }
 
 fn main() {
