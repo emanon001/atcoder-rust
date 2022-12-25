8d6bef6a3b1831766b0b4ee584c37ab44bdc9dec
emanon001 <emanon001@gmail.com>
2022-12-25 23:58:35 +0900

    tessoku-book/a45

--- tessoku-book/src/bin/a45.rs
+++ tessoku-book/src/bin/a45.rs
@@ -8,9 +8,28 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn color_to_score(c: char) -> usize {
+    match c {
+        'W' => 0,
+        'B' => 1,
+        'R' => 2,
+        _ => unreachable!(),
+    }
+}
+
 fn solve() {
     input! {
+        _: usize, c: char,
+        av: Chars
     };
+
+    let sum = av.into_iter().fold(0, |acc, c| acc + color_to_score(c));
+    let res = if sum % 3 == color_to_score(c) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", res);
 }
 
 fn main() {
