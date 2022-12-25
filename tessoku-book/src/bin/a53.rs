519a73c0d1df9178465d28059a7bc13ae32410d7
emanon001 <emanon001@gmail.com>
2022-12-26 05:22:09 +0900

    tessoku-book/a53

--- tessoku-book/src/bin/a53.rs
+++ tessoku-book/src/bin/a53.rs
@@ -5,12 +5,32 @@ use num::*;
 use proconio::input;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use std::cmp::Reverse;
 #[allow(unused_imports)]
 use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+
+    let mut heap = BinaryHeap::new();
+    for _ in 0..q {
+        input! { kind: usize };
+        match kind {
+            1 => {
+                input! { x: usize };
+                heap.push(Reverse(x));
+            }
+            2 => {
+                println!("{}", heap.peek().unwrap().0);
+            }
+            3 => {
+                heap.pop();
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
