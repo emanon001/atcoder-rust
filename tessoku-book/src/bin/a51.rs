361ace15bb406983c76c1c7cf0cfd6a7c27fd505
emanon001 <emanon001@gmail.com>
2022-12-26 04:58:01 +0900

    tessoku-book/a51

--- tessoku-book/src/bin/a51.rs
+++ tessoku-book/src/bin/a51.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+
+    let mut stack = Vec::new();
+    for _ in 0..q {
+        input! { kind: usize };
+        match kind {
+            1 => {
+                input! { x: String };
+                stack.push(x);
+            }
+            2 => {
+                println!("{}", stack.last().unwrap());
+            }
+            3 => {
+                stack.pop();
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
