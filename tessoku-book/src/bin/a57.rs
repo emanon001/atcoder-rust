567b2827604708c60ea08a63cfb06e5817dfb74f
emanon001 <emanon001@gmail.com>
2022-12-30 03:09:07 +0900

    tessoku-book/a57

--- tessoku-book/src/bin/a57.rs
+++ tessoku-book/src/bin/a57.rs
@@ -10,7 +10,31 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
+        av: [Usize1; n],
+        xyv: [(Usize1, i64); q]
     };
+
+    let mut doubling = Vec::new();
+    doubling.push(av);
+    for _ in 0..30 {
+        let prev = doubling.last().unwrap();
+        let av = prev.iter().copied().map(|a| prev[a]).collect::<Vec<_>>();
+        doubling.push(av);
+    }
+    for (x, y) in xyv {
+        let mut pos = x;
+        let mut rest = y;
+        while rest > 0 {
+            let mut i = 0_usize;
+            while 2.pow((i + 1) as u32) < rest {
+                i += 1;
+            }
+            rest -= 2.pow(i as u32);
+            pos = doubling[i][pos];
+        }
+        println!("{}", pos + 1);
+    }
 }
 
 fn main() {
