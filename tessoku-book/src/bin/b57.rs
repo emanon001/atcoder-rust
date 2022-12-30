bb6180c5a25d6336bfb2b9611ce38f17911f02f8
emanon001 <emanon001@gmail.com>
2022-12-30 23:27:19 +0900

    update tessoku-book/b57

--- tessoku-book/src/bin/b57.rs
+++ tessoku-book/src/bin/b57.rs
@@ -8,9 +8,40 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn sum(n: usize) -> usize {
+    n.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .sum()
+}
+
 fn solve() {
     input! {
+        n: usize, k: usize,
     };
+    let mut dp = vec![vec![0; n + 1]; 30];
+    for j in 0..=n {
+        let k = j - sum(j);
+        dp[0][j] = k;
+    }
+    for i in 1..30 {
+        for j in 0..=n {
+            let k = dp[i - 1][dp[i - 1][j]];
+            dp[i][j] = k;
+        }
+    }
+
+    for n in 1..=n {
+        let mut n = n;
+        let mut rest = k;
+        for d in (0..30).rev() {
+            if rest & 2.pow(d) != 0 {
+                rest -= 2.pow(d);
+                n = dp[d as usize][n];
+            }
+        }
+        println!("{}", n);
+    }
 }
 
 fn main() {
