b491743030b15c0639daf59fda541cdf54cea78f
emanon001 <emanon001@gmail.com>
2022-12-31 04:30:24 +0900

    update tessoku-book/b59 (BIT)

--- tessoku-book/src/bin/b59.rs
+++ tessoku-book/src/bin/b59.rs
@@ -99,6 +99,62 @@ impl Monoid for i64 {
     }
 }
 
+/// `T` is numeric only
+pub struct Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    n: usize,
+    data: Vec<T>,
+}
+/// 0-origin
+/// [0, n)
+impl<T> Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    pub fn new(n: usize) -> Self {
+        Self {
+            n,
+            data: vec![T::zero(); n + 1],
+        }
+    }
+    /// 0-origin
+    pub fn add(&mut self, i: usize, x: T) {
+        if i >= self.n {
+            panic!();
+        }
+        let mut i = i + 1;
+        while i <= self.n {
+            self.data[i] += x.clone();
+            i += ((i as isize) & -(i as isize)) as usize;
+        }
+    }
+    /// [0, i)
+    pub fn sum(&self, i: usize) -> T {
+        if i > self.n {
+            panic!();
+        }
+        let mut i = i;
+        let mut res = T::zero();
+        while i > 0 {
+            res += self.data[i].clone();
+            i -= ((i as isize) & -(i as isize)) as usize;
+        }
+        res
+    }
+    /// [i, j)
+    pub fn range_sum(&self, i: usize, j: usize) -> T {
+        if i > self.n || j > self.n {
+            panic!();
+        }
+        if i >= j {
+            return T::zero();
+        }
+        self.sum(j) - self.sum(i)
+    }
+}
+
 fn solve() {
     input! {
         n: usize,
@@ -115,12 +171,27 @@ fn solve() {
     println!("{}", res);
 }
 
+fn solve_bit() {
+    input! {
+        n: usize,
+        av: [usize; n]
+    };
+
+    let mut bit = Bit::new(n + 1);
+    let mut res = 0_u64;
+    for a in av {
+        res += bit.range_sum(a + 1, n + 1);
+        bit.add(a, 1);
+    }
+    println!("{}", res);
+}
+
 fn main() {
     std::thread::Builder::new()
         .name("big stack size".into())
         .stack_size(256 * 1024 * 1024)
         .spawn(|| {
-            solve();
+            solve_bit();
         })
         .unwrap()
         .join()
