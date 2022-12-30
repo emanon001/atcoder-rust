f3936861dfe641b3cd4d57f4e2a42af68cfae47d
emanon001 <emanon001@gmail.com>
2022-12-31 08:06:52 +0900

    tessoku-book/a60

--- tessoku-book/src/bin/a60.rs
+++ tessoku-book/src/bin/a60.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let order = av
+        .into_iter()
+        .enumerate()
+        .sorted_by_key(|(_, a)| *a)
+        .collect::<Vec<_>>();
+    let mut set = (0..n).into_iter().collect::<BTreeSet<_>>();
+    let mut res = vec![-1; n];
+    for (i, _) in order {
+        let j = set.range(..i).next_back();
+        let j = if let Some(&j) = j { j as isize + 1 } else { -1 };
+        res[i] = j;
+        set.remove(&i);
+    }
+    println!("{}", res.iter().join(" "));
 }
 
 fn main() {
