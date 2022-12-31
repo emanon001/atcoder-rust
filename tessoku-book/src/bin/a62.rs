8d7cf93cd2cca059344bcb01109d039c5dc0382c
emanon001 <emanon001@gmail.com>
2023-01-01 00:58:25 +0900

    tessoku-book/a62

--- tessoku-book/src/bin/a62.rs
+++ tessoku-book/src/bin/a62.rs
@@ -10,7 +10,33 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut visited = vec![false; n];
+    dfs(0, &graph, &mut visited);
+    let res = if visited.into_iter().all(|b| b) {
+        "The graph is connected."
+    } else {
+        "The graph is not connected."
+    };
+    println!("{}", res);
+}
+
+fn dfs(u: usize, graph: &[Vec<usize>], visited: &mut [bool]) {
+    visited[u] = true;
+    for &v in &graph[u] {
+        if visited[v] {
+            continue;
+        }
+        dfs(v, graph, visited);
+    }
 }
 
 fn main() {
