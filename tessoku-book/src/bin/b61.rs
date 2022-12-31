e57c47bfd14bd4f433764ca6f313bb47fa13cc85
emanon001 <emanon001@gmail.com>
2023-01-01 00:49:29 +0900

    tessoku-book/b61

--- tessoku-book/src/bin/b61.rs
+++ tessoku-book/src/bin/b61.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let res = (1..=n)
+        .into_iter()
+        .sorted_by_key(|i| graph[i - 1].len())
+        .last()
+        .unwrap();
+    println!("{}", res);
 }
 
 fn main() {
