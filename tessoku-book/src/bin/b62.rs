a7f2344a004a98c28e1d4b9c508a83a9d6c3c6e7
emanon001 <emanon001@gmail.com>
2023-01-01 01:11:50 +0900

    tessoku-book/b62

--- tessoku-book/src/bin/b62.rs
+++ tessoku-book/src/bin/b62.rs
@@ -10,7 +10,34 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut visited = vec![false; n];
+    let mut path = Vec::new();
+    dfs(0, &mut path, &mut visited, &graph);
+}
+
+fn dfs(u: usize, path: &mut Vec<usize>, visited: &mut [bool], graph: &[Vec<usize>]) {
+    visited[u] = true;
+    path.push(u);
+    if u == graph.len() - 1 {
+        println!("{}", path.iter().map(|v| v + 1).join(" "));
+        return;
+    }
+    for &v in &graph[u] {
+        if visited[v] {
+            continue;
+        }
+        dfs(v, path, visited, graph);
+        path.pop();
+    }
 }
 
 fn main() {
