1fcad26124dce2339df0eca4bf04d810d151b034
emanon001 <emanon001@gmail.com>
2023-01-01 09:02:29 +0900

    tessoku-book/a65

--- tessoku-book/src/bin/a65.rs
+++ tessoku-book/src/bin/a65.rs
@@ -10,7 +10,31 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [Usize1; n - 1]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for u in 1..n {
+        let v = av[u - 1];
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut dp = vec![0; n];
+    dfs(0, 1 << 30, &graph, &mut dp);
+    println!("{}", dp.iter().join(" "));
+}
+
+fn dfs(u: usize, parent: usize, graph: &[Vec<usize>], dp: &mut [usize]) -> usize {
+    let mut res = 0;
+    for &v in &graph[u] {
+        if v == parent {
+            continue;
+        }
+        res += dfs(v, u, graph, dp) + 1;
+    }
+    dp[u] = res;
+    res
 }
 
 fn main() {
