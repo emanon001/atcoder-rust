d5b0c949c45a6aabd4698cac1e6d807aae0444d8
emanon001 <emanon001@gmail.com>
2023-01-01 09:08:31 +0900

    tessoku-book/b65

--- tessoku-book/src/bin/b65.rs
+++ tessoku-book/src/bin/b65.rs
@@ -8,9 +8,45 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize, t: Usize1,
+        abv: [(Usize1, Usize1); n - 1]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in abv {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut dp = vec![0; n];
+    dfs(t, 1 << 30, &graph, &mut dp);
+    println!("{}", dp.iter().join(" "));
+}
+
+fn dfs(u: usize, parent: usize, graph: &[Vec<usize>], dp: &mut [usize]) -> usize {
+    let mut res = 0;
+    for &v in &graph[u] {
+        if v == parent {
+            continue;
+        }
+        chmax!(res, dfs(v, u, graph, dp) + 1);
+    }
+    dp[u] = res;
+    res
 }
 
 fn main() {
