7b377bbea28bc3edd96b076107fd6a62e54318f6
emanon001 <emanon001@gmail.com>
2023-01-09 00:13:41 +0900

    tessoku-book/a75

--- tessoku-book/src/bin/a75.rs
+++ tessoku-book/src/bin/a75.rs
@@ -8,9 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        mut tdv: [(usize, usize); n]
     };
+
+    tdv.sort_by_key(|(_, d)| *d);
+    let max_time = 1440;
+    let mut dp = vec![vec![0; max_time + 1]; n + 1];
+    for (i, (t, v)) in tdv.into_iter().enumerate() {
+        for j in 0..=max_time {
+            chmax!(dp[i + 1][j], dp[i][j]);
+            if j + t <= v {
+                chmax!(dp[i + 1][j + t], dp[i][j] + 1);
+            }
+        }
+    }
+    println!("{}", dp[n].iter().max().unwrap());
 }
 
 fn main() {
