2765b55e362deaa288e45b3880995048d26ff0e1
emanon001 <emanon001@gmail.com>
2023-01-09 18:32:39 +0900

    tessoku-book/c04

--- tessoku-book/src/bin/c04.rs
+++ tessoku-book/src/bin/c04.rs
@@ -8,9 +8,30 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: u64) -> Vec<u64> {
+    let mut res = Vec::new();
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            res.push(x);
+            let y = n / x;
+            if y != x {
+                res.push(y);
+            }
+        }
+        x += 1;
+    }
+    res
+}
+
 fn solve() {
     input! {
+        n: u64
     };
+
+    let mut divisors = divisors(n);
+    divisors.sort();
+    println!("{}", divisors.iter().join("\n"));
 }
 
 fn main() {
