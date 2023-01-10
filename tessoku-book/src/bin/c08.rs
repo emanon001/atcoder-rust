1aa6fb12ff4be4b6f20ea5a5e49d78c8342adca3
emanon001 <emanon001@gmail.com>
2023-01-10 23:18:19 +0900

    tessoku-book/c08

--- tessoku-book/src/bin/c08.rs
+++ tessoku-book/src/bin/c08.rs
@@ -10,7 +10,30 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        stv: [(usize, usize); n]
     };
+
+    let mut res = Vec::new();
+    for tosen in 0..10000 {
+        let tosen = format!("{:0>4}", tosen).chars().collect::<Vec<_>>();
+        let valid = stv.iter().all(|&(s, t)| {
+            let s = format!("{:0>4}", s).chars().collect::<Vec<_>>();
+            let mut same_count = 0;
+            for i in 0..4 {
+                same_count += if tosen[i] == s[i] { 1 } else { 0 };
+            }
+            (same_count == 4 && t == 1) || (same_count == 3 && t == 2) || (same_count < 3 && t == 3)
+        });
+        if valid {
+            res.push(tosen.iter().join(""));
+        }
+    }
+    if res.len() >= 2 {
+        println!("Can't Solve");
+    } else {
+        println!("{}", res[0]);
+    }
 }
 
 fn main() {
