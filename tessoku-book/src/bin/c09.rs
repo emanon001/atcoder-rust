a6d6fd060c19f4e4d514fc5542126e570a5e4edb
emanon001 <emanon001@gmail.com>
2023-01-11 23:53:40 +0900

    tessoku-book/c09

--- tessoku-book/src/bin/c09.rs
+++ tessoku-book/src/bin/c09.rs
@@ -8,9 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let mut dp = vec![vec![0; 2]; n + 1];
+    for i in 0..n {
+        // 勉強しない
+        chmax!(dp[i + 1][0], dp[i][0]);
+        chmax!(dp[i + 1][0], dp[i][1]);
+
+        // 勉強する
+        chmax!(dp[i + 1][1], dp[i][0] + av[i]);
+    }
+
+    let res = dp[n][0].max(dp[n][1]);
+    println!("{}", res);
 }
 
 fn main() {
