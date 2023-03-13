b30cac071436f19c2b3f8c1805c02b52b23ace88
emanon001 <emanon001@gmail.com>
2023-03-14 04:53:26 +0900

    abc234/b

--- abc234/src/bin/b.rs
+++ abc234/src/bin/b.rs
@@ -8,9 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        xyv: [(f64, f64); n]
     };
+
+    let mut res = 0_f64;
+    for i in 0..n {
+        let p1 = xyv[i];
+        for j in i + 1..n {
+            let p2 = xyv[j];
+            let a = (p1.0 - p2.0).abs();
+            let b = (p1.1 - p2.1).abs();
+            let d = ((a * a) + (b * b)).sqrt();
+            chmax!(res, d);
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
