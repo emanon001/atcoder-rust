ef5b233c125e529dd17bc88be57c931463e4d6ed
emanon001 <emanon001@gmail.com>
2023-03-14 05:38:49 +0900

    abc234/d

--- abc234/src/bin/d.rs
+++ abc234/src/bin/d.rs
@@ -5,12 +5,34 @@ use num::*;
 use proconio::input;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use std::cmp::Reverse;
 #[allow(unused_imports)]
 use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        pv: [i64; n]
     };
+
+    let mut res = Vec::new();
+    let mut heap = BinaryHeap::new();
+    for i in 0..k {
+        heap.push(Reverse(pv[i]));
+    }
+    let Reverse(x) = *heap.peek().unwrap();
+    println!("{}", x);
+    for i in k..n {
+        let p = pv[i];
+        let Reverse(x) = *heap.peek().unwrap();
+        if p > x {
+            heap.pop();
+            heap.push(Reverse(p));
+        }
+        let Reverse(x) = *heap.peek().unwrap();
+        res.push(x);
+    }
+    println!("{}", res.iter().join("\n"));
 }
 
 fn main() {
