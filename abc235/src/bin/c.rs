16fd931756cd3ef019218fcae655e1e55db0e5e0
emanon001 <emanon001@gmail.com>
2023-03-18 23:51:22 +0900

    abc235/c

--- abc235/src/bin/c.rs
+++ abc235/src/bin/c.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
+        av: [usize; n],
+        xkv: [(usize, usize); q]
     };
+
+    let mut count_map = HashMap::new();
+    let mut position_map = HashMap::new();
+    for i in 0..n {
+        let a = av[i];
+        let c = count_map.entry(a).or_insert(0);
+        *c += 1;
+        let v = i + 1;
+        position_map.insert((a, *c), v);
+    }
+    for xk in xkv {
+        let res = if let Some(&v) = position_map.get(&xk) {
+            v as isize
+        } else {
+            -1
+        };
+        println!("{}", res);
+    }
 }
 
 fn main() {
