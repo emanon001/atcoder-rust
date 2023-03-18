fba24de94ef2193baed564beae19ba9b10d2c2bb
emanon001 <emanon001@gmail.com>
2023-03-19 02:57:33 +0900

    abc237/c

--- abc237/src/bin/c.rs
+++ abc237/src/bin/c.rs
@@ -10,7 +10,25 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars
     };
+
+    let mut s = s.into_iter().collect::<VecDeque<_>>();
+    let front_a_count = s.iter().take_while(|&c| c == &'a').count();
+    let back_a_count = s.iter().rev().take_while(|&c| c == &'a').count();
+    if front_a_count > back_a_count {
+        println!("No");
+        return;
+    }
+    for _ in 0..(back_a_count - front_a_count) {
+        s.push_front('a');
+    }
+    let res = if s.iter().join("") == s.iter().rev().join("") {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", res);
 }
 
 fn main() {
