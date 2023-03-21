895f26ba42af9a575294ff85cf64fcb2f06355fe
emanon001 <emanon001@gmail.com>
2023-03-21 20:38:11 +0900

    abc238/b

--- abc238/src/bin/b.rs
+++ abc238/src/bin/b.rs
@@ -8,9 +8,38 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let mut cur = 0;
+    let mut v = vec![cur];
+    for a in av {
+        cur = (cur + a) % 360;
+        v.push(cur);
+    }
+    v.sort();
+    v.push(360);
+    let mut res = 0;
+    for (a, b) in v.into_iter().tuple_windows() {
+        chmax!(res, b - a);
+    }
+    println!("{}", res)
 }
 
 fn main() {
