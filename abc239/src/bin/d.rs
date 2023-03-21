1db5558f250e3ee54b8fa6079ae9eee54801fa63
emanon001 <emanon001@gmail.com>
2023-03-22 00:29:12 +0900

    abc239/d

--- abc239/src/bin/d.rs
+++ abc239/src/bin/d.rs
@@ -8,9 +8,35 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn is_prime(n: u64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
 fn solve() {
     input! {
+        a: u64, b: u64, c: u64, d: u64
     };
+
+    let takahashi_win = (a..=b).any(|x| {
+        for y in c..=d {
+            if is_prime(x + y) {
+                return false;
+            }
+        }
+        true
+    });
+    let res = if takahashi_win { "Takahashi" } else { "Aoki" };
+    println!("{}", res);
 }
 
 fn main() {
