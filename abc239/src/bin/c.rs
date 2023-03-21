72ca4335d21d743f21aab2f5f262cc0d2fa98174
emanon001 <emanon001@gmail.com>
2023-03-22 00:21:35 +0900

    abc239/c

--- abc239/src/bin/c.rs
+++ abc239/src/bin/c.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        x1: i64, y1: i64, x2: i64, y2: i64,
     };
+
+    for ax in -5_i64..=5_i64 {
+        for ay in -5_i64..=5_i64 {
+            let x3 = x1 + ax;
+            let y3 = y1 + ay;
+            let d1 = (x1 - x3).pow(2) + (y1 - y3).pow(2);
+            let d2 = (x2 - x3).pow(2) + (y2 - y3).pow(2);
+            if d1 == 5 && d2 == 5 {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
 
 fn main() {
