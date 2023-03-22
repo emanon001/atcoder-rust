aa3573bea1d87bd774331524bc3478a400650901
emanon001 <emanon001@gmail.com>
2023-03-23 06:04:53 +0900

    abc240/c

--- abc240/src/bin/c.rs
+++ abc240/src/bin/c.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, x: usize,
+        abv: [(usize, usize); n]
     };
+
+    let max_pos = 100 * 100;
+    let mut dp = vec![vec![false; max_pos + 10]; n + 1];
+    dp[0][0] = true;
+    for i in 0..n {
+        let (a, b) = abv[i];
+        for j in 0..=max_pos {
+            if j + a <= max_pos {
+                dp[i + 1][j + a] |= dp[i][j];
+            }
+            if j + b <= max_pos {
+                dp[i + 1][j + b] |= dp[i][j];
+            }
+        }
+    }
+    let res = if dp[n][x] { "Yes" } else { "No" };
+    println!("{}", res);
 }
 
 fn main() {
