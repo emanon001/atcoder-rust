7b57cdcd7534828307bfdbb2f98d7ecb4ce2be1b
emanon001 <emanon001@gmail.com>
2023-03-23 06:19:41 +0900

    abc240/d

--- abc240/src/bin/d.rs
+++ abc240/src/bin/d.rs
@@ -10,7 +10,33 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let mut v = Vec::new();
+    let mut count = 0_i64;
+    for a in av {
+        if let Some((pa, pc)) = v.pop() {
+            if pa == a {
+                if pc + 1 == a {
+                    // already pop
+                    count -= pc;
+                } else {
+                    v.push((a, pc + 1));
+                    count += 1;
+                }
+            } else {
+                v.push((pa, pc));
+                v.push((a, 1));
+                count += 1;
+            }
+        } else {
+            v.push((a, 1));
+            count = 1;
+        }
+        println!("{}", count);
+    }
 }
 
 fn main() {
