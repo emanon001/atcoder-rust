a4ad49c976d5cd2925e839297175494b6435a50f
emanon001 <emanon001@gmail.com>
2023-03-25 00:29:39 +0900

    abc241/d

--- abc241/src/bin/d.rs
+++ abc241/src/bin/d.rs
@@ -10,7 +10,53 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+    let mut id = 1;
+    let mut set = BTreeSet::new();
+    for _ in 0..q {
+        input! { kind: usize }
+        match kind {
+            1 => {
+                input! {
+                    x: i64
+                }
+                set.insert((x, id));
+                id += 1;
+            }
+            2 => {
+                input! {
+                    x: i64, k: usize
+                }
+                let mut iter = set.range(..=(x, 10.pow(9)));
+                for _ in 0..k - 1 {
+                    iter.next_back();
+                }
+                let res = if let Some(res) = iter.next_back() {
+                    res.0
+                } else {
+                    -1_i64
+                };
+                println!("{}", res);
+            }
+            3 => {
+                input! {
+                    x: i64, k: usize
+                }
+                let mut iter = set.range((x, 0)..);
+                for _ in 0..k - 1 {
+                    iter.next();
+                }
+                let res = if let Some(res) = iter.next() {
+                    res.0
+                } else {
+                    -1_i64
+                };
+                println!("{}", res);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
