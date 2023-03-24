a8d7cdb7f2c32444fa5891d582f6f954d541d80e
emanon001 <emanon001@gmail.com>
2023-03-24 23:40:18 +0900

    abc241/a

--- abc241/src/bin/a.rs
+++ abc241/src/bin/a.rs
@@ -10,7 +10,13 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        av: [usize; 10]
     };
+
+    let x1 = av[0];
+    let x2 = av[x1];
+    let x3 = av[x2];
+    println!("{}", x3);
 }
 
 fn main() {
