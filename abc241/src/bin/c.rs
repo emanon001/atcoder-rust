bea56003631eb178270fe25f49da64b4c9fcc4b1
emanon001 <emanon001@gmail.com>
2023-03-25 00:03:35 +0900

    abc241/c

--- abc241/src/bin/c.rs
+++ abc241/src/bin/c.rs
@@ -8,9 +8,68 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn check(i: usize, j: usize, grid: &[Vec<char>]) -> bool {
+    let n = grid.len();
+    let fill_count = 6;
+    // 縦
+    if i + fill_count <= n {
+        let mut white_count = 0;
+        for di in 0..fill_count {
+            white_count += if grid[i + di][j] == '.' { 1 } else { 0 };
+        }
+        if white_count <= 2 {
+            return true;
+        }
+    }
+
+    // 横
+    if j + fill_count <= n {
+        let mut white_count = 0;
+        for dj in 0..fill_count {
+            white_count += if grid[i][j + dj] == '.' { 1 } else { 0 };
+        }
+        if white_count <= 2 {
+            return true;
+        }
+    }
+
+    // 斜め
+    if i + fill_count <= n && j + fill_count <= n {
+        let mut white_count = 0;
+        for dij in 0..fill_count {
+            white_count += if grid[i + dij][j + dij] == '.' { 1 } else { 0 };
+        }
+        if white_count <= 2 {
+            return true;
+        }
+    }
+    if i + fill_count <= n && j >= fill_count - 1 {
+        let mut white_count = 0;
+        for dij in 0..fill_count {
+            white_count += if grid[i + dij][j - dij] == '.' { 1 } else { 0 };
+        }
+        if white_count <= 2 {
+            return true;
+        }
+    }
+    false
+}
+
 fn solve() {
     input! {
+        n: usize,
+        grid: [Chars; n]
     };
+
+    for i in 0..n {
+        for j in 0..n {
+            if check(i, j, &grid) {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
 
 fn main() {
