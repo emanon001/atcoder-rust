1531da0b430c675fff572b321104553f9e753cb0
emanon001 <emanon001@gmail.com>
2023-03-28 02:50:50 +0900

    abc243/d

--- abc243/src/bin/d.rs
+++ abc243/src/bin/d.rs
@@ -10,7 +10,43 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        _: usize, x: u64,
+        s: Chars
     };
+
+    let mut route = VecDeque::new();
+    let mut x = x;
+    while x > 1 {
+        if x % 2 == 0 {
+            route.push_front('L');
+        } else {
+            route.push_front('R');
+        }
+        x /= 2;
+    }
+    for ch in s {
+        match ch {
+            'U' => {
+                route.pop_back();
+            }
+            'L' => {
+                route.push_back('L');
+            }
+            'R' => {
+                route.push_back('R');
+            }
+            _ => unreachable!(),
+        }
+    }
+    let mut res = 1_u64;
+    for c in route {
+        if c == 'L' {
+            res *= 2;
+        } else {
+            res = res * 2 + 1;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
