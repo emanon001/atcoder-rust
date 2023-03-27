1fb535d85d9f72d596d3c5c2aaa34e79a0dd35d8
emanon001 <emanon001@gmail.com>
2023-03-28 02:35:33 +0900

    abc243/c

--- abc243/src/bin/c.rs
+++ abc243/src/bin/c.rs
@@ -10,7 +10,42 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        xyv: [(i64, i64); n],
+        s: Chars
     };
+
+    let mut y_map = HashMap::new();
+    for i in 0..n {
+        let (x, y) = xyv[i];
+        let ch = s[i];
+        y_map.entry(y).or_insert(Vec::new()).push((x, ch));
+    }
+    for (_, mut v) in y_map {
+        if v.len() <= 1 {
+            continue;
+        }
+        v.sort();
+        let mut l = 10_i64.pow(10);
+        for (x, c) in &v {
+            if c == &'R' {
+                l = *x;
+                break;
+            }
+        }
+        let mut r = -1;
+        for (x, c) in v.iter().rev() {
+            if c == &'L' {
+                r = *x;
+                break;
+            }
+        }
+        if l <= r {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
