1bb298785a20463837ea0258118893cb48f1c97c
emanon001 <emanon001@gmail.com>
2023-03-30 04:46:43 +0900

    abc244/d

--- abc244/src/bin/d.rs
+++ abc244/src/bin/d.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s1: char, s2: char, s3: char,
+        t1: char, t2: char, t3: char,
     };
+
+    let mut c = 0;
+    if s1 == t1 {
+        c += 1;
+    }
+    if s2 == t2 {
+        c += 1;
+    }
+    if s3 == t3 {
+        c += 1;
+    }
+    let res = if c == 1 { "No" } else { "Yes" };
+    println!("{}", res);
 }
 
 fn main() {
