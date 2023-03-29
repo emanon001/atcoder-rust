d89cf49494b533ae615a783869f2166ab9524619
emanon001 <emanon001@gmail.com>
2023-03-30 04:27:54 +0900

    abc244/c

--- abc244/src/bin/c.rs
+++ abc244/src/bin/c.rs
@@ -8,9 +8,21 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+use whiteread;
+
 fn solve() {
-    input! {
-    };
+    let n: usize = whiteread::parse_line().unwrap();
+    let mut candidates = (1..=n * 2 + 1).collect::<BTreeSet<usize>>();
+    loop {
+        let m = *candidates.iter().next().unwrap();
+        println!("{}", m);
+        candidates.remove(&m);
+        let m: usize = whiteread::parse_line().unwrap();
+        if m == 0 {
+            return;
+        }
+        candidates.remove(&m);
+    }
 }
 
 fn main() {
