588473d7d87a525f378028038c232ef6977ad331
emanon001 <emanon001@gmail.com>
2023-04-01 23:04:52 +0900

    abc245/c

--- abc245/src/bin/c.rs
+++ abc245/src/bin/c.rs
@@ -10,7 +10,31 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: i64,
+        av: [i64; n],
+        bv: [i64; n],
     };
+
+    let mut xv = vec![av[0], bv[0]].into_iter().collect::<HashSet<_>>();
+    for i in 1..n {
+        let mut next_xv = HashSet::new();
+        for x in xv {
+            let a = av[i];
+            let b = bv[i];
+            if (x - a).abs() <= k {
+                next_xv.insert(a);
+            }
+            if (x - b).abs() <= k {
+                next_xv.insert(b);
+            }
+        }
+        if next_xv.is_empty() {
+            println!("No");
+            return;
+        }
+        xv = next_xv;
+    }
+    println!("Yes");
 }
 
 fn main() {
