50b859157078dd87514a96cab09d5536c447b849
emanon001 <emanon001@gmail.com>
2023-04-02 22:56:13 +0900

    abc246/a

--- abc246/src/bin/a.rs
+++ abc246/src/bin/a.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        x1: isize, y1: isize,
+        x2: isize, y2: isize,
+        x3: isize, y3: isize,
     };
+
+    let x4 = if x1 == x2 {
+        x3
+    } else if x1 == x3 {
+        x2
+    } else {
+        x1
+    };
+    let y4 = if y1 == y2 {
+        y3
+    } else if y1 == y3 {
+        y2
+    } else {
+        y1
+    };
+    println!("{} {}", x4, y4);
 }
 
 fn main() {
