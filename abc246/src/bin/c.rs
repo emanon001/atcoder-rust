5432884a0506457c3838488888cc76293cee4644
emanon001 <emanon001@gmail.com>
2023-04-09 00:35:03 +0900

    abc246/c

--- abc246/src/bin/c.rs
+++ abc246/src/bin/c.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize, x: i64,
+        av: [i64; n]
     };
+
+    let mut rest_count = k as i64;
+    let mut av2 = Vec::new();
+    for a in av {
+        let c = (a / x).min(rest_count);
+        av2.push(a - x * c);
+        rest_count -= c;
+    }
+    av2.sort_by(|a, b| b.cmp(a));
+    let mut res = 0_i64;
+    for a in av2 {
+        let c = ((a + x - 1) / x).min(rest_count);
+        if a > c * x {
+            res += a - c * x;
+        }
+        rest_count -= c;
+    }
+    println!("{}", res);
 }
 
 fn main() {
