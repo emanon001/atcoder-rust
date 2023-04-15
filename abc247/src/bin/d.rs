da0604ad8f8c443058a2ba185763f43f6c470f83
emanon001 <emanon001@gmail.com>
2023-04-16 00:33:25 +0900

    abc247/d

--- abc247/src/bin/d.rs
+++ abc247/src/bin/d.rs
@@ -10,7 +10,41 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+
+    let mut v = VecDeque::new();
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    x: i64, c: i64
+                };
+                v.push_back((x, c));
+            }
+            2 => {
+                input! {
+                    c: i64
+                };
+                let mut sum = 0_i64;
+                let mut rest_count = c;
+                while rest_count > 0 {
+                    let (x, c2) = v.pop_front().unwrap();
+                    let choice_c = (c2).min(rest_count);
+                    rest_count -= choice_c;
+                    sum += x * choice_c;
+                    if choice_c < c2 {
+                        v.push_front((x, c2 - choice_c));
+                    }
+                }
+                println!("{}", sum);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
