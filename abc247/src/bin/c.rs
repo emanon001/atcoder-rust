3c1bd4201797f6d424f3bbc652417cd055743820
emanon001 <emanon001@gmail.com>
2023-04-16 00:25:41 +0900

    abc247/c

--- abc247/src/bin/c.rs
+++ abc247/src/bin/c.rs
@@ -8,9 +8,26 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn dfs(n: usize, dp: &mut [Option<Vec<String>>]) -> Vec<String> {
+    if n == 1 {
+        return vec!["1".to_string()];
+    }
+    if let Some(res) = &dp[n] {
+        return res.clone();
+    }
+    let mut res = dfs(n - 1, dp);
+    res.push(n.to_string());
+    res.append(&mut dfs(n - 1, dp));
+    dp[n] = Some(res.clone());
+    res
+}
+
 fn solve() {
     input! {
+        n: usize
     };
+    let mut dp = vec![None; n + 1];
+    println!("{}", dfs(n, &mut dp).iter().join(" "));
 }
 
 fn main() {
