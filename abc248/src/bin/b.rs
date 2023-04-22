cf2334aff6b9b8bcc132bd365da6e4a6cd11955c
emanon001 <emanon001@gmail.com>
2023-04-22 17:45:03 +0900

    abc248/b

--- abc248/src/bin/b.rs
+++ abc248/src/bin/b.rs
@@ -10,7 +10,15 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        mut a: i64, b: i64, k: i64,
     };
+
+    let mut res = 0;
+    while a < b {
+        a *= k;
+        res += 1;
+    }
+    println!("{}", res);
 }
 
 fn main() {
