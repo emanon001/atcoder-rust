bbaeac0a92d11b9de7c4c265abb56f1772667293
emanon001 <emanon001@gmail.com>
2023-04-23 23:00:59 +0900

    abc249/c

--- abc249/src/bin/c.rs
+++ abc249/src/bin/c.rs
@@ -8,9 +8,40 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize, k: usize,
+        sv: [Chars; n]
     };
+
+    let mut res = 0;
+    for bits in 0..(1 << n) {
+        let mut map = HashMap::new();
+        for i in 0..n {
+            if (bits >> i) & 1 == 0 {
+                continue;
+            }
+            for &ch in &sv[i] {
+                *map.entry(ch).or_insert(0_usize) += 1;
+            }
+        }
+        let c = map.into_iter().filter(|(_, v)| v == &k).count();
+        chmax!(res, c);
+    }
+    println!("{}", res);
 }
 
 fn main() {
