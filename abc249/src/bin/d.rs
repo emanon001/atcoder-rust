4c195432146c99c8b0d7195553fd7d9e84a79ec6
emanon001 <emanon001@gmail.com>
2023-04-24 23:54:37 +0900

    abc249/d

--- abc249/src/bin/d.rs
+++ abc249/src/bin/d.rs
@@ -10,7 +10,37 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n]
     };
+
+    let mut counts: HashMap<usize, u64> = HashMap::new();
+    let mut map: HashMap<usize, HashSet<usize>> = HashMap::new();
+    for &a in &av {
+        let mut m = a;
+        *counts.entry(a).or_insert(0) += 1;
+        while m <= 2 * 10.pow(5) {
+            if map.contains_key(&m) && map[&m].contains(&a) {
+                break;
+            }
+            map.entry(m).or_insert(HashSet::new()).insert(a);
+            m += a;
+        }
+    }
+    let mut res = 0_u64;
+    for (a, a_count) in &counts {
+        let set = &map[a];
+        for b in set {
+            let b_count = &counts[b];
+            let c = a / b;
+            if !counts.contains_key(&c) {
+                continue;
+            }
+            let c_count = &counts[&c];
+            res += a_count * b_count * c_count;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
