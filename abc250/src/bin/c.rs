8a9b0434fd457dd9026474959d67e05ea3771ec0
emanon001 <emanon001@gmail.com>
2023-04-27 23:21:57 +0900

    abc250/c

--- abc250/src/bin/c.rs
+++ abc250/src/bin/c.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
+        xv: [Usize1; q]
     };
+
+    let mut idx_to_value = (0..n).collect::<Vec<usize>>();
+    let mut value_to_idx = (0..n).collect::<Vec<usize>>();
+    for x in xv {
+        let idx = value_to_idx[x];
+        let idx2 = if idx == n - 1 { idx - 1 } else { idx + 1 };
+        let y = idx_to_value[idx2];
+        value_to_idx[x] = idx2;
+        value_to_idx[y] = idx;
+        idx_to_value[idx] = y;
+        idx_to_value[idx2] = x;
+    }
+    println!("{}", idx_to_value.iter().map(|x| x + 1).join(" "));
 }
 
 fn main() {
