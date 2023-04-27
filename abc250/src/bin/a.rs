bdcf563a00742bf85bdaa759669dee4c706f64c3
emanon001 <emanon001@gmail.com>
2023-04-27 22:50:08 +0900

    abc250/a

--- abc250/src/bin/a.rs
+++ abc250/src/bin/a.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: isize, w: isize,
+        r: isize, c: isize,
     };
+
+    let mut res = 0;
+    if h > 1 && r > 1 {
+        res += 1;
+    }
+    if h > 1 && r < h {
+        res += 1;
+    }
+    if w > 1 && c > 1 {
+        res += 1;
+    }
+    if w > 1 && c < w {
+        res += 1;
+    }
+    println!("{}", res);
 }
 
 fn main() {
