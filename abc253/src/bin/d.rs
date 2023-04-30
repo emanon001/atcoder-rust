855679eeacb5ae4a509aca4e9a30a4032b9acd1a
emanon001 <emanon001@gmail.com>
2023-04-30 22:40:48 +0900

    abc253/d

--- abc253/src/bin/d.rs
+++ abc253/src/bin/d.rs
@@ -10,7 +10,18 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: i128, a: i128, b: i128
     };
+
+    let a_count = n / a;
+    let b_count = n / b;
+    let ab = a.lcm(&b);
+    let ab_count = n / ab;
+    let res = ((1 + n) * n / 2)
+        - (a * ((1 + a_count) * a_count / 2))
+        - (b * ((1 + b_count) * b_count / 2))
+        + (ab * ((1 + ab_count) * ab_count / 2));
+    println!("{}", res);
 }
 
 fn main() {
