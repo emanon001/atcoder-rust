c0b28186eb28554811fae94003e4dfb00419c6c0
emanon001 <emanon001@gmail.com>
2023-04-30 22:40:43 +0900

    abc253/c

--- abc253/src/bin/c.rs
+++ abc253/src/bin/c.rs
@@ -10,7 +10,40 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        q: usize,
     };
+
+    let mut map = BTreeMap::new();
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    x: usize
+                };
+                *map.entry(x).or_insert(0) += 1;
+            }
+            2 => {
+                input! {
+                    x: usize, c: usize
+                };
+                if let Some(v) = map.get_mut(&x) {
+                    *v -= (*v).min(c);
+                    if *v == 0 {
+                        map.remove(&x);
+                    }
+                }
+            }
+            3 => {
+                let (min, _) = map.iter().next().unwrap();
+                let (max, _) = map.iter().next_back().unwrap();
+                println!("{}", max - min);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
