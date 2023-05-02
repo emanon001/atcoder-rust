22eca31cbe975d170924160cda8bd8e0e00a8cef
emanon001 <emanon001@gmail.com>
2023-05-03 05:53:32 +0900

    abc240/e

--- abc240/src/bin/e.rs
+++ abc240/src/bin/e.rs
@@ -8,9 +8,35 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn dfs(graph: &[Vec<usize>], u: usize, p: usize, max: usize, res: &mut [(usize, usize)]) -> usize {
+    let l = max;
+    let mut r = max;
+    for &v in &graph[u] {
+        if v == p {
+            continue;
+        }
+        r = dfs(graph, v, u, r, res);
+    }
+    r = (r - 1).max(l);
+    res[u] = (l, r);
+    r + 1
+}
+
 fn solve() {
     input! {
+        n: usize,
+        edges: [(Usize1, Usize1); n - 1]
     };
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut res = vec![(0, 0); n];
+    dfs(&graph, 0, 0, 1, &mut res);
+    for (l, r) in res {
+        println!("{} {}", l, r);
+    }
 }
 
 fn main() {
