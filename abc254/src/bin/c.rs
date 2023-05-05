923edb7b1d7583859a46c892f7bbf3801771bd53
emanon001 <emanon001@gmail.com>
2023-05-06 00:07:27 +0900

    abc254/c

--- abc254/src/bin/c.rs
+++ abc254/src/bin/c.rs
@@ -10,7 +10,28 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        av: [usize; n]
     };
+
+    let sorted = av.iter().sorted().copied().collect::<Vec<_>>();
+    let mut actual = vec![0; n];
+    for i in 0..k {
+        let mut v = Vec::new();
+        let mut pos = i;
+        while pos < n {
+            v.push(av[pos]);
+            pos += k;
+        }
+        v.sort();
+        let mut pos = i;
+        for a in v {
+            actual[pos] = a;
+            pos += k;
+        }
+    }
+    let res = if actual == sorted { "Yes" } else { "No" };
+    println!("{}", res);
 }
 
 fn main() {
