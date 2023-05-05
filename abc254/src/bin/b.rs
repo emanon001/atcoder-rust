bfa3b9e868e793e4476355d5a348981a2aee503a
emanon001 <emanon001@gmail.com>
2023-05-05 23:49:39 +0900

    abc254/b

--- abc254/src/bin/b.rs
+++ abc254/src/bin/b.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize
     };
+
+    let mut a: Vec<Vec<i32>> = Vec::new();
+    for i in 0..n {
+        let mut row = Vec::new();
+        for j in 0..i + 1 {
+            if j == 0 || j == i {
+                row.push(1);
+            } else {
+                row.push(a[i - 1][j - 1] + a[i - 1][j]);
+            }
+        }
+        a.push(row);
+    }
+    for row in a {
+        println!("{}", row.iter().join(" "));
+    }
 }
 
 fn main() {
