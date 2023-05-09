5b9bb8485c6f6690f063a68d41301b0a5c04070b
emanon001 <emanon001@gmail.com>
2023-05-09 23:06:28 +0900

    abc256/b

--- abc256/src/bin/b.rs
+++ abc256/src/bin/b.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n]
     };
+
+    let mut blocks = vec![1, 0, 0, 0];
+    let mut res = 0;
+    for a in av {
+        for i in (0..blocks.len()).rev() {
+            if i + a >= 4 {
+                res += blocks[i];
+            } else {
+                blocks[i + a] = blocks[i];
+            }
+            blocks[i] = 0;
+        }
+        blocks[0] = 1;
+    }
+    println!("{}", res);
 }
 
 fn main() {
