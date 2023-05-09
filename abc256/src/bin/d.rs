ec95f04ff1f43962e73568848235559c956aa096
emanon001 <emanon001@gmail.com>
2023-05-10 00:02:15 +0900

    abc256/d

--- abc256/src/bin/d.rs
+++ abc256/src/bin/d.rs
@@ -8,9 +8,40 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        mut lrv: [(usize, usize); n]
     };
+
+    lrv.sort_by_key(|(l, _)| *l);
+    lrv.push((10.pow(6), 10.pow(6)));
+    let mut res = Vec::new();
+    let mut cur = lrv[0];
+    for (l, r) in lrv.into_iter().skip(1) {
+        if l <= cur.1 {
+            chmax!(cur.1, r);
+        } else {
+            res.push(cur);
+            cur = (l, r);
+        }
+    }
+    for (l, r) in res {
+        println!("{} {}", l, r);
+    }
 }
 
 fn main() {
