536f881ec251dad08ffb9fd68097dae7cc94e4a5
emanon001 <emanon001@gmail.com>
2023-05-09 23:41:24 +0900

    abc256/c

--- abc256/src/bin/c.rs
+++ abc256/src/bin/c.rs
@@ -10,7 +10,40 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h1: usize, h2: usize, h3: usize, w1: usize, w2: usize, w3: usize
     };
+
+    let mut res = 0;
+    for comb in (1..h1).combinations(2) {
+        let mut comb = comb;
+        comb.sort();
+        let a = comb[0];
+        let b = comb[1] - comb[0];
+        let c = h1 - comb[1];
+        for comb2 in (1..h2).combinations(2) {
+            let mut comb2 = comb2;
+            comb2.sort();
+            let a2 = comb2[0];
+            let b2 = comb2[1] - comb2[0];
+            let c2 = h2 - comb2[1];
+            if a + a2 >= w1 {
+                continue;
+            }
+            let a3 = w1 - a - a2;
+            if b + b2 >= w2 {
+                continue;
+            }
+            let b3 = w2 - b - b2;
+            if c + c2 >= w3 {
+                continue;
+            }
+            let c3 = w3 - c - c2;
+            if a3 + b3 + c3 == h3 {
+                res += 1;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
