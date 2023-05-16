88f6566820941ba70d6b8eff974c1c6d1d82dbdd
emanon001 <emanon001@gmail.com>
2023-05-17 00:10:20 +0900

    abc258/c

--- abc258/src/bin/c.rs
+++ abc258/src/bin/c.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
+        s: Chars,
+        queries: [(usize, usize); q]
     };
+
+    let mut top = 0;
+    for (kind, x) in queries {
+        if kind == 1 {
+            top = (top + x) % n;
+        } else {
+            let i = n - top;
+            let res = s[(i + x - 1) % n];
+            println!("{}", res);
+        }
+    }
 }
 
 fn main() {
