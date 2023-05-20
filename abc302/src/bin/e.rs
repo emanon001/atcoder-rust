e48f6a8b00ee65bdb60e84da62f7412cf9cc8c7a
emanon001 <emanon001@gmail.com>
2023-05-20 23:37:43 +0900

    abc302/e

--- abc302/src/bin/e.rs
+++ abc302/src/bin/e.rs
@@ -10,7 +10,50 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, q: usize,
     };
+
+    let mut vs = vec![HashSet::new(); n];
+    let mut undirected = n;
+    for _ in 0..q {
+        input! {
+            kind: usize,
+        }
+        match kind {
+            1 => {
+                input! {
+                    u: Usize1,
+                    v: Usize1,
+                }
+                if vs[u].is_empty() {
+                    undirected -= 1;
+                }
+                vs[u].insert(v);
+                if vs[v].is_empty() {
+                    undirected -= 1;
+                }
+                vs[v].insert(u);
+                println!("{}", undirected);
+            }
+            2 => {
+                input! {
+                    v: Usize1,
+                }
+                for u in vs[v].clone() {
+                    vs[u].remove(&v);
+                    if vs[u].is_empty() {
+                        undirected += 1;
+                    }
+                }
+                if !vs[v].is_empty() {
+                    undirected += 1;
+                }
+                vs[v] = HashSet::new();
+                println!("{}", undirected);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
