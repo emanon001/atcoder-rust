f134880e576d79dc30dba3a44ac48ffb2b272660
emanon001 <emanon001@gmail.com>
2023-05-21 23:45:41 +0900

    abc259/b

--- abc259/src/bin/b.rs
+++ abc259/src/bin/b.rs
@@ -10,7 +10,14 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: f64, b: f64, d: f64
     };
+
+    let rad = d.to_radians();
+
+    let a2 = a * rad.cos() - b * rad.sin();
+    let b2 = a * rad.sin() + b * rad.cos();
+    println!("{} {}", a2, b2);
 }
 
 fn main() {
