8d88cab7a15067708405ea4f9834ba24ac200912
emanon001 <emanon001@gmail.com>
2023-06-08 22:09:25 +0900

    past202203-open/d

--- past202203-open/src/bin/d.rs
+++ past202203-open/src/bin/d.rs
@@ -10,7 +10,18 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        t: usize, n: usize,
+        pv: [[usize; n]; t]
     };
+
+    let mut max_v = vec![0; n];
+    for ti in 0..t {
+        for ni in 0..n {
+            max_v[ni] = max_v[ni].max(pv[ti][ni]);
+        }
+        let res: usize = max_v.iter().sum();
+        println!("{}", res);
+    }
 }
 
 fn main() {
