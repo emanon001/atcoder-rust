b787be5d175e15bddf7207541bd6abc559f3f454
emanon001 <emanon001@gmail.com>
2023-06-10 23:11:29 +0900

    past202203-open/f

--- past202203-open/src/bin/f.rs
+++ past202203-open/src/bin/f.rs
@@ -8,9 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+/// 上下左右 (i, j)
+pub const UDLR_DIRS: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
 fn solve() {
     input! {
+        h: usize, w: usize, n: usize,
+        grid: [[Usize1; w]; h],
+        cv: [usize; n]
     };
+
+    for i in 0..h {
+        for j in 0..w {
+            let s = grid[i][j];
+            let c = cv[s];
+            for (di, dj) in &UDLR_DIRS {
+                let i2 = i as isize + di;
+                let j2 = j as isize + dj;
+                if (0..h as isize).contains(&i2) && (0..w as isize).contains(&j2) {
+                    let i2 = i2 as usize;
+                    let j2 = j2 as usize;
+                    let s2 = grid[i2][j2];
+                    let c2 = cv[s2];
+                    if s != s2 && c == c2 {
+                        println!("No");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    println!("Yes");
 }
 
 fn main() {
