a1727744750765672165b68f642cedc983f5591e
emanon001 <emanon001@gmail.com>
2023-06-23 03:21:12 +0900

    abc261/b

--- abc261/src/bin/b.rs
+++ abc261/src/bin/b.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        grid: [Chars; n]
     };
+
+    for i in 0..n {
+        for j in i + 1..n {
+            let ch1 = grid[i][j];
+            let ch2 = grid[j][i];
+            let valid = matches!((ch1, ch2), ('W', 'L') | ('L', 'W') | ('D', 'D'));
+            if !valid {
+                println!("incorrect");
+                return;
+            }
+        }
+    }
+    println!("correct");
 }
 
 fn main() {
