7ae0dcf654ed54ca59a27f631f3b1deb67fbe86a
emanon001 <emanon001@gmail.com>
2023-06-27 23:57:51 +0900

    abc259/d

--- abc259/src/bin/d.rs
+++ abc259/src/bin/d.rs
@@ -8,9 +8,55 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+type PR = (isize, isize, isize);
+
+fn can_move(from: PR, to: PR) -> bool {
+    !in_circle(from, to) && !out_circle(from, to)
+}
+
+fn in_circle(p1: PR, p2: PR) -> bool {
+    let dx = (p1.0 - p2.0).abs();
+    let dy = (p1.1 - p2.1).abs();
+    let dr = (p1.2 - p2.2).abs();
+    dx * dx + dy * dy < dr * dr
+}
+
+fn out_circle(p1: PR, p2: PR) -> bool {
+    let dx = (p1.0 - p2.0).abs();
+    let dy = (p1.1 - p2.1).abs();
+    let dr = (p1.2 + p2.2).abs();
+    dx * dx + dy * dy > dr * dr
+}
+
 fn solve() {
     input! {
+        n: usize,
+        sx: isize, sy: isize, tx: isize, ty: isize,
+        xyrv: [(isize, isize, isize); n]
     };
+    let sc = (sx, sy, 0);
+    let tc = (tx, ty, 0_isize);
+
+    let mut que = VecDeque::new();
+    que.push_back(sc);
+    let mut visited = HashSet::new();
+    while let Some(p1) = que.pop_front() {
+        eprintln!("{:?}", p1);
+        if can_move(p1, tc) {
+            println!("Yes");
+            return;
+        }
+        for p2 in xyrv.iter().copied() {
+            if visited.contains(&p2) {
+                continue;
+            }
+            if can_move(p1, p2) {
+                visited.insert(p2);
+                que.push_back(p2);
+            }
+        }
+    }
+    println!("No");
 }
 
 fn main() {
