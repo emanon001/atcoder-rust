54166ce83f0defa30494c9c779ef8226966af53c
emanon001 <emanon001@gmail.com>
2023-07-01 22:02:47 +0900

    abc262/b

--- abc262/src/bin/b.rs
+++ abc262/src/bin/b.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![vec![false; n]; n];
+    for (u, v) in edges {
+        graph[u][v] = true;
+        graph[v][u] = true;
+    }
+    let mut res = 0;
+    for a in 0..n {
+        for b in a + 1..n {
+            for c in b + 1..n {
+                if graph[a][b] && graph[b][c] && graph[c][a] {
+                    res += 1;
+                }
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
