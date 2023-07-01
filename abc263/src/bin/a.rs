522dfb1e2f150898a94e2b2889d1cbbe0cc1beb1
emanon001 <emanon001@gmail.com>
2023-07-02 05:49:15 +0900

    abc263/a

--- abc263/src/bin/a.rs
+++ abc263/src/bin/a.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: usize, b: usize, c: usize, d: usize, e: usize,
     };
+
+    let mut map = HashMap::new();
+    *map.entry(&a).or_insert(0) += 1;
+    *map.entry(&b).or_insert(0) += 1;
+    *map.entry(&c).or_insert(0) += 1;
+    *map.entry(&d).or_insert(0) += 1;
+    *map.entry(&e).or_insert(0) += 1;
+    let res = if map.len() == 2 && map.values().any(|x| x == &2) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", res);
 }
 
 fn main() {
