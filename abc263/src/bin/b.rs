b7c89a3a9e2ac31bff42a4aaeb4313bec594151b
emanon001 <emanon001@gmail.com>
2023-07-02 05:55:57 +0900

    abc263/b

--- abc263/src/bin/b.rs
+++ abc263/src/bin/b.rs
@@ -10,7 +10,17 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        pv: [usize; n - 1]
     };
+
+    let mut res = 0;
+    let mut cur = n;
+    while cur > 1 {
+        res += 1;
+        cur = pv[cur - 2];
+    }
+    println!("{}", res);
 }
 
 fn main() {
