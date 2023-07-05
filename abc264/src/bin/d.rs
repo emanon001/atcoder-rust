28ee1c088c35491224b4463324f4d2b37e22150c
emanon001 <emanon001@gmail.com>
2023-07-06 00:33:45 +0900

    abc264/d

--- abc264/src/bin/d.rs
+++ abc264/src/bin/d.rs
@@ -8,9 +8,45 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        s: String
     };
+
+    let mut res: HashMap<String, usize> = HashMap::new();
+    let mut heap = BinaryHeap::new();
+    heap.push((s.clone(), 0));
+    res.insert(s, 0);
+    while let Some((s, c)) = heap.pop() {
+        if res.contains_key(&s) && res.get(&s).unwrap() < &c {
+            continue;
+        }
+        for i in 0..6 {
+            let mut chars = s.chars().collect::<Vec<_>>();
+            chars.swap(i, i + 1);
+            let new_s = chars.iter().join("");
+            let new_c = c + 1;
+            if res.contains_key(&new_s) && res.get(&new_s).unwrap() <= &new_c {
+                continue;
+            }
+            res.insert(new_s.clone(), new_c);
+            heap.push((new_s, new_c));
+        }
+    }
+    println!("{}", res.get("atcoder").unwrap());
 }
 
 fn main() {
