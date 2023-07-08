f4459f6b36020fa9aa1a7b38a2ab95b162223974
emanon001 <emanon001@gmail.com>
2023-07-08 23:50:07 +0900

    abc265/b

--- abc265/src/bin/b.rs
+++ abc265/src/bin/b.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize, t: i64,
+        av: [i64; n - 1],
+        xyv: [(Usize1, i64); m]
     };
+    let bonus_map = xyv.into_iter().collect::<HashMap<usize, i64>>();
+    let mut t = t;
+    for (i, a) in av.into_iter().enumerate() {
+        if t > a {
+            t -= a;
+            t += bonus_map.get(&(i + 1)).unwrap_or(&0);
+        } else {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
 
 fn main() {
