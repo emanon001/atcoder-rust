14addc4ef0afdbbfaf129f6bb906c5f6add97597
emanon001 <emanon001@gmail.com>
2023-07-09 00:03:20 +0900

    abc265/c

--- abc265/src/bin/c.rs
+++ abc265/src/bin/c.rs
@@ -10,7 +10,50 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [Chars; h]
     };
+
+    let mut pos = (0, 0);
+    let mut visited = HashSet::new();
+    loop {
+        visited.insert(pos);
+        match grid[pos.0][pos.1] {
+            'U' => {
+                if pos.0 == 0 {
+                    println!("{} {}", pos.0 + 1, pos.1 + 1);
+                    return;
+                }
+                pos = (pos.0 - 1, pos.1);
+            }
+            'D' => {
+                if pos.0 == h - 1 {
+                    println!("{} {}", pos.0 + 1, pos.1 + 1);
+                    return;
+                }
+                pos = (pos.0 + 1, pos.1);
+            }
+            'L' => {
+                if pos.1 == 0 {
+                    println!("{} {}", pos.0 + 1, pos.1 + 1);
+                    return;
+                }
+                pos = (pos.0, pos.1 - 1);
+            }
+            'R' => {
+                if pos.1 == w - 1 {
+                    println!("{} {}", pos.0 + 1, pos.1 + 1);
+                    return;
+                }
+                pos = (pos.0, pos.1 + 1);
+            }
+            _ => unreachable!(),
+        }
+        if visited.contains(&pos) {
+            println!("-1");
+            return;
+        }
+    }
 }
 
 fn main() {
