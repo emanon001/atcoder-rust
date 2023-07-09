459d363a6de65c332abbb2ffaff0a76002177c07
emanon001 <emanon001@gmail.com>
2023-07-09 23:08:40 +0900

    abc309/c

--- abc309/src/bin/c.rs
+++ abc309/src/bin/c.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: i64,
+        mut abv: [(i64, i64); n]
     };
+
+    let mut sum: i64 = abv.iter().map(|(_, c)| c).sum();
+    let mut days = BTreeMap::new();
+    days.insert(1, 0);
+    for (a, b) in abv {
+        *days.entry(a + 1).or_insert(0_i64) += b;
+    }
+    for (d, c) in days {
+        sum -= c;
+        if sum <= k {
+            println!("{}", d);
+            return;
+        }
+    }
 }
 
 fn main() {
