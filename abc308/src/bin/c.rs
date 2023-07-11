f6457d11882274d7a51d9eeb29f96b413a982b2f
emanon001 <emanon001@gmail.com>
2023-07-11 23:55:26 +0900

    abc308/c

--- abc308/src/bin/c.rs
+++ abc308/src/bin/c.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        abv: [(u64, u64); n]
     };
+
+    let res = abv
+        .into_iter()
+        .enumerate()
+        .sorted_by(|(i, (a, b)), (j, (a2, b2))| {
+            let lcm = (a + b).lcm(&(a2 + b2));
+            (lcm / (a2 + b2) * a2)
+                .cmp(&(lcm / (a + b) * a))
+                .then(i.cmp(j))
+        })
+        .map(|(i, _)| i + 1)
+        .join(" ");
+    println!("{}", res);
 }
 
 fn main() {
