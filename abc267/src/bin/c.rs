3384df464e566603e7683b8977b73e77ce334fdb
emanon001 <emanon001@gmail.com>
2023-07-14 01:05:25 +0900

    abc267/c

--- abc267/src/bin/c.rs
+++ abc267/src/bin/c.rs
@@ -8,9 +8,47 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [i64; n]
     };
+
+    let mut cusum = vec![0_i64; n + 1];
+    for i in 0..n {
+        cusum[i + 1] = cusum[i] + av[i];
+    }
+    let mut cur = av
+        .iter()
+        .take(m)
+        .enumerate()
+        .fold(0_i64, |acc, (i, a)| acc + (i + 1) as i64 * a);
+    let mut res = cur;
+    for i in 0..n {
+        // eprintln!("{}", cur);
+        if i + m >= n {
+            break;
+        }
+        let sub = cusum[i + m] - cusum[i];
+        let add = m as i64 * av[i + m];
+        cur = cur - sub + add;
+        // eprintln!("cur - {} + {} = {}", sub, add, cur);
+        chmax!(res, cur);
+    }
+    println!("{}", res);
 }
 
 fn main() {
