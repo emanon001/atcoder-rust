efa866cf7e2f0705466f98a27381ae4165e92af3
emanon001 <emanon001@gmail.com>
2023-07-14 00:07:07 +0900

    abc267/b

--- abc267/src/bin/b.rs
+++ abc267/src/bin/b.rs
@@ -10,7 +10,59 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars,
     };
+
+    let mut v = vec![Vec::new(); 7];
+    for (i, ch) in s.into_iter().enumerate() {
+        let no = i + 1;
+        let state = ch == '1';
+        match no {
+            7 => {
+                v[0].push(state);
+            }
+            4 => {
+                v[1].push(state);
+            }
+            2 | 8 => {
+                v[2].push(state);
+            }
+            1 | 5 => {
+                v[3].push(state);
+            }
+            3 | 9 => {
+                v[4].push(state);
+            }
+            6 => {
+                v[5].push(state);
+            }
+            10 => {
+                v[6].push(state);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    if v[3][0] {
+        println!("No");
+        return;
+    }
+
+    for i in 0..7 {
+        for j in i + 2..7 {
+            if !v[i].contains(&true) {
+                continue;
+            }
+            if !v[j].contains(&true) {
+                continue;
+            }
+            if (i + 1..j).any(|k| v[k].iter().all(|st| !st)) {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
 
 fn main() {
