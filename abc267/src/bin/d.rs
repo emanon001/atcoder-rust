8c342d1444ed0403ccdd555556e7e250e51ba3a8
emanon001 <emanon001@gmail.com>
2023-07-14 00:28:51 +0900

    abc267/d

--- abc267/src/bin/d.rs
+++ abc267/src/bin/d.rs
@@ -8,9 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [i64; n]
     };
+
+    let mut dp = vec![vec![-(1_i64 << 60); m + 10]; n + 10];
+    dp[0][0] = 0;
+    for i in 0..n {
+        for j in 0..=m {
+            // 選ばない場合
+            chmax!(dp[i + 1][j], dp[i][j]);
+            // 選んだ場合
+            chmax!(dp[i + 1][j + 1], dp[i][j] + av[i] * (j + 1) as i64);
+        }
+    }
+    let res = dp[n][m];
+    println!("{}", res);
 }
 
 fn main() {
