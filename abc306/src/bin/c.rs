35596f1fbd4edb2b07e2819afb29f21605e84673
emanon001 <emanon001@gmail.com>
2023-07-16 23:13:13 +0900

    abc306/c

--- abc306/src/bin/c.rs
+++ abc306/src/bin/c.rs
@@ -10,7 +10,20 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [Usize1; 3 * n]
     };
+
+    let mut pos = vec![0; n];
+    let mut count = vec![0; n];
+    for (i, a) in av.into_iter().enumerate() {
+        if count[a] == 1 {
+            pos[a] = i;
+        }
+        count[a] += 1;
+    }
+    let res = (0..n).sorted_by_key(|a| pos[*a]).map(|a| a + 1).join(" ");
+    println!("{}", res);
 }
 
 fn main() {
