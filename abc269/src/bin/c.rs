2370fd43ee1f870a62d79f7ad86d9a7fbeba5773
emanon001 <emanon001@gmail.com>
2023-07-17 20:01:44 +0900

    abc269/c

--- abc269/src/bin/c.rs
+++ abc269/src/bin/c.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
     };
+    let binary = format!("{:b}", n).chars().collect::<Vec<_>>();
+    let mut binary_one_pos = Vec::new();
+    for i in 0..binary.len() {
+        if binary[binary.len() - i - 1] == '1' {
+            binary_one_pos.push(i);
+        }
+    }
+    let mut res = Vec::new();
+    for bits in 0..(1 << binary_one_pos.len()) {
+        let mut m = 0_u64;
+        for (i, pos) in binary_one_pos.iter().enumerate() {
+            if (bits >> i) & 1 == 1 {
+                m += 2.pow(*pos as u32);
+            }
+        }
+        res.push(m);
+    }
+    res.sort();
+    println!("{}", res.iter().join("\n"));
 }
 
 fn main() {
