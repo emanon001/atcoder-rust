ddcb44a4fddc73861ef4399f880cc25c0c4a3c66
emanon001 <emanon001@gmail.com>
2023-07-17 20:16:11 +0900

    abc269/d

--- abc269/src/bin/d.rs
+++ abc269/src/bin/d.rs
@@ -8,9 +8,44 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn dfs(
+    i: i64,
+    j: i64,
+    id: usize,
+    pos_set: &HashSet<(i64, i64)>,
+    visited: &mut HashMap<(i64, i64), usize>,
+) {
+    if visited.contains_key(&(i, j)) {
+        return;
+    }
+    visited.insert((i, j), id);
+    let dirs = vec![(-1, -1), (-1, 0), (0, -1), (0, 1), (1, 0), (1, 1)];
+    for (di, dj) in dirs {
+        let new_i = i + di;
+        let new_j = j + dj;
+        if !pos_set.contains(&(new_i, new_j)) {
+            continue;
+        }
+        if visited.contains_key(&(new_i, new_j)) {
+            continue;
+        }
+        dfs(new_i, new_j, id, pos_set, visited);
+    }
+}
+
 fn solve() {
     input! {
+        n: usize,
+        xyv: [(i64, i64); n]
     };
+
+    let pos_set = xyv.iter().copied().collect::<HashSet<_>>();
+    let mut visited = HashMap::new();
+    for (id, (x, y)) in xyv.into_iter().enumerate() {
+        dfs(x, y, id, &pos_set, &mut visited);
+    }
+    let res = visited.values().unique().count();
+    println!("{}", res);
 }
 
 fn main() {
