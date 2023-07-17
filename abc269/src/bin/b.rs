4f2902a4b424b432213f3a40c441df44d431a964
emanon001 <emanon001@gmail.com>
2023-07-17 19:45:40 +0900

    abc269/b

--- abc269/src/bin/b.rs
+++ abc269/src/bin/b.rs
@@ -10,7 +10,47 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        grid: [Chars; 10]
     };
+
+    let mut a = 0;
+    'outer: for i in 0..10 {
+        for j in 0..10 {
+            if grid[i][j] == '#' {
+                a = i;
+                break 'outer;
+            }
+        }
+    }
+    let mut b = 0;
+    'outer: for i in (0..10).rev() {
+        for j in 0..10 {
+            if grid[i][j] == '#' {
+                b = i;
+                break 'outer;
+            }
+        }
+    }
+    let mut c = 0;
+    'outer: for i in 0..10 {
+        for j in 0..10 {
+            if grid[i][j] == '#' {
+                c = j;
+                break 'outer;
+            }
+        }
+    }
+    let mut d = 0;
+    'outer: for i in 0..10 {
+        for j in (0..10).rev() {
+            if grid[i][j] == '#' {
+                d = j;
+                break 'outer;
+            }
+        }
+    }
+    println!("{} {}", a + 1, b + 1);
+    println!("{} {}", c + 1, d + 1);
 }
 
 fn main() {
