966788961ba5c0c3ba5b8a2f5281d8b0eeb3fb0f
emanon001 <emanon001@gmail.com>
2023-07-20 02:22:21 +0900

    abc305/a

--- abc305/src/bin/a.rs
+++ abc305/src/bin/a.rs
@@ -8,9 +8,30 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: isize
     };
+
+    let res = (0_isize..=100)
+        .step_by(5)
+        .sorted_by_key(|x| (n - x).abs())
+        .next()
+        .unwrap();
+    println!("{}", res);
 }
 
 fn main() {
