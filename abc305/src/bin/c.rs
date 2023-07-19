ea5d0aa05737e74231e017f1cfdd5a3f51468d77
emanon001 <emanon001@gmail.com>
2023-07-20 02:35:34 +0900

    abc305/c

--- abc305/src/bin/c.rs
+++ abc305/src/bin/c.rs
@@ -10,7 +10,38 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [Chars; h]
     };
+
+    let mut cookie_count = BTreeMap::new();
+    for i in 0..h {
+        let mut c = 0;
+        for j in 0..w {
+            if grid[i][j] == '#' {
+                c += 1;
+            }
+        }
+        if c > 0 {
+            cookie_count.insert(c, i + 1);
+        }
+    }
+    let mut cookie_count2 = BTreeMap::new();
+    for j in 0..w {
+        let mut c = 0;
+        for i in 0..h {
+            if grid[i][j] == '#' {
+                c += 1;
+            }
+        }
+        if c > 0 {
+            cookie_count2.insert(c, j + 1);
+        }
+    }
+
+    let i = cookie_count.iter().next().unwrap().1;
+    let j = cookie_count2.iter().next().unwrap().1;
+    println!("{} {}", i, j);
 }
 
 fn main() {
