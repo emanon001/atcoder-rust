4db436ac0f81ce01d81466e340e02c77a58dc3df
emanon001 <emanon001@gmail.com>
2023-07-23 00:44:20 +0900

    abc270/c

--- abc270/src/bin/c.rs
+++ abc270/src/bin/c.rs
@@ -10,7 +10,36 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, x: Usize1, y: Usize1,
+        edges: [(Usize1, Usize1); n - 1]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    let mut parents = vec![n; n];
+    let mut que = VecDeque::new();
+    que.push_back((x, n));
+    while let Some((u, p)) = que.pop_front() {
+        for &v in &graph[u] {
+            if v == p {
+                continue;
+            }
+            parents[v] = u;
+            que.push_back((v, u));
+        }
+    }
+    let mut res = VecDeque::new();
+    res.push_back(y);
+    let mut v = y;
+    while v != x {
+        v = parents[v];
+        res.push_front(v);
+    }
+    println!("{}", res.iter().map(|u| u + 1).join(" "));
 }
 
 fn main() {
