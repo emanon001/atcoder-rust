3c6f24a039a93a8e94a14b937efe7c2b8e1b1a0f
emanon001 <emanon001@gmail.com>
2023-07-23 00:28:26 +0900

    abc270/b

--- abc270/src/bin/b.rs
+++ abc270/src/bin/b.rs
@@ -8,9 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        x: isize, y: isize, z: isize
     };
+    let max = 1000;
+    let mut res = max;
+    // x, _, _
+    if 0 < x && x < y || y < x && x < 0 || x.is_positive() != y.is_positive() {
+        chmin!(res, x.abs());
+    }
+    // z, y, x
+    if 0 < z && z < y || y < z && z < 0 || z.is_positive() != y.is_positive() {
+        chmin!(res, z.abs() + (z - x).abs());
+    };
+    if res == max {
+        res = -1;
+    }
+    println!("{}", res);
 }
 
 fn main() {
