6c6676e91977610a94c00000b4913009bc20dc0d
emanon001 <emanon001@gmail.com>
2023-07-23 23:27:20 +0900

    abc270/e

--- abc270/src/bin/e.rs
+++ abc270/src/bin/e.rs
@@ -8,9 +8,94 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<usize> for usize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self as isize - *ng as isize).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, k: i64,
+        av: [i64; n]
     };
+
+    let loop_count = bsearch(0_i64, k + 1, |x| {
+        let mut sum = 0;
+        for &a in &av {
+            sum += x.min(a);
+        }
+        sum <= k
+    })
+    .unwrap_or(0);
+    let mut k = k;
+    for i in 0..n {
+        k -= av[i].min(loop_count);
+    }
+    let mut res = vec![0; n];
+    for i in 0..n {
+        let mut m = (av[i] - loop_count).max(0);
+        if k > 0 && m > 0 {
+            m -= 1;
+            k -= 1;
+        }
+        res[i] = m;
+    }
+    println!("{}", res.iter().join(" "));
 }
 
 fn main() {
