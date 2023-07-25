e6f9bcbbf6d9e18e82e624ff27d4c202d5ff6d58
emanon001 <emanon001@gmail.com>
2023-07-26 00:01:25 +0900

    abc304/b

--- abc304/src/bin/b.rs
+++ abc304/src/bin/b.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
     };
+
+    let res = if (0..10.pow(3)).contains(&n) {
+        n
+    } else if (10.pow(3)..10.pow(4)).contains(&n) {
+        n - (n % 10)
+    } else if (10.pow(4)..10.pow(5)).contains(&n) {
+        n - (n % 100)
+    } else if (10.pow(5)..10.pow(6)).contains(&n) {
+        n - (n % 1000)
+    } else if (10.pow(6)..10.pow(7)).contains(&n) {
+        n - (n % 10000)
+    } else if (10.pow(7)..10.pow(8)).contains(&n) {
+        n - (n % 100000)
+    } else if (10.pow(8)..10.pow(9)).contains(&n) {
+        n - (n % 1000000)
+    } else {
+        unreachable!()
+    };
+    println!("{}", res);
 }
 
 fn main() {
