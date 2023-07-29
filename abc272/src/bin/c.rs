ee3bd167c6ac20c1a19d8b41597af8782260b7ca
emanon001 <emanon001@gmail.com>
2023-07-30 06:43:33 +0900

    abc272/c

--- abc272/src/bin/c.rs
+++ abc272/src/bin/c.rs
@@ -8,9 +8,47 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        av: [i64; n]
     };
+
+    let odds = av
+        .iter()
+        .copied()
+        .filter(i64::is_odd)
+        .sorted()
+        .rev()
+        .collect::<Vec<_>>();
+    let evens = av
+        .iter()
+        .copied()
+        .filter(i64::is_even)
+        .sorted()
+        .rev()
+        .collect::<Vec<_>>();
+    let mut res = -1_i64;
+    if odds.len() >= 2 {
+        chmax!(res, odds[0] + odds[1]);
+    }
+    if evens.len() >= 2 {
+        chmax!(res, evens[0] + evens[1]);
+    }
+    println!("{}", res);
 }
 
 fn main() {
