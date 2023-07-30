6d6519a81e30fbbf9e044ee3f934707e0b04a2ac
emanon001 <emanon001@gmail.com>
2023-07-30 23:39:41 +0900

    abc272/d

--- abc272/src/bin/d.rs
+++ abc272/src/bin/d.rs
@@ -8,9 +8,217 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Clone)]
+pub struct Graph<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    graph: Vec<Vec<(usize, Cost)>>,
+    vc: usize,
+    inf: Cost,
+}
+#[derive(Clone)]
+pub struct Edge<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    from: usize,
+    to: usize,
+    cost: Cost,
+}
+impl<Cost> From<(usize, usize, Cost)> for Edge<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    fn from(e: (usize, usize, Cost)) -> Edge<Cost> {
+        Edge {
+            from: e.0,
+            to: e.1,
+            cost: e.2,
+        }
+    }
+}
+impl<Cost> From<(usize, usize)> for Edge<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    fn from(e: (usize, usize)) -> Edge<Cost> {
+        Edge {
+            from: e.0,
+            to: e.1,
+            cost: Cost::one(),
+        }
+    }
+}
+impl<Cost> Graph<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    pub fn new_undirected(edges: Vec<impl Into<Edge<Cost>>>, vc: usize, inf: Cost) -> Self {
+        let mut graph = vec![Vec::new(); vc];
+        for e in edges {
+            let e = e.into();
+            graph[e.from].push((e.to, e.cost));
+            graph[e.to].push((e.from, e.cost));
+        }
+        Self { graph, vc, inf }
+    }
+    pub fn new_directed(edges: Vec<impl Into<Edge<Cost>>>, vc: usize, inf: Cost) -> Self {
+        let mut graph = vec![Vec::new(); vc];
+        for e in edges {
+            let e = e.into();
+            graph[e.from].push((e.to, e.cost));
+        }
+        Self { graph, vc, inf }
+    }
+    pub fn new_empty(vc: usize, inf: Cost) -> Self {
+        let graph = vec![Vec::new(); vc];
+        Self { graph, vc, inf }
+    }
+    pub fn add_undirected_edge(&mut self, e: impl Into<Edge<Cost>>) {
+        let e = e.into();
+        self.graph[e.from].push((e.to, e.cost));
+        self.graph[e.to].push((e.from, e.cost));
+    }
+    pub fn add_directed_edge(&mut self, e: impl Into<Edge<Cost>>) {
+        let e = e.into();
+        self.graph[e.from].push((e.to, e.cost));
+    }
+    pub fn rev(&self) -> Graph<Cost> {
+        let mut edges: Vec<Edge<Cost>> = Vec::new();
+        for u in 0..self.vc {
+            for &(v, w) in &self.graph[u] {
+                edges.push((v, u, w).into());
+            }
+        }
+        Self::new_directed(edges, self.vc, self.inf)
+    }
+    pub fn reachable_vertexes(&self, s: usize) -> std::collections::HashSet<usize> {
+        let mut visited = std::collections::HashSet::new();
+        let mut queue = std::collections::VecDeque::new();
+        visited.insert(s);
+        queue.push_back(s);
+        while let Some(u) = queue.pop_front() {
+            for &(v, _) in &self.graph[u] {
+                if visited.contains(&v) {
+                    continue;
+                }
+                visited.insert(v);
+                queue.push_back(v);
+            }
+        }
+        visited
+    }
+    pub fn edges<'a>(&self, u: usize) -> &Vec<(usize, Cost)> {
+        &self.graph[u]
+    }
+    pub fn vertex_count(&self) -> usize {
+        self.vc
+    }
+}
+
+pub fn compress_zahyo<T: Ord + std::hash::Hash + Clone>(
+    zahyo: &[T],
+) -> std::collections::HashMap<T, usize> {
+    let mut set = std::collections::BTreeSet::new();
+    for x in zahyo {
+        set.insert(x.clone());
+    }
+    let mut map = std::collections::HashMap::new();
+    for (i, x) in set.into_iter().enumerate() {
+        map.insert(x, i);
+    }
+    map
+}
+
+impl<Cost> Graph<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    pub fn shortest_path_1(&self, start: usize) -> Vec<Option<Cost>> {
+        let mut cost_list = vec![None; self.vc];
+        let mut que = std::collections::VecDeque::new();
+        cost_list[start] = Some(Cost::zero());
+        que.push_back(start);
+        while let Some(u) = que.pop_front() {
+            for &(v, w) in &self.graph[u] {
+                if !w.is_one() {
+                    panic!("weight is not 1");
+                }
+                if cost_list[v].is_some() {
+                    continue;
+                }
+                let new_cost = cost_list[u].unwrap() + Cost::one();
+                cost_list[v] = Some(new_cost);
+                que.push_back(v);
+            }
+        }
+        cost_list
+    }
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
     };
+
+    let mut vertexes = Vec::new();
+    for i in 0..n {
+        for j in 0..n {
+            vertexes.push((i, j));
+        }
+    }
+    let zahyo = compress_zahyo(&vertexes);
+
+    let dv = {
+        let mut dv = Vec::new();
+        for di in 0..=n {
+            for dj in 0..n {
+                let m2 = di * di + dj * dj;
+                if m2 == m {
+                    dv.push((di as isize, dj as isize));
+                }
+            }
+        }
+        dv
+    };
+    let edges = {
+        let mut edges = Vec::new();
+        for &(i, j) in &vertexes {
+            let u = zahyo[&(i, j)];
+            let i = i as isize;
+            let j = j as isize;
+            for &(xd, yd) in &dv {
+                let signs = vec![(1, 1), (1, -1), (-1, -1), (-1, 1)];
+                for (xs, ys) in signs {
+                    let xd = xd * xs;
+                    let yd = yd * ys;
+                    let new_i = i + xd;
+                    let new_j = j + yd;
+                    if new_i < 0 || new_i >= n as isize || new_j < 0 || new_j >= n as isize {
+                        continue;
+                    }
+                    let new_i = new_i as usize;
+                    let new_j = new_j as usize;
+                    let v = zahyo[&(new_i, new_j)];
+                    edges.push((u, v));
+                }
+            }
+        }
+        edges
+    };
+    let graph = Graph::new_directed(edges, n * n, 1_i64 << 60);
+    let s = zahyo[&(0, 0)];
+    let dist = graph.shortest_path_1(s);
+    let mut res = vec![vec![-1; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            let v = zahyo[&(i, j)];
+            let d = dist[v].unwrap_or(-1);
+            res[i][j] = d;
+        }
+    }
+    println!("{}", res.iter().map(|row| row.iter().join(" ")).join("\n"));
 }
 
 fn main() {
