434f3eca96df4eeb8aa368e19b4d4d682fc2a140
emanon001 <emanon001@gmail.com>
2023-08-03 00:02:39 +0900

    abc303/b

--- abc303/src/bin/b.rs
+++ abc303/src/bin/b.rs
@@ -10,7 +10,26 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [[Usize1; n]; m]
     };
+
+    let mut set = HashSet::new();
+    for i in 0..m {
+        for j in 0..n - 1 {
+            set.insert((av[i][j].min(av[i][j + 1]), av[i][j].max(av[i][j + 1])));
+        }
+    }
+
+    let mut res = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            if !set.contains(&(i, j)) {
+                res += 1;
+            }
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
