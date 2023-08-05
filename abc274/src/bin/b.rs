0a5264cd36f9e5e2add92b15305d97b5df5b2bc8
emanon001 <emanon001@gmail.com>
2023-08-05 19:54:18 +0900

    abc274/b

--- abc274/src/bin/b.rs
+++ abc274/src/bin/b.rs
@@ -10,7 +10,21 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [Chars; h]
     };
+
+    let mut res = Vec::new();
+    for j in 0..w {
+        let mut count = 0;
+        for i in 0..h {
+            if grid[i][j] == '#' {
+                count += 1;
+            }
+        }
+        res.push(count);
+    }
+    println!("{}", res.iter().join(" "));
 }
 
 fn main() {
