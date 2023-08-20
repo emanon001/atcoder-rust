3289f98ebf4ccb2530ac5e9de2e94020e8bbe232
emanon001 <emanon001@gmail.com>
2023-08-20 23:44:05 +0900

    abc275/a

--- abc275/src/bin/a.rs
+++ abc275/src/bin/a.rs
@@ -8,9 +8,33 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        hv: [isize; n]
     };
+
+    let mut res = 0;
+    let mut max = 0;
+    for (i, h) in hv.into_iter().enumerate() {
+        if chmax!(max, h) {
+            res = i + 1;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
