82080d081874f3f0dd57ec032c90dad165a52b55
emanon001 <emanon001@gmail.com>
2023-08-21 23:24:58 +0900

    abc275/d

--- abc275/src/bin/d.rs
+++ abc275/src/bin/d.rs
@@ -8,9 +8,27 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn dfs(k: u64, dp: &mut HashMap<u64, u64>) -> u64 {
+    if k == 0 {
+        return 1;
+    }
+    if let Some(res) = dp.get(&k) {
+        return *res;
+    }
+
+    let res = dfs(k / 2, dp) + dfs(k / 3, dp);
+    dp.insert(k, res);
+    res
+}
+
 fn solve() {
     input! {
+        n: u64
     };
+
+    let mut dp = HashMap::new();
+    let res = dfs(n, &mut dp);
+    println!("{}", res);
 }
 
 fn main() {
