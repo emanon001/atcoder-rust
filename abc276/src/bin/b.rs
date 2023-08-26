4353c9d3efabb7bcadf7ef0b951d78566b985213
emanon001 <emanon001@gmail.com>
2023-08-26 23:53:58 +0900

    abc276/b

--- abc276/src/bin/b.rs
+++ abc276/src/bin/b.rs
@@ -10,7 +10,24 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![BTreeSet::new(); n];
+    for (u, v) in edges {
+        graph[u].insert(v + 1);
+        graph[v].insert(u + 1);
+    }
+    for set in graph {
+        let len = set.len();
+        let res = if len == 0 {
+            format!("{}", len)
+        } else {
+            format!("{} {}", len, set.iter().join(" "))
+        };
+        println!("{}", res);
+    }
 }
 
 fn main() {
