ab1c5ac4191a49d5c7272cda7f5b46d996be044f
emanon001 <emanon001@gmail.com>
2023-08-27 00:38:10 +0900

    abc276/c

--- abc276/src/bin/c.rs
+++ abc276/src/bin/c.rs
@@ -8,9 +8,43 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize,
+        p: [usize; n]
     };
+
+    for i in (0..n - 1).rev() {
+        let mut set = (1..=n).collect::<BTreeSet<_>>();
+        let mut new_perm = Vec::new();
+        for j in 0..i {
+            new_perm.push(p[j]);
+            set.remove(&p[j]);
+        }
+        let a = p[i];
+        if let Some(&b) = set.range(..a).next_back() {
+            new_perm.push(b);
+            set.remove(&b);
+            for c in set.into_iter().rev() {
+                new_perm.push(c);
+            }
+            println!("{}", new_perm.iter().join(" "));
+            return;
+        }
+    }
 }
 
 fn main() {
