a831deb0edeb6e2b6fac9c6cd93c7bd89a2ee9ab
emanon001 <emanon001@gmail.com>
2023-09-01 23:43:40 +0900

    abc278/d

--- abc278/src/bin/d.rs
+++ abc278/src/bin/d.rs
@@ -10,7 +10,46 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [u64; n],
+        q: usize,
     };
+
+    let mut all: Option<u64> = None;
+    let mut add_map: HashMap<usize, u64> = HashMap::new();
+    for _ in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    x: u64
+                };
+                all = Some(x);
+                add_map = HashMap::new();
+            }
+            2 => {
+                input! {
+                    i: Usize1,
+                    x: u64
+                };
+                *add_map.entry(i).or_insert(0_u64) += x;
+            }
+            3 => {
+                input! {
+                    i: Usize1
+                };
+                let res = if let Some(all) = all {
+                    all + add_map.get(&i).unwrap_or(&0_u64)
+                } else {
+                    av[i] + add_map.get(&i).unwrap_or(&0_u64)
+                };
+                println!("{}", res);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
