2594cd28b94ed77a9fbcbf530ea4b1b394e67720
emanon001 <emanon001@gmail.com>
2023-09-01 23:33:23 +0900

    abc278/c

--- abc278/src/bin/c.rs
+++ abc278/src/bin/c.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        _: usize, q: usize,
+        queries: [(usize, usize, usize); q]
     };
+
+    let mut follow_map = HashMap::new();
+    for (t, a, b) in queries {
+        match t {
+            1 => {
+                follow_map.entry(a).or_insert(HashSet::new()).insert(b);
+            }
+            2 => {
+                if let Some(set) = follow_map.get_mut(&a) {
+                    set.remove(&b);
+                }
+            }
+            3 => {
+                let is_ok = match (follow_map.get(&a), follow_map.get(&b)) {
+                    (Some(set1), Some(set2)) => set1.contains(&b) && set2.contains(&a),
+                    _ => false,
+                };
+                let res = if is_ok { "Yes" } else { "No" };
+                println!("{}", res);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
