ed78483b77b6ce1464ec7225659faa751cda35bf
emanon001 <emanon001@gmail.com>
2023-09-01 23:25:21 +0900

    abc278/b

--- abc278/src/bin/b.rs
+++ abc278/src/bin/b.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, m: usize,
     };
+
+    for add in 0..60 * 24 {
+        let minutes = (h * 60 + m + add) % (60 * 24);
+        let new_h = minutes / 60;
+        let new_m = minutes % 60;
+        let a = new_h / 10;
+        let b = new_h % 10;
+        let c = new_m / 10;
+        let d = new_m % 10;
+        if a * 10 + c <= 23 && b * 10 + d <= 59 {
+            println!("{}{} {}{}", a, b, c, d);
+            return;
+        }
+    }
 }
 
 fn main() {
