546b85a76d7ad7b16bcedf0955e03aa19c744b98
emanon001 <emanon001@gmail.com>
2023-09-02 23:23:51 +0900

    abc279/c

--- abc279/src/bin/c.rs
+++ abc279/src/bin/c.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        s: [Chars; h],
+        t: [Chars; h],
     };
+
+    let mut s2 = vec![Vec::new(); w];
+    let mut t2 = vec![Vec::new(); w];
+    for i in 0..w {
+        for j in 0..h {
+            s2[i].push(s[j][i]);
+            t2[i].push(t[j][i]);
+        }
+    }
+    s2.sort();
+    t2.sort();
+    let res = if s2 == t2 { "Yes" } else { "No" };
+    println!("{}", res);
 }
 
 fn main() {
