fa0401010c638e9b5ec6d7bdfe1207c5e3f966f8
emanon001 <emanon001@gmail.com>
2023-09-03 22:40:04 +0900

    abc280/c

--- abc280/src/bin/c.rs
+++ abc280/src/bin/c.rs
@@ -10,7 +10,17 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars,
+        t: Chars
     };
+
+    for i in 0..s.len() {
+        if s[i] != t[i] {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+    println!("{}", t.len());
 }
 
 fn main() {
