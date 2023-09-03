1c5cb9b26f91404dbc88a906f0490ca76033b9e2
emanon001 <emanon001@gmail.com>
2023-09-04 02:12:41 +0900

    feat: 入力を受け取ってsolverを作成

--- ahc023/src/bin/a.rs
+++ ahc023/src/bin/a.rs
@@ -8,9 +8,65 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+struct Solver {
+    t: usize,
+    h: usize,
+    w: usize,
+    i0: usize,
+    h_grid: Vec<Vec<char>>,
+    v_grid: Vec<Vec<char>>,
+    k: usize,
+    sdv: Vec<(usize, usize)>,
+}
+
+struct Input {
+    t: usize,
+    h: usize,
+    w: usize,
+    i0: usize,
+    h_grid: Vec<Vec<char>>,
+    v_grid: Vec<Vec<char>>,
+    k: usize,
+    sdv: Vec<(usize, usize)>,
+}
+
+impl Solver {
+    fn new(input: Input) -> Self {
+        Self {
+            t: input.t,
+            h: input.h,
+            w: input.w,
+            i0: input.i0,
+            h_grid: input.h_grid,
+            v_grid: input.v_grid,
+            k: input.k,
+            sdv: input.sdv,
+        }
+    }
+    fn solve(mut self) {}
+}
+
 fn solve() {
     input! {
+        t: usize, h: usize, w: usize, i0: usize,
+        h_grid: [Chars; h - 1],
+        v_grid: [Chars; h],
+        k: usize,
+        sdv: [(usize, usize); k]
     };
+
+    let input = Input {
+        t,
+        h,
+        w,
+        i0,
+        h_grid,
+        v_grid,
+        k,
+        sdv,
+    };
+    let mut solver = Solver::new(input);
+    solver.solve();
 }
 
 fn main() {
