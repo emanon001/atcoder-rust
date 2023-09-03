dd249a61259ec8658a58f21a3a2f95e2ba648beb
emanon001 <emanon001@gmail.com>
2023-09-03 22:37:14 +0900

    abc280/b

--- abc280/src/bin/b.rs
+++ abc280/src/bin/b.rs
@@ -10,7 +10,16 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        sv: [i64; n]
     };
+
+    let mut res = vec![sv[0]];
+    for i in 1..n {
+        let x = sv[i] - sv[i - 1];
+        res.push(x);
+    }
+    println!("{}", res.into_iter().join(" "));
 }
 
 fn main() {
