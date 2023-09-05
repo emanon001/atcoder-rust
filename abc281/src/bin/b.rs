938e0d16afa23a368a9bdd582a618bb32ab75a0e
emanon001 <emanon001@gmail.com>
2023-09-05 23:20:39 +0900

    abc281/b

--- abc281/src/bin/b.rs
+++ abc281/src/bin/b.rs
@@ -10,7 +10,36 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars
     };
+
+    if s.len() != 8 {
+        println!("No");
+        return;
+    }
+
+    if !s[0].is_ascii_uppercase() {
+        println!("No");
+        return;
+    }
+
+    let ns = s[1..7].iter().join("").parse::<usize>();
+    if let Ok(n) = ns {
+        if n.to_string().len() != 6 {
+            println!("No");
+            return;
+        }
+    } else {
+        println!("No");
+        return;
+    }
+
+    if !s[7].is_ascii_uppercase() {
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
 }
 
 fn main() {
