7614f1b4ace3993879ea5331d10b3b3f052a324d
emanon001 <emanon001@gmail.com>
2023-09-09 23:45:29 +0900

    abc319/b

--- abc319/src/bin/b.rs
+++ abc319/src/bin/b.rs
@@ -10,7 +10,19 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
     };
+
+    let res = (0..=n)
+        .map(|i| {
+            (1..=9)
+                .filter(|j| n % j == 0 && i % (n / j) == 0)
+                .min()
+                .map(|j| j.to_string())
+                .unwrap_or("-".to_string())
+        })
+        .join("");
+    println!("{}", res);
 }
 
 fn main() {
