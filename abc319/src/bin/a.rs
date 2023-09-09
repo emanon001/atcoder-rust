9a414ddb2da6e2acde5b37372d382aa2eedf8b05
emanon001 <emanon001@gmail.com>
2023-09-09 23:33:45 +0900

    abc319/a

--- abc319/src/bin/a.rs
+++ abc319/src/bin/a.rs
@@ -10,7 +10,25 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: String
     };
+
+    let rate_map = vec![
+        ("tourist", 3858),
+        ("ksun48", 3679),
+        ("Benq", 3658),
+        ("Um_nik", 3648),
+        ("apiad", 3638),
+        ("Stonefeang", 3630),
+        ("ecnerwala", 3613),
+        ("mnbvmar", 3555),
+        ("newbiedmy", 3516),
+        ("semiexp", 3481),
+    ]
+    .into_iter()
+    .collect::<HashMap<_, _>>();
+    let res = rate_map.get(&s.as_ref()).unwrap();
+    println!("{}", res);
 }
 
 fn main() {
