103669f6aa283979cdea57ec4892623188b61ef1
emanon001 <emanon001@gmail.com>
2023-09-16 20:24:17 +0900

    abc282/c

--- abc282/src/bin/c.rs
+++ abc282/src/bin/c.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        _n: usize,
+        s: Chars
     };
+
+    let mut res = Vec::new();
+    let mut quote_count = 0;
+    for ch in s {
+        match ch {
+            '"' => {
+                quote_count += 1;
+                res.push('"');
+            }
+            ',' => {
+                res.push(if quote_count % 2 == 0 { '.' } else { ',' });
+            }
+            ch => {
+                res.push(ch);
+            }
+        }
+    }
+    println!("{}", res.iter().join(""));
 }
 
 fn main() {
