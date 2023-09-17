fc632ac9ef139a05ab113d254b2c911a44c7bc6d
emanon001 <emanon001@gmail.com>
2023-09-17 20:15:06 +0900

    abc283/c

--- abc283/src/bin/c.rs
+++ abc283/src/bin/c.rs
@@ -8,9 +8,42 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn compress_list<T: Copy + std::cmp::PartialEq>(list: Vec<T>) -> Vec<(T, usize)> {
+    let mut res = Vec::new();
+    if list.is_empty() {
+        return res;
+    }
+    let mut cur_v = list[0];
+    let mut count = 1;
+    for v in list.into_iter().skip(1) {
+        if v == cur_v {
+            count += 1;
+        } else {
+            res.push((cur_v, count));
+            count = 1;
+        }
+        cur_v = v;
+    }
+    res.push((cur_v, count));
+    res
+}
+
 fn solve() {
     input! {
+        s: Chars,
     };
+
+    let mut res = 0;
+    let compressed = compress_list(s);
+    for (ch, count) in compressed {
+        if ch == '0' {
+            res += count / 2;
+            res += count % 2;
+        } else {
+            res += count;
+        }
+    }
+    println!("{}", res);
 }
 
 fn main() {
