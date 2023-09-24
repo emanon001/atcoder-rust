9890c76923e3cba6af1c95e857549f5677e08d4a
emanon001 <emanon001@gmail.com>
2023-09-24 15:22:25 +0900

    feat: 仮実装

--- ahc024/src/bin/a.rs
+++ ahc024/src/bin/a.rs
@@ -8,9 +8,69 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+type Grid = Vec<Vec<usize>>;
+
+struct CityMap {
+    n: usize,
+    m: usize,
+    grid: Grid,
+}
+
+impl CityMap {
+    fn new(n: usize, m: usize, grid: Grid) -> Self {
+        Self { n, m, grid }
+    }
+}
+
+struct Solver {
+    n: usize,
+    m: usize,
+    city_map: CityMap,
+}
+
+impl Solver {
+    fn new(input: Input) -> Self {
+        Solver {
+            n: input.n,
+            m: input.m,
+            city_map: CityMap::new(input.n, input.m, input.grid),
+        }
+    }
+
+    fn solve(self) -> Output {
+        Output {
+            grid: vec![vec![0; self.n]; self.n],
+        }
+    }
+}
+
+struct Output {
+    grid: Grid,
+}
+
+struct Input {
+    n: usize,
+    m: usize,
+    grid: Grid,
+}
+
 fn solve() {
     input! {
+        n: usize,
+        m: usize,
+        grid: [[Usize1; n]; n]
     };
+
+    let solver = Solver::new(Input { n, m, grid });
+    let output = solver.solve();
+    println!(
+        "{}",
+        output
+            .grid
+            .into_iter()
+            .map(|row| row.into_iter().join(" "))
+            .join("\n")
+    );
 }
 
 fn main() {
