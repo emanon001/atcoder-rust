95083cfd839221c8490488b16c51fbe9f4c0ad0b
emanon001 <emanon001@gmail.com>
2023-09-25 23:46:49 +0900

    abc286/a

--- abc286/src/bin/a.rs
+++ abc286/src/bin/a.rs
@@ -10,7 +10,14 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, p: Usize1, q: Usize1, r: Usize1, _s: Usize1,
+        mut av: [i64; n]
     };
+
+    for i in 0..(q - p + 1) {
+        av.swap(p + i, r + i);
+    }
+    println!("{}", av.iter().join(" "));
 }
 
 fn main() {
