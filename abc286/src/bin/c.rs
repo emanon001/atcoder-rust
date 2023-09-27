75ce4566da06b82f6da959a9bb62faeadd50f8e7
emanon001 <emanon001@gmail.com>
2023-09-27 23:20:10 +0900

    abc286/c

--- abc286/src/bin/c.rs
+++ abc286/src/bin/c.rs
@@ -8,9 +8,36 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize, a: u64, b: u64,
+        s: Chars
     };
+
+    let mut res = 1_u64 << 60;
+    for a_count in 0..n {
+        let mut sum = a * a_count as u64;
+        for i in 0..n / 2 {
+            let pos = (i + a_count) % n;
+            let pos2 = ((n - i - 1) + a_count) % n;
+            sum += if s[pos] == s[pos2] { 0 } else { b };
+        }
+        chmin!(res, sum);
+    }
+    println!("{}", res);
 }
 
 fn main() {
