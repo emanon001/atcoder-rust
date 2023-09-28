278755a4c8a8c66710b8321bf6501ce2b73694ba
emanon001 <emanon001@gmail.com>
2023-09-28 23:58:02 +0900

    abc287/a

--- abc287/src/bin/a.rs
+++ abc287/src/bin/a.rs
@@ -8,9 +8,29 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn count_list<T: Ord + std::hash::Hash + Clone>(
+    list: Vec<T>,
+) -> std::collections::HashMap<T, usize> {
+    let mut map = std::collections::HashMap::new();
+    for v in list {
+        *map.entry(v).or_insert(0) += 1;
+    }
+    map
+}
+
 fn solve() {
     input! {
+        n: usize,
+        sv: [String; n]
     };
+
+    let map = count_list(sv);
+    let res = if map.get("For").unwrap_or(&0) > map.get("Against").unwrap_or(&0) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", res);
 }
 
 fn main() {
