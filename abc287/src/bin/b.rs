35684d1faa5db44ed8d6f67b51445b58801cc24c
emanon001 <emanon001@gmail.com>
2023-09-29 00:02:33 +0900

    abc287/b

--- abc287/src/bin/b.rs
+++ abc287/src/bin/b.rs
@@ -10,7 +10,14 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        sv: [String; n],
+        tv: [String; m],
     };
+
+    let t_set = tv.into_iter().collect::<HashSet<_>>();
+    let res = sv.into_iter().filter(|s| t_set.contains(&s[3..])).count();
+    println!("{}", res);
 }
 
 fn main() {
