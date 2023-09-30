f43443b26131149c55a3a90ab54c4e73acf31168
emanon001 <emanon001@gmail.com>
2023-10-01 03:11:49 +0900

    abc287/e

--- abc287/src/bin/e.rs
+++ abc287/src/bin/e.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        sv: [Chars; n]
     };
+
+    let mut count_map = HashMap::new();
+    let m = 2_u128.pow(61) - 1;
+    let b = 100000007_u128;
+    for s in &sv {
+        let mut h = 0_u128;
+        for ch in s {
+            h = (h * b + (*ch as u8) as u128) % m;
+            *count_map.entry(h).or_insert(0) += 1;
+        }
+    }
+    for s in sv {
+        let mut res = 0;
+        let mut h = 0_u128;
+        for ch in s {
+            h = (h * b + (ch as u8) as u128) % m;
+            if count_map.get(&h).unwrap_or(&0) <= &1 {
+                break;
+            }
+            res += 1;
+        }
+        println!("{}", res);
+    }
 }
 
 fn main() {
