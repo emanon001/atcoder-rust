812071d31cae5b57d34c44a29e5b253521e8b7e3
emanon001 <emanon001@gmail.com>
2023-09-30 23:06:20 +0900

    abc287/d

--- abc287/src/bin/d.rs
+++ abc287/src/bin/d.rs
@@ -10,7 +10,32 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        s: Chars,
+        t: Chars,
     };
+
+    let mut forward = vec![false; t.len() + 1];
+    forward[0] = true;
+    for i in 0..t.len() {
+        forward[i + 1] = forward[i] && (s[i] == '?' || t[i] == '?' || s[i] == t[i]);
+    }
+    let mut backward = vec![false; t.len() + 1];
+    backward[0] = true;
+    for i in 0..t.len() {
+        let si = s.len() - 1 - i;
+        let ti = t.len() - 1 - i;
+        backward[i + 1] = backward[i] && (s[si] == '?' || t[ti] == '?' || s[si] == t[ti]);
+    }
+    // eprintln!("{:?}", forward);
+    // eprintln!("{:?}", backward);
+    for i in 0..=t.len() {
+        let res = if forward[i] && backward[t.len() - i] {
+            "Yes"
+        } else {
+            "No"
+        };
+        println!("{}", res);
+    }
 }
 
 fn main() {
