7081264e1f41755333a718fd59ec877d47be3f4d
emanon001 <emanon001@gmail.com>
2023-10-02 05:01:50 +0900

    abc284/e

--- abc284/src/bin/e.rs
+++ abc284/src/bin/e.rs
@@ -10,7 +10,38 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, m: usize,
+        edges: [(Usize1, Usize1); m]
     };
+
+    let mut graph = vec![Vec::new(); n];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    let mut visited = vec![false; n];
+    visited[0] = true;
+    let ans = dfs(0, &mut visited, &graph);
+    println!("{}", ans);
+}
+
+const MAX_ANS: usize = 1000000;
+
+fn dfs(u: usize, visited: &mut Vec<bool>, graph: &Vec<Vec<usize>>) -> usize {
+    let mut res = 1;
+    for &v in &graph[u] {
+        if visited[v] {
+            continue;
+        }
+        visited[v] = true;
+        res = (res + dfs(v, visited, graph)).min(MAX_ANS);
+        visited[v] = false;
+        if res == MAX_ANS {
+            break;
+        }
+    }
+    res
 }
 
 fn main() {
