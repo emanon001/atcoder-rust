43771d18ab73d26c831ac2b52c74b67c80915e5d
emanon001 <emanon001@gmail.com>
2023-10-02 02:49:48 +0900

    abc308/e

--- abc308/src/bin/e.rs
+++ abc308/src/bin/e.rs
@@ -10,7 +10,59 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n],
+        s: Chars
     };
+
+    // dp[i][j][k][l] = count
+    // i: i番目まで見た
+    // j: Mに対応する値(0, 1, 2, 3 = 未設定)
+    // k: Eに対応する値(0, 1, 2, 3)
+    // l: Xに対応する値(0, 1, 2, 3)
+    let mut dp = vec![vec![vec![vec![0_u64; 4]; 4]; 4]; n + 1];
+    dp[0][3][3][3] = 1;
+    for i in 0..n {
+        let a = av[i];
+        let ch = s[i];
+        if ch == 'M' {
+            dp[i + 1][a][3][3] += 1;
+        }
+        if ch == 'E' {
+            for j in 0..3 {
+                dp[i + 1][j][a][3] += dp[i][j][3][3];
+            }
+        }
+        if ch == 'X' {
+            for j in 0..3 {
+                for k in 0..3 {
+                    dp[i + 1][j][k][a] += dp[i][j][k][3];
+                }
+            }
+        }
+        for j in 0..4 {
+            for k in 0..4 {
+                for l in 0..4 {
+                    dp[i + 1][j][k][l] += dp[i][j][k][l];
+                }
+            }
+        }
+    }
+    // eprintln!("{:?}", dp);
+    let mut ans = 0_u64;
+    for j in 0..3 {
+        for k in 0..3 {
+            for l in 0..3 {
+                for add in 0..=4 {
+                    if add != j && add != k && add != l {
+                        ans += add as u64 * dp[n][j][k][l];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
