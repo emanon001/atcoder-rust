f391984bbad6e09e160ddf471bf733e743f4d692
emanon001 <emanon001@gmail.com>
2023-10-04 23:55:35 +0900

    abc302/a

--- abc302/src/bin/a.rs
+++ abc302/src/bin/a.rs
@@ -10,7 +10,13 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        a: i64, b: i64
     };
+
+    let mut ans = a / b;
+    let c = a % b;
+    ans += if c > 0 { 1 } else { 0 };
+    println!("{}", ans);
 }
 
 fn main() {
