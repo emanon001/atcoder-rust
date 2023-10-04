d225d913399d5be97a6f861c506c78f152c29617
emanon001 <emanon001@gmail.com>
2023-10-05 00:10:42 +0900

    abc302/b

--- abc302/src/bin/b.rs
+++ abc302/src/bin/b.rs
@@ -10,7 +10,45 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        h: usize, w: usize,
+        grid: [Chars; h]
     };
+
+    let snuke = "snuke".chars().collect::<Vec<_>>();
+
+    for i in 0..h {
+        for j in 0..w {
+            let dirs: Vec<(isize, isize)> = vec![
+                (0, 1),
+                (0, -1),
+                (1, 0),
+                (-1, 0),
+                (1, 1),
+                (-1, -1),
+                (1, -1),
+                (-1, 1),
+            ];
+
+            'dirs: for (di, dj) in dirs {
+                let mut ans = Vec::new();
+                for k in 0..5 {
+                    let new_i = i as isize + di * k as isize;
+                    let new_j = j as isize + dj * k as isize;
+                    if new_i < 0 || new_i >= h as isize || new_j < 0 || new_j >= w as isize {
+                        continue 'dirs;
+                    }
+                    if snuke[k] != grid[new_i as usize][new_j as usize] {
+                        continue 'dirs;
+                    }
+                    ans.push((new_i + 1, new_j + 1));
+                }
+                println!(
+                    "{}",
+                    ans.iter().map(|(i, j)| format!("{} {}", i, j)).join("\n")
+                );
+            }
+        }
+    }
 }
 
 fn main() {
