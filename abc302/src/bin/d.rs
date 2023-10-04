2355151bfd445ed5ce70edff5f93e5f5e5209385
emanon001 <emanon001@gmail.com>
2023-10-05 00:30:52 +0900

    abc302/d

--- abc302/src/bin/d.rs
+++ abc302/src/bin/d.rs
@@ -8,9 +8,36 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize, d: i64,
+        av: [i64; n],
+        bv: [i64; m],
     };
+
+    let mut ans = -1_i64;
+    let b_set = bv.into_iter().collect::<BTreeSet<_>>();
+    for a in av {
+        let range = (a - d)..=(a + d);
+        if let Some(b) = b_set.range(range).next_back() {
+            let sum = a + b;
+            chmax!(ans, sum);
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
