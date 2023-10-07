06283117a0f623aff89ea783802cd2e26011f555
emanon001 <emanon001@gmail.com>
2023-10-07 23:51:34 +0900

    abc289/d

--- abc289/src/bin/d.rs
+++ abc289/src/bin/d.rs
@@ -10,7 +10,38 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize,
+        av: [usize; n],
+        m: usize,
+        bv: [usize; m],
+        x: usize,
     };
+
+    let bv = bv.into_iter().collect::<HashSet<_>>();
+    let mut dp = vec![None; x + 1];
+    let is_ok = dfs(x, &av, &bv, &mut dp);
+    let ans = if is_ok { "Yes" } else { "No" };
+    println!("{}", ans);
+}
+
+fn dfs(floor: usize, av: &[usize], bv: &HashSet<usize>, dp: &mut Vec<Option<bool>>) -> bool {
+    if floor == 0 {
+        return true;
+    }
+    if bv.contains(&floor) {
+        return false;
+    }
+    if let Some(ans) = dp[floor] {
+        return ans;
+    }
+    let mut ans = false;
+    for &a in av {
+        if floor >= a {
+            ans = ans || dfs(floor - a, av, bv, dp);
+        }
+    }
+    dp[floor] = Some(ans);
+    ans
 }
 
 fn main() {
