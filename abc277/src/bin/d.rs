09d19d6d14d2bd13d6c86e88599c2c5f67e7bc82
emanon001 <emanon001@gmail.com>
2023-10-08 11:50:29 +0900

    abc277/d

--- abc277/src/bin/d.rs
+++ abc277/src/bin/d.rs
@@ -8,9 +8,137 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub struct UnionFind {
+    n: usize,
+    root: Vec<usize>,
+    rank: Vec<usize>,
+    size: Vec<usize>,
+}
+impl UnionFind {
+    pub fn new(n: usize) -> Self {
+        let root = (0..n).collect();
+        let rank = vec![0; n];
+        let size = vec![1; n];
+        Self {
+            n,
+            root,
+            rank,
+            size,
+        }
+    }
+    pub fn find(&mut self, x: usize) -> usize {
+        assert!(x < self.n);
+        if self.root[x] == x {
+            x
+        } else {
+            let root = self.find(self.root[x]);
+            self.root[x] = root;
+            root
+        }
+    }
+    pub fn unite(&mut self, x: usize, y: usize) {
+        assert!(x < self.n && y < self.n);
+        let x_root = self.find(x);
+        let y_root = self.find(y);
+        if x_root == y_root {
+            return;
+        }
+        if self.rank[x_root] < self.rank[y_root] {
+            self.root[x_root] = y_root;
+            self.size[y_root] += self.size[x_root];
+        } else {
+            self.root[y_root] = x_root;
+            self.size[x_root] += self.size[y_root];
+            if self.rank[x_root] == self.rank[y_root] {
+                self.rank[x_root] += 1;
+            }
+        }
+    }
+    pub fn size(&mut self, x: usize) -> usize {
+        assert!(x < self.n);
+        let x_root = self.find(x);
+        self.size[x_root]
+    }
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        assert!(x < self.n && y < self.n);
+        self.find(x) == self.find(y)
+    }
+    pub fn groups(&mut self) -> Vec<Vec<usize>> {
+        let mut groups = std::collections::HashMap::new();
+        for x in 0..self.n {
+            let k = self.find(x);
+            groups.entry(k).or_insert(Vec::new()).push(x);
+        }
+        groups.values().cloned().collect::<Vec<_>>()
+    }
+}
+
+pub fn compress_zahyo<T: Ord + std::hash::Hash + Clone>(
+    zahyo: &[T],
+) -> std::collections::HashMap<T, usize> {
+    let mut set = std::collections::BTreeSet::new();
+    for x in zahyo {
+        set.insert(x.clone());
+    }
+    let mut map = std::collections::HashMap::new();
+    for (i, x) in set.into_iter().enumerate() {
+        map.insert(x, i);
+    }
+    map
+}
+
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input! {
+        n: usize, m: usize,
+        av: [usize; n]
     };
+
+    let mut count_map = HashMap::new();
+    for a in &av {
+        *count_map.entry(*a).or_insert(0) += 1;
+    }
+
+    let compressed = compress_zahyo(&av);
+    let mut inverted_compressed = HashMap::new();
+    for (&k, &v) in &compressed {
+        inverted_compressed.insert(v, k);
+    }
+
+    let mut uf = UnionFind::new(compressed.len());
+    for (k, v) in &compressed {
+        let k2 = (k + 1) % m;
+        if let Some(v2) = compressed.get(&k2) {
+            uf.unite(*v, *v2);
+        }
+    }
+
+    let mut sum = 0;
+    for (k, v) in &count_map {
+        sum += k * v;
+    }
+    let mut ans = sum;
+    for g in uf.groups() {
+        let mut g_sum = 0;
+        for u in g {
+            let a = inverted_compressed[&u];
+            g_sum += a * count_map[&a];
+        }
+        chmin!(ans, sum - g_sum);
+    }
+    println!("{}", ans);
 }
 
 fn main() {
