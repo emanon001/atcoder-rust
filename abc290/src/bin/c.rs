9132f1f38c0f007d9b62207c1bcb467ea5d79c3b
emanon001 <emanon001@gmail.com>
2023-10-08 23:53:47 +0900

    abc290/c

--- abc290/src/bin/c.rs
+++ abc290/src/bin/c.rs
@@ -10,7 +10,18 @@ use std::collections::*;
 
 fn solve() {
     input! {
+        n: usize, k: usize,
+        av: [usize; n]
     };
+
+    let mut exists = vec![false; n];
+    for a in av {
+        if a < k {
+            exists[a] = true;
+        }
+    }
+    let ans = exists.into_iter().take_while(|v| *v).take(k).count();
+    println!("{}", ans);
 }
 
 fn main() {
