4f31fe9d9a2679c41971c3f8720b454506fc342d
emanon001 <emanon001@gmail.com>
2023-10-14 23:23:11 +0900

    abc291/c

--- abc291/src/bin/c.rs
+++ abc291/src/bin/c.rs
@@ -8,9 +8,32 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+/// 上下左右 (i, j)
+pub const UDLR_DIRS: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
 fn solve() {
     input! {
+        _n: usize,
+        s: Chars
     };
+
+    let mut pos = (0_isize, 0_isize);
+    let mut pos_set = HashSet::new();
+    for ch in s {
+        pos_set.insert(pos);
+        pos = match ch {
+            'R' => (pos.0 + 1, pos.1),
+            'L' => (pos.0 - 1, pos.1),
+            'U' => (pos.0, pos.1 + 1),
+            'D' => (pos.0, pos.1 - 1),
+            _ => unreachable!()
+        };
+        if pos_set.contains(&pos) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
