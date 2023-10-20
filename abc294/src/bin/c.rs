b55672afe0a1d759899601d92f3352cddc7f9924
emanon001 <emanon001@gmail.com>
2023-10-20 23:47:19 +0900

    abc294/c

--- abc294/src/bin/c.rs
+++ abc294/src/bin/c.rs
@@ -13,7 +13,18 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize, m: usize,
+        av: [u64; n],
+        bv: [u64; m],
     };
+
+    let mut order_map = HashMap::new();
+    let sorted = av.clone().into_iter().chain(bv.clone()).sorted();
+    for (i, v) in sorted.enumerate() {
+        order_map.insert(v, i + 1);
+    }
+    println!("{}", av.into_iter().map(|a| order_map[&a]).join(" "));
+    println!("{}", bv.into_iter().map(|a| order_map[&a]).join(" "));
 }
 
 fn main() {
