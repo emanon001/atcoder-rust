a812bcabaebd513d1d4ae344523d10c3ba8509a4
emanon001 <emanon001@gmail.com>
2023-10-20 23:41:02 +0900

    abc294/b

--- abc294/src/bin/b.rs
+++ abc294/src/bin/b.rs
@@ -13,7 +13,18 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        h: usize, w: usize,
+        grid: [[u8; w]; h]
     };
+
+    let ans = grid.into_iter().map(|row| {
+        row.into_iter().map(|a| if a == 0 {
+            '.'
+        } else {
+            (b'A' + a - 1) as char
+        }).join("")
+    }).join("\n");
+    println!("{}", ans);
 }
 
 fn main() {
