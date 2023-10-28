20839db8ba99f02318002ce224d9f8a37f99c0cf
emanon001 <emanon001@gmail.com>
2023-10-28 23:30:08 +0900

    abc311/b

--- abc311/src/bin/b.rs
+++ abc311/src/bin/b.rs
@@ -9,11 +9,44 @@ use proconio::marker::*;
 use std::collections::*;
 use std::io::{stdin, BufReader};
 
+pub fn compress_list<T: Copy + std::cmp::PartialEq>(list: Vec<T>) -> Vec<(T, usize)> {
+    let mut res = Vec::new();
+    if list.is_empty() {
+        return res;
+    }
+    let mut cur_v = list[0];
+    let mut count = 1;
+    for v in list.into_iter().skip(1) {
+        if v == cur_v {
+            count += 1;
+        } else {
+            res.push((cur_v, count));
+            count = 1;
+        }
+        cur_v = v;
+    }
+    res.push((cur_v, count));
+    res
+}
+
 fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize, d: usize,
+        s: [Chars; n]
     };
+
+    let mut counts = vec![0; d];
+    for i in 0..n {
+        for j in 0..d {
+            if s[i][j] == 'o' {
+                counts[j] += 1;
+            }
+        }
+    }
+    let ans = compress_list(counts).into_iter().filter(|(c, _)| c == &n).map(|(_, days)| days).max().unwrap_or(0);
+    println!("{}", ans);
 }
 
 fn main() {
