253c9058fbb0d15ace4a119d1f773b971df03cb1
emanon001 <emanon001@gmail.com>
2023-10-31 23:01:51 +0900

    abc312/c

--- abc312/src/bin/c.rs
+++ abc312/src/bin/c.rs
@@ -9,11 +9,89 @@ use proconio::marker::*;
 use std::collections::*;
 use std::io::{stdin, BufReader};
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<isize> for isize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self - *ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize, m: usize,
+        mut a: [i64; n],
+        mut b: [i64; m]
     };
+
+    a.sort();
+    b.sort();
+    let ans = bsearch(10_i64.pow(10), 0, |x| {
+        let c1 = bsearch(-1_isize, n as isize, |i| {
+            let i = i as usize;
+            a[i] <= x
+        }).map(|i| i as usize + 1).unwrap_or(0);
+        let c2 = bsearch(m as isize, -1_isize, |i| {
+            let i = i as usize;
+            b[i] >= x
+        }).map(|i| m - i as usize).unwrap_or(0);
+        c1 >= c2
+    }).unwrap();
+    println!("{}", ans);
 }
 
 fn main() {
