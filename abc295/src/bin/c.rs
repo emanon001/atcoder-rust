434b3b947325e5c0efa9001d13ff383446ada22d
emanon001 <emanon001@gmail.com>
2023-11-04 00:15:53 +0900

    abc295/c

--- abc295/src/bin/c.rs
+++ abc295/src/bin/c.rs
@@ -2,18 +2,33 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
+
+pub fn count_list<T: Ord + std::hash::Hash + Clone>(
+    list: Vec<T>,
+) -> std::collections::HashMap<T, usize> {
+    let mut map = std::collections::HashMap::new();
+    for v in list {
+        *map.entry(v).or_insert(0) += 1;
+    }
+    map
+}
 
 fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize,
+        a: [usize; n]
     };
+
+    let ans: usize = count_list(a).into_values().map(|c| c / 2).sum();
+    println!("{}", ans);
 }
 
 fn main() {
