35a4663153c56cfd3c41d4e5ffe9f74f95227641
emanon001 <emanon001@gmail.com>
2023-11-05 23:31:39 +0900

    abc296/c

--- abc296/src/bin/c.rs
+++ abc296/src/bin/c.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,22 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize, x: i64,
+        a: [i64; n]
     };
+
+    let mut set = HashSet::new();
+    for &a_i in &a {
+        set.insert(a_i);
+    }
+    for a_i in a {
+        let b = -(x - a_i);
+        if set.contains(&b) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
