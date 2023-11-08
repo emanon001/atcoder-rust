0d2ecd8becf51f026cdfa518318a66603c3a9a94
emanon001 <emanon001@gmail.com>
2023-11-08 23:55:04 +0900

    abc297/b

--- abc297/src/bin/b.rs
+++ abc297/src/bin/b.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,25 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        s: Chars
     };
+
+    let mut b = vec![];
+    for (i, ch) in s.iter().enumerate() {
+        if ch == &'B' {
+            b.push(i);
+        }
+    }
+    let s = s
+        .into_iter()
+        .filter(|&ch| ch == 'K' || ch == 'R')
+        .collect::<Vec<_>>();
+    let ans = if b[0] % 2 != b[1] % 2 && (s[0] == 'R' && s[1] == 'K' && s[2] == 'R') {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
 
 fn main() {
