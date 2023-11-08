645f4615142744047e59f5eef79f168c8e5296b1
emanon001 <emanon001@gmail.com>
2023-11-08 23:44:43 +0900

    bin/old abc297

---
+++ abc297/src/bin/f.rs
@@ -0,0 +1,29 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::{input, source::line::LineSource};
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{stdin, BufReader};
+
+fn solve() {
+    let mut source = LineSource::new(BufReader::new(stdin()));
+    macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
+    input! {
+    };
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("big stack size".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(|| {
+            solve();
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+}
