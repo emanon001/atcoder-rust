1db59a20f753df335e7bab9ba9d346126bb4c10c
emanon001 <emanon001@gmail.com>
2023-11-09 00:02:44 +0900

    abc297/c

--- abc297/src/bin/c.rs
+++ abc297/src/bin/c.rs
@@ -2,18 +2,57 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
+
+pub fn compress_list<T: Copy + std::cmp::PartialEq>(list: Vec<T>) -> Vec<(T, usize)> {
+    let mut res = Vec::new();
+    if list.is_empty() {
+        return res;
+    }
+    let mut cur_v = list[0];
+    let mut count = 1;
+    for v in list.into_iter().skip(1) {
+        if v == cur_v {
+            count += 1;
+        } else {
+            res.push((cur_v, count));
+            count = 1;
+        }
+        cur_v = v;
+    }
+    res.push((cur_v, count));
+    res
+}
 
 fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        h: usize, _: usize,
+        s: [Chars; h]
     };
+
+    let mut ans = vec![];
+    for row in s {
+        ans.push(
+            compress_list(row)
+                .into_iter()
+                .map(|(ch, c)| {
+                    if ch != 'T' {
+                        ch.to_string().repeat(c)
+                    } else {
+                        format!("{}{}", "PC".repeat(c / 2), "T".repeat(c % 2))
+                    }
+                })
+                .join(""),
+        );
+    }
+    println!("{}", ans.iter().join("\n"));
 }
 
 fn main() {
