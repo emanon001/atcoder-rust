b67faca171ec3b6d78c784fd18fd8de41b401ca1
emanon001 <emanon001@gmail.com>
2023-11-09 00:09:56 +0900

    abc297/d

--- abc297/src/bin/d.rs
+++ abc297/src/bin/d.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,26 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        mut a: i64, mut b: i64
     };
+
+    let mut ans = 0;
+    while a != b {
+        let max = a.max(b);
+        let min = a.min(b);
+        let diff = (max - min).abs();
+        let mut c = diff / min;
+        if diff % min != 0 {
+            c += 1;
+        }
+        ans += c;
+        if max == a {
+            a -= b * c;
+        } else {
+            b -= a * c;
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
