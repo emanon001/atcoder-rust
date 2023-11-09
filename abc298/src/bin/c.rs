eb1c361a6a52a02d9bd88d32525046769aeadcd4
emanon001 <emanon001@gmail.com>
2023-11-09 23:59:03 +0900

    abc298/c

--- abc298/src/bin/c.rs
+++ abc298/src/bin/c.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,39 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize,
+        q: usize,
     };
+
+    let mut box_numbers = vec![BTreeSet::new(); n + 10];
+    let mut number_boxes = vec![BTreeSet::new(); 2 * 10.pow(5) + 10];
+    for q_i in 0..q {
+        input! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input! {
+                    i: usize, j: usize,
+                };
+                box_numbers[j].insert((i, q_i));
+                number_boxes[i].insert(j);
+            }
+            2 => {
+                input! {
+                    i: usize
+                };
+                println!("{}", box_numbers[i].iter().map(|(n, _)| n).join(" "));
+            }
+            3 => {
+                input! {
+                    i: usize
+                };
+                println!("{}", number_boxes[i].iter().join(" "));
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
