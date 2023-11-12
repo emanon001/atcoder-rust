00b7dec2dc531fd431633031e9ec3974102f67f4
emanon001 <emanon001@gmail.com>
2023-11-12 22:51:54 +0900

    abc299/c

--- abc299/src/bin/c.rs
+++ abc299/src/bin/c.rs
@@ -2,18 +2,52 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
+
+pub fn compress_list<T: Copy + std::cmp::PartialEq>(list: Vec<T>) -> Vec<(T, usize)> {
+    let mut res = Vec::new();
+    if list.is_empty() {
+        return res;
+    }
+    let mut cur_v = list[0];
+    let mut count = 1;
+    for v in list.into_iter().skip(1) {
+        if v == cur_v {
+            count += 1;
+        } else {
+            res.push((cur_v, count));
+            count = 1;
+        }
+        cur_v = v;
+    }
+    res.push((cur_v, count));
+    res
+}
 
 fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        _: usize,
+        s: Chars
     };
+
+    let ans: isize = compress_list(s)
+        .into_iter()
+        .tuple_windows()
+        .map(|(l1, l2)| match (l1, l2) {
+            (_, ('o', c)) => c as isize,
+            (('o', c), _) => c as isize,
+            _ => -1,
+        })
+        .max()
+        .unwrap_or(-1);
+    println!("{}", ans);
 }
 
 fn main() {
