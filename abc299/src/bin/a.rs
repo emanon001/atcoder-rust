7cc24bffe2b9a9d4e2cf29e66f974c83030eb548
emanon001 <emanon001@gmail.com>
2023-11-12 22:33:39 +0900

    abc299/a

--- abc299/src/bin/a.rs
+++ abc299/src/bin/a.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,19 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        _: usize,
+        s: Chars
     };
+
+    let l = s.iter().position(|&ch| ch == '|').unwrap();
+    let r = s.iter().rposition(|&ch| ch == '|').unwrap();
+    let i = s.iter().rposition(|&ch| ch == '*').unwrap();
+    let ans = if ((l + 1)..r).contains(&i) {
+        "in"
+    } else {
+        "out"
+    };
+    println!("{}", ans);
 }
 
 fn main() {
