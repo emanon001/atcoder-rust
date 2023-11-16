79093574f07ad4674077295bc897ad7ce280f6ce
emanon001 <emanon001@gmail.com>
2023-11-16 23:54:05 +0900

    abc300/b

--- abc300/src/bin/b.rs
+++ abc300/src/bin/b.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,30 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        h: usize, w: usize,
+        a: [Chars; h],
+        b: [Chars; h],
     };
+
+    for s in 0..h {
+        for t in 0..w {
+            if b == shift(s, t, &a, h, w) {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
+}
+
+fn shift(s: usize, t: usize, grid: &[Vec<char>], h: usize, w: usize) -> Vec<Vec<char>> {
+    let mut ans = vec![vec![' '; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            ans[(i + s) % h][(j + t) % w] = grid[i][j];
+        }
+    }
+    ans
 }
 
 fn main() {
