52754fab5053e9f77bc0bba63f71fbaa538262b2
emanon001 <emanon001@gmail.com>
2023-11-18 23:05:35 +0900

    abc301/b

--- abc301/src/bin/b.rs
+++ abc301/src/bin/b.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,29 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize,
+        a: [i64; n]
     };
+
+    let mut ans = Vec::new();
+    for i in 0..n {
+        ans.push(a[i]);
+        if i == n - 1 {
+            break;
+        }
+        let x = a[i];
+        let y = a[i + 1];
+        if x < y {
+            for v in x + 1..y {
+                ans.push(v);
+            }
+        } else if x > y {
+            for v in (y + 1..x).rev() {
+                ans.push(v);
+            }
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
