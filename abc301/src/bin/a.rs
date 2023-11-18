3616c0661001a349cb5ead2db6c5ebf7f78e0df3
emanon001 <emanon001@gmail.com>
2023-11-18 22:53:57 +0900

    abc301/a

--- abc301/src/bin/a.rs
+++ abc301/src/bin/a.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,27 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        _: usize,
+        s: Chars
     };
+
+    let mut takahashi_wins = 0;
+    let mut aoki_wins = 0;
+    let mut ans = 'T';
+    for ch in s {
+        if ch == 'T' {
+            takahashi_wins += 1;
+            if takahashi_wins > aoki_wins {
+                ans = 'T';
+            }
+        } else {
+            aoki_wins += 1;
+            if aoki_wins > takahashi_wins {
+                ans = 'A';
+            }
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
