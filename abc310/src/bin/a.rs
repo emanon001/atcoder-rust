9af4fbf21a3687dd7cb0ee6d48e3437d71c226d8
emanon001 <emanon001@gmail.com>
2023-11-19 23:46:12 +0900

    abc310/a

--- abc310/src/bin/a.rs
+++ abc310/src/bin/a.rs
@@ -2,9 +2,9 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::{input, source::line::LineSource};
 #[allow(unused_imports)]
 use std::collections::*;
 use std::io::{stdin, BufReader};
@@ -13,7 +13,12 @@ fn solve() {
     let mut source = LineSource::new(BufReader::new(stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        n: usize, p: i64, q: i64,
+        d: [i64; n]
     };
+
+    let ans = p.min(q + d.into_iter().min().unwrap());
+    println!("{}", ans);
 }
 
 fn main() {
