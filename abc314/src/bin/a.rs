85b1775eecb7f5034507c532c2e5b47457e7a1ba
emanon001 <emanon001@gmail.com>
2023-11-25 01:11:18 +0900

    abc314/a

--- abc314/src/bin/a.rs
+++ abc314/src/bin/a.rs
@@ -10,7 +10,12 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        n: usize
     };
+
+    let pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    let ans = pi.chars().take(n + 2).join("");
+    println!("{ans}");
 }
 
 fn main() {
