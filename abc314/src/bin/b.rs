3df55be4660f9fe4595ca282cdc0c729d6e29328
emanon001 <emanon001@gmail.com>
2023-11-25 01:21:12 +0900

    abc314/b

--- abc314/src/bin/b.rs
+++ abc314/src/bin/b.rs
@@ -10,7 +10,36 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        n: usize,
     };
+
+    let mut v = vec![];
+    for i in 0..n {
+        let i = i + 1;
+        input_interactive! {
+            c: usize,
+            a: [usize; c]
+        };
+        v.push((i, c, a.into_iter().collect::<HashSet<_>>()));
+    }
+    input_interactive! {
+        x: usize,
+    };
+    let filtered = v
+        .into_iter()
+        .filter(|(_, _, a)| a.contains(&x))
+        .collect_vec();
+    if filtered.is_empty() {
+        println!("0");
+        return;
+    }
+    let min_count = *filtered.iter().map(|(_, c, _)| c).min().unwrap();
+    let filtered = filtered
+        .into_iter()
+        .filter(|(_, c, _)| c == &min_count)
+        .collect_vec();
+    println!("{}", filtered.len());
+    println!("{}", filtered.iter().map(|(i, _, _)| i).join(" "));
 }
 
 fn main() {
