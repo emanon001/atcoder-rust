23a0b7a5e8a3e35e4dee15d4c324e8359fdd66aa
emanon001 <emanon001@gmail.com>
2023-11-25 01:32:52 +0900

    abc314/c

--- abc314/src/bin/c.rs
+++ abc314/src/bin/c.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        n: usize, m: usize,
+        s1: Chars,
+        c: [usize; n]
     };
+
+    let mut pos_map = HashMap::new();
+    for (i, p) in c.into_iter().enumerate() {
+        pos_map.entry(p).or_insert(Vec::new()).push(i);
+    }
+    let mut ans = vec![' '; n];
+    for (c, pv) in pos_map {
+        let m = pv.len();
+        for (i, &p) in pv.iter().enumerate() {
+            ans[pv[(i + 1) % m]] = s1[p];
+        }
+    }
+    println!("{}", ans.iter().join(""));
 }
 
 fn main() {
