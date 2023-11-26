1d7ae5f39480c7ce773b1919eda4f0e81bfd92b2
emanon001 <emanon001@gmail.com>
2023-11-26 23:21:41 +0900

    abc315/b

--- abc315/src/bin/b.rs
+++ abc315/src/bin/b.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        m: usize,
+        d: [usize; m]
     };
+
+    let sum: usize = d.iter().sum();
+    let mid = sum / 2 + 1;
+    let mut l = 0;
+    for (i, d_i) in d.into_iter().enumerate() {
+        let m = i + 1;
+        let r = l + d_i;
+        if (l..=r).contains(&mid) {
+            println!("{} {}", m, mid - l);
+            return;
+        }
+        l = r;
+    }
 }
 
 fn main() {
