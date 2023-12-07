3dc0196177d11fc7277537b4eb7c639a88d16533
emanon001 <emanon001@gmail.com>
2023-12-07 23:02:49 +0900

    abc320/b

--- abc320/src/bin/b.rs
+++ abc320/src/bin/b.rs
@@ -8,9 +8,34 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input_interactive! {
+        s: Chars
     };
+
+    let mut ans = 0;
+    for i in 0..s.len() {
+        for j in i + 1..=s.len() {
+            let s = &s[i..j];
+            if s.iter().join("") == s.iter().rev().collect::<String>() {
+                chmax!(ans, s.len());
+            }
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
