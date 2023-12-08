11187e13ef6e0c6557db2e7365c4d64e22e9aecd
emanon001 <emanon001@gmail.com>
2023-12-08 23:58:10 +0900

    abc321/b

--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -10,7 +10,27 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        n: usize, x: i64,
+        a: [i64; n - 1]
     };
+
+    let mut ans: Option<i64> = None;
+    for s in 0..=100 {
+        let a = a
+            .clone()
+            .into_iter()
+            .chain(vec![s])
+            .sorted()
+            .collect::<Vec<_>>();
+        let sum = a[1..n - 1].iter().sum::<i64>();
+        if sum >= x {
+            ans = match ans {
+                Some(m) => Some(m.min(s)),
+                None => Some(s),
+            }
+        }
+    }
+    println!("{}", ans.unwrap_or(-1));
 }
 
 fn main() {
