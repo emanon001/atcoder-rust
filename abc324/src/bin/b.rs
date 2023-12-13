b6e8c7c153e04bb9851cb3427fdc078611067498
emanon001 <emanon001@gmail.com>
2023-12-13 22:53:51 +0900

    abc324/b

--- abc324/src/bin/b.rs
+++ abc324/src/bin/b.rs
@@ -10,7 +10,22 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        n: u128
     };
+
+    for x in 0..66 {
+        for y in 0..40 {
+            let m = 2_u128.pow(x) * 3_u128.pow(y);
+            if m > n {
+                break;
+            }
+            if m == n {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
 
 fn main() {
