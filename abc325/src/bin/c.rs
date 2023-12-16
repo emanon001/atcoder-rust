8eae42c339294a3ace1ba7f9a5d86b3c96aafe97
emanon001 <emanon001@gmail.com>
2023-12-17 00:10:10 +0900

    abc325/c

--- abc325/src/bin/c.rs
+++ abc325/src/bin/c.rs
@@ -10,7 +10,47 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        h: usize, w: usize,
+        grid: [Chars; h],
     };
+
+    let mut ans = 0;
+    let mut visited = vec![vec![false; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            if grid[i][j] == '.' {
+                continue;
+            }
+            if visited[i][j] {
+                continue;
+            }
+            ans += 1;
+            traverse(i, j, h, w, &grid, &mut visited);
+        }
+    }
+    println!("{}", ans);
+}
+
+fn traverse(i: usize, j: usize, h: usize, w: usize, grid: &[Vec<char>], visited: &mut [Vec<bool>]) {
+    visited[i][j] = true;
+    for di in -1..=1 {
+        for dj in -1..=1 {
+            let ni = i as isize + di;
+            let nj = j as isize + dj;
+            if ni < 0 || ni >= h as isize || nj < 0 || nj >= w as isize {
+                continue;
+            }
+            let ni = ni as usize;
+            let nj = nj as usize;
+            if grid[ni][nj] == '.' {
+                continue;
+            }
+            if visited[ni][nj] {
+                continue;
+            }
+            traverse(ni, nj, h, w, grid, visited);
+        }
+    }
 }
 
 fn main() {
