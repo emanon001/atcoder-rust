34cac30a44f6cbf2b6894bdc2d695ee38b3eb888
emanon001 <emanon001@gmail.com>
2023-12-16 23:44:31 +0900

    bin/old abc325

---
+++ abc325/src/bin/d.rs
@@ -0,0 +1,26 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::input_interactive;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+fn solve() {
+    input_interactive! {
+    };
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("big stack size".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(|| {
+            solve();
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+}
