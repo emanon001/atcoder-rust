adafeedc5d74692b473f2e0fab1ebc0c3f5f88d7
emanon001 <emanon001@gmail.com>
2023-12-30 04:05:06 +0900

    abc330/b

--- abc330/src/bin/b.rs
+++ abc330/src/bin/b.rs
@@ -10,7 +10,23 @@ use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        n: usize, l: i64, r: i64,
+        a: [i64; n]
     };
+
+    let ans = a
+        .into_iter()
+        .map(|a_i| {
+            if (l..=r).contains(&a_i) {
+                a_i
+            } else if (l - a_i).abs() <= (r - a_i).abs() {
+                l
+            } else {
+                r
+            }
+        })
+        .join(" ");
+    println!("{}", ans);
 }
 
 fn main() {
