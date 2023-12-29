e757043d958e05466354ea8d80ba63edbb50f4b8
emanon001 <emanon001@gmail.com>
2023-12-29 23:18:12 +0900

    abc329/d

--- abc329/src/bin/d.rs
+++ abc329/src/bin/d.rs
@@ -5,12 +5,27 @@ use num::*;
 use proconio::input_interactive;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use std::cmp::Reverse;
 #[allow(unused_imports)]
 use std::collections::*;
 
 fn solve() {
     input_interactive! {
+        n: usize, m: usize,
+        a: [Usize1; m]
     };
+
+    let mut counts = vec![0; n];
+    let mut set = BTreeSet::new();
+    for a_i in a {
+        let cur_count = counts[a_i];
+        let new_count = cur_count + 1;
+        counts[a_i] = new_count;
+        set.remove(&(cur_count, Reverse(a_i)));
+        set.insert((cur_count + 1, Reverse(a_i)));
+        let ans = set.iter().last().unwrap().1 .0 + 1;
+        println!("{}", ans);
+    }
 }
 
 fn main() {
