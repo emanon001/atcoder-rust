c161eec8082131e45f18acc845272c035d4232f3
emanon001 <emanon001@gmail.com>
2023-12-29 23:06:50 +0900

    abc329/c

--- abc329/src/bin/c.rs
+++ abc329/src/bin/c.rs
@@ -8,9 +8,39 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn compress_list<T: Copy + std::cmp::PartialEq>(list: Vec<T>) -> Vec<(T, usize)> {
+    let mut res = Vec::new();
+    if list.is_empty() {
+        return res;
+    }
+    let mut cur_v = list[0];
+    let mut count = 1;
+    for v in list.into_iter().skip(1) {
+        if v == cur_v {
+            count += 1;
+        } else {
+            res.push((cur_v, count));
+            count = 1;
+        }
+        cur_v = v;
+    }
+    res.push((cur_v, count));
+    res
+}
+
 fn solve() {
     input_interactive! {
+        _: usize,
+        s: Chars
     };
+
+    let mut count_map = HashMap::new();
+    for (ch, count) in compress_list(s) {
+        let count = count.max(*count_map.get(&ch).unwrap_or(&0));
+        count_map.insert(ch, count);
+    }
+    let ans: usize = count_map.values().sum();
+    println!("{}", ans);
 }
 
 fn main() {
