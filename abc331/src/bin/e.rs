9bb6a942a196bd22e58ce4c4fb261cd7da40a32d
emanon001 <emanon001@gmail.com>
2023-12-31 00:35:24 +0900

    abc331/e

--- abc331/src/bin/e.rs
+++ abc331/src/bin/e.rs
@@ -8,9 +8,45 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 fn solve() {
     input_interactive! {
+        N: usize, M: usize, L: usize,
+        A: [usize; N],
+        B: [usize; M],
+        CD: [(Usize1, Usize1); L],
     };
+
+    let sorted_b = B
+        .into_iter()
+        .enumerate()
+        .sorted_by_key(|(_, b)| *b)
+        .rev()
+        .collect_vec();
+    let ng_set = CD.into_iter().collect::<HashSet<_>>();
+    let mut ans = 0;
+    for (i, a) in A.into_iter().enumerate() {
+        for (j, b) in &sorted_b {
+            if ng_set.contains(&(i, *j)) {
+                continue;
+            }
+            chmax!(ans, a + b);
+            break;
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
