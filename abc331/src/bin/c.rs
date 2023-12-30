47c8def7c5f19b44ddb9c9e8320deb6b1acd1d76
emanon001 <emanon001@gmail.com>
2023-12-31 00:14:24 +0900

    abc331/c

--- abc331/src/bin/c.rs
+++ abc331/src/bin/c.rs
@@ -8,9 +8,88 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+/// `T` is numeric only
+pub struct Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::SubAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    n: usize,
+    data: Vec<T>,
+}
+/// 0-origin
+/// [0, n)
+impl<T> Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::SubAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    pub fn new(n: usize) -> Self {
+        Self {
+            n,
+            data: vec![T::zero(); n + 1],
+        }
+    }
+    /// 0-origin
+    pub fn add(&mut self, i: usize, x: T) {
+        if i >= self.n {
+            panic!();
+        }
+        let mut i = i + 1;
+        while i <= self.n {
+            self.data[i] += x.clone();
+            i += ((i as isize) & -(i as isize)) as usize;
+        }
+    }
+    /// 0-origin
+    pub fn sub(&mut self, i: usize, x: T) {
+        if i >= self.n {
+            panic!();
+        }
+        let mut i = i + 1;
+        while i <= self.n {
+            self.data[i] -= x.clone();
+            i += ((i as isize) & -(i as isize)) as usize;
+        }
+    }
+    /// [0, i)
+    pub fn sum(&self, i: usize) -> T {
+        if i > self.n {
+            panic!();
+        }
+        let mut i = i;
+        let mut res = T::zero();
+        while i > 0 {
+            res += self.data[i].clone();
+            i -= ((i as isize) & -(i as isize)) as usize;
+        }
+        res
+    }
+    /// [i, j)
+    pub fn range_sum(&self, i: usize, j: usize) -> T {
+        if i > self.n || j > self.n {
+            panic!();
+        }
+        if i >= j {
+            return T::zero();
+        }
+        self.sum(j) - self.sum(i)
+    }
+}
+
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [usize; N],
     };
+
+    let mut bit = Bit::new(10.pow(6) + 1);
+    for &a in &A {
+        bit.add(a, a);
+    }
+    let ans = A
+        .into_iter()
+        .map(|a| bit.range_sum(a + 1, 10.pow(6) + 1))
+        .join(" ");
+    println!("{}", ans);
 }
 
 fn main() {
