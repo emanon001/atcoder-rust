b2dc5bebf632416e95c454a411838c29df3562db
emanon001 <emanon001@gmail.com>
2024-01-01 11:17:31 +0900

    abc333/a

--- abc333/src/bin/a.rs
+++ abc333/src/bin/a.rs
@@ -11,7 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
     };
+
+    let ans = N.to_string().repeat(N);
+    println!("{}", ans);
 }
 
 fn main() {
