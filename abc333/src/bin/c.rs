eb80a215850387d05ba09822990ebdcc6a04b799
emanon001 <emanon001@gmail.com>
2024-01-01 11:45:18 +0900

    abc333/c

--- abc333/src/bin/c.rs
+++ abc333/src/bin/c.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
     };
+
+    let ans = (1..=19)
+        .combinations_with_replacement(3)
+        .map(|v| {
+            let d1 = v[0];
+            let d2 = v[1];
+            let d3 = v[2];
+            "1".repeat(d1).parse::<i64>().unwrap()
+                + "1".repeat(d2).parse::<i64>().unwrap()
+                + "1".repeat(d3).parse::<i64>().unwrap()
+        })
+        .sorted()
+        .nth(N - 1)
+        .unwrap();
+    println!("{}", ans);
 }
 
 fn main() {
