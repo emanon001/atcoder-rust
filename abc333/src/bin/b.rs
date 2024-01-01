1ed1e8949fb1e8436fe6e46d4eae4112a4467a4c
emanon001 <emanon001@gmail.com>
2024-01-01 11:28:52 +0900

    abc333/b

--- abc333/src/bin/b.rs
+++ abc333/src/bin/b.rs
@@ -11,7 +11,20 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
+        T: Chars
     };
+
+    let s1 = S[0] as u8 as isize;
+    let s2 = S[1] as u8 as isize;
+    let t1 = T[0] as u8 as isize;
+    let t2 = T[1] as u8 as isize;
+
+    let d1 = (s1 - s2).abs().min(5 - (s1 - s2).abs());
+    let d2 = (t1 - t2).abs().min(5 - (t1 - t2).abs());
+
+    let ans = if d1 == d2 { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
