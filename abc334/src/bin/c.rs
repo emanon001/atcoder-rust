7ff68937c71760a324b88ea4bf5fff89ef3523ea
emanon001 <emanon001@gmail.com>
2024-01-02 07:02:39 +0900

    abc334/c

--- abc334/src/bin/c.rs
+++ abc334/src/bin/c.rs
@@ -11,7 +11,38 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        _: usize, K: usize,
+        A: [Usize1; K]
     };
+
+    let diffs = A
+        .into_iter()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect_vec();
+    let ans: usize = if K.is_even() {
+        diffs.into_iter().step_by(2).sum()
+    } else {
+        let cusum1 = vec![0]
+            .into_iter()
+            .chain(diffs.iter().step_by(2).scan(0, |state, x| {
+                *state += x;
+                Some(*state)
+            }))
+            .collect_vec();
+        let cusum2 = vec![0]
+            .into_iter()
+            .chain(diffs.iter().skip(1).step_by(2).scan(0, |state, x| {
+                *state += x;
+                Some(*state)
+            }))
+            .collect_vec();
+        (0..cusum1.len())
+            .map(|i| cusum1[i] + cusum2[cusum2.len() - 1] - cusum2[i])
+            .min()
+            .unwrap_or(0)
+    };
+    println!("{}", ans);
 }
 
 fn main() {
