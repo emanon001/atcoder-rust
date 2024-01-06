98d16ceb7a203089108a684685742332314e725c
emanon001 <emanon001@gmail.com>
2024-01-06 22:34:35 +0900

    abc321/d

--- abc321/src/bin/d.rs
+++ abc321/src/bin/d.rs
@@ -8,9 +8,94 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<isize> for isize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self - *ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
+#[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize, P: i64,
+        A: [i64; N],
+        mut B: [i64; M],
     };
+
+    B.sort();
+    let cusum = vec![0]
+        .into_iter()
+        .chain(B.iter().copied().scan(0, |acc, x| {
+            *acc += x;
+            Some(*acc)
+        }))
+        .collect_vec();
+    let mut ans = 0_i64;
+    for a in A {
+        let i = bsearch(-1, M as isize, |x| {
+            let j = x as usize;
+            a + B[j] < P
+        })
+        .map(|j| j as usize + 1)
+        .unwrap_or(0);
+        ans += a * i as i64 + cusum[i];
+        ans += (M - i) as i64 * P;
+    }
+    println!("{}", ans);
 }
 
 fn main() {
