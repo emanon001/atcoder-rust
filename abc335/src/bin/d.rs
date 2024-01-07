c02a722c0c3b7b4173f8548ee21a36e9f3d63fdf
emanon001 <emanon001@gmail.com>
2024-01-08 00:04:47 +0900

    abc335/d

--- abc335/src/bin/d.rs
+++ abc335/src/bin/d.rs
@@ -11,7 +11,38 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize
     };
+    let mut grid = vec![vec!["".to_string(); N]; N];
+    grid[N / 2][N / 2] = "T".to_string();
+    let mut id = 1;
+    for p in 0..(N / 2) {
+        go_around(&mut id, p, N, &mut grid);
+    }
+    println!("{}", grid.iter().map(|row| row.iter().join(" ")).join("\n"));
+}
+
+fn go_around(id: &mut usize, p: usize, n: usize, grid: &mut [Vec<String>]) {
+    // →
+    for j in p..(n - p - 1) {
+        grid[p][j] = id.to_string();
+        *id += 1;
+    }
+    // ↓
+    for i in p..(n - p) {
+        grid[i][n - p - 1] = id.to_string();
+        *id += 1;
+    }
+    // ←
+    for j in (p..(n - p - 1)).rev() {
+        grid[n - p - 1][j] = id.to_string();
+        *id += 1;
+    }
+    // ↑
+    for i in ((p + 1)..(n - p - 1)).rev() {
+        grid[i][p] = id.to_string();
+        *id += 1;
+    }
 }
 
 fn main() {
