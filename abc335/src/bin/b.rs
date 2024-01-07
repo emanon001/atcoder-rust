c6543eb1964303c8d201edf94827570710eab046
emanon001 <emanon001@gmail.com>
2024-01-07 23:24:06 +0900

    abc335/b

--- abc335/src/bin/b.rs
+++ abc335/src/bin/b.rs
@@ -11,7 +11,26 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N:  usize,
     };
+
+    let mut ans = Vec::new();
+    for x in 0..=21 {
+        for y in 0..=21 {
+            for z in 0..=21 {
+                if x + y + z <= N {
+                    ans.push((x, y, z));
+                }
+            }
+        }
+    }
+    ans.sort();
+    println!(
+        "{}",
+        ans.iter()
+            .map(|(x, y, z)| format!("{x} {y} {z}"))
+            .join("\n")
+    );
 }
 
 fn main() {
