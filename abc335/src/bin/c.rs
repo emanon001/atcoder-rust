c1023a848026c4a647a329ac5c8ea0fb1641f525
emanon001 <emanon001@gmail.com>
2024-01-07 23:46:11 +0900

    abc335/c

--- abc335/src/bin/c.rs
+++ abc335/src/bin/c.rs
@@ -11,7 +11,49 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, Q: usize,
     };
+    let mut positions = Vec::new();
+    for i in (1..=N).rev() {
+        positions.push((i as isize, 0_isize));
+    }
+    let mut current_pos = positions[positions.len() - 1];
+    for _ in 0..Q {
+        input_interactive! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input_interactive! {
+                    C: char
+                };
+                match C {
+                    'R' => {
+                        current_pos.0 += 1;
+                    }
+                    'L' => {
+                        current_pos.0 -= 1;
+                    }
+                    'U' => {
+                        current_pos.1 += 1;
+                    }
+                    'D' => {
+                        current_pos.1 -= 1;
+                    }
+                    _ => unreachable!(),
+                }
+                positions.push(current_pos);
+            }
+            2 => {
+                input_interactive! {
+                    P: usize
+                };
+                let p = positions[positions.len() - P];
+                println!("{} {}", p.0, p.1);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
