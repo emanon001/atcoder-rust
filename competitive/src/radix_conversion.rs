0683779e11ea5a22dd47df3751aa0d1e6b4b687c
emanon001 <emanon001@gmail.com>
2024-01-22 00:43:29 +0900

    refactor: 関数名を変更

--- competitive/src/radix_conversion.rs
+++ competitive/src/radix_conversion.rs
@@ -1,8 +1,8 @@
 use cargo_snippet::snippet;
 
 #[snippet("radix_conversion")]
-pub fn decimal_to_base_radix(x: u64, base: u64) -> Vec<u64> {
-    let mut x = x;
+pub fn convert_radix(radix10: u64, base: u64) -> Vec<u64> {
+    let mut x = radix10;
     let mut ans = Vec::new();
     loop {
         ans.push(x % base);
@@ -19,23 +19,31 @@ pub fn decimal_to_base_radix(x: u64, base: u64) -> Vec<u64> {
 mod tests {
     use super::*;
     #[test]
-    fn test_decimal_base_radix_2() {
-        assert_eq!(decimal_to_base_radix(0, 2), vec![0]);
-        assert_eq!(decimal_to_base_radix(1, 2), vec![1]);
-        assert_eq!(decimal_to_base_radix(2, 2), vec![1, 0]);
-        assert_eq!(decimal_to_base_radix(3, 2), vec![1, 1]);
+    fn test_convert_radix_2() {
+        assert_eq!(convert_radix(0, 2), vec![0]);
+        assert_eq!(convert_radix(1, 2), vec![1]);
+        assert_eq!(convert_radix(2, 2), vec![1, 0]);
+        assert_eq!(convert_radix(3, 2), vec![1, 1]);
     }
 
     #[test]
-    fn test_decimal_base_radix_3() {
-        assert_eq!(decimal_to_base_radix(0, 3), vec![0]);
-        assert_eq!(decimal_to_base_radix(1, 3), vec![1]);
-        assert_eq!(decimal_to_base_radix(2, 3), vec![2]);
-        assert_eq!(decimal_to_base_radix(3, 3), vec![1, 0]);
-        assert_eq!(decimal_to_base_radix(4, 3), vec![1, 1]);
-        assert_eq!(decimal_to_base_radix(5, 3), vec![1, 2]);
-        assert_eq!(decimal_to_base_radix(6, 3), vec![2, 0]);
-        assert_eq!(decimal_to_base_radix(7, 3), vec![2, 1]);
-        assert_eq!(decimal_to_base_radix(8, 3), vec![2, 2]);
+    fn test_convert_radix_3() {
+        assert_eq!(convert_radix(0, 3), vec![0]);
+        assert_eq!(convert_radix(1, 3), vec![1]);
+        assert_eq!(convert_radix(2, 3), vec![2]);
+        assert_eq!(convert_radix(3, 3), vec![1, 0]);
+        assert_eq!(convert_radix(4, 3), vec![1, 1]);
+        assert_eq!(convert_radix(5, 3), vec![1, 2]);
+        assert_eq!(convert_radix(6, 3), vec![2, 0]);
+        assert_eq!(convert_radix(7, 3), vec![2, 1]);
+        assert_eq!(convert_radix(8, 3), vec![2, 2]);
+    }
+
+    #[test]
+    fn test_convert_radix_10() {
+        assert_eq!(convert_radix(0, 10), vec![0]);
+        assert_eq!(convert_radix(1, 10), vec![1]);
+        assert_eq!(convert_radix(10, 10), vec![1, 0]);
+        assert_eq!(convert_radix(19, 10), vec![1, 9]);
     }
 }
