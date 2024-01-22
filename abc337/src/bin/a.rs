4f892f7eb29f1810232a34e9d3cd447e48914804
emanon001 <emanon001@gmail.com>
2024-01-22 23:35:52 +0900

    abc337/a

--- abc337/src/bin/a.rs
+++ abc337/src/bin/a.rs
@@ -1,17 +1,34 @@
 #[allow(unused_imports)]
 use itertools::Itertools;
+use ndarray::Order;
 #[allow(unused_imports)]
 use num::*;
 use proconio::input_interactive;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use std::cmp::Ordering;
 #[allow(unused_imports)]
 use std::collections::*;
 
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        XY: [(usize, usize); N]
     };
+
+    let mut x_sum = 0;
+    let mut y_sum = 0;
+    for (x, y) in XY {
+        x_sum += x;
+        y_sum += y;
+    }
+    let ans = match x_sum.cmp(&y_sum) {
+        Ordering::Equal => "Draw",
+        Ordering::Greater => "Takahashi",
+        Ordering::Less => "Aoki",
+    };
+    println!("{}", ans);
 }
 
 fn main() {
