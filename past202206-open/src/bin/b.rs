fb4707a20f9aea8d5293a10bcc9dc6f0da0fe551
emanon001 <emanon001@gmail.com>
2024-01-23 23:51:13 +0900

    past202206-open/b

--- past202206-open/src/bin/b.rs
+++ past202206-open/src/bin/b.rs
@@ -11,7 +11,20 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars
     };
+
+    let mut count_map = HashMap::new();
+    for i in 0..S.len() - 1 {
+        let key = format!("{}{}", S[i], S[i + 1]);
+        *count_map.entry(key).or_insert(0) += 1;
+    }
+    let mut tree = BTreeSet::new();
+    for (key, count) in count_map {
+        tree.insert((std::cmp::Reverse(count), key));
+    }
+    let ans = &tree.iter().next().unwrap().1;
+    println!("{}", ans);
 }
 
 fn main() {
