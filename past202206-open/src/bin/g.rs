ae1547eaca882532607a23d466679587878fb7b8
emanon001 <emanon001@gmail.com>
2024-01-25 10:36:16 +0900

    past202206-open/g

--- past202206-open/src/bin/g.rs
+++ past202206-open/src/bin/g.rs
@@ -11,7 +11,38 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        edges: [(Usize1, Usize1); N - 1]
     };
+
+    let mut graph = vec![vec![]; N];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    let mut visited = vec![false; N];
+    let ans = if traverse(0, N, &graph, &mut visited) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
+}
+
+fn traverse(u: usize, p: usize, graph: &[Vec<usize>], visited: &mut Vec<bool>) -> bool {
+    visited[u] = true;
+    let mut res = true;
+    for &v in &graph[u] {
+        if v == p {
+            continue;
+        }
+        if v != p && visited[v] {
+            return false;
+        }
+        res &= traverse(v, u, graph, visited);
+    }
+    res
 }
 
 fn main() {
