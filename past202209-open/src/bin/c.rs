6bac3cc0c8d8921edacdf273ace32cd8dac9313c
emanon001 <emanon001@gmail.com>
2024-01-27 00:04:19 +0900

    past202209-open/c

--- past202209-open/src/bin/c.rs
+++ past202209-open/src/bin/c.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        P: [[f64; 6]; 3]
     };
+
+    for k in 1..=18 {
+        let mut ans = 0_f64;
+        for a in 1..=6 {
+            for b in 1..=6 {
+                for c in 1..=6 {
+                    if a + b + c == k {
+                        ans +=
+                            (P[0][a - 1] / 100.0) * (P[1][b - 1] / 100.0) * (P[2][c - 1] / 100.0);
+                    }
+                }
+            }
+        }
+        println!("{}", ans);
+    }
 }
 
 fn main() {
