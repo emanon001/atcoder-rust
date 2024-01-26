7c60d8cef965520b2e65c34d507c9a92f15328c2
emanon001 <emanon001@gmail.com>
2024-01-26 23:50:04 +0900

    past202209-open/a

--- past202209-open/src/bin/a.rs
+++ past202209-open/src/bin/a.rs
@@ -11,7 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        X: usize, Y: usize, Z: usize
     };
+
+    let ans = (Z + X).max(Y);
+    println!("{}", ans);
 }
 
 fn main() {
