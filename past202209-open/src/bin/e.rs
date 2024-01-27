0ce7377d40301311b50284a10c715261691e722d
emanon001 <emanon001@gmail.com>
2024-01-28 00:14:39 +0900

    past202209-open/e

--- past202209-open/src/bin/e.rs
+++ past202209-open/src/bin/e.rs
@@ -11,7 +11,34 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        R: isize, N: isize, M: isize, L: isize,
+        S: [isize; L]
     };
+
+    let mut round = 1;
+    let mut count = 0;
+    let mut sum = 0;
+    for s in S {
+        count += 1;
+        sum += s;
+
+        if sum > N {
+            println!("No");
+            return;
+        }
+
+        if sum == N || count == M {
+            sum = 0;
+            count = 0;
+            round += 1;
+        }
+    }
+    if round != R + 1 {
+        println!("No");
+        return;
+    }
+    let ans = if count == 0 { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
