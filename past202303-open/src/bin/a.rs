92a8269ac56db0dae6f0df0303f9cf8987d3842e
emanon001 <emanon001@gmail.com>
2024-02-04 23:56:38 +0900

    past202303-open/a

--- past202303-open/src/bin/a.rs
+++ past202303-open/src/bin/a.rs
@@ -11,7 +11,18 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, S: usize, T: usize,
     };
+
+    let win = match (S, T) {
+        (0, 0) => N.is_odd(),
+        (0, 1) => N.is_even(),
+        (1, 0) => N.is_even(),
+        (1, 1) => N.is_odd(),
+        _ => unreachable!(),
+    };
+    let ans = if win { "Alice" } else { "Bob" };
+    println!("{}", ans);
 }
 
 fn main() {
