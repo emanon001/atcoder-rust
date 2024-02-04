e18ead0361e51339f6a96336ec709b44255bbfcc
emanon001 <emanon001@gmail.com>
2024-02-05 00:18:43 +0900

    past202212-open/f

--- past202212-open/src/bin/f.rs
+++ past202212-open/src/bin/f.rs
@@ -8,10 +8,85 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<usize> for usize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self as isize - *ng as isize).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: u128,
+        A: u128, B: u128, C: u128, D: u128,
+        X: String
     };
+
+    let x = X.replace('.', "").parse::<u128>().unwrap();
+    let sum = A + 2 * B + 3 * C + 4 * D;
+    let ans = bsearch(-1_i64, 10_i64.pow(16), |c| {
+        let c = c as u128;
+        let sum = (sum + c) * 1000;
+        let m = N + c;
+        sum > x * m
+    })
+    .map(|x| x + 1)
+    .unwrap_or(0);
+    println!("{}", ans);
 }
 
 fn main() {
