da4c56bcef08163e694b7fde3e099ed4e2e6fc6d
emanon001 <emanon001@gmail.com>
2024-02-05 21:20:49 +0900

    past202212-open/i

--- past202212-open/src/bin/i.rs
+++ past202212-open/src/bin/i.rs
@@ -8,10 +8,185 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[derive(Clone)]
+pub struct Graph<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    graph: Vec<Vec<(usize, Cost)>>,
+    vc: usize,
+    inf: Cost,
+}
+#[derive(Clone)]
+pub struct Edge<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    from: usize,
+    to: usize,
+    cost: Cost,
+}
+impl<Cost> From<(usize, usize, Cost)> for Edge<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    fn from(e: (usize, usize, Cost)) -> Edge<Cost> {
+        Edge {
+            from: e.0,
+            to: e.1,
+            cost: e.2,
+        }
+    }
+}
+impl<Cost> From<(usize, usize)> for Edge<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    fn from(e: (usize, usize)) -> Edge<Cost> {
+        Edge {
+            from: e.0,
+            to: e.1,
+            cost: Cost::one(),
+        }
+    }
+}
+impl<Cost> Graph<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    pub fn new_undirected(edges: Vec<impl Into<Edge<Cost>>>, vc: usize, inf: Cost) -> Self {
+        let mut graph = vec![Vec::new(); vc];
+        for e in edges {
+            let e = e.into();
+            graph[e.from].push((e.to, e.cost));
+            graph[e.to].push((e.from, e.cost));
+        }
+        Self { graph, vc, inf }
+    }
+    pub fn new_directed(edges: Vec<impl Into<Edge<Cost>>>, vc: usize, inf: Cost) -> Self {
+        let mut graph = vec![Vec::new(); vc];
+        for e in edges {
+            let e = e.into();
+            graph[e.from].push((e.to, e.cost));
+        }
+        Self { graph, vc, inf }
+    }
+    pub fn new_empty(vc: usize, inf: Cost) -> Self {
+        let graph = vec![Vec::new(); vc];
+        Self { graph, vc, inf }
+    }
+    pub fn add_undirected_edge(&mut self, e: impl Into<Edge<Cost>>) {
+        let e = e.into();
+        self.graph[e.from].push((e.to, e.cost));
+        self.graph[e.to].push((e.from, e.cost));
+    }
+    pub fn add_directed_edge(&mut self, e: impl Into<Edge<Cost>>) {
+        let e = e.into();
+        self.graph[e.from].push((e.to, e.cost));
+    }
+    pub fn rev(&self) -> Graph<Cost> {
+        let mut edges: Vec<Edge<Cost>> = Vec::new();
+        for u in 0..self.vc {
+            for &(v, w) in &self.graph[u] {
+                edges.push((v, u, w).into());
+            }
+        }
+        Self::new_directed(edges, self.vc, self.inf)
+    }
+    pub fn reachable_vertexes(&self, s: usize) -> std::collections::HashSet<usize> {
+        let mut visited = std::collections::HashSet::new();
+        let mut queue = std::collections::VecDeque::new();
+        visited.insert(s);
+        queue.push_back(s);
+        while let Some(u) = queue.pop_front() {
+            for &(v, _) in &self.graph[u] {
+                if visited.contains(&v) {
+                    continue;
+                }
+                visited.insert(v);
+                queue.push_back(v);
+            }
+        }
+        visited
+    }
+    pub fn edges<'a>(&self, u: usize) -> &Vec<(usize, Cost)> {
+        &self.graph[u]
+    }
+    pub fn vertex_count(&self) -> usize {
+        self.vc
+    }
+    fn optionalize(&self, v: Vec<Cost>) -> Vec<Option<Cost>> {
+        v.into_iter()
+            .map(|x| if x == self.inf { None } else { Some(x) })
+            .collect::<Vec<_>>()
+    }
+}
+
+impl<Cost> Graph<Cost>
+where
+    Cost: PartialOrd + Copy + num::traits::NumAssign,
+{
+    pub fn scc(&self) -> Vec<Vec<usize>> {
+        let vc = self.vc;
+        let mut id = 0;
+        let mut ids = vec![0; vc];
+        let mut used = vec![false; vc];
+        for u in 0..vc {
+            if used[u] {
+                continue;
+            }
+            self.scc_dfs1(u, &mut id, &mut ids, &mut used);
+        }
+        let mut u_with_id = ids.into_iter().enumerate().collect::<Vec<_>>();
+        u_with_id.sort_by_key(|(_, id)| -(*id as isize));
+        let rev_graph = self.rev();
+        let mut groups = Vec::new();
+        let mut used = vec![false; vc];
+        for (u, _) in u_with_id {
+            if used[u] {
+                continue;
+            }
+            let mut group = Vec::new();
+            rev_graph.scc_dfs2(u, &mut group, &mut used);
+            groups.push(group);
+        }
+        groups
+    }
+    fn scc_dfs1(&self, u: usize, id: &mut usize, ids: &mut Vec<usize>, used: &mut Vec<bool>) {
+        used[u] = true;
+        for &(v, _) in &self.graph[u] {
+            if used[v] {
+                continue;
+            }
+            self.scc_dfs1(v, id, ids, used);
+        }
+        *id += 1;
+        ids[u] = *id;
+    }
+    fn scc_dfs2(&self, u: usize, group: &mut Vec<usize>, used: &mut Vec<bool>) {
+        group.push(u);
+        used[u] = true;
+        for &(v, _) in &self.graph[u] {
+            if used[v] {
+                continue;
+            }
+            self.scc_dfs2(v, group, used);
+        }
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        AB: [(Usize1, Usize1); M]
     };
+
+    let g = Graph::new_directed(AB, N, 1_i64 << 60);
+    let scc = g.scc();
+    let has_loop = scc.into_iter().any(|group| group.len() > 1);
+    let ans = if has_loop { "No" } else { "Yes" };
+    println!("{}", ans);
 }
 
 fn main() {
