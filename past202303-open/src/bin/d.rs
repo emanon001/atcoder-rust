caf8ba9d899c84f5a7068ef5e68bd7f6185cac11
emanon001 <emanon001@gmail.com>
2024-02-06 22:23:36 +0900

    past202303-open/d

--- past202303-open/src/bin/d.rs
+++ past202303-open/src/bin/d.rs
@@ -8,10 +8,42 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        H: i64, A: i64, B: i64, C: i64, D: i64
     };
+
+    let mut ans = std::i64::MAX;
+    for c in 0..40 {
+        let mut h = H;
+        let mut money = 0;
+        for _ in 0..c {
+            money += D;
+            h -= C;
+            if h > 0 {
+                h -= h / 2;
+            }
+        }
+        if h > 0 {
+            money += (h + A - 1) / A * B;
+        }
+        chmin!(ans, money);
+    }
+    println!("{}", ans);
 }
 
 fn main() {
