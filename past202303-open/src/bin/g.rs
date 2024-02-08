192d846e17e2f7c7a023310ba54ed3309a0bc0ad
emanon001 <emanon001@gmail.com>
2024-02-08 22:34:00 +0900

    past202303-open/g

--- past202303-open/src/bin/g.rs
+++ past202303-open/src/bin/g.rs
@@ -11,7 +11,31 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        H: usize, W: usize,
+        A: [[usize; W]; H]
     };
+
+    let mut ans = BTreeSet::new();
+    for i in 0..H {
+        for j in 0..W {
+            for (di, dj) in [(0, 1), (1, 0), (0, -1), (-1, 0)].iter() {
+                let ni = i as isize + di;
+                let nj = j as isize + dj;
+                if ni < 0 || ni >= H as isize || nj < 0 || nj >= W as isize {
+                    continue;
+                }
+                let ni = ni as usize;
+                let nj = nj as usize;
+                let a = A[i][j];
+                let b = A[ni][nj];
+                ans.insert((a.min(b), a.max(b)));
+            }
+        }
+    }
+    println!(
+        "{}",
+        ans.iter().map(|(a, b)| format!("{} {}", a, b)).join("\n")
+    );
 }
 
 fn main() {
