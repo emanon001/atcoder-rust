70eb7dc18d2c3e377a137b5aaad41b142e9566a8
emanon001 <emanon001@gmail.com>
2024-02-09 23:54:07 +0900

    abc338/c

--- abc338/src/bin/c.rs
+++ abc338/src/bin/c.rs
@@ -8,10 +8,108 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<isize> for isize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self - *ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        Q: [i64; N],
+        A: [i64; N],
+        B: [i64; N],
     };
+
+    let mut ans = 0;
+    'outer: for c1 in 0..=10.pow(6) {
+        let mut rest_q = vec![0; N];
+        for i in 0..N {
+            let rest_c = Q[i] - A[i] * c1;
+            if rest_c < 0 {
+                continue 'outer;
+            }
+            rest_q[i] = rest_c;
+        }
+        let c2 = bsearch(-1, 10.pow(6) + 10, |c2| {
+            (0..N).all(|i| {
+                let rest_c = rest_q[i] - B[i] * c2;
+                rest_c >= 0
+            })
+        })
+        .unwrap_or(0);
+        chmax!(ans, c1 + c2);
+    }
+    println!("{}", ans);
 }
 
 fn main() {
