54cad4109032037f6a4a0e33d82c217b8e8f52cb
emanon001 <emanon001@gmail.com>
2024-02-11 22:51:40 +0900

    abc340/c

--- abc340/src/bin/c.rs
+++ abc340/src/bin/c.rs
@@ -11,7 +11,24 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: u64,
     };
+
+    let mut dp = HashMap::new();
+    let ans = sum(N, &mut dp);
+    println!("{}", ans);
+}
+
+fn sum(n: u64, dp: &mut HashMap<u64, u64>) -> u64 {
+    if n < 2 {
+        return 0;
+    }
+    if let Some(x) = dp.get(&n) {
+        return *x;
+    }
+    let ans = sum(n / 2, dp) + sum((n + 1) / 2, dp) + n;
+    dp.insert(n, ans);
+    ans
 }
 
 fn main() {
