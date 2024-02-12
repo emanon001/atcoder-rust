e715003624d9042cb9b9ceca22c9b51596f17bb3
emanon001 <emanon001@gmail.com>
2024-02-13 00:41:22 +0900

    past15-open/d

--- past15-open/src/bin/d.rs
+++ past15-open/src/bin/d.rs
@@ -11,7 +11,26 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: i64, B: i64, C: i64, D: i64, R: i64
     };
+
+    let check = || {
+        let mut times = HashSet::new();
+        let mut t = 0;
+        while t <= 3000 {
+            let (p, q) = if t < B { (A, A + R) } else { (C, C + R) };
+            if t < q {
+                for t2 in p.max(t)..q {
+                    times.insert(t2);
+                }
+            }
+            t += D;
+        }
+        (C..C + R).all(|t| times.contains(&t))
+    };
+
+    let ans = if check() { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
