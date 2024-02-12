57d5ab80edd2e57b029bc669e249e0e0cc060c85
emanon001 <emanon001@gmail.com>
2024-02-13 01:08:38 +0900

    past15-open/f

--- past15-open/src/bin/f.rs
+++ past15-open/src/bin/f.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [usize; N]
     };
+
+    let mut position_map = BTreeMap::new();
+    for (i, &a) in A.iter().enumerate() {
+        position_map.entry(a).or_insert(vec![]).push(i);
+    }
+    let mut ans = vec![0; N];
+    let mut x = 1;
+    for (_, positions) in position_map {
+        for i in positions {
+            ans[i] = x;
+            x += 1;
+        }
+    }
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
