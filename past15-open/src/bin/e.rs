0f4126c8ae61e4f22b1c50b2d688b10124c9a350
emanon001 <emanon001@gmail.com>
2024-02-15 23:50:28 +0900

    past15-open/e

--- past15-open/src/bin/e.rs
+++ past15-open/src/bin/e.rs
@@ -11,7 +11,16 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        A: [u64; N]
     };
+
+    let ans: u64 = A
+        .into_iter()
+        .combinations(K)
+        .map(|v| v.into_iter().sum::<u64>())
+        .sum();
+    println!("{}", ans);
 }
 
 fn main() {
