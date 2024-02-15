2c6cd9d8fdb98c81b72db17d703f0eb61cd6513a
emanon001 <emanon001@gmail.com>
2024-02-15 23:49:44 +0900

    past16-open/e

--- past16-open/src/bin/e.rs
+++ past16-open/src/bin/e.rs
@@ -11,7 +11,14 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        X: String
     };
+
+    let counts = X.chars().counts();
+    let power_of_10 =
+        *counts.get(&'1').unwrap_or(&0) == 1 && *counts.get(&'0').unwrap_or(&0) == X.len() - 1;
+    let ans = if power_of_10 { X.len() - 1 } else { X.len() };
+    println!("{}", ans);
 }
 
 fn main() {
