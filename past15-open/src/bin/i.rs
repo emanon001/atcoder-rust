90848f02ce6d37329379994b38c596092dbf21e9
emanon001 <emanon001@gmail.com>
2024-02-17 06:48:12 +0900

    past15-open/i

--- past15-open/src/bin/i.rs
+++ past15-open/src/bin/i.rs
@@ -8,10 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: usize, counts: &mut [usize]) {
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            counts[x] += 1;
+            let y = n / x;
+            if y != x {
+                counts[y] += 1;
+            }
+        }
+        x += 1;
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        A: [usize; N]
     };
+
+    let mut counts = vec![0; 10.pow(6) + 1];
+    for a in A {
+        divisors(a, &mut counts);
+    }
+    for (i, &c) in counts.iter().enumerate().rev() {
+        if c >= K {
+            println!("{}", i);
+            return;
+        }
+    }
 }
 
 fn main() {
