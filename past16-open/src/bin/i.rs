3ecfc981df5ae9df87bcbc8d2a1cac6c1764d5ca
emanon001 <emanon001@gmail.com>
2024-02-18 17:35:06 +0900

    past16-open/i

--- past16-open/src/bin/i.rs
+++ past16-open/src/bin/i.rs
@@ -8,10 +8,84 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i128> for i128 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: i128, K: i128,
+        A: [i128; N]
     };
+
+    let base = bsearch(-1, 10.pow(20), |x| {
+        let mut count = 0;
+        for &a in &A {
+            if a >= x {
+                continue;
+            }
+            let rest = x - a;
+            count += (rest + K - 1) / K;
+        }
+        count <= M
+    })
+    .unwrap();
+
+    let mut ans = vec![];
+    let mut rest_count = M;
+    for a in A {
+        let c = if a >= base {
+            0
+        } else {
+            ((base - a + K - 1) / K).min(rest_count)
+        };
+        ans.push(a + K * c);
+        rest_count -= c;
+    }
+    for i in 0..N {
+        if rest_count > 0 && ans[i] == base {
+            ans[i] += K;
+            rest_count -= 1;
+        }
+    }
+
+    assert!(rest_count == 0);
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
