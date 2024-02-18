4959e696925725160565cd1c48e00ed80a96b260
emanon001 <emanon001@gmail.com>
2024-02-18 16:19:49 +0900

    past16-open/h

--- past16-open/src/bin/h.rs
+++ past16-open/src/bin/h.rs
@@ -8,10 +8,49 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        A: [u64; N]
     };
+
+    let mut dp = vec![vec![vec![0; 2]; M + 1]; N + 1];
+    for i in 0..N {
+        for j in 0..=(i + 1) / 2 {
+            if j > M {
+                continue;
+            }
+            for k in 0..2 {
+                chmax!(dp[i + 1][j][k], dp[i][j][k]);
+                if k == 0 {
+                    // 前日に宿題を行っていない
+                    if j < M {
+                        chmax!(dp[i + 1][j + 1][1], dp[i][j][k]);
+                    }
+                    chmax!(dp[i + 1][j][0], dp[i][j][k] + A[i]);
+                } else {
+                    // 前日に宿題を行った
+                    chmax!(dp[i + 1][j][0], dp[i][j][k] + A[i]);
+                }
+            }
+        }
+    }
+    let ans = dp[N][M][0].max(dp[N][M][1]);
+    println!("{}", ans);
 }
 
 fn main() {
