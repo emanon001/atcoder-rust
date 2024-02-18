5cca098ae37a3bfcf42d94803878a5e39350d7d6
emanon001 <emanon001@gmail.com>
2024-02-19 04:48:40 +0900

    past16-open/j

--- past16-open/src/bin/j.rs
+++ past16-open/src/bin/j.rs
@@ -8,10 +8,45 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: u64) -> Vec<u64> {
+    let mut res = Vec::new();
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            res.push(x);
+            let y = n / x;
+            if y != x {
+                res.push(y);
+            }
+        }
+        x += 1;
+    }
+    res
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        A: [u64; K]
     };
+
+    let min_distance = A
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| *b - *a)
+        .reduce(|a, b| a.gcd(&b))
+        .unwrap();
+    let l = *A.iter().min().unwrap();
+    let r = *A.iter().max().unwrap();
+    let mut ans = BTreeSet::new();
+    for d in divisors(min_distance) {
+        if (r - l) / d < N as u64 {
+            ans.insert(d);
+        }
+    }
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
