7d5d92833632e130652f4a6f26f1a4ceeb933675
emanon001 <emanon001@gmail.com>
2024-02-22 03:48:07 +0900

    abc341/e

--- abc341/src/bin/e.rs
+++ abc341/src/bin/e.rs
@@ -8,10 +8,137 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait Monoid {
+    fn mempty() -> Self;
+    fn mappend(&self, other: &Self) -> Self;
+}
+
+impl Monoid for bool {
+    fn mempty() -> Self {
+        true
+    }
+    fn mappend(&self, other: &Self) -> Self {
+        *self && *other
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    size: usize,
+    data: Vec<T>,
+}
+impl<T> SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    pub fn new(size: usize) -> Self {
+        let size = Self::normalize_data_size(size);
+        let data = vec![T::mempty(); size * 2 - 1];
+        Self { size, data }
+    }
+    /// 0-origin
+    pub fn update(&mut self, i: usize, v: T) {
+        let mut i = i + self.size - 1;
+        self.data[i] = v;
+        while i > 0 {
+            i = (i - 1) / 2;
+            self.data[i] = self.data[i * 2 + 1].mappend(&self.data[i * 2 + 2]);
+        }
+    }
+    /// [a, b)
+    /// 0-origin
+    pub fn query(&self, a: usize, b: usize) -> T {
+        self.execute_query(a, b, 0, 0, self.size)
+    }
+    fn normalize_data_size(size: usize) -> usize {
+        let mut n = 1;
+        while n < size {
+            n *= 2;
+        }
+        n
+    }
+    fn execute_query(&self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
+        if r <= a || b <= l {
+            return T::mempty();
+        }
+        if a <= l && r <= b {
+            return self.data[i].clone();
+        }
+        let vl = self.execute_query(a, b, i * 2 + 1, l, (l + r) / 2);
+        let vr = self.execute_query(a, b, i * 2 + 2, (l + r) / 2, r);
+        vl.mappend(&vr)
+    }
+}
+impl<T> From<&[T]> for SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    fn from(values: &[T]) -> Self {
+        let mut st = SegmentTree::new(values.len());
+        for (i, v) in values.into_iter().enumerate() {
+            st.data[i + st.size - 1] = v.clone();
+        }
+        if st.size < 2 {
+            return st;
+        }
+        for i in (0..=(st.size - 2)).rev() {
+            st.data[i] = st.data[i * 2 + 1].mappend(&st.data[i * 2 + 2]);
+        }
+        st
+    }
+}
+impl<T> From<Vec<T>> for SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    fn from(values: Vec<T>) -> Self {
+        let values: &[T] = &values;
+        Self::from(values)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, Q: usize,
+        S: Chars,
+        queries: [(usize, Usize1, Usize1); Q],
     };
+
+    let v = S
+        .into_iter()
+        .tuple_windows()
+        .map(|(a, b)| a != b)
+        .collect_vec();
+    let mut st = SegmentTree::from(v);
+    for (t, l, r) in queries {
+        match t {
+            1 => {
+                if l > 0 {
+                    st.update(l - 1, !st.query(l - 1, l));
+                }
+                if r < N - 1 {
+                    st.update(r, !st.query(r, r + 1));
+                }
+            }
+            2 => {
+                let ans = if N == 1 {
+                    "Yes"
+                } else {
+                    if st.query(l, r) {
+                        "Yes"
+                    } else {
+                        "No"
+                    }
+                };
+                println!("{}", ans);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
