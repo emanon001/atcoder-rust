3fd867f8f1c71a8d96e3c85ce38e9c96ca649d0d
emanon001 <emanon001@gmail.com>
2024-02-22 00:19:00 +0900

    abc341/d

--- abc341/src/bin/d.rs
+++ abc341/src/bin/d.rs
@@ -8,10 +8,59 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i128> for i128 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: i128, M: i128, K: i128,
     };
+
+    let ans = bsearch(100_000_000_000_000_000_000_128, N.min(M) - 1, |x| {
+        let n_count = x / N;
+        let m_count = x / M;
+        let nm_count = x / N.lcm(&M);
+        let count = n_count + m_count - nm_count * 2;
+        count >= K
+    })
+    .unwrap();
+    println!("{}", ans);
 }
 
 fn main() {
