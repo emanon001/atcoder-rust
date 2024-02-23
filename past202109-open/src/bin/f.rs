0492392142ee85eec6d31724d0f402fe09150fa8
emanon001 <emanon001@gmail.com>
2024-02-24 00:12:45 +0900

    past202109-open/f

--- past202109-open/src/bin/f.rs
+++ past202109-open/src/bin/f.rs
@@ -11,7 +11,35 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        _: usize,
+        S: Chars,
     };
+
+    let zero_count = S.iter().filter(|&c| c == &'0').count();
+    let ans = if zero_count == 1 {
+        "-1".to_string()
+    } else {
+        let mut zero_values = vec![];
+        for (i, ch) in S.iter().enumerate() {
+            if ch == &'0' {
+                zero_values.push(i + 1);
+            }
+        }
+        let mut zero_i = 0;
+        S.iter()
+            .enumerate()
+            .map(|(i, ch)| {
+                if ch == &'0' {
+                    let res = zero_values[(zero_i + 1) % zero_values.len()].to_string();
+                    zero_i += 1;
+                    res
+                } else {
+                    (i + 1).to_string()
+                }
+            })
+            .join(" ")
+    };
+    println!("{}", ans);
 }
 
 fn main() {
