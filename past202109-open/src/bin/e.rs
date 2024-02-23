fe78acee43d3d8fd21c0192610f881072eb758a2
emanon001 <emanon001@gmail.com>
2024-02-23 23:55:17 +0900

    past202109-open/e

--- past202109-open/src/bin/e.rs
+++ past202109-open/src/bin/e.rs
@@ -11,7 +11,24 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        C: [usize; N],
+        P: [i64; N],
     };
+
+    let mut map = HashMap::new();
+    for (c, p) in C.into_iter().zip(P.into_iter()) {
+        let v = map.entry(c).or_insert(i64::max_value());
+        if p < *v {
+            *v = p;
+        }
+    }
+    let ans = if map.len() < K {
+        -1
+    } else {
+        map.values().sorted().take(K).sum::<i64>()
+    };
+    println!("{}", ans);
 }
 
 fn main() {
