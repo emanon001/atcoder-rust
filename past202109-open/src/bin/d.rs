9d9bcf03e5b3239104946b1bf62d5f0de08f63a4
emanon001 <emanon001@gmail.com>
2024-02-23 23:49:01 +0900

    past202109-open/d

--- past202109-open/src/bin/d.rs
+++ past202109-open/src/bin/d.rs
@@ -8,10 +8,36 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: u64) -> Vec<u64> {
+    let mut res = Vec::new();
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            res.push(x);
+            let y = n / x;
+            if y != x {
+                res.push(y);
+            }
+        }
+        x += 1;
+    }
+    res
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        X: u64, Y: u64
     };
+
+    let xc = divisors(X).len();
+    let yc = divisors(Y).len();
+    let ans = match xc.cmp(&yc) {
+        std::cmp::Ordering::Greater => "X",
+        std::cmp::Ordering::Less => "Y",
+        std::cmp::Ordering::Equal => "Z",
+    };
+    println!("{}", ans);
 }
 
 fn main() {
