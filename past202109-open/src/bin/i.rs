e1c066bf5f93d084d29778ac71c7539653c8952a
emanon001 <emanon001@gmail.com>
2024-02-25 20:26:03 +0900

    past202109-open/i

--- past202109-open/src/bin/i.rs
+++ past202109-open/src/bin/i.rs
@@ -11,7 +11,28 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [u64; N],
     };
+
+    let mut two_count = 0;
+    let mut av = BTreeSet::new();
+    for (i, a) in A.into_iter().enumerate() {
+        let mut a = a;
+        while a % 2 == 0 {
+            two_count += 1;
+            a /= 2;
+        }
+        av.insert((a, i));
+    }
+    for _ in 0..two_count {
+        let &item = av.iter().next().unwrap();
+        av.remove(&item);
+        let (a, i) = item;
+        av.insert((a * 3, i));
+    }
+    let ans = av.iter().next().unwrap().0;
+    println!("{}", ans);
 }
 
 fn main() {
