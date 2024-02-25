5b1e9fef9c5a5f8c4f318f82ce6d8d7101587ade
emanon001 <emanon001@gmail.com>
2024-02-25 20:04:43 +0900

    past202109-open/h

--- past202109-open/src/bin/h.rs
+++ past202109-open/src/bin/h.rs
@@ -11,7 +11,40 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, X: usize,
+        edges: [(Usize1, Usize1, usize); N - 1],
     };
+
+    let mut graph = vec![vec![]; N];
+    for (u, v, c) in edges {
+        graph[u].push((v, c));
+        graph[v].push((u, c));
+    }
+    for u in 0..N {
+        if is_reachable(u, 1 << 30, 0, X, &graph) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
+}
+
+fn is_reachable(u: usize, p: usize, d: usize, x: usize, graph: &Vec<Vec<(usize, usize)>>) -> bool {
+    if d == x {
+        return true;
+    }
+    if d > x {
+        return false;
+    }
+    for &(v, c) in &graph[u] {
+        if v == p {
+            continue;
+        }
+        if is_reachable(v, u, d + c, x, graph) {
+            return true;
+        }
+    }
+    false
 }
 
 fn main() {
