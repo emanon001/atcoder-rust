ee6cfa0da9c74af76ea100916589b349e20283c8
emanon001 <emanon001@gmail.com>
2024-02-26 17:53:20 +0900

    past202107-open/f

--- past202107-open/src/bin/f.rs
+++ past202107-open/src/bin/f.rs
@@ -11,7 +11,24 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        DST: [(usize, usize, usize); N],
     };
+
+    let mut imos = vec![0_isize; 10.pow(5) * 30];
+    for (d, s, t) in DST {
+        imos[d * 24 + s] += 1;
+        imos[d * 24 + t] -= 1;
+    }
+    for i in 0..imos.len() - 1 {
+        imos[i + 1] += imos[i];
+    }
+    let ans = if *imos.iter().max().unwrap() > 1 {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
 
 fn main() {
