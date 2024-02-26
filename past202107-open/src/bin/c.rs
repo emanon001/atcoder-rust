3ecabb0c860ae038c33db6d83248f14d331a066c
emanon001 <emanon001@gmail.com>
2024-02-26 17:33:34 +0900

    past202107-open/c

--- past202107-open/src/bin/c.rs
+++ past202107-open/src/bin/c.rs
@@ -11,7 +11,21 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: String,
+        L: u64, R: u64,
     };
+
+    if S.starts_with('0') && S.len() > 1 {
+        println!("No");
+        return;
+    }
+    if S.len() > 9 {
+        println!("No");
+        return;
+    }
+    let s = S.parse::<u64>().unwrap();
+    let ans = if s >= L && s <= R { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
