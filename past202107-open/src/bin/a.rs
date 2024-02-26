0e2d3b56971b665c42f91053bd0b9e2a30bbcf49
emanon001 <emanon001@gmail.com>
2024-02-26 17:25:23 +0900

    past202107-open/a

--- past202107-open/src/bin/a.rs
+++ past202107-open/src/bin/a.rs
@@ -11,7 +11,31 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars
     };
+
+    let s = S
+        .into_iter()
+        .map(|ch| ch.to_digit(10).unwrap())
+        .collect_vec();
+    let check_digit = s[s.len() - 1];
+    let s = &s[..s.len() - 1];
+    let a = s
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(_, &x)| x)
+        .sum::<u32>()
+        * 3;
+    let b = s
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 1)
+        .map(|(_, &x)| x)
+        .sum::<u32>();
+    let c = (a + b) % 10;
+    let ans = if c == check_digit { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
