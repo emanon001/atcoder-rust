36e38ce7d827b96f56161617d73fbd4988f16de2
emanon001 <emanon001@gmail.com>
2024-02-28 19:36:15 +0900

    past202012-open/c

--- past202012-open/src/bin/c.rs
+++ past202012-open/src/bin/c.rs
@@ -8,10 +8,34 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn convert_radix(radix10: u64, base: u64) -> Vec<u64> {
+    let mut x = radix10;
+    let mut ans = Vec::new();
+    loop {
+        ans.push(x % base);
+        x /= base;
+        if x == 0 {
+            break;
+        }
+    }
+    ans.reverse();
+    ans
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: u64,
     };
+
+    let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .collect::<Vec<_>>();
+    let ans = convert_radix(N, 36)
+        .into_iter()
+        .map(|x| chars[x as usize])
+        .join("");
+    println!("{}", ans);
 }
 
 fn main() {
