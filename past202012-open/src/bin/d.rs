88ed94d0264faee5fefdf214f892743aa623e77c
emanon001 <emanon001@gmail.com>
2024-02-28 19:46:21 +0900

    past202012-open/d

--- past202012-open/src/bin/d.rs
+++ past202012-open/src/bin/d.rs
@@ -11,7 +11,22 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        S: [String; N],
     };
+
+    let max_len = S.iter().map(|s| s.len()).max().unwrap();
+    let ans = S
+        .into_iter()
+        .sorted_by_key(|s| {
+            let c = s.chars().take_while(|&ch| ch == '0').count();
+            (
+                format!("{:0>width$}", s, width = max_len),
+                std::cmp::Reverse(c),
+            )
+        })
+        .join("\n");
+    println!("{}", ans);
 }
 
 fn main() {
