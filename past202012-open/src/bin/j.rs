c2b8ff9a8b5a9396eef161899e557b751e79351a
emanon001 <emanon001@gmail.com>
2024-02-29 02:44:03 +0900

    past202012-open/j

--- past202012-open/src/bin/j.rs
+++ past202012-open/src/bin/j.rs
@@ -8,10 +8,55 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+enum Event {
+    Alphabet { ch: char, count: u64 },
+    Numeric { base: u64 },
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
+        X: u64,
     };
+
+    let mut count = 0_u64;
+    let mut stack = vec![];
+    for ch in S {
+        if count >= X {
+            break;
+        }
+        if ch.is_ascii_alphabetic() {
+            count += 1;
+            stack.push(Event::Alphabet { ch, count });
+        } else {
+            let n = ch.to_digit(10).unwrap() as u64;
+            let base = count;
+            count *= n + 1;
+            stack.push(Event::Numeric { base });
+        }
+    }
+
+    let mut rest = X;
+    while let Some(event) = stack.pop() {
+        match event {
+            Event::Alphabet { ch, count } => {
+                if count == rest {
+                    println!("{}", ch);
+                    return;
+                }
+            }
+            Event::Numeric { base } => {
+                if base > rest {
+                    continue;
+                }
+                rest %= base;
+                if rest == 0 {
+                    rest = base;
+                }
+            }
+        }
+    }
 }
 
 fn main() {
