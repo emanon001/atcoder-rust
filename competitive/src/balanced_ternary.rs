edbd8be222422d1ef781d274ffb88457deb23461
emanon001 <emanon001@gmail.com>
2024-02-28 19:34:22 +0900

    [lib] feat: `balanced_ternary()`の戻り値の順番を変更

--- competitive/src/balanced_ternary.rs
+++ competitive/src/balanced_ternary.rs
@@ -1,26 +1,28 @@
+use std::collections::VecDeque;
+
 use cargo_snippet::snippet;
 
 #[snippet("balanced_ternary")]
 pub fn balanced_ternary(n: i64) -> Vec<i64> {
-    let mut res = vec![];
+    let mut res = VecDeque::new();
     let mut n = n;
     while n != 0 {
         let m = (n % 3 + 3) % 3;
         match m % 3 {
-            0 => res.push(0),
+            0 => res.push_front(0),
             1 => {
-                res.push(1);
+                res.push_front(1);
                 n -= 1;
             }
             2 => {
-                res.push(-1);
+                res.push_front(-1);
                 n += 1;
             }
             _ => unreachable!(),
         };
         n /= 3;
     }
-    res
+    res.into_iter().collect()
 }
 
 #[cfg(test)]
@@ -32,21 +34,21 @@ mod test {
         assert_eq!(balanced_ternary(0), vec![]);
         assert_eq!(balanced_ternary(1), vec![1]);
         assert_eq!(balanced_ternary(-1), vec![-1]);
-        assert_eq!(balanced_ternary(2), vec![-1, 1]);
-        assert_eq!(balanced_ternary(-2), vec![1, -1]);
-        assert_eq!(balanced_ternary(3), vec![0, 1]);
-        assert_eq!(balanced_ternary(-3), vec![0, -1]);
+        assert_eq!(balanced_ternary(2), vec![1, -1]);
+        assert_eq!(balanced_ternary(-2), vec![-1, 1]);
+        assert_eq!(balanced_ternary(3), vec![1, 0]);
+        assert_eq!(balanced_ternary(-3), vec![-1, 0]);
         assert_eq!(balanced_ternary(4), vec![1, 1]);
         assert_eq!(balanced_ternary(-4), vec![-1, -1]);
-        assert_eq!(balanced_ternary(5), vec![-1, -1, 1]);
-        assert_eq!(balanced_ternary(-5), vec![1, 1, -1]);
-        assert_eq!(balanced_ternary(6), vec![0, -1, 1]);
-        assert_eq!(balanced_ternary(-6), vec![0, 1, -1]);
+        assert_eq!(balanced_ternary(5), vec![1, -1, -1]);
+        assert_eq!(balanced_ternary(-5), vec![-1, 1, 1]);
+        assert_eq!(balanced_ternary(6), vec![1, -1, 0]);
+        assert_eq!(balanced_ternary(-6), vec![-1, 1, 0]);
         assert_eq!(balanced_ternary(7), vec![1, -1, 1]);
         assert_eq!(balanced_ternary(-7), vec![-1, 1, -1]);
-        assert_eq!(balanced_ternary(8), vec![-1, 0, 1]);
-        assert_eq!(balanced_ternary(-8), vec![1, 0, -1]);
-        assert_eq!(balanced_ternary(9), vec![0, 0, 1]);
-        assert_eq!(balanced_ternary(-9), vec![0, 0, -1]);
+        assert_eq!(balanced_ternary(8), vec![1, 0, -1]);
+        assert_eq!(balanced_ternary(-8), vec![-1, 0, 1]);
+        assert_eq!(balanced_ternary(9), vec![1, 0, 0]);
+        assert_eq!(balanced_ternary(-9), vec![-1, 0, 0]);
     }
 }
