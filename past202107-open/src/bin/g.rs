7cb86f365997cc440dc9ec90ce7ac86431d2f6ce
emanon001 <emanon001@gmail.com>
2024-02-28 15:47:37 +0900

    past202107-open/g

--- past202107-open/src/bin/g.rs
+++ past202107-open/src/bin/g.rs
@@ -11,7 +11,40 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: i64,
     };
+
+    let mut ans = vec![];
+    for (i, v) in balanced_ternary(N).into_iter().enumerate() {
+        match v {
+            1 => ans.push(3_i64.pow(i as u32)),
+            -1 => ans.push(-(3_i64.pow(i as u32))),
+            _ => { /* do nothing */ }
+        }
+    }
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
+}
+
+fn balanced_ternary(n: i64) -> Vec<i64> {
+    let mut res = vec![];
+    let mut n = n;
+    while n > 0 {
+        match n % 3 {
+            0 => res.push(0),
+            1 => {
+                res.push(1);
+                n -= 1;
+            }
+            2 => {
+                res.push(-1);
+                n += 1;
+            }
+            _ => unreachable!(),
+        };
+        n /= 3;
+    }
+    res
 }
 
 fn main() {
