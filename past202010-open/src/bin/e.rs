61a65288b82f206798698e901bd1dcb72dd1fd34
emanon001 <emanon001@gmail.com>
2024-03-02 23:23:15 +0900

    past202010-open/e

--- past202010-open/src/bin/e.rs
+++ past202010-open/src/bin/e.rs
@@ -11,7 +11,18 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        S: Chars,
     };
+
+    for orders in (0..N).permutations(N) {
+        let t = orders.iter().map(|&i| S[i]).collect_vec();
+        if t != S && t.iter().copied().rev().collect_vec() != S {
+            println!("{}", t.iter().join(""));
+            return;
+        }
+    }
+    println!("None");
 }
 
 fn main() {
