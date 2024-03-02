bb5329d77093f03522afae23c58d70467619a52a
emanon001 <emanon001@gmail.com>
2024-03-02 23:36:09 +0900

    past202010-open/f

--- past202010-open/src/bin/f.rs
+++ past202010-open/src/bin/f.rs
@@ -11,7 +11,28 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        S: [String; N],
     };
+
+    let counts = S.into_iter().counts();
+    let mut count_to_words = BTreeMap::new();
+    for (s, c) in counts {
+        count_to_words.entry(c).or_insert(vec![]).push(s);
+    }
+    let mut count = 0;
+    for (_, words) in count_to_words.into_iter().rev() {
+        let c = words.len();
+        count += c;
+        if c > 1 && count >= K {
+            println!("AMBIGUOUS");
+            return;
+        }
+        if count >= K {
+            println!("{}", words[0]);
+            return;
+        }
+    }
 }
 
 fn main() {
