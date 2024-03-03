31ee97cd14fb1cbfd0b2e8a6fb07521c3a34c089
emanon001 <emanon001@gmail.com>
2024-03-03 23:31:41 +0900

    past202010-open/h

--- past202010-open/src/bin/h.rs
+++ past202010-open/src/bin/h.rs
@@ -11,7 +11,34 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize, K: usize,
+        S: [Chars; N],
     };
+
+    for n in (1..=N).rev() {
+        for i in 0..N {
+            if i + n > N {
+                break;
+            }
+            for j in 0..M {
+                if j + n > M {
+                    break;
+                }
+                let mut counts = vec![0; 10];
+                for di in 0..n {
+                    for dj in 0..n {
+                        let v = S[i + di][j + dj].to_digit(10).unwrap() as usize;
+                        counts[v] += 1;
+                    }
+                }
+                let max_count = counts.iter().max().unwrap();
+                if max_count + K >= n * n {
+                    println!("{}", n);
+                    return;
+                }
+            }
+        }
+    }
 }
 
 fn main() {
