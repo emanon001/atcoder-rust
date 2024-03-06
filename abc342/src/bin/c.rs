445bc6bd7a8322ecc93927a4d68f46d18487ef04
emanon001 <emanon001@gmail.com>
2024-03-06 23:31:54 +0900

    abc342/c

--- abc342/src/bin/c.rs
+++ abc342/src/bin/c.rs
@@ -8,10 +8,36 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn make_ascii_lowercase_chars() -> Vec<char> {
+    let base = 0x61;
+    make_chars(base..base + 26)
+}
+fn make_chars(range: std::ops::Range<u8>) -> Vec<char> {
+    range.into_iter().map(char::from).collect::<Vec<_>>()
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        _: usize,
+        S: Chars,
+        Q: usize,
+        CD: [(char, char); Q],
     };
+
+    let mut char_map = make_ascii_lowercase_chars()
+        .into_iter()
+        .map(|ch| (ch, ch))
+        .collect::<HashMap<_, _>>();
+    for (c, d) in CD {
+        for ch in make_ascii_lowercase_chars() {
+            if char_map[&ch] == c {
+                char_map.insert(ch, d);
+            }
+        }
+    }
+    let ans = S.into_iter().map(|ch| char_map[&ch]).collect::<String>();
+    println!("{}", ans);
 }
 
 fn main() {
