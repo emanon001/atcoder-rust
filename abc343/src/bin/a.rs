9d72b576a1f02a7b9c254e92fceb068eb554f2e5
emanon001 <emanon001@gmail.com>
2024-03-08 14:33:55 +0900

    abc343/a

--- abc343/src/bin/a.rs
+++ abc343/src/bin/a.rs
@@ -11,7 +11,15 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: usize, B: usize,
     };
+
+    for x in 0..=9 {
+        if x != A + B {
+            println!("{}", x);
+            return;
+        }
+    }
 }
 
 fn main() {
