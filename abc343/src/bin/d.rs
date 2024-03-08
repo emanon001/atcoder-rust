3027bcc1fa9a991f3786aa323009d3350c33076e
emanon001 <emanon001@gmail.com>
2024-03-08 14:52:32 +0900

    abc343/d

--- abc343/src/bin/d.rs
+++ abc343/src/bin/d.rs
@@ -11,7 +11,29 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, T: usize,
+        AB: [(Usize1, u64); T],
     };
+
+    let mut id_to_point = vec![0_u64; N];
+    let mut point_to_ids = HashMap::new();
+    let initial_ids = (0..N).collect::<HashSet<_>>();
+    point_to_ids.insert(0_u64, initial_ids);
+
+    for (a, b) in AB {
+        let point = id_to_point[a];
+        point_to_ids.get_mut(&point).unwrap().remove(&a);
+        if point_to_ids[&point].is_empty() {
+            point_to_ids.remove(&point);
+        }
+        let new_point = point + b;
+        id_to_point[a] = new_point;
+        point_to_ids
+            .entry(new_point)
+            .or_insert(HashSet::new())
+            .insert(a);
+        println!("{}", point_to_ids.len());
+    }
 }
 
 fn main() {
