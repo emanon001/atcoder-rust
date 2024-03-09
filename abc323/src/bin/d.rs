6092ec6c9bbb8a35e3beac43ad27cb80388fcf7d
emanon001 <emanon001@gmail.com>
2024-03-09 21:34:12 +0900

    abc323/d

--- abc323/src/bin/d.rs
+++ abc323/src/bin/d.rs
@@ -8,9 +8,31 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        SC: [(u64, u64); N],
     };
+
+    let mut ans = 0;
+    let mut map = SC.into_iter().collect::<BTreeMap<_, _>>();
+    loop {
+        if map.is_empty() {
+            break;
+        }
+        let (&s, &c) = map.iter().next().unwrap();
+        map.remove(&s);
+        if c % 2 == 1 {
+            ans += 1;
+        }
+        let new_s = s * 2;
+        let new_c = c / 2;
+        if new_c > 0 {
+            *map.entry(new_s).or_insert(0) += new_c;
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
