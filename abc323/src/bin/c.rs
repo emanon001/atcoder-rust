595f7331b5aa1b9bbcfe2abd98c3a15222c882cb
emanon001 <emanon001@gmail.com>
2024-03-09 21:15:19 +0900

    abc323/c

--- abc323/src/bin/c.rs
+++ abc323/src/bin/c.rs
@@ -8,9 +8,50 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        A: [u64; M],
+        S: [Chars; N],
     };
+
+    let mut points = vec![0_u64; N];
+    for i in 0..N {
+        let mut point = i as u64 + 1;
+        for j in 0..M {
+            if S[i][j] == 'o' {
+                point += A[j];
+            }
+        }
+        points[i] = point;
+    }
+    let sorted_a = A
+        .into_iter()
+        .enumerate()
+        .sorted_by_key(|(_, a)| std::cmp::Reverse(*a))
+        .collect::<Vec<_>>();
+    for (i, s) in S.into_iter().enumerate() {
+        let max_point = *points
+            .iter()
+            .enumerate()
+            .filter(|(j, _)| i != *j)
+            .max_by_key(|(_, p)| *p)
+            .unwrap()
+            .1;
+        let mut cur_point = points[i];
+        let mut ans = 0;
+        for &(j, p) in &sorted_a {
+            if cur_point > max_point {
+                break;
+            }
+            if s[j] == 'x' {
+                cur_point += p;
+                ans += 1;
+            }
+        }
+        println!("{}", ans);
+    }
 }
 
 fn main() {
