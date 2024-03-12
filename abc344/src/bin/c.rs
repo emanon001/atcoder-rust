42e4c4bdd3a466a9b7ba74b6bf5d93cf5c2670c6
emanon001 <emanon001@gmail.com>
2024-03-12 23:49:20 +0900

    abc344/c

--- abc344/src/bin/c.rs
+++ abc344/src/bin/c.rs
@@ -11,7 +11,27 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [i64; N],
+        M: usize,
+        B: [i64; M],
+        L: usize,
+        C: [i64; L],
+        Q: usize,
+        X: [i64; Q],
     };
+
+    let mut set = HashSet::new();
+    for i in 0..N {
+        for j in 0..M {
+            set.insert(A[i] + B[j]);
+        }
+    }
+    for x in X {
+        let is_ok = C.iter().any(|&c| set.contains(&(x - c)));
+        let ans = if is_ok { "Yes" } else { "No" };
+        println!("{}", ans);
+    }
 }
 
 fn main() {
