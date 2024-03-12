73319a8884a9a74a24abf9e7eaf4b23e82a3e812
emanon001 <emanon001@gmail.com>
2024-03-12 23:43:55 +0900

    abc344/b

--- abc344/src/bin/b.rs
+++ abc344/src/bin/b.rs
@@ -10,8 +10,18 @@ use std::collections::*;
 
 #[allow(non_snake_case)]
 fn solve() {
-    input_interactive! {
-    };
+    let mut v = vec![];
+    loop {
+        input_interactive! {
+            A: usize,
+        };
+        v.push(A);
+        if A == 0 {
+            break;
+        }
+    }
+    let ans = v.iter().rev().join("\n");
+    println!("{}", ans);
 }
 
 fn main() {
