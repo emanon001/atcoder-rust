6a646d04d552d95e17a922e79de54d689fc8e11e
emanon001 <emanon001@gmail.com>
2024-03-13 00:28:01 +0900

    abc344/e

--- abc344/src/bin/e.rs
+++ abc344/src/bin/e.rs
@@ -11,7 +11,74 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [i64; N],
+        Q: usize,
     };
+
+    let mut head = A[0];
+    let mut next_map = HashMap::new();
+    let mut prev_map = HashMap::new();
+    for i in 0..N {
+        if i + 1 < N {
+            next_map.insert(A[i], A[i + 1]);
+        }
+        if i > 0 {
+            prev_map.insert(A[i], A[i - 1]);
+        }
+    }
+    for _ in 0..Q {
+        input_interactive! {
+            kind: usize,
+        };
+        match kind {
+            1 => {
+                input_interactive! {
+                    x: i64, y: i64,
+                };
+                let cur_next = next_map.get(&x).copied();
+                next_map.insert(x, y);
+                prev_map.insert(y, x);
+                if let Some(cur_next) = cur_next {
+                    next_map.insert(y, cur_next);
+                    prev_map.insert(cur_next, y);
+                }
+            }
+            2 => {
+                input_interactive! {
+                    x: i64,
+                };
+                let cur_prev = prev_map.get(&x).copied();
+                let cur_next = next_map.get(&x).copied();
+                next_map.remove(&x);
+                prev_map.remove(&x);
+                match (cur_prev, cur_next) {
+                    (Some(cur_prev), Some(cur_next)) => {
+                        next_map.insert(cur_prev, cur_next);
+                        prev_map.insert(cur_next, cur_prev);
+                    }
+                    (Some(cur_prev), None) => {
+                        next_map.remove(&cur_prev);
+                    }
+                    (None, Some(cur_next)) => {
+                        prev_map.remove(&cur_next);
+                        head = cur_next;
+                    }
+                    (None, None) => {
+                        unreachable!()
+                    }
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+    let mut head = head;
+    let mut ans = vec![head];
+    while let Some(v) = next_map.get(&head).copied() {
+        ans.push(v);
+        head = v;
+    }
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
