e2a307a4bcc54a6772ef1e65951efd9621c6ae61
emanon001 <emanon001@gmail.com>
2024-03-13 19:01:37 +0900

    math-and-algorithm/011

--- math-and-algorithm/src/bin/011.rs
+++ math-and-algorithm/src/bin/011.rs
@@ -8,10 +8,35 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn primes(n: usize) -> Vec<usize> {
+    if n < 2 {
+        return Vec::new();
+    }
+    let mut is_prime = vec![true; n + 1];
+    is_prime[0] = false;
+    is_prime[1] = false;
+    let mut res = Vec::new();
+    for i in 2..=n {
+        if is_prime[i] {
+            res.push(i);
+            let mut j = 2 * i;
+            while j <= n {
+                is_prime[j] = false;
+                j += i;
+            }
+        }
+    }
+    res
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
     };
+
+    let ans = primes(N).iter().join(" ");
+    println!("{}", ans);
 }
 
 fn main() {
