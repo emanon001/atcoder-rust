f5fc96aa75fd2d6185f885227fc13c43206729fe
emanon001 <emanon001@gmail.com>
2024-03-13 18:13:45 +0900

    bin/old math-and-algorithm

---
+++ math-and-algorithm/src/bin/069.rs
@@ -0,0 +1,27 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::input_interactive;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+#[allow(non_snake_case)]
+fn solve() {
+    input_interactive! {
+    };
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("big stack size".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(|| {
+            solve();
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+}
