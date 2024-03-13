be7b639eb5f91743fc87b57cbbe9aa9a23a74cfb
emanon001 <emanon001@gmail.com>
2024-03-13 18:49:56 +0900

    math-and-algorithm/009

--- math-and-algorithm/src/bin/009.rs
+++ math-and-algorithm/src/bin/009.rs
@@ -11,7 +11,22 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, S: usize,
+        A: [usize; N],
     };
+
+    let mut dp = vec![vec![false; S + 1]; N + 1];
+    dp[0][0] = true;
+    for i in 0..N {
+        for j in 0..=S {
+            dp[i + 1][j] |= dp[i][j];
+            if j + A[i] <= S {
+                dp[i + 1][j + A[i]] |= dp[i][j];
+            }
+        }
+    }
+    let ans = if dp[N][S] { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
