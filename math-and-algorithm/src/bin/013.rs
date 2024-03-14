f84c8801894441ce669f4061d4d7b664c002f93c
emanon001 <emanon001@gmail.com>
2024-03-14 20:08:31 +0900

    math-and-algorithm/013

--- math-and-algorithm/src/bin/013.rs
+++ math-and-algorithm/src/bin/013.rs
@@ -8,10 +8,30 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn divisors(n: u64) -> Vec<u64> {
+    let mut res = Vec::new();
+    let mut x = 1;
+    while x * x <= n {
+        if n % x == 0 {
+            res.push(x);
+            let y = n / x;
+            if y != x {
+                res.push(y);
+            }
+        }
+        x += 1;
+    }
+    res
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: u64,
     };
+
+    let ans = divisors(N).iter().join("\n");
+    println!("{}", ans);
 }
 
 fn main() {
