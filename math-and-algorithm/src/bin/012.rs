e8e1bc9783e07bfbe884a48cf652f9adf2519b5d
emanon001 <emanon001@gmail.com>
2024-03-14 20:06:43 +0900

    math-and-algorithm/012

--- math-and-algorithm/src/bin/012.rs
+++ math-and-algorithm/src/bin/012.rs
@@ -8,10 +8,28 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn is_prime(n: u64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: u64,
     };
+
+    let ans = if is_prime(N) { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
