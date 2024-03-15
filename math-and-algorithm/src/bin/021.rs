d433b79afc3f128f8ecea4813493eae56b78bcbc
emanon001 <emanon001@gmail.com>
2024-03-15 19:12:14 +0900

    math-and-algorithm/021

--- math-and-algorithm/src/bin/021.rs
+++ math-and-algorithm/src/bin/021.rs
@@ -11,7 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: u64, R: u64,
     };
+
+    let ans = (1..=N).product::<u64>() / (1..=N - R).product::<u64>() / (1..=R).product::<u64>();
+    println!("{}", ans);
 }
 
 fn main() {
