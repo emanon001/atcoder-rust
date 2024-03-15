5829a9f734cdeec9cd190b562134aa94bd935f6f
emanon001 <emanon001@gmail.com>
2024-03-15 15:25:42 +0900

    math-and-algorithm/018

--- math-and-algorithm/src/bin/018.rs
+++ math-and-algorithm/src/bin/018.rs
@@ -11,7 +11,21 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [i64; N],
     };
+
+    let mut counts = vec![0; 5];
+    for &a in &A {
+        counts[a as usize / 100] += 1;
+    }
+
+    let mut ans = 0_i64;
+    for i in 1..=4 {
+        ans += counts[i] * counts[5 - i];
+    }
+    ans /= 2;
+    println!("{}", ans);
 }
 
 fn main() {
