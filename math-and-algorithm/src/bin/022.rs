049436c7c64426b39df8b636b5a5be82018ccf57
emanon001 <emanon001@gmail.com>
2024-03-15 19:23:49 +0900

    math-and-algorithm/022

--- math-and-algorithm/src/bin/022.rs
+++ math-and-algorithm/src/bin/022.rs
@@ -11,7 +11,27 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [u64; N],
     };
+
+    let mut counts = HashMap::new();
+    for &a in &A {
+        *counts.entry(a).or_insert(0_u64) += 1;
+    }
+    let mut ans = 0_u64;
+    for (&a, &c) in &counts {
+        let b = 100000 - a;
+        ans += if a == b {
+            c * (c - 1)
+        } else if let Some(c2) = counts.get(&b) {
+            c * c2
+        } else {
+            0
+        }
+    }
+    ans /= 2;
+    println!("{}", ans);
 }
 
 fn main() {
