0359d397c91476266f8a3802fc350fb78db17e82
emanon001 <emanon001@gmail.com>
2024-03-17 22:46:10 +0900

    math-and-algorithm/031

--- math-and-algorithm/src/bin/031.rs
+++ math-and-algorithm/src/bin/031.rs
@@ -8,10 +8,36 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [i64; N],
     };
+
+    let mut dp = vec![vec![0; 2]; N + 1];
+    for i in 0..N {
+        // 勉強しない場合(0)
+        chmax!(dp[i + 1][0], dp[i][0]);
+        chmax!(dp[i + 1][0], dp[i][1]);
+        // 勉強する場合(1)
+        chmax!(dp[i + 1][1], dp[i][0] + A[i]);
+    }
+    let ans = dp[N][0].max(dp[N][1]);
+    println!("{}", ans);
 }
 
 fn main() {
