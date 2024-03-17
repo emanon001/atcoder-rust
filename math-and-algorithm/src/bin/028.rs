04cf5c1a74a3f5698a7330cf43e444d2264bfd55
emanon001 <emanon001@gmail.com>
2024-03-17 20:32:02 +0900

    math-and-algorithm/028

--- math-and-algorithm/src/bin/028.rs
+++ math-and-algorithm/src/bin/028.rs
@@ -8,10 +8,37 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        H: [isize; N],
     };
+
+    let mut dp = vec![1 << 30; N];
+    dp[0] = 0;
+    for i in 0..N {
+        if i + 1 < N {
+            chmin!(dp[i + 1], dp[i] + (H[i] - H[i + 1]).abs());
+        }
+        if i + 2 < N {
+            chmin!(dp[i + 2], dp[i] + (H[i] - H[i + 2]).abs());
+        }
+    }
+    println!("{}", dp[N - 1]);
 }
 
 fn main() {
