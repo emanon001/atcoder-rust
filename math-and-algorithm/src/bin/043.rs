6d2ba8bf750cd95fe638ed79cd8cb9d46e203a20
emanon001 <emanon001@gmail.com>
2024-03-19 05:21:27 +0900

    math-and-algorithm/043

--- math-and-algorithm/src/bin/043.rs
+++ math-and-algorithm/src/bin/043.rs
@@ -11,7 +11,34 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        AB: [(Usize1, Usize1); M],
     };
+
+    let mut graph = vec![vec![]; N];
+    for (u, v) in AB {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut visited = vec![false; N];
+    traverse(0, &mut visited, &graph);
+    let connected = visited.into_iter().all(|x| x);
+    let ans = if connected {
+        "The graph is connected."
+    } else {
+        "The graph is not connected."
+    };
+    println!("{}", ans);
+}
+
+fn traverse(u: usize, visited: &mut Vec<bool>, graph: &Vec<Vec<usize>>) {
+    if visited[u] {
+        return;
+    }
+    visited[u] = true;
+    for &v in &graph[u] {
+        traverse(v, visited, graph);
+    }
 }
 
 fn main() {
