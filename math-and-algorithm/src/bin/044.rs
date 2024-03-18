ce683d4826adbb488aece9d7d23f595a030e40bb
emanon001 <emanon001@gmail.com>
2024-03-19 05:28:00 +0900

    math-and-algorithm/044

--- math-and-algorithm/src/bin/044.rs
+++ math-and-algorithm/src/bin/044.rs
@@ -11,7 +11,35 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        AB: [(Usize1, Usize1); M],
     };
+
+    let mut graph = vec![vec![]; N];
+    for (u, v) in AB {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let ans = shortest_path(&graph).into_iter().join(" ");
+    println!("{}", ans);
+}
+
+fn shortest_path(graph: &Vec<Vec<usize>>) -> Vec<i64> {
+    let mut dist_list = vec![-1; graph.len()];
+    let mut que = VecDeque::new();
+    que.push_back((0, 0));
+    while let Some((u, d)) = que.pop_front() {
+        if dist_list[u] != -1 {
+            continue;
+        }
+        dist_list[u] = d;
+        for &v in &graph[u] {
+            if dist_list[v] == -1 {
+                que.push_back((v, d + 1));
+            }
+        }
+    }
+    dist_list
 }
 
 fn main() {
