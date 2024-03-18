4263e501b0c65faebb888dd7ff17abf643618941
emanon001 <emanon001@gmail.com>
2024-03-19 05:03:09 +0900

    math-and-algorithm/041

--- math-and-algorithm/src/bin/041.rs
+++ math-and-algorithm/src/bin/041.rs
@@ -11,7 +11,21 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        T: usize,
+        N: usize,
+        LR: [(usize, usize); N],
     };
+
+    let mut imos = vec![0; T + 1];
+    for (l, r) in LR {
+        imos[l] += 1;
+        imos[r] -= 1;
+    }
+    for i in 0..T {
+        imos[i + 1] += imos[i];
+    }
+    let ans = imos.into_iter().take(T).join("\n");
+    println!("{}", ans);
 }
 
 fn main() {
