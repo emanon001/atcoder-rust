846e94f66267c701798284709295e1ab33843fa7
emanon001 <emanon001@gmail.com>
2024-03-20 23:41:40 +0900

    math-and-algorithm/050

--- math-and-algorithm/src/bin/050.rs
+++ math-and-algorithm/src/bin/050.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        a: u64, b: u64,
     };
+
+    let ans = pow(a, b);
+    println!("{}", ans);
+}
+
+fn pow(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        return 1;
+    }
+    let mut res = pow(a, b >> 1);
+    res = (res * res) % 1000000007;
+    if b & 1 == 1 {
+        res = (a * res) % 1000000007;
+    }
+    res
 }
 
 fn main() {
