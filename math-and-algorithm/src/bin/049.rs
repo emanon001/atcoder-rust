225c5d73a25216e5ec925531c2fb664f48ce139d
emanon001 <emanon001@gmail.com>
2024-03-20 23:35:24 +0900

    math-and-algorithm/049

--- math-and-algorithm/src/bin/049.rs
+++ math-and-algorithm/src/bin/049.rs
@@ -11,7 +11,17 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
     };
+
+    let m = 1000000007;
+    let mut fib = vec![0; N];
+    fib[0] = 1;
+    fib[1] = 1;
+    for i in 2..N {
+        fib[i] = (fib[i - 1] + fib[i - 2]) % m;
+    }
+    println!("{}", fib[N - 1]);
 }
 
 fn main() {
