56a9d1c0d9f8cc8b80d305c9eb39b79ad07e615c
emanon001 <emanon001@gmail.com>
2024-03-22 23:35:46 +0900

    abc345/a

--- abc345/src/bin/a.rs
+++ abc345/src/bin/a.rs
@@ -11,7 +11,15 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: String,
     };
+
+    let ans = if S == format!("<{}>", "=".repeat(S.len() - 2)) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
 
 fn main() {
