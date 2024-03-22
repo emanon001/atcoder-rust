c78599ec7725e5912ede389d619a993fe3e3e63b
emanon001 <emanon001@gmail.com>
2024-03-23 00:20:08 +0900

    abc345/c

--- abc345/src/bin/c.rs
+++ abc345/src/bin/c.rs
@@ -11,7 +11,20 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
     };
+
+    let n = S.len();
+    let counts = S.into_iter().counts();
+    let mut ans = 0_u64;
+    let mut is_duplicate = false;
+    for (_, c) in counts {
+        is_duplicate |= c > 1;
+        ans += c as u64 * (n - c) as u64;
+    }
+    ans /= 2;
+    ans = if is_duplicate { ans + 1 } else { ans };
+    println!("{}", ans);
 }
 
 fn main() {
