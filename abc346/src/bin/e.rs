c5f70f8396567e284b0a06da7226f8346f5d6b37
emanon001 <emanon001@gmail.com>
2024-03-26 19:20:51 +0900

    abc346/e

--- abc346/src/bin/e.rs
+++ abc346/src/bin/e.rs
@@ -11,7 +11,39 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        H: usize, W: usize, M: usize,
+        TAX: [(usize, usize, usize); M],
     };
+
+    let mut h_set = HashSet::new();
+    let mut v_set = HashSet::new();
+    let mut counts = BTreeMap::new();
+    for (t, a, x) in TAX.into_iter().rev() {
+        if t == 1 {
+            // 行(横)
+            if h_set.contains(&a) {
+                continue;
+            }
+            h_set.insert(a);
+            let c = W - v_set.len();
+            *counts.entry(x).or_insert(0) += c;
+        } else {
+            // 列(縦)
+            if v_set.contains(&a) {
+                continue;
+            }
+            v_set.insert(a);
+            let c = H - h_set.len();
+            *counts.entry(x).or_insert(0) += c;
+        }
+    }
+    let rest_c = H * W - counts.values().sum::<usize>();
+    *counts.entry(0).or_insert(0) += rest_c;
+    let counts = counts.into_iter().filter(|(_, v)| v > &0).collect_vec();
+    println!("{}", counts.len());
+    for (k, v) in counts {
+        println!("{} {}", k, v);
+    }
 }
 
 fn main() {
