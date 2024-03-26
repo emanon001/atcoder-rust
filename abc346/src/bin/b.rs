74c1e2046681f315dd16641fbacf1bcda803ffd4
emanon001 <emanon001@gmail.com>
2024-03-26 18:35:52 +0900

    abc346/b

--- abc346/src/bin/b.rs
+++ abc346/src/bin/b.rs
@@ -11,7 +11,26 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        W: usize, B: usize,
     };
+
+    let s = "wbwbwwbwbwbw".repeat(200).chars().collect_vec();
+    for i in 0..s.len() - W - B {
+        let mut w_c = 0;
+        let mut b_c = 0;
+        for j in i..i + W + B {
+            if s[j] == 'w' {
+                w_c += 1;
+            } else {
+                b_c += 1;
+            }
+        }
+        if w_c == W && b_c == B {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
