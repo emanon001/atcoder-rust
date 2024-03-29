f2b96a5ac89904e2ff21602fc0629699cfe5712f
emanon001 <emanon001@gmail.com>
2024-03-29 21:20:17 +0900

    past202104-open/h

--- past202104-open/src/bin/h.rs
+++ past202104-open/src/bin/h.rs
@@ -8,9 +8,63 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn bsearch<F>(ok: i64, ng: i64, pred: F) -> Option<i64>
+where
+    F: Fn(i64) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while (ok - ng).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if pred(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
+#[allow(non_snake_case)]
 fn solve() {
     input! {
+        N: usize, M: usize, K: usize, Q: i64,
+        mut PT: [(i64, usize); N],
     };
+
+    PT.sort();
+    let mut cusum1 = vec![0];
+    let mut cusum2 = vec![0];
+    for (p, t) in PT {
+        if t == 0 {
+            cusum1.push(cusum1.last().unwrap() + p);
+        } else {
+            cusum2
+                .push(cusum2.last().unwrap() + p + if (cusum2.len() - 1) % K == 0 { Q } else { 0 });
+        }
+    }
+
+    let ans = bsearch(10_i64.pow(18), 0, |x| {
+        for i in 0..cusum1.len() {
+            if i > M {
+                break;
+            }
+            if cusum2.len() <= M - i {
+                continue;
+            }
+            if cusum1[i] + cusum2[M - i] <= x {
+                return true;
+            }
+        }
+        false
+    })
+    .unwrap();
+    println!("{}", ans);
 }
 
 fn main() {
