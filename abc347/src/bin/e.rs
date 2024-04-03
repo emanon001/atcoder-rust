db2a563b0521ca0f0a0c24699ef4883b308a1f5a
emanon001 <emanon001@gmail.com>
2024-04-03 23:21:02 +0900

    abc347/e

--- abc347/src/bin/e.rs
+++ abc347/src/bin/e.rs
@@ -11,7 +11,40 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, Q: usize,
+        X: [Usize1; Q],
     };
+
+    let mut set = HashSet::new();
+    let mut cusum = vec![0; Q + 1];
+    let mut event_indexes = vec![vec![]; N];
+    for (i, x) in X.into_iter().enumerate() {
+        event_indexes[x].push(i);
+        if set.contains(&x) {
+            set.remove(&x);
+        } else {
+            set.insert(x);
+        }
+        cusum[i + 1] = cusum[i] + set.len();
+    }
+
+    let mut ans = vec![0; N];
+    for i in 0..N {
+        let mut s = None;
+        for &j in &event_indexes[i] {
+            if s.is_none() {
+                s = Some(j);
+            } else {
+                let e = j;
+                ans[i] += cusum[e] - cusum[s.unwrap()];
+                s = None;
+            }
+        }
+        if let Some(s) = s {
+            ans[i] += cusum[Q] - cusum[s];
+        }
+    }
+    println!("{}", ans.into_iter().join(" "));
 }
 
 fn main() {
