2d7ab9a3b63a9deec587dbe5285c3678fe5a90e2
emanon001 <emanon001@gmail.com>
2024-04-03 15:39:48 +0900

    abc347/a

--- abc347/src/bin/a.rs
+++ abc347/src/bin/a.rs
@@ -11,7 +11,16 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        A: [usize; N],
     };
+
+    let ans = A
+        .into_iter()
+        .filter(|a| a % K == 0)
+        .map(|a| a / K)
+        .join(" ");
+    println!("{}", ans);
 }
 
 fn main() {
