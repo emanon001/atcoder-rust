976ceb5c0edb8a05d1fd1c7abc8ac459f20e2378
emanon001 <emanon001@gmail.com>
2024-04-07 22:11:45 +0900

    abc348/b

--- abc348/src/bin/b.rs
+++ abc348/src/bin/b.rs
@@ -11,7 +11,29 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        XY: [(isize, isize); N],
     };
+
+    for i in 0..N {
+        let (x, y) = XY[i];
+        let mut ans = i;
+        let mut max_d = 0;
+        for j in 0..N {
+            if i == j {
+                continue;
+            }
+            let (x2, y2) = XY[j];
+            let xd = (x - x2).abs();
+            let yd = (y - y2).abs();
+            let d = xd * xd + yd * yd;
+            if d > max_d {
+                max_d = d;
+                ans = j + 1;
+            }
+        }
+        println!("{}", ans);
+    }
 }
 
 fn main() {
