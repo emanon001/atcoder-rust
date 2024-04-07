fc6acae7bd007b0d1079b7e33ee06d4ce01ef205
emanon001 <emanon001@gmail.com>
2024-04-07 22:11:41 +0900

    abc348/c

--- abc348/src/bin/c.rs
+++ abc348/src/bin/c.rs
@@ -11,7 +11,17 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        AC: [(i64, usize); N],
     };
+
+    let mut min_map = HashMap::new();
+    for (a, c) in AC {
+        let e = min_map.entry(c).or_insert(a);
+        *e = (*e).min(a);
+    }
+    let ans = min_map.into_iter().max_by_key(|(_, v)| *v).unwrap().1;
+    println!("{}", ans);
 }
 
 fn main() {
