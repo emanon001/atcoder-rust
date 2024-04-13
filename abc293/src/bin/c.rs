ac46a8644c1465ae423989fa7ddd826c40b1ad6a
emanon001 <emanon001@gmail.com>
2024-04-13 22:50:08 +0900

    abc293/c

--- abc293/src/bin/c.rs
+++ abc293/src/bin/c.rs
@@ -2,18 +2,45 @@
 use itertools::Itertools;
 #[allow(unused_imports)]
 use num::*;
-use proconio::{input, source::line::LineSource};
+use proconio::input;
 #[allow(unused_imports)]
 use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
-use std::io::{stdin, BufReader};
 
+#[allow(non_snake_case)]
 fn solve() {
-    let mut source = LineSource::new(BufReader::new(stdin()));
-    macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut source, $($tt)*)));
     input! {
+        H: usize, W: usize,
+        grid: [[usize; W]; H]
     };
+
+    let STEP = H + W - 2;
+    let mut ans = 0;
+    for bits in 0..1 << STEP {
+        let mut i = 0;
+        let mut j = 0;
+        let mut set = HashSet::new();
+        set.insert(grid[i][j]);
+        for s in 0..STEP {
+            if (bits >> s & 1) == 0 {
+                i += 1;
+                if i >= H {
+                    break;
+                }
+            } else {
+                j += 1;
+                if j >= W {
+                    break;
+                }
+            }
+            set.insert(grid[i][j]);
+        }
+        if i == H - 1 && j == W - 1 && set.len() == STEP + 1 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
