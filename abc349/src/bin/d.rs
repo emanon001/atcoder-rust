da7569de75704c4ad803f865d506960a29a6b8c7
emanon001 <emanon001@gmail.com>
2024-04-28 22:26:35 +0900

    abc349/d

--- abc349/src/bin/d.rs
+++ abc349/src/bin/d.rs
@@ -8,10 +8,46 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        L: u128, R: u128,
     };
+
+    let mut ans = vec![];
+    let mut l = L;
+    while l < R {
+        let mut r = l;
+        for i in 0..=60 {
+            let two_pow = 2.pow(i);
+            if l == 0 || (two_pow <= l && l % two_pow == 0) {
+                let j = l / two_pow;
+                let tmp_r = two_pow * (j + 1);
+                if tmp_r <= R {
+                    chmax!(r, tmp_r);
+                }
+            }
+        }
+        ans.push((l, r));
+        l = r;
+    }
+    println!("{}", ans.len());
+    for (l, r) in ans {
+        println!("{} {}", l, r);
+    }
 }
 
 fn main() {
