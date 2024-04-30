21ab918af54d4bdf872bc515409e8cb70e751c33
emanon001 <emanon001@gmail.com>
2024-05-01 06:09:18 +0900

    abc350/c

--- abc350/src/bin/c.rs
+++ abc350/src/bin/c.rs
@@ -11,7 +11,35 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        mut A: [usize; N],
     };
+
+    let mut a_set = BTreeSet::new();
+    let mut a_to_i = vec![0; N + 1];
+    for (i, &a) in A.iter().enumerate() {
+        a_set.insert(a);
+        a_to_i[a] = i;
+    }
+    let mut ans = vec![];
+    for i in 0..N - 1 {
+        let a = A[i];
+        let b = *a_set.iter().next().unwrap();
+        if a == b {
+            a_set.remove(&a);
+            continue;
+        }
+        let j = a_to_i[b];
+        A.swap(i, j);
+        ans.push((i + 1, j + 1));
+        a_set.remove(&b);
+        a_to_i[a] = j;
+        a_to_i[b] = i;
+    }
+    println!("{}", ans.len());
+    for (i, j) in ans {
+        println!("{} {}", i, j);
+    }
 }
 
 fn main() {
