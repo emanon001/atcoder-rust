fdc79a0de6ed7de7e505c54861d3309d590e5d57
emanon001 <emanon001@gmail.com>
2024-05-02 05:56:37 +0900

    abc351/c

--- abc351/src/bin/c.rs
+++ abc351/src/bin/c.rs
@@ -11,7 +11,28 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [usize; N],
     };
+
+    let mut v = vec![];
+    for a in A {
+        v.push(a);
+        loop {
+            if v.len() <= 1 {
+                break;
+            }
+            if v[v.len() - 1] != v[v.len() - 2] {
+                break;
+            }
+            let b = v[v.len() - 1];
+            v.pop();
+            v.pop();
+            v.push(b + 1);
+        }
+    }
+    eprintln!("{:?}", v);
+    println!("{}", v.len());
 }
 
 fn main() {
