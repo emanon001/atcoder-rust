ed44e4fd177a2813a03801e427085f31fb987153
emanon001 <emanon001@gmail.com>
2024-05-04 13:55:32 +0900

    abc194/d

--- abc194/src/bin/d.rs
+++ abc194/src/bin/d.rs
@@ -8,9 +8,18 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[allow(non_snake_case)]
 fn solve() {
     input! {
+        N: usize,
     };
+
+    let mut ans = 1_f64;
+    for i in 2..=N {
+        ans += 1_f64 / i as f64;
+    }
+    ans = ans * N as f64 - 1_f64;
+    println!("{}", ans);
 }
 
 fn main() {
