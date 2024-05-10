67918b93e00422de872cc53a0be74370fc408cd9
emanon001 <emanon001@gmail.com>
2024-05-10 23:52:39 +0900

    past17-open/a

--- past17-open/src/bin/a.rs
+++ past17-open/src/bin/a.rs
@@ -11,7 +11,18 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        H: usize, W: usize,
     };
+
+    let v = W * 10000 / (H * H);
+    let ans = if v < 20 {
+        "A"
+    } else if 20 <= 20 && v < 25 {
+        "B"
+    } else {
+        "C"
+    };
+    println!("{}", ans);
 }
 
 fn main() {
