3bb343eb2a1041a28352e25719e6ee5edafd790b
emanon001 <emanon001@gmail.com>
2024-05-11 22:09:27 +0900

    past17-open/f

--- past17-open/src/bin/f.rs
+++ past17-open/src/bin/f.rs
@@ -8,10 +8,30 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+const MOD: i64 = 998244353;
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        P: [Usize1; N - 1],
+        S: [String; N],
     };
+
+    let mut graph = vec![vec![]; N];
+    for (i, &p) in P.iter().enumerate() {
+        graph[p].push(i + 1);
+    }
+    let ans = dfs(0, &graph, &S);
+    println!("{}", ans);
+}
+
+fn dfs(u: usize, graph: &[Vec<usize>], value: &[String]) -> i64 {
+    (match value[u].as_ref() {
+        "x" => dfs(graph[u][0], graph, value) * dfs(graph[u][1], graph, value),
+        "+" => dfs(graph[u][0], graph, value) + dfs(graph[u][1], graph, value),
+        x => x.parse::<i64>().unwrap(),
+    }) % MOD
 }
 
 fn main() {
