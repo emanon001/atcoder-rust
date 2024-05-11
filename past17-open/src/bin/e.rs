496013daed27ad9240ec24fff3f3164199c7c729
emanon001 <emanon001@gmail.com>
2024-05-11 21:54:18 +0900

    past17-open/e

--- past17-open/src/bin/e.rs
+++ past17-open/src/bin/e.rs
@@ -8,10 +8,34 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn compress_list<T: Copy + std::cmp::PartialEq>(list: Vec<T>) -> Vec<(T, usize)> {
+    let mut res = Vec::new();
+    if list.is_empty() {
+        return res;
+    }
+    let mut cur_v = list[0];
+    let mut count = 1;
+    for v in list.into_iter().skip(1) {
+        if v == cur_v {
+            count += 1;
+        } else {
+            res.push((cur_v, count));
+            count = 1;
+        }
+        cur_v = v;
+    }
+    res.push((cur_v, count));
+    res
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
     };
+
+    let ans = compress_list(S).into_iter().map(|(c, count)| format!("{} {}", c, count)).join(" ");
+    println!("{}", ans);
 }
 
 fn main() {
