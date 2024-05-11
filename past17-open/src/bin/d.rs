7487a932ebd5b2a80d7701269f2436978f637563
emanon001 <emanon001@gmail.com>
2024-05-11 21:51:19 +0900

    past17-open/d

--- past17-open/src/bin/d.rs
+++ past17-open/src/bin/d.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: u64, B: u64, C: u64,
+        X: [u64; 12],
     };
+
+    let mut free = 0_u64;
+    let mut basic = B;
+    let premium = C;
+    for x in X {
+        if x > 3 {
+            free += (x - 3) * A;
+        }
+        if x > 50 {
+            basic += (x - 50) * A;
+        }
+    }
+    let ans = free.min(basic).min(premium);
+    println!("{}", ans);
 }
 
 fn main() {
