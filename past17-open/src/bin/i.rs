0f6388645902dd5f8a6f4c78c0adae01b8810a0f
emanon001 <emanon001@gmail.com>
2024-05-12 22:24:06 +0900

    past17-open/i

--- past17-open/src/bin/i.rs
+++ past17-open/src/bin/i.rs
@@ -11,7 +11,35 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        S: [String; N],
     };
+
+    let mut count_map = HashMap::new();
+    for s in &S {
+        *count_map.entry(s).or_insert(0_u64) += 1;
+    }
+    let mut ans = 0_u64;
+    for s in &S {
+        let len = s.len();
+        let chars = s.chars().collect::<Vec<_>>();
+        let mut t_set = HashSet::new();
+        for bits in 0..1 << len {
+            let mut t = String::new();
+            for i in 0..len {
+                if (bits >> i) & 1 == 1 {
+                    t.push(chars[i]);
+                }
+            }
+            t_set.insert(t);
+        }
+        for t in t_set {
+            if let Some(c) = count_map.get(&t) {
+                ans += *c;
+            }
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
