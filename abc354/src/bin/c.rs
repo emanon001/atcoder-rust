ab02590e056fe1b3b7c8202668d8a9ab570ac674
emanon001 <emanon001@gmail.com>
2024-05-19 23:31:21 +0900

    abc354/c

--- abc354/src/bin/c.rs
+++ abc354/src/bin/c.rs
@@ -11,7 +11,30 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        AC: [(usize, usize); N],
     };
+
+    let sorted = AC.iter().enumerate().sorted_by_key(|(_, &(a, _))| a).collect::<Vec<_>>();
+    let mut c_set = BTreeSet::new();
+    for (_, (_, c)) in sorted.iter() {
+        c_set.insert(*c);
+    }
+    let mut ans = vec![];
+    for (i, (_, c)) in sorted {
+        c_set.remove(&c);
+        match c_set.iter().next() {
+            Some(c2) if c > c2 => {
+                // ignore
+            },
+            _ => {
+                ans.push(i + 1);
+            }
+        }
+    }
+    ans.sort();
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
