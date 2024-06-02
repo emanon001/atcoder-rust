938f956c76e51f23cecf92a460dbb76be230053b
emanon001 <emanon001@gmail.com>
2024-06-02 23:59:14 +0900

    abc355/b

--- abc355/src/bin/b.rs
+++ abc355/src/bin/b.rs
@@ -11,7 +11,20 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        A: [i64; N],
+        B: [i64; M],
     };
+
+    let C = A.iter().chain(&B).sorted().copied().collect::<Vec<i64>>();
+    let a_set = A.iter().copied().collect::<HashSet<_>>();
+    for i in 0..C.len() - 1 {
+        if a_set.contains(&C[i]) && a_set.contains(&C[i + 1]) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
