76847e0c65e6cf28dd8beb1c783967e327ac8572
emanon001 <emanon001@gmail.com>
2024-06-09 23:30:17 +0900

    abc356/b

--- abc356/src/bin/b.rs
+++ abc356/src/bin/b.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        A: [usize; M],
+        X: [[usize; M]; N],
     };
+
+    let mut sum = vec![0; M];
+    for i in 0..N {
+        for j in 0..M {
+            sum[j] += X[i][j];
+        }
+    }
+    let ans = if sum.into_iter().enumerate().all(|(i, x)| x >= A[i]) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
 
 fn main() {
