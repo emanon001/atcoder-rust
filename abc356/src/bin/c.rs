9425d28276325448acf4610857cd5f48b56e6160
emanon001 <emanon001@gmail.com>
2024-06-09 23:38:15 +0900

    abc356/c

--- abc356/src/bin/c.rs
+++ abc356/src/bin/c.rs
@@ -11,7 +11,32 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize, K: usize,
+        C: [([Usize1], char); M],
     };
+
+    let mut ans = 0;
+    for bits in 0..1 << N {
+        let mut ok = true;
+        for (v, r) in &C {
+            let mut count = 0;
+            for &i in v {
+                if bits >> i & 1 == 1 {
+                    count += 1;
+                }
+            }
+            ok = ok
+                && match (count >= K, r) {
+                    (true, 'o') => true,
+                    (false, 'x') => true,
+                    _ => false,
+                };
+        }
+        if ok {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
