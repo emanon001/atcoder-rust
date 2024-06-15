2359b4de38324b29e9f434c5be0f886af45075fa
emanon001 <emanon001@gmail.com>
2024-06-15 22:40:35 +0900

    abc357/c

--- abc357/src/bin/c.rs
+++ abc357/src/bin/c.rs
@@ -11,7 +11,36 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
     };
+    let carpet = carpet(N);
+    let ans = carpet.into_iter().map(|row| row.iter().join("")).join("\n");
+    println!("{}", ans);
+}
+
+fn carpet(k: usize) -> Vec<Vec<char>> {
+    if k == 0 {
+        return vec![vec!['#']];
+    }
+    let size = 3.pow(k as u32);
+    let mut res = vec![vec!['.'; size]; size];
+    let child_size = 3.pow(k as u32 - 1);
+    let child_carpet = carpet(k - 1);
+    for i in 0..3 {
+        for j in 0..3 {
+            for i2 in 0..child_size {
+                for j2 in 0..child_size {
+                    res[i * child_size + i2][j * child_size + j2] = child_carpet[i2][j2];
+                }
+            }
+        }
+    }
+    for i in 0..child_size {
+        for j in 0..child_size {
+            res[child_size + i][child_size + j] = '.'
+        }
+    }
+    res
 }
 
 fn main() {
