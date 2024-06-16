2209f2b26504d2f63fc941ebe09c57106e01feec
emanon001 <emanon001@gmail.com>
2024-06-16 23:02:14 +0900

    abc358/c

--- abc358/src/bin/c.rs
+++ abc358/src/bin/c.rs
@@ -11,7 +11,29 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        S: [Chars; N],
     };
+
+    let mut ans = 1 << 30;
+    for bits in 0..1 << N {
+        let mut set = HashSet::new();
+        let mut count = 0;
+        for i in 0..N {
+            if (bits >> i) & 1 == 1 {
+                count += 1;
+                for j in 0..M {
+                    if S[i][j] == 'o' {
+                        set.insert(j);
+                    }
+                }
+            }
+        }
+        if set.len() == M {
+            ans = ans.min(count);
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
