f6558cc068794522fc028cbf77cff4cdd13b7b4f
emanon001 <emanon001@gmail.com>
2024-06-25 23:41:36 +0900

    typical90/001

--- typical90/src/bin/001.rs
+++ typical90/src/bin/001.rs
@@ -8,10 +8,107 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<i128> for i128 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<isize> for isize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self - *ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, L: i64,
+        K: usize,
+        A: [i64; N],
     };
+
+    let blocks = vec![0]
+        .into_iter()
+        .chain(A.into_iter())
+        .chain(vec![L])
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect::<Vec<_>>();
+
+    let ans = bsearch(0, 10_i64.pow(18), |x| {
+        let mut count = 0;
+        let mut width = 0;
+        for &w in &blocks {
+            width += w;
+            if width >= x {
+                count += 1;
+                width = 0;
+            }
+        }
+        count >= K + 1
+    })
+    .unwrap_or(0);
+    println!("{}", ans);
 }
 
 fn main() {
