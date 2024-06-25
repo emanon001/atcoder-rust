6169ccb4124cc8460ab4568d9b735cf3281842cd
emanon001 <emanon001@gmail.com>
2024-06-25 23:49:13 +0900

    typical90/002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -11,7 +11,32 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
     };
+
+    let mut ans = vec![];
+    for bits in 0..1 << N {
+        let mut v = vec![];
+        let mut c = 0_isize;
+        for i in 0..N {
+            if bits & 1 << i == 0 {
+                c += 1;
+                v.push('(');
+            } else {
+                c -= 1;
+                v.push(')');
+            }
+            if c < 0 {
+                break;
+            }
+        }
+        if c != 0 {
+            continue;
+        }
+        ans.push(v);
+    }
+    ans.sort();
+    println!("{}", ans.iter().map(|v| v.iter().join("")).join("\n"));
 }
 
 fn main() {
