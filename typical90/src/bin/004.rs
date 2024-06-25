c40d4d2823d1b26aecea96345bf096570b0da1ab
emanon001 <emanon001@gmail.com>
2024-06-26 01:32:37 +0900

    typical90/004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -11,7 +11,30 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        H: usize, W: usize,
+        A: [[i64; W]; H],
     };
+
+    let mut vertical_sum = vec![0; W];
+    for j in 0..W {
+        for i in 0..H {
+            vertical_sum[j] += A[i][j];
+        }
+    }
+    let mut horizontal_sum = vec![0; H];
+    for i in 0..H {
+        for j in 0..W {
+            horizontal_sum[i] += A[i][j];
+        }
+    }
+
+    let mut ans = vec![vec![0; W]; H];
+    for i in 0..H {
+        for j in 0..W {
+            ans[i][j] = vertical_sum[j] + horizontal_sum[i] - A[i][j];
+        }
+    }
+    println!("{}", ans.iter().map(|v| v.iter().join(" ")).join("\n"));
 }
 
 fn main() {
