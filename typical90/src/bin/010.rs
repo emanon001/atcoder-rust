173bdeedc3a108fa0ece6ec52cef858d63a5f3de
emanon001 <emanon001@gmail.com>
2024-06-30 20:56:54 +0900

    typical90/010

--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -8,10 +8,96 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+/// `T` is numeric only
+pub struct Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::SubAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    n: usize,
+    data: Vec<T>,
+}
+/// 0-origin
+/// [0, n)
+impl<T> Bit<T>
+where
+    T: std::ops::AddAssign + std::ops::SubAssign + std::ops::Sub<Output = T> + num::Zero + Clone,
+{
+    pub fn new(n: usize) -> Self {
+        Self {
+            n,
+            data: vec![T::zero(); n + 1],
+        }
+    }
+    /// 0-origin
+    pub fn add(&mut self, i: usize, x: T) {
+        if i >= self.n {
+            panic!();
+        }
+        let mut i = i + 1;
+        while i <= self.n {
+            self.data[i] += x.clone();
+            i += ((i as isize) & -(i as isize)) as usize;
+        }
+    }
+    /// 0-origin
+    pub fn sub(&mut self, i: usize, x: T) {
+        if i >= self.n {
+            panic!();
+        }
+        let mut i = i + 1;
+        while i <= self.n {
+            self.data[i] -= x.clone();
+            i += ((i as isize) & -(i as isize)) as usize;
+        }
+    }
+    /// [0, i)
+    pub fn sum(&self, i: usize) -> T {
+        if i > self.n {
+            panic!();
+        }
+        let mut i = i;
+        let mut res = T::zero();
+        while i > 0 {
+            res += self.data[i].clone();
+            i -= ((i as isize) & -(i as isize)) as usize;
+        }
+        res
+    }
+    /// [i, j)
+    pub fn range_sum(&self, i: usize, j: usize) -> T {
+        if i > self.n || j > self.n {
+            panic!();
+        }
+        if i >= j {
+            return T::zero();
+        }
+        self.sum(j) - self.sum(i)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        CP: [(usize, i64); N],
+        Q: usize,
+        LR: [(Usize1, Usize1); Q],
     };
+
+    let mut bit1 = Bit::new(N);
+    let mut bit2 = Bit::new(N);
+    for (i, (c, p)) in CP.into_iter().enumerate() {
+        if c == 1 {
+            bit1.add(i, p);
+        } else {
+            bit2.add(i, p);
+        }
+    }
+    for (l, r) in LR {
+        let sum1 = bit1.range_sum(l, r + 1);
+        let sum2 = bit2.range_sum(l, r + 1);
+        println!("{} {}", sum1, sum2);
+    }
 }
 
 fn main() {
