c332522a1d23de448d3fe6077618df082cd44f40
emanon001 <emanon001@gmail.com>
2024-07-05 00:01:28 +0900

    abc360/c

--- abc360/src/bin/c.rs
+++ abc360/src/bin/c.rs
@@ -11,7 +11,24 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [Usize1; N],
+        W: [i64; N],
     };
+
+    let mut box_list = vec![vec![]; N];
+    for (i, &a) in A.iter().enumerate() {
+        box_list[a].push(W[i]);
+    }
+    let mut ans = 0;
+    for b in box_list {
+        ans += b
+            .iter()
+            .sorted_by_key(|w| std::cmp::Reverse(*w))
+            .skip(1)
+            .sum::<i64>();
+    }
+    println!("{}", ans);
 }
 
 fn main() {
