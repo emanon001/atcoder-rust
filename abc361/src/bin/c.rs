c235335335a1b1754d5cdc7444e9e60f1dda5066
emanon001 <emanon001@gmail.com>
2024-07-13 23:24:15 +0900

    abc361/c

--- abc361/src/bin/c.rs
+++ abc361/src/bin/c.rs
@@ -8,10 +8,32 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        mut A: [usize; N],
     };
+    A.sort();
+    let mut ans = usize::max_value();
+    for i in 0..=K {
+        let j = N - (K - i) - 1;
+        chmin!(ans, A[j] - A[i]);
+    }
+    println!("{}", ans);
 }
 
 fn main() {
