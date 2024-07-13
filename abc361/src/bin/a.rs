01f8f8224c47abd6f76a87de45fd397c7b659d2e
emanon001 <emanon001@gmail.com>
2024-07-13 23:24:12 +0900

    abc361/a

--- abc361/src/bin/a.rs
+++ abc361/src/bin/a.rs
@@ -11,7 +11,12 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize, X: usize,
+        A: [usize; N]
     };
+
+    let ans = &A[..K].iter().chain(&vec![X]).chain(A[K..].iter()).join(" ");
+    println!("{}", ans);
 }
 
 fn main() {
