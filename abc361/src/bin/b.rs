10504086ff6066ddbbde29035c62bab3eb8180dc
emanon001 <emanon001@gmail.com>
2024-07-14 00:16:59 +0900

    abc361/b

--- abc361/src/bin/b.rs
+++ abc361/src/bin/b.rs
@@ -8,10 +8,33 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn is_sections_overlapping(l1: usize, r1: usize, l2: usize, r2: usize) -> bool {
+    // l1, l2, r2, r1
+    (l1 <= l2 && r2 <= r1)
+        // l2, l1, r1, r2
+        || (l2 <= l1 && r1 <= r2)
+        // l1, l2, r1
+        || (l1 <= l2 && l2 < r1)
+        // l2, l1, r2
+        || (l2 <= l1 && l1 < r2)
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        a: usize, b: usize, c: usize, d: usize, e: usize, f: usize,
+        g: usize, h: usize, i: usize, j: usize, k: usize, l: usize,
     };
+
+    let ans = if is_sections_overlapping(a, d, g, j)
+        && is_sections_overlapping(b, e, h, k)
+        && is_sections_overlapping(c, f, i, l)
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans)
 }
 
 fn main() {
