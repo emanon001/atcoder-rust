a536a8fcc1622569ec3ea547e911c4354f69a31e
emanon001 <emanon001@gmail.com>
2024-07-15 22:54:50 +0900

    abc362/b

--- abc362/src/bin/b.rs
+++ abc362/src/bin/b.rs
@@ -11,7 +11,27 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        p1: (isize, isize),
+        p2: (isize, isize),
+        p3: (isize, isize),
     };
+
+    let ans = if is_right_triangle(p1, p2, p3) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
+}
+
+fn is_right_triangle(p1: (isize, isize), p2: (isize, isize), p3: (isize, isize)) -> bool {
+    // p1, p2間の距離
+    let d1 = (p2.0 - p1.0).pow(2) + (p2.1 - p1.1).pow(2);
+    // p2, p3間の距離
+    let d2 = (p2.0 - p3.0).pow(2) + (p2.1 - p3.1).pow(2);
+    // p3, p1間の距離
+    let d3 = (p3.0 - p1.0).pow(2) + (p3.1 - p1.1).pow(2);
+    d1 + d2 == d3 || d1 + d3 == d2 || d2 + d3 == d1
 }
 
 fn main() {
