8057cb206c83842e479ee687e59ec5a9431d6188
emanon001 <emanon001@gmail.com>
2024-07-15 22:26:01 +0900

    abc362/a

--- abc362/src/bin/a.rs
+++ abc362/src/bin/a.rs
@@ -11,7 +11,17 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        RGB: [usize; 3],
+        C: String
     };
+
+    let ans = match C.as_ref() {
+        "Red" => RGB[1].min(RGB[2]),
+        "Green" => RGB[0].min(RGB[2]),
+        "Blue" => RGB[0].min(RGB[1]),
+        _ => unreachable!(),
+    };
+    println!("{}", ans);
 }
 
 fn main() {
