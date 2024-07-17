adc6ec2fb60ad0b88c6efc8af1067a825e2af518
emanon001 <emanon001@gmail.com>
2024-07-17 21:44:38 +0900

    typical90/016

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -8,10 +8,41 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: i64,
+        A: i64, B: i64, C: i64,
     };
+
+    let mut ans = i64::max_value();
+    for a in 0..=9999 {
+        for b in 0..=9999 {
+            let ab_sum = a * A + b * B;
+            if ab_sum > N {
+                break;
+            }
+            if (N - ab_sum) % C != 0 {
+                continue;
+            }
+            let c = (N - ab_sum) / C;
+            chmin!(ans, a + b + c);
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
