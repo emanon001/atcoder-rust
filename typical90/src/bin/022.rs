22e06d2e68b569db5f4866af2ecc0309caad1ae0
emanon001 <emanon001@gmail.com>
2024-07-27 21:48:57 +0900

    typical90/022

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -11,7 +11,12 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: u64, B: u64, C: u64,
     };
+
+    let gcd = A.gcd(&B).gcd(&C);
+    let ans = (A / gcd - 1) + (B / gcd - 1) + (C / gcd - 1);
+    println!("{}", ans);
 }
 
 fn main() {
