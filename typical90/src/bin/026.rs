58bde0e3f652fc27d314ca7d6bc13445914dba3d
emanon001 <emanon001@gmail.com>
2024-07-28 23:35:46 +0900

    typical90/026

--- typical90/src/bin/026.rs
+++ typical90/src/bin/026.rs
@@ -11,7 +11,34 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        edges: [(Usize1, Usize1); N - 1],
     };
+
+    let mut graph = vec![vec![]; N];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let mut ans = vec![];
+    dfs(0, N, &graph, &mut ans);
+    let ans = ans.iter().take(N / 2).join(" ");
+    println!("{}", ans);
+}
+
+fn dfs(u: usize, p: usize, g: &Vec<Vec<usize>>, ans: &mut Vec<usize>) -> bool {
+    let mut is_children_choose = false;
+    for v in &g[u] {
+        if *v == p {
+            continue;
+        }
+        is_children_choose |= dfs(*v, u, g, ans);
+    }
+    let res = !is_children_choose;
+    if res {
+        ans.push(u + 1);
+    }
+    res
 }
 
 fn main() {
