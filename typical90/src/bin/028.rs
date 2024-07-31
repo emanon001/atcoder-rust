1b385aa0a9789c7c944f4953f0107520443cce4c
emanon001 <emanon001@gmail.com>
2024-07-31 22:07:34 +0900

    typical90/028

--- typical90/src/bin/028.rs
+++ typical90/src/bin/028.rs
@@ -11,7 +11,39 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        rectangles: [(usize, usize, usize, usize); N],
     };
+
+    let mut grid = vec![vec![0_isize; 1001]; 1001];
+    for (lx, ly, rx, ry) in rectangles {
+        grid[ly][lx] += 1;
+        grid[ly][rx] -= 1;
+        grid[ry][lx] -= 1;
+        grid[ry][rx] += 1;
+    }
+    for i in 0..=1000 {
+        for j in 1..=1000 {
+            grid[i][j] += grid[i][j - 1];
+        }
+    }
+    for j in 0..=1000 {
+        for i in 1..=1000 {
+            grid[i][j] += grid[i - 1][j];
+        }
+    }
+    let mut counts = vec![0_usize; N + 1];
+    for i in 0..=1000 {
+        for j in 0..=1000 {
+            let c = grid[i][j];
+            if c >= 0 {
+                counts[c as usize] += 1;
+            }
+        }
+    }
+    for k in 1..=N {
+        println!("{}", counts[k]);
+    }
 }
 
 fn main() {
