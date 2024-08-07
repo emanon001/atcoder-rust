ec30263ce72fe7e4d645faa7a9543e9666295ce9
emanon001 <emanon001@gmail.com>
2024-08-07 21:49:53 +0900

    typical90/032

--- typical90/src/bin/032.rs
+++ typical90/src/bin/032.rs
@@ -8,10 +8,50 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [[i64; N]; N],
+        M: usize,
+        XY: [(Usize1, Usize1); M],
     };
+
+    let mut ng_set = HashSet::new();
+    for (x, y) in XY {
+        ng_set.insert((x, y));
+        ng_set.insert((y, x));
+    }
+    let inf = 1_i64 << 60;
+    let mut ans = inf;
+    for perm in (0..N).permutations(N) {
+        let mut cost = 0;
+        let mut prev = N;
+        for (j, i) in perm.into_iter().enumerate() {
+            if ng_set.contains(&(prev, i)) {
+                cost = inf;
+                break;
+            }
+            cost += A[i][j];
+            prev = i;
+        }
+        chmin!(ans, cost);
+    }
+    let ans = if ans == inf { -1 } else { ans };
+    println!("{}", ans);
 }
 
 fn main() {
