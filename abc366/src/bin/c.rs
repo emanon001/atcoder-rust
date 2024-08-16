e6125017073838e2818035ce6f9a6a4c562884b4
emanon001 <emanon001@gmail.com>
2024-08-17 00:07:37 +0900

    abc366/c

--- abc366/src/bin/c.rs
+++ abc366/src/bin/c.rs
@@ -11,7 +11,37 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        Q: usize,
     };
+
+    let mut counts = HashMap::new();
+    for _ in 0..Q {
+        input_interactive! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                input_interactive! {
+                    x: usize,
+                };
+                *counts.entry(x).or_insert(0) += 1;
+            }
+            2 => {
+                input_interactive! {
+                    x: usize,
+                };
+                let entry = counts.entry(x).or_insert(0);
+                *entry -= 1;
+                if *entry == 0 {
+                    counts.remove(&x);
+                }
+            }
+            3 => {
+                println!("{}", counts.len());
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
