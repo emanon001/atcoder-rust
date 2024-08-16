291d931b6382fbb145f7bf690154467f475cfdb0
emanon001 <emanon001@gmail.com>
2024-08-17 01:28:08 +0900

    abc366/d

--- abc366/src/bin/d.rs
+++ abc366/src/bin/d.rs
@@ -11,7 +11,32 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [[i64; N]; N * N],
+        Q: usize,
+        L: [(usize, usize, usize, usize, usize, usize); Q]
     };
+
+    let mut cusum = vec![vec![vec![0; N + 1]; N + 1]; N];
+    for i in 0..N {
+        for j in 0..N {
+            for k in 0..N {
+                cusum[i][j + 1][k + 1] =
+                    cusum[i][j][k + 1] + cusum[i][j + 1][k] - cusum[i][j][k] + A[i * N + j][k];
+            }
+        }
+    }
+
+    for (lx, rx, ly, ry, lz, rz) in L {
+        let mut ans = 0;
+        for i in lx..=rx {
+            let i = i - 1;
+            ans += cusum[i][ry][rz] + cusum[i][ly - 1][lz - 1]
+                - cusum[i][ry][lz - 1]
+                - cusum[i][ly - 1][rz];
+        }
+        println!("{}", ans);
+    }
 }
 
 fn main() {
