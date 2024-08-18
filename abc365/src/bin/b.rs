89257e97291afad06be1c690d696bf63d1a05a78
emanon001 <emanon001@gmail.com>
2024-08-18 23:32:57 +0900

    abc365/b

--- abc365/src/bin/b.rs
+++ abc365/src/bin/b.rs
@@ -11,7 +11,18 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [usize; N],
     };
+
+    let a = A
+        .into_iter()
+        .enumerate()
+        .map(|(i, a)| (a, i))
+        .sorted()
+        .rev()
+        .collect::<Vec<_>>();
+    println!("{}", a[1].1 + 1);
 }
 
 fn main() {
