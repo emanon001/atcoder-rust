adefb76383da9e5a6ad66b3b622ad94fc578905b
emanon001 <emanon001@gmail.com>
2024-08-24 23:43:34 +0900

    abc363/b

--- abc363/src/bin/b.rs
+++ abc363/src/bin/b.rs
@@ -11,7 +11,17 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, T: usize, P: usize,
+        L: [usize; N],
     };
+
+    for d in 0..=100 {
+        let c = L.iter().filter(|&l| l + d >= T).count();
+        if c >= P {
+            println!("{}", d);
+            return;
+        }
+    }
 }
 
 fn main() {
