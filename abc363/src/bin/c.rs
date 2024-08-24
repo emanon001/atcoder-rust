0231aeccb041384377994461ffecc746383f4f28
emanon001 <emanon001@gmail.com>
2024-08-25 00:14:43 +0900

    abc363/c

--- abc363/src/bin/c.rs
+++ abc363/src/bin/c.rs
@@ -8,10 +8,44 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+fn is_k_palindrome(s: &[char], k: usize) -> bool {
+    for i in 0..s.len() {
+        if i + k > s.len() {
+            break;
+        }
+        let mut ok = true;
+        for j in 0..k / 2 {
+            if s[i + j] != s[i + k - 1 - j] {
+                ok = false;
+                break;
+            }
+        }
+        if ok {
+            return true;
+        }
+    }
+    false
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        S: Chars,
     };
+
+    let mut ans = vec![];
+    for perm in (0..N).permutations(N) {
+        let mut s = vec![];
+        for i in perm {
+            s.push(S[i]);
+        }
+        if !is_k_palindrome(&s, K) {
+            ans.push(s);
+        }
+    }
+    let ans = ans.iter().unique().count();
+    println!("{}", ans);
 }
 
 fn main() {
