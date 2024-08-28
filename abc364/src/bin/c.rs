ba851a682c41229ef88f7462e257c35e0c417c56
emanon001 <emanon001@gmail.com>
2024-08-28 23:44:25 +0900

    abc364/c

--- abc364/src/bin/c.rs
+++ abc364/src/bin/c.rs
@@ -8,10 +8,51 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmin {
+    ($ min : expr , $ v : expr ) => {{
+        let v = $v;
+        if $min > v {
+            $min = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, X: i64, Y: i64,
+        mut A: [i64; N],
+        mut B: [i64; N],
     };
+
+    A.sort();
+    A.reverse();
+    B.sort();
+    B.reverse();
+
+    let mut ans = N;
+
+    let mut sum = 0;
+    for (i, a) in A.into_iter().enumerate() {
+        sum += a;
+        if sum > X {
+            chmin!(ans, i + 1);
+            break;
+        }
+    }
+    let mut sum = 0;
+    for (i, b) in B.into_iter().enumerate() {
+        sum += b;
+        if sum > Y {
+            chmin!(ans, i + 1);
+            break;
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
