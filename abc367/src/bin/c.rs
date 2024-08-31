502b41aca010e2fc2460637402b8c62e3fa0f24a
emanon001 <emanon001@gmail.com>
2024-09-01 08:36:25 +0900

    abc367/c

--- abc367/src/bin/c.rs
+++ abc367/src/bin/c.rs
@@ -11,7 +11,33 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        R: [usize; N],
     };
+
+    let mut candidates: Vec<Vec<usize>> = vec![vec![]];
+    for i in 0..N {
+        let mut new_candidates = vec![];
+        for x in 1..=R[i] {
+            for c in &candidates {
+                let mut c = c.clone();
+                c.push(x);
+                new_candidates.push(c);
+            }
+        }
+        candidates = new_candidates;
+    }
+    let ans = candidates
+        .into_iter()
+        .filter(|c| {
+            let sum = c.iter().sum::<usize>();
+            sum % K == 0
+        })
+        .sorted()
+        .collect::<Vec<_>>();
+    for v in ans {
+        println!("{}", v.iter().join(" "));
+    }
 }
 
 fn main() {
