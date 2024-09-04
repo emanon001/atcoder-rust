4ce01d17279efe31fc7d25eae50c394b7a01a505
emanon001 <emanon001@gmail.com>
2024-09-04 22:47:33 +0900

    abc368/d

--- abc368/src/bin/d.rs
+++ abc368/src/bin/d.rs
@@ -11,7 +11,34 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, K: usize,
+        AB: [(Usize1, Usize1); N - 1],
+        V: [Usize1; K],
     };
+
+    let mut graph = vec![vec![]; N];
+    for (a, b) in AB {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    let u = V[0];
+    let set = V.into_iter().collect::<HashSet<_>>();
+    let ans = dfs(u, N, &graph, &set);
+    println!("{}", ans);
+}
+
+fn dfs(u: usize, p: usize, g: &Vec<Vec<usize>>, set: &HashSet<usize>) -> usize {
+    let mut res = 0;
+    for &v in &g[u] {
+        if v == p {
+            continue;
+        }
+        res += dfs(v, u, g, set);
+    }
+    if res > 0 || set.contains(&u) {
+        res += 1;
+    }
+    res
 }
 
 fn main() {
