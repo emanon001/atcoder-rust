5349787e28611aee9fcdc2d6905359a14c500f2e
emanon001 <emanon001@gmail.com>
2024-09-12 00:09:45 +0900

    abc370/c

--- abc370/src/bin/c.rs
+++ abc370/src/bin/c.rs
@@ -11,7 +11,31 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
+        T: Chars,
     };
+    let len = S.len();
+    let mut ans = vec![];
+    let mut s = S;
+    while s != T {
+        let mut v = vec![];
+        for i in 0..len {
+            if s[i] != T[i] {
+                let mut new_s = s.clone();
+                new_s[i] = T[i];
+                v.push(new_s);
+            }
+        }
+        v.sort();
+        s = v[0].clone();
+        ans.push(s.clone());
+    }
+    println!("{}", ans.len());
+    if ans.len() > 0 {
+        for v in ans {
+            println!("{}", v.iter().join(""));
+        }
+    }
 }
 
 fn main() {
