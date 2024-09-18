cd0e3a70714e30cd4ae6920b294614b8602537de
emanon001 <emanon001@gmail.com>
2024-09-18 23:02:30 +0900

    typical90/044

--- typical90/src/bin/044.rs
+++ typical90/src/bin/044.rs
@@ -11,7 +11,37 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, Q: usize,
+        mut A: [i64; N],
+        TXY: [(usize, usize, usize); Q],
     };
+
+    let mut shift = 0;
+    for (t, x, y) in TXY {
+        match t {
+            1 => {
+                let i = pos(x - 1, shift, N);
+                let j = pos(y - 1, shift, N);
+                A.swap(i, j);
+            }
+            2 => {
+                shift = (shift + 1) % N;
+            }
+            3 => {
+                let i = pos(x - 1, shift, N);
+                println!("{}", A[i]);
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn pos(x: usize, shift: usize, n: usize) -> usize {
+    if x >= shift {
+        x - shift
+    } else {
+        n - (shift - x)
+    }
 }
 
 fn main() {
