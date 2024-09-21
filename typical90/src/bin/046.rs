47a1bbfe54c2e5118a09538d6421f0ed8059e47b
emanon001 <emanon001@gmail.com>
2024-09-21 22:58:48 +0900

    typical90/046

--- typical90/src/bin/046.rs
+++ typical90/src/bin/046.rs
@@ -11,7 +11,36 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [usize; N],
+        B: [usize; N],
+        C: [usize; N],
     };
+
+    let _mod = 46;
+
+    let mut m64_a = vec![0_u64; _mod];
+    for a in A {
+        m64_a[a % _mod] += 1;
+    }
+    let mut m64_b = vec![0_u64; _mod];
+    for b in B {
+        m64_b[b % _mod] += 1;
+    }
+    let mut m64_c = vec![0_u64; _mod];
+    for c in C {
+        m64_c[c % _mod] += 1;
+    }
+
+    let mut ans = 0;
+    for a in 0.._mod {
+        for b in 0.._mod {
+            let ab = (a + b) % _mod;
+            let c = (_mod - ab) % _mod;
+            ans += m64_a[a] * m64_b[b] * m64_c[c];
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
