ab287fa29ad07448c1316519f9352d9575a783c2
emanon001 <emanon001@gmail.com>
2024-09-23 20:57:32 +0900

    typical90/052

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -8,10 +8,35 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+const M: u64 = 1000000007;
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [[u64; 6]; N],
     };
+
+    let mut dp = vec![None; N];
+    let ans = dfs(0, &A, &mut dp);
+    println!("{}", ans);
+}
+
+fn dfs(i: usize, a: &[Vec<u64>], dp: &mut Vec<Option<u64>>) -> u64 {
+    if let Some(res) = dp[i] {
+        return res;
+    }
+    if i == a.len() - 1 {
+        let sum = a[i].iter().sum::<u64>() % M;
+        dp[i] = Some(sum);
+        return sum;
+    }
+    let mut res = 0_u64;
+    for j in 0..6 {
+        res = (res + (a[i][j] * dfs(i + 1, a, dp) % M)) % M;
+    }
+    dp[i] = Some(res);
+    res
 }
 
 fn main() {
