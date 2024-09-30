6eca83a62b635931a5cbec75a55152ac9635f289
emanon001 <emanon001@gmail.com>
2024-09-30 23:38:10 +0900

    abc372/c

--- abc372/src/bin/c.rs
+++ abc372/src/bin/c.rs
@@ -11,7 +11,36 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, Q: usize,
+        mut S: Chars,
+        queries: [(Usize1, char); Q],
     };
+
+    let mut count = 0;
+    for i in 2..N {
+        if S[i - 2] == 'A' && S[i - 1] == 'B' && S[i] == 'C' {
+            count += 1;
+        }
+    }
+    for (x, c) in queries {
+        if S[x] == c {
+            println!("{}", count);
+            continue;
+        }
+        count -= if is_abc(x, &S) { 1 } else { 0 };
+        S[x] = c;
+        count += if is_abc(x, &S) { 1 } else { 0 };
+        println!("{}", count);
+    }
+}
+
+fn is_abc(i: usize, s: &[char]) -> bool {
+    match s[i] {
+        'A' => i + 2 < s.len() && s[i + 1] == 'B' && s[i + 2] == 'C',
+        'B' => i >= 1 && i + 1 < s.len() && s[i - 1] == 'A' && s[i + 1] == 'C',
+        'C' => i >= 2 && s[i - 2] == 'A' && s[i - 1] == 'B',
+        _ => false,
+    }
 }
 
 fn main() {
