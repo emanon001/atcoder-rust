c6f8057f97ef9f97414586576361ced3bcfda477
emanon001 <emanon001@gmail.com>
2024-09-30 23:22:26 +0900

    abc372/b

--- abc372/src/bin/b.rs
+++ abc372/src/bin/b.rs
@@ -11,7 +11,22 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        mut M: isize,
     };
+
+    let mut ans = vec![];
+    while M > 0 {
+        let mut c = 0;
+        let mut m = 1;
+        while m * 3 <= M {
+            m *= 3;
+            c += 1;
+        }
+        ans.push(c);
+        M -= m;
+    }
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
