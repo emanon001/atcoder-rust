16baf5625c2cc228b30ec35f411befd46832afae
emanon001 <emanon001@gmail.com>
2024-10-12 21:24:30 +0900

    typical90/056

--- typical90/src/bin/056.rs
+++ typical90/src/bin/056.rs
@@ -11,7 +11,44 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, S: usize,
+        AB: [(usize, usize); N],
     };
+
+    let mut dp = vec![vec![vec![false; 2]; S + 1]; N + 1];
+    dp[0][0][0] = true;
+    for i in 0..N {
+        let (a, b) = AB[i];
+        for j in 0..=S {
+            for k in 0..2 {
+                if !dp[i][j][k] {
+                    continue;
+                }
+                if j + a <= S {
+                    dp[i + 1][j + a][0] = true;
+                }
+                if j + b <= S {
+                    dp[i + 1][j + b][1] = true;
+                }
+            }
+        }
+    }
+    if !dp[N][S][0] && !dp[N][S][1] {
+        println!("Impossible");
+        return;
+    }
+    let mut ans = VecDeque::new();
+    let mut s = S;
+    for i in (1..=N).rev() {
+        for k in 0..2 {
+            if dp[i][s][k] {
+                ans.push_front(if k == 0 { "A" } else { "B" });
+                s -= if k == 0 { AB[i - 1].0 } else { AB[i - 1].1 };
+                break;
+            }
+        }
+    }
+    println!("{}", ans.iter().join(""));
 }
 
 fn main() {
