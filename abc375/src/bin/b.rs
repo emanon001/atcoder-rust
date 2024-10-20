22eda41c256ac801db4281806844ebb93a1a4c0f
emanon001 <emanon001@gmail.com>
2024-10-20 22:59:34 +0900

    abc375/b

--- abc375/src/bin/b.rs
+++ abc375/src/bin/b.rs
@@ -11,7 +11,17 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        XY: [(f64, f64); N],
     };
+
+    let mut ans = 0_f64;
+    let mut point = (0_f64, 0_f64);
+    for (x, y) in XY.into_iter().chain(std::iter::once((0_f64, 0_f64))) {
+        ans += ((point.0 - x).powi(2) + (point.1 - y).powi(2)).sqrt();
+        point = (x, y);
+    }
+    println!("{}", ans);
 }
 
 fn main() {
