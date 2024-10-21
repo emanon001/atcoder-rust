5e0bebb753ee7bb2191d5d6a4aeca6f4306ec636
emanon001 <emanon001@gmail.com>
2024-10-21 23:46:12 +0900

    abc376/c

--- abc376/src/bin/c.rs
+++ abc376/src/bin/c.rs
@@ -11,7 +11,29 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        mut A: [isize; N],
+        mut B: [isize; N - 1],
     };
+
+    A.sort_by_key(|&x| std::cmp::Reverse(x));
+    B.sort_by_key(|&x| std::cmp::Reverse(x));
+    B.push(-1_isize);
+    let mut b_i = 0;
+    let mut ans = None;
+    for a in A {
+        if B[b_i] >= a {
+            b_i += 1;
+        } else if ans.is_some() {
+            // すでに箱を購入している場合は条件を達成できない
+            println!("-1");
+            return;
+        } else {
+            // 箱を購入する
+            ans = Some(a);
+        }
+    }
+    println!("{}", ans.unwrap());
 }
 
 fn main() {
