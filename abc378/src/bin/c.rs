8571060e6b13f8fba2e28ca4366a1766f49d5bbd
emanon001 <emanon001@gmail.com>
2024-11-04 00:05:14 +0900

    abc378/c

--- abc378/src/bin/c.rs
+++ abc378/src/bin/c.rs
@@ -11,7 +11,22 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        A: [usize; N],
     };
+
+    let mut prev_pos_map = HashMap::new();
+    let mut ans = vec![];
+    for (i, &a) in A.iter().enumerate() {
+        let prev_pos = if let Some(pos) = prev_pos_map.get(&a) {
+            (pos + 1) as isize
+        } else {
+            -1
+        };
+        ans.push(prev_pos);
+        prev_pos_map.insert(a, i);
+    }
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
