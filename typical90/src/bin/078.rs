df7c8996324774d6991b69fc67a62ae5aece5b43
emanon001 <emanon001@gmail.com>
2024-11-13 23:46:55 +0900

    typical90/078

--- typical90/src/bin/078.rs
+++ typical90/src/bin/078.rs
@@ -11,7 +11,21 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        edges: [(Usize1, Usize1); M],
     };
+
+    let mut graph = vec![vec![]; N];
+    for (u, v) in edges {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    let ans = (0..N)
+        .map(|u| graph[u].iter().filter(|&v| v < &u).count())
+        .filter(|&c| c == 1)
+        .count();
+    println!("{}", ans);
 }
 
 fn main() {
