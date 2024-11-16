4210c627f02d1a58ea6188f7266529f955781a7b
emanon001 <emanon001@gmail.com>
2024-11-16 23:45:09 +0900

    abc380/a

--- abc380/src/bin/a.rs
+++ abc380/src/bin/a.rs
@@ -11,7 +11,14 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: Chars,
     };
+
+    let yes = N.iter().filter(|&ch| *ch == '1').count() == 1
+        && N.iter().filter(|&ch| *ch == '2').count() == 2
+        && N.iter().filter(|&ch| *ch == '3').count() == 3;
+    let ans = if yes { "Yes" } else { "No" };
+    println!("{}", ans);
 }
 
 fn main() {
