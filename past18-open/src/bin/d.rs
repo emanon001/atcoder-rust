3d187d7c5ece1d391dccc1b5e5b6f2d00a71f435
emanon001 <emanon001@gmail.com>
2024-12-14 00:16:57 +0900

    past18-open/d

--- past18-open/src/bin/d.rs
+++ past18-open/src/bin/d.rs
@@ -11,7 +11,37 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
     };
+
+    let mut scores = vec![0_isize; N];
+    let mut last_atack_players = vec![None; N];
+    for _ in 0..M {
+        input_interactive! {
+            kind: usize,
+        };
+        match kind {
+            1 => {
+                input_interactive! {
+                    x: Usize1,
+                    y: Usize1,
+                };
+                last_atack_players[y] = Some(x);
+            }
+            2 => {
+                input_interactive! {
+                    z: Usize1,
+                };
+                scores[z] -= 1;
+                if let Some(x) = last_atack_players[z] {
+                    scores[x] += 1;
+                    last_atack_players[z] = None;
+                }
+            }
+            _ => unreachable!(),
+        };
+    }
+    println!("{}", scores.iter().join(" "));
 }
 
 fn main() {
