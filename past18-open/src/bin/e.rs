57ebd75a348d11999c321f75391d953671df536d
emanon001 <emanon001@gmail.com>
2024-12-14 21:20:14 +0900

    past18-open/e

--- past18-open/src/bin/e.rs
+++ past18-open/src/bin/e.rs
@@ -11,7 +11,31 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        S: [[usize]; N],
     };
+
+    let mut ans = 0_usize;
+    for bits in 0..1 << N {
+        let mut s_count = 0;
+        let mut counts = HashMap::new();
+        for i in 0..N {
+            if (bits >> i) & 1 == 1 {
+                s_count += 1;
+                for &a in &S[i] {
+                    *counts.entry(a).or_insert(0) += 1;
+                }
+            }
+        }
+        if s_count < 2 {
+            continue;
+        }
+        let is_ok = counts.iter().all(|(k, c)| !(k.is_even() && c == &s_count));
+        if is_ok {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
