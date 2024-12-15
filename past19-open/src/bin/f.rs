ae3ee5cd585ce37c9d5830a8d6fac0ef8a44479d
emanon001 <emanon001@gmail.com>
2024-12-15 22:59:40 +0900

    past19-open/f

--- past19-open/src/bin/f.rs
+++ past19-open/src/bin/f.rs
@@ -11,7 +11,33 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        X: String, Y: String,
+        ST: [(String, String); N],
     };
+
+    let mut relations = HashMap::new();
+    for (s, t) in ST {
+        relations.entry(s).or_insert(Vec::new()).push(t);
+    }
+    let mut visited = HashSet::new();
+    visited.insert(X.clone());
+    let mut que = VecDeque::new();
+    que.push_back(X.clone());
+    while let Some(x) = que.pop_front() {
+        for y in relations.get(&x).unwrap_or(&Vec::new()) {
+            if visited.contains(y) {
+                continue;
+            }
+            visited.insert(y.clone());
+            if y == &Y {
+                println!("Yes");
+                return;
+            }
+            que.push_back(y.clone());
+        }
+    }
+    println!("No");
 }
 
 fn main() {
