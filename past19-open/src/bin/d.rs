c89dd6b4431f6ae4c06237cf8b78fb15e3bb403a
emanon001 <emanon001@gmail.com>
2024-12-15 22:25:36 +0900

    past19-open/d

--- past19-open/src/bin/d.rs
+++ past19-open/src/bin/d.rs
@@ -11,7 +11,19 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, T: isize,
+        AB: [(isize, isize); N],
     };
+
+    let (ta, tb) = AB
+        .iter()
+        .cloned()
+        .sorted_by_key(|(a, b)| (std::cmp::Reverse(*a), *b))
+        .collect::<Vec<_>>()[0];
+    for (a, b) in AB {
+        let ans = T * (ta - a) + (b - tb);
+        println!("{}", ans);
+    }
 }
 
 fn main() {
