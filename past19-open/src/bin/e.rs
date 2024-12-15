7d77db1c1445c499b78b4b55d46b7d4d2a57d5f6
emanon001 <emanon001@gmail.com>
2024-12-15 22:43:21 +0900

    past19-open/e

--- past19-open/src/bin/e.rs
+++ past19-open/src/bin/e.rs
@@ -11,7 +11,43 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        S: Chars,
     };
+    let mut i = 0;
+    let mut ans = Vec::new();
+    while i < N {
+        match S[i] {
+            '/' => {
+                if i + 1 < N && S[i + 1] == '*' {
+                    // start comment
+                    let mut end_found = false;
+                    for j in i + 2..N - 1 {
+                        if S[j] == '*' && S[j + 1] == '/' {
+                            // end comment
+                            end_found = true;
+                            i = j + 2;
+                            break;
+                        }
+                    }
+                    if !end_found {
+                        for j in i..N {
+                            ans.push(S[j]);
+                        }
+                        i = N;
+                    }
+                } else {
+                    ans.push('/');
+                    i += 1;
+                }
+            }
+            ch => {
+                ans.push(ch);
+                i += 1;
+            }
+        };
+    }
+    println!("{}", ans.iter().join(""));
 }
 
 fn main() {
