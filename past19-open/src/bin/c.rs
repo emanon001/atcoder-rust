d429a549f91581cf026d5ff4db7f793508d2ce90
emanon001 <emanon001@gmail.com>
2024-12-15 22:18:51 +0900

    past19-open/c

--- past19-open/src/bin/c.rs
+++ past19-open/src/bin/c.rs
@@ -11,7 +11,31 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: Chars,
     };
+
+    let N = N
+        .into_iter()
+        .map(|c| c.to_digit(10).unwrap() as isize)
+        .collect::<Vec<_>>();
+    for p in 0..N.len() {
+        for q in 0..=9 {
+            if p == 0 && q == 0 {
+                continue;
+            }
+            let mut digits = N.clone();
+            digits[p] = q;
+            let is_ok = digits
+                .into_iter()
+                .tuple_windows()
+                .all(|(a, b)| (a - b).abs() <= 1);
+            if is_ok {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
 
 fn main() {
