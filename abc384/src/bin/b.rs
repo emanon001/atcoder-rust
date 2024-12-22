e01637f2c2d89338810aa6ddcb3a634c54b660e4
emanon001 <emanon001@gmail.com>
2024-12-22 23:34:41 +0900

    abc384/b

--- abc384/src/bin/b.rs
+++ abc384/src/bin/b.rs
@@ -11,7 +11,27 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, R: isize,
+        DA: [(usize, isize); N],
     };
+
+    let mut ans = R;
+    for (d, a) in DA {
+        match d {
+            1 => {
+                if (1600..=2799).contains(&ans) {
+                    ans += a;
+                }
+            }
+            2 => {
+                if (1200..=2399).contains(&ans) {
+                    ans += a;
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
