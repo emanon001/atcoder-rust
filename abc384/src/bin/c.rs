94ba836577a08f48d79bae7941fc22714db55b8c
emanon001 <emanon001@gmail.com>
2024-12-22 23:42:51 +0900

    abc384/c

--- abc384/src/bin/c.rs
+++ abc384/src/bin/c.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        ABCDE: [u64; 5],
     };
+
+    let mut ans = Vec::new();
+    for bits in 1..(1 << ABCDE.len()) {
+        let mut name = Vec::new();
+        let mut score = 0;
+        for i in 0..ABCDE.len() {
+            if (bits >> i) & 1 == 1 {
+                name.push(('A' as u8 + i as u8) as char);
+                score += ABCDE[i];
+            }
+        }
+        ans.push((score, name.into_iter().join("")));
+    }
+    ans.sort_by_key(|(s, n)| (std::cmp::Reverse(*s), n.clone()));
+    println!("{}", ans.into_iter().map(|(_, n)| n).join("\n"));
 }
 
 fn main() {
