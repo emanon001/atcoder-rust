2517e19bb8dea84a5f2e536ef6a46d625de80ec2
emanon001 <emanon001@gmail.com>
2024-12-25 00:17:14 +0900

    abc385/c

--- abc385/src/bin/c.rs
+++ abc385/src/bin/c.rs
@@ -8,10 +8,43 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        H: [usize; N],
     };
+
+    let mut ans = 1;
+    for i in 0..N {
+        let h = H[i];
+        for j in 1..N {
+            let mut i = i;
+            let mut count = 1;
+            while i + j < N {
+                if h != H[i + j] {
+                    break;
+                }
+                i += j;
+                count += 1;
+            }
+            chmax!(ans, count);
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
