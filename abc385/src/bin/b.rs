c6e652e545b7dc9fd7ca328582c420662ad8fb4a
emanon001 <emanon001@gmail.com>
2024-12-24 23:56:23 +0900

    abc385/b

--- abc385/src/bin/b.rs
+++ abc385/src/bin/b.rs
@@ -11,7 +11,37 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        H: usize, W: usize, X: Usize1, Y: Usize1,
+        S: [Chars; H],
+        T: Chars,
     };
+
+    let mut house_set = HashSet::new();
+    let mut pos = (X, Y);
+    for t in T {
+        let x = pos.0 as isize;
+        let y = pos.1 as isize;
+        let (new_x, new_y) = match t {
+            'U' => (x - 1, y),
+            'D' => (x + 1, y),
+            'L' => (x, y - 1),
+            'R' => (x, y + 1),
+            _ => unreachable!(),
+        };
+        if new_x < 0 || new_x >= H as isize || new_y < 0 || new_y >= W as isize {
+            continue;
+        }
+        let x = new_x as usize;
+        let y = new_y as usize;
+        if S[x][y] == '#' {
+            continue;
+        }
+        if S[x][y] == '@' {
+            house_set.insert((x, y));
+        }
+        pos = (x, y);
+    }
+    println!("{} {} {}", pos.0 + 1, pos.1 + 1, house_set.len());
 }
 
 fn main() {
