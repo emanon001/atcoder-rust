8c7c67eb70b6471d4c079dc0bf848df9496669db
emanon001 <emanon001@gmail.com>
2025-01-12 22:34:15 +0900

    abc386/a

--- abc386/src/bin/a.rs
+++ abc386/src/bin/a.rs
@@ -11,7 +11,27 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: usize, B: usize, C: usize, D: usize,
     };
+
+    for e in 1..=13 {
+        let cards = vec![A, B, C, D, e];
+        if is_full_house(&cards) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
+}
+
+fn is_full_house(cards: &[usize]) -> bool {
+    assert!(cards.len() == 5);
+    let counts = cards.into_iter().counts();
+    if counts.len() != 2 {
+        return false;
+    }
+    let c = *counts.iter().next().unwrap().1;
+    c == 2 || c == 3
 }
 
 fn main() {
