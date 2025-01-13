1fc599a6b1d7542a6738af1db878800303876ea5
emanon001 <emanon001@gmail.com>
2025-01-13 22:43:24 +0900

    abc387/a

--- abc387/src/bin/a.rs
+++ abc387/src/bin/a.rs
@@ -11,7 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: u64, B: u64,
     };
+
+    let ans = (A + B).pow(2);
+    println!("{}", ans);
 }
 
 fn main() {
