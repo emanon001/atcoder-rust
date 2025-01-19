55842111346bbc305827853a0c42a2de3a32f166
emanon001 <emanon001@gmail.com>
2025-01-19 21:28:21 +0900

    abc389/c

--- abc389/src/bin/c.rs
+++ abc389/src/bin/c.rs
@@ -11,7 +11,39 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        Q: usize,
     };
+
+    let mut snake_list = VecDeque::new();
+    let mut l_sum = 0;
+    let mut offset = 0;
+    for _ in 0..Q {
+        input_interactive! {
+            kind: usize,
+        };
+        match kind {
+            1 => {
+                input_interactive! {
+                    l: usize,
+                };
+                snake_list.push_back((l_sum, l));
+                l_sum += l;
+            }
+            2 => {
+                let (_, l) = snake_list.pop_front().unwrap();
+                offset += l;
+            }
+            3 => {
+                input_interactive! {
+                    k: Usize1,
+                };
+                let (pos, _) = snake_list[k];
+                let ans = pos - offset;
+                println!("{}", ans);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
