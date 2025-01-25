e846da3eaf64736c1176f03ddd8670e33c94ab2a
emanon001 <emanon001@gmail.com>
2025-01-25 23:39:07 +0900

    abc390/a

--- abc390/src/bin/a.rs
+++ abc390/src/bin/a.rs
@@ -11,7 +11,19 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: [usize; 5],
     };
+
+    let expected = vec![1, 2, 3, 4, 5];
+    for i in 0..4 {
+        let mut a = A.clone();
+        a.swap(i, i + 1);
+        if a == expected {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
 
 fn main() {
