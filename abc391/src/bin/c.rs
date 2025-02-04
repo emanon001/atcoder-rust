12eebe014716fca4192c4a79f4252294d04667c8
emanon001 <emanon001@gmail.com>
2025-02-04 22:29:55 +0900

    abc391/c

--- abc391/src/bin/c.rs
+++ abc391/src/bin/c.rs
@@ -11,7 +11,39 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, Q: usize,
     };
+
+    let mut hole_to_p_count = vec![1; N];
+    let mut p_to_hole = (0..N).collect::<Vec<_>>();
+    let mut count = 0;
+    for _ in 0..Q {
+        input_interactive! {
+            kind: usize,
+        };
+        match kind {
+            1 => {
+                input_interactive! {
+                    p: Usize1, h: Usize1,
+                };
+
+                let cur_h = p_to_hole[p];
+                p_to_hole[p] = h;
+                hole_to_p_count[cur_h] -= 1;
+                if hole_to_p_count[cur_h] == 1 {
+                    count -= 1;
+                }
+                hole_to_p_count[h] += 1;
+                if hole_to_p_count[h] == 2 {
+                    count += 1;
+                }
+            }
+            2 => {
+                println!("{}", count);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
