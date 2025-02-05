41bb40e9a4e610ce7509ec4830da0e13e51b62cb
emanon001 <emanon001@gmail.com>
2025-02-05 22:54:44 +0900

    abc379/d

--- abc379/src/bin/d.rs
+++ abc379/src/bin/d.rs
@@ -8,10 +8,120 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<i128> for i128 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<isize> for isize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self - *ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        Q: usize,
     };
+
+    let mut v = VecDeque::<i64>::new();
+    let mut time = 0;
+    for _ in 0..Q {
+        input_interactive! {
+            kind: usize
+        };
+        match kind {
+            1 => {
+                v.push_back(time);
+            }
+            2 => {
+                input_interactive! {
+                    t: i64
+                };
+                time += t;
+            }
+            3 => {
+                input_interactive! {
+                    h: i64
+                };
+                let i = bsearch(-1, v.len() as i64, |x| {
+                    let x = x as usize;
+                    time - v[x] >= h
+                });
+                let ans = if let Some(i) = i {
+                    for _ in 0..=i {
+                        v.pop_front();
+                    }
+                    i + 1
+                } else {
+                    0
+                };
+                println!("{}", ans);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
 fn main() {
