6d91898c3d5280f43c4f64b15f2f33bd869ea778
emanon001 <emanon001@gmail.com>
2025-02-07 07:26:43 +0900

    abc377/d

--- abc377/src/bin/d.rs
+++ abc377/src/bin/d.rs
@@ -8,10 +8,198 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub trait BinarySearchOk<T>: PartialEq + Copy {
+    fn bs_needs_next_search(&self, ng: &T) -> bool;
+    fn bs_mid_value(&self, ng: &T) -> Self;
+    fn bs_into(&self) -> T;
+}
+impl BinarySearchOk<i64> for i64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<i128> for i128 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<f64> for f64 {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (self - ng).abs() > 1.0
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2.0
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+impl BinarySearchOk<isize> for isize {
+    fn bs_needs_next_search(&self, ng: &Self) -> bool {
+        (*self - *ng).abs() > 1
+    }
+    fn bs_mid_value(&self, ng: &Self) -> Self {
+        (self + ng) / 2
+    }
+    fn bs_into(&self) -> Self {
+        *self
+    }
+}
+pub fn bsearch<T, Num: BinarySearchOk<T>, F>(ok: Num, ng: T, pred: F) -> Option<Num>
+where
+    F: Fn(T) -> bool,
+{
+    let orig_ok = ok;
+    let mut ok = ok;
+    let mut ng = ng;
+    while ok.bs_needs_next_search(&ng) {
+        let mid = ok.bs_mid_value(&ng);
+        if pred(mid.bs_into()) {
+            ok = mid;
+        } else {
+            ng = mid.bs_into();
+        }
+    }
+    if ok == orig_ok {
+        None
+    } else {
+        Some(ok)
+    }
+}
+
+pub trait Monoid {
+    fn mempty() -> Self;
+    fn mappend(&self, other: &Self) -> Self;
+}
+#[derive(Clone, Debug)]
+pub struct SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    size: usize,
+    data: Vec<T>,
+}
+impl<T> SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    pub fn new(size: usize) -> Self {
+        let size = Self::normalize_data_size(size);
+        let data = vec![T::mempty(); size * 2 - 1];
+        Self { size, data }
+    }
+    /// 0-origin
+    pub fn update(&mut self, i: usize, v: T) {
+        let mut i = i + self.size - 1;
+        self.data[i] = v;
+        while i > 0 {
+            i = (i - 1) / 2;
+            self.data[i] = self.data[i * 2 + 1].mappend(&self.data[i * 2 + 2]);
+        }
+    }
+    /// 0-origin
+    pub fn get(&self, a: usize) -> T {
+        self.query(a, a + 1)
+    }
+    /// [a, b)
+    /// 0-origin
+    pub fn query(&self, a: usize, b: usize) -> T {
+        self.execute_query(a, b, 0, 0, self.size)
+    }
+    fn normalize_data_size(size: usize) -> usize {
+        let mut n = 1;
+        while n < size {
+            n *= 2;
+        }
+        n
+    }
+    fn execute_query(&self, a: usize, b: usize, i: usize, l: usize, r: usize) -> T {
+        if r <= a || b <= l {
+            return T::mempty();
+        }
+        if a <= l && r <= b {
+            return self.data[i].clone();
+        }
+        let vl = self.execute_query(a, b, i * 2 + 1, l, (l + r) / 2);
+        let vr = self.execute_query(a, b, i * 2 + 2, (l + r) / 2, r);
+        vl.mappend(&vr)
+    }
+}
+impl<T> From<&[T]> for SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    fn from(values: &[T]) -> Self {
+        let mut st = SegmentTree::new(values.len());
+        for (i, v) in values.into_iter().enumerate() {
+            st.data[i + st.size - 1] = v.clone();
+        }
+        if st.size < 2 {
+            return st;
+        }
+        for i in (0..=(st.size - 2)).rev() {
+            st.data[i] = st.data[i * 2 + 1].mappend(&st.data[i * 2 + 2]);
+        }
+        st
+    }
+}
+impl<T> From<Vec<T>> for SegmentTree<T>
+where
+    T: Monoid + Clone,
+{
+    fn from(values: Vec<T>) -> Self {
+        let values: &[T] = &values;
+        Self::from(values)
+    }
+}
+
+impl Monoid for usize {
+    fn mempty() -> Self {
+        usize::MAX
+    }
+    fn mappend(&self, other: &Self) -> Self {
+        *self.min(other)
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        mut LR: [(Usize1, Usize1); N],
     };
+
+    LR.sort_by_key(|(l, _)| *l);
+    let st = SegmentTree::from(LR.iter().map(|(_, r)| *r).collect_vec());
+    let mut ans = 0;
+    for l in 0..M {
+        let i = bsearch(N as isize, -1, |x| {
+            let x = x as usize;
+            LR[x].0 >= l
+        });
+        match i {
+            Some(i) => {
+                let r = st.query(i as usize, N);
+                ans += r - l;
+            }
+            None => {
+                ans += M - l;
+            }
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
