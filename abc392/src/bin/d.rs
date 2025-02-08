8ec555a4961d79624e16764b56e7380f22cf3e37
emanon001 <emanon001@gmail.com>
2025-02-09 00:21:48 +0900

    abc392/d

--- abc392/src/bin/d.rs
+++ abc392/src/bin/d.rs
@@ -8,10 +8,52 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        K: [[usize]; N],
     };
+
+    let mut counts = vec![vec![0; 10.pow(5) + 1]; N];
+    for i in 0..N {
+        for &k in &K[i] {
+            counts[i][k] += 1;
+        }
+    }
+
+    let mut ans: num::rational::Ratio<usize> = num::rational::Ratio::new(0, 1);
+    for i in 0..N {
+        for j in i + 1..N {
+            let all_count = K[i].len() * K[j].len();
+            let mut count = 0;
+            if K[i].len() <= K[j].len() {
+                for &k in &K[i] {
+                    count += counts[j][k];
+                }
+            } else {
+                for &k in &K[j] {
+                    count += counts[i][k];
+                }
+            }
+            let rational = num::rational::Ratio::new(count, all_count);
+            chmax!(ans, rational);
+        }
+    }
+    println!("{:.8}", ans.to_f64().unwrap());
 }
 
 fn main() {
