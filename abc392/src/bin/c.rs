6b2edb417373322b3e77aa3677ae5bd2f4b2389f
emanon001 <emanon001@gmail.com>
2025-02-08 23:38:56 +0900

    abc392/c

--- abc392/src/bin/c.rs
+++ abc392/src/bin/c.rs
@@ -11,7 +11,23 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        P: [Usize1; N],
+        Q: [Usize1; N],
     };
+
+    let q_to_i = Q
+        .iter()
+        .enumerate()
+        .map(|(i, q)| (*q, i))
+        .collect::<HashMap<_, _>>();
+    let ans = (0..N)
+        .map(|q| {
+            let i = q_to_i[&q];
+            Q[P[i]] + 1
+        })
+        .join(" ");
+    println!("{}", ans);
 }
 
 fn main() {
