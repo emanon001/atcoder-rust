6773dad40a92d858369d68332bac3c7bef17554b
emanon001 <emanon001@gmail.com>
2025-02-13 07:27:44 +0900

    abc379/c

--- abc379/src/bin/c.rs
+++ abc379/src/bin/c.rs
@@ -11,7 +11,31 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        X: [i128; M],
+        mut A: [i128; M],
     };
+
+    let sum = A.iter().sum::<i128>();
+    if sum != N as i128 {
+        println!("-1");
+        return;
+    }
+
+    let mut xa = X.into_iter().zip(A.into_iter()).collect::<Vec<_>>();
+    xa.sort_by_key(|&(x, _)| x);
+    let mut ans = 0;
+    let mut j = N as i128;
+    for (i, a) in xa.into_iter().rev() {
+        let count = (j - i + 1).min(a);
+        ans += ((j - i + 1) * (j - i) / 2) - ((j - i - count + 1) * (j - i - count) / 2);
+        j -= count;
+    }
+    if j > 0 {
+        println!("-1");
+        return;
+    }
+    println!("{}", ans);
 }
 
 fn main() {
