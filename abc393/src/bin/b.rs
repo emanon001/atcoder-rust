8e4c6e62d3d1748bba5257bb1124ed1042a249b5
emanon001 <emanon001@gmail.com>
2025-02-15 23:13:08 +0900

    abc393/b

--- abc393/src/bin/b.rs
+++ abc393/src/bin/b.rs
@@ -11,7 +11,20 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
     };
+
+    let mut ans = 0;
+    for i in 0..S.len() {
+        for j in i + 1..S.len() {
+            for k in j + 1..S.len() {
+                if j - i == k - j && S[i] == 'A' && S[j] == 'B' && S[k] == 'C' {
+                    ans += 1;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
