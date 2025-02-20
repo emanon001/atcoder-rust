c6f70ad039162904034d0735471de3588fc59bda
emanon001 <emanon001@gmail.com>
2025-02-20 16:30:16 +0900

    past19-open/h

--- past19-open/src/bin/h.rs
+++ past19-open/src/bin/h.rs
@@ -11,7 +11,44 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, S: u128,
+        A: [u128; N],
     };
+
+    for a_perm in A.into_iter().permutations(N) {
+        for bits in 0..(1 << (N - 1)) {
+            let mut s = Vec::new();
+            for i in 0..N - 1 {
+                let op = if (bits >> i) & 1 == 0 { "+" } else { "x" }.to_owned();
+                s.push(op);
+                s.push(a_perm[i + 1].to_string());
+            }
+            if eval(a_perm[0] as u128, &s) == S {
+                println!("Yes");
+                println!("{}{}", a_perm[0], s.iter().join(""));
+                return;
+            }
+        }
+    }
+    println!("No");
+}
+
+fn eval(l: u128, rest: &[String]) -> u128 {
+    // eprintln!("l: {}, rest: {:?}", l, rest);
+    if rest.is_empty() {
+        return l;
+    }
+    match rest[0].as_str() {
+        "x" => {
+            let r = rest[1].parse::<u128>().unwrap();
+            eval(l * r, &rest[2..])
+        }
+        "+" => {
+            let r = rest[1].parse::<u128>().unwrap();
+            l + eval(r, &rest[2..])
+        }
+        _ => unreachable!(),
+    }
 }
 
 fn main() {
