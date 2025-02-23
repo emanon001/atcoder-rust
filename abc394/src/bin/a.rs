9e2e0da31dadf4e247a8e5a8ef4d3623054ad221
emanon001 <emanon001@gmail.com>
2025-02-23 22:35:41 +0900

    abc394/a

--- abc394/src/bin/a.rs
+++ abc394/src/bin/a.rs
@@ -11,7 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
     };
+
+    let ans = S.into_iter().filter(|c| c == &'2').join("");
+    println!("{}", ans);
 }
 
 fn main() {
