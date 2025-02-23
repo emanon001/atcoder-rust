91273ab455649fce0086c7fbc8f7334186ef17c6
emanon001 <emanon001@gmail.com>
2025-02-23 22:35:43 +0900

    abc394/c

--- abc394/src/bin/c.rs
+++ abc394/src/bin/c.rs
@@ -5,13 +5,25 @@ use num::*;
 use proconio::input_interactive;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use regex::Regex;
 #[allow(unused_imports)]
 use std::collections::*;
 
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: String,
     };
+
+    // W+A を AC+ に変換
+    let re = Regex::new(r"W+A").unwrap();
+    let ans = re
+        .replace_all(&S, |caps: &regex::Captures| {
+            let len = caps[0].len();
+            format!("A{}", "C".repeat(len - 1))
+        })
+        .to_string();
+    println!("{}", ans);
 }
 
 fn main() {
