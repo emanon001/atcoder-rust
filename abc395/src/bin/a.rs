f20136d95e91f248ec493988a9f3c81a3c4f3d3d
emanon001 <emanon001@gmail.com>
2025-03-03 23:53:47 +0900

    abc395/a

---
+++ abc395/src/bin/a.rs
@@ -0,0 +1,33 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::input_interactive;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+#[allow(non_snake_case)]
+fn solve() {
+    input_interactive! {
+        N: usize,
+        A: [usize; N],
+    };
+
+    let is_ok = A.into_iter().tuple_windows().all(|(a, b)| a < b);
+    let ans = if is_ok { "Yes" } else { "No" };
+    println!("{}", ans);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("big stack size".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(|| {
+            solve();
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+}
