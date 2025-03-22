08b0760e1e2a4b5fa98857e084f6e6828bfd099a
emanon001 <emanon001@gmail.com>
2025-03-22 23:51:08 +0900

    abc398/b

--- abc398/src/bin/b.rs
+++ abc398/src/bin/b.rs
@@ -11,7 +11,18 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        A: [Usize1; 7]
     };
+
+    for comb in A.into_iter().combinations(5) {
+        let counts = comb.iter().counts();
+        if counts.len() == 2 && counts.values().any(|&c| c == 2 || c == 3) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
 
 fn main() {
