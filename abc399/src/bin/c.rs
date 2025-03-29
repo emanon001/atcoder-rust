3741f5a21fe1b4797b71669d24cb2f4736342142
emanon001 <emanon001@gmail.com>
2025-03-30 00:05:02 +0900

    abc399/c

--- abc399/src/bin/c.rs
+++ abc399/src/bin/c.rs
@@ -8,10 +8,98 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub struct UnionFind {
+    n: usize,
+    root: Vec<usize>,
+    rank: Vec<usize>,
+    size: Vec<usize>,
+}
+impl UnionFind {
+    pub fn new(n: usize) -> Self {
+        let root = (0..n).collect();
+        let rank = vec![0; n];
+        let size = vec![1; n];
+        Self {
+            n,
+            root,
+            rank,
+            size,
+        }
+    }
+    pub fn find(&mut self, x: usize) -> usize {
+        assert!(x < self.n);
+        if self.root[x] == x {
+            x
+        } else {
+            let root = self.find(self.root[x]);
+            self.root[x] = root;
+            root
+        }
+    }
+    pub fn unite(&mut self, x: usize, y: usize) {
+        assert!(x < self.n && y < self.n);
+        let x_root = self.find(x);
+        let y_root = self.find(y);
+        if x_root == y_root {
+            return;
+        }
+        if self.rank[x_root] < self.rank[y_root] {
+            self.root[x_root] = y_root;
+            self.size[y_root] += self.size[x_root];
+        } else {
+            self.root[y_root] = x_root;
+            self.size[x_root] += self.size[y_root];
+            if self.rank[x_root] == self.rank[y_root] {
+                self.rank[x_root] += 1;
+            }
+        }
+    }
+    pub fn size(&mut self, x: usize) -> usize {
+        assert!(x < self.n);
+        let x_root = self.find(x);
+        self.size[x_root]
+    }
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        assert!(x < self.n && y < self.n);
+        self.find(x) == self.find(y)
+    }
+    pub fn groups(&mut self) -> Vec<Vec<usize>> {
+        let mut groups = std::collections::HashMap::new();
+        for x in 0..self.n {
+            let k = self.find(x);
+            groups.entry(k).or_insert(Vec::new()).push(x);
+        }
+        groups.values().cloned().collect::<Vec<_>>()
+    }
+}
+
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        edges: [(Usize1, Usize1); M],
     };
+
+    let mut edge_counts = vec![0; N];
+    for &(a, _) in &edges {
+        edge_counts[a] += 1;
+    }
+
+    let mut uf = UnionFind::new(N);
+    for (a, b) in edges {
+        uf.unite(a, b);
+    }
+
+    let mut ans = 0;
+    for group in uf.groups() {
+        let count = group.len();
+        let edge_count = group.iter().map(|&x| edge_counts[x]).sum::<usize>();
+        if edge_count + 1 < count {
+            continue;
+        }
+        ans += edge_count + 1 - count;
+    }
+    println!("{}", ans);
 }
 
 fn main() {
