27179320b7f9b6a918bdf60eb90b5baf22c39cf9
emanon001 <emanon001@gmail.com>
2025-03-29 23:55:25 +0900

    abc399/b

--- abc399/src/bin/b.rs
+++ abc399/src/bin/b.rs
@@ -11,7 +11,25 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize,
+        P: [usize; N],
     };
+
+    let mut map = BTreeMap::new();
+    for (i, p) in P.into_iter().enumerate() {
+        map.entry(p).or_insert(Vec::new()).push(i);
+    }
+
+    let mut ans = vec![0; N];
+    let mut rank = 1;
+    for (_, users) in map.into_iter().rev() {
+        let count = users.len();
+        for user in users {
+            ans[user] = rank;
+        }
+        rank += count;
+    }
+    println!("{}", ans.iter().join(" "));
 }
 
 fn main() {
