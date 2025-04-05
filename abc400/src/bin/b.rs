14c8ef6f0b63db97006619da495b7b3aae57bf38
emanon001 <emanon001@gmail.com>
2025-04-05 23:52:52 +0900

    abc400/b

--- abc400/src/bin/b.rs
+++ abc400/src/bin/b.rs
@@ -11,7 +11,26 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: u128, M: u32,
     };
+
+    let mut ans = 0_u128;
+    for i in 0..=M {
+        match (N as u32).checked_pow(i) {
+            Some(x) => {
+                ans += x as u128;
+                if ans > 1_000_000_000 {
+                    println!("inf");
+                    return;
+                }
+            }
+            None => {
+                println!("inf");
+                return;
+            }
+        }
+    }
+    println!("{}", ans);
 }
 
 fn main() {
