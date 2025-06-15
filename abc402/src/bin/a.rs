0e9e675096b22e63d879af6d554ac314d375b7a5
emanon001 <emanon001@gmail.com>
2025-06-15 23:29:34 +0900

    abc402/a

--- abc402/src/bin/a.rs
+++ abc402/src/bin/a.rs
@@ -11,7 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        S: Chars,
     };
+
+    let ans = S.into_iter().filter(|&c| c.is_uppercase()).join("");
+    println!("{}", ans);
 }
 
 fn main() {
