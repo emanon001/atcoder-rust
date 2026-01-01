b29d3001a6e4e78b062eec5fadb801e2b764eb39
emanon001 <emanon001@gmail.com>
2026-01-02 00:59:31 +0900

    abc403/a

--- abc403/src/bin/a.rs
+++ abc403/src/bin/a.rs
@@ -11,5 +11,10 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize,
+        A: [i64; N],
     };
+
+    let ans = A.into_iter().step_by(2).sum::<i64>();
+    println!("{}", ans);
 }
