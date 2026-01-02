55173547abb41fed8640f21bc64fd7137e4206f2
emanon001 <emanon001@gmail.com>
2026-01-02 23:46:12 +0900

    abc403/c

--- abc403/src/bin/c.rs
+++ abc403/src/bin/c.rs
@@ -11,5 +11,43 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize, M: usize, Q: usize,
     };
+
+    let all_contest_page = M;
+    let mut can_access = vec![HashSet::new(); N];
+    for _ in 0..Q {
+        input_interactive! {
+            kind: usize,
+        };
+        match kind {
+            1 => {
+                input_interactive! {
+                    x: Usize1,
+                    y: Usize1,
+                };
+                can_access[x].insert(y);
+            }
+            2 => {
+                input_interactive! {
+                    x: Usize1,
+                };
+                can_access[x].insert(all_contest_page);
+            }
+            3 => {
+                input_interactive! {
+                    x: Usize1,
+                    y: Usize1,
+                };
+                let ans = if can_access[x].contains(&all_contest_page) || can_access[x].contains(&y)
+                {
+                    "Yes"
+                } else {
+                    "No"
+                };
+                println!("{}", ans);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
