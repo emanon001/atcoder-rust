e5c3ebf36c61ad9ee4575e824e63533ff2329127
emanon001 <emanon001@gmail.com>
2026-01-03 22:48:03 +0900

    abc405/a

--- abc405/src/bin/a.rs
+++ abc405/src/bin/a.rs
@@ -11,5 +11,14 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        R: i64, X: i64,
     };
+
+    let is_rated = if X == 1 {
+        (1600..=2999).contains(&R)
+    } else {
+        (1200..=2399).contains(&R)
+    };
+    let ans = if is_rated { "Yes" } else { "No" };
+    println!("{}", ans);
 }
