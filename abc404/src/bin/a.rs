bd5c0efe86e0c4cc97a8b613ddf4b6425f4b2535
emanon001 <emanon001@gmail.com>
2026-01-03 22:28:32 +0900

    abc404/a

--- abc404/src/bin/a.rs
+++ abc404/src/bin/a.rs
@@ -8,8 +8,26 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+pub fn make_ascii_lowercase_chars() -> Vec<char> {
+    let base = 0x61;
+    make_chars(base..base + 26)
+}
+fn make_chars(range: std::ops::Range<u8>) -> Vec<char> {
+    range.into_iter().map(char::from).collect::<Vec<_>>()
+}
+
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        S: Chars,
     };
+
+    let mut set = make_ascii_lowercase_chars()
+        .into_iter()
+        .collect::<HashSet<_>>();
+    for ch in S {
+        set.remove(&ch);
+    }
+    let ans = set.into_iter().next().unwrap();
+    println!("{}", ans);
 }
