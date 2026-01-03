bc14f51fadbbfc3eb80d18d11452aaba01b2ea4a
emanon001 <emanon001@gmail.com>
2026-01-04 00:04:54 +0900

    abc402/c

--- abc402/src/bin/c.rs
+++ abc402/src/bin/c.rs
@@ -11,7 +11,33 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn solve() {
     input_interactive! {
+        N: usize, M: usize,
+        K: [[Usize1]; M],
+        B: [Usize1; N]
     };
+
+    // 食材を使用している料理のインデックス
+    let mut food_indexies = vec![HashSet::new(); N];
+    for i in 0..M {
+        for &k in &K[i] {
+            food_indexies[k].insert(i);
+        }
+    }
+
+    let mut count = M;
+    let mut ans = VecDeque::new();
+    for b in B.into_iter().rev() {
+        ans.push_front(count);
+        let indexies = food_indexies[b].clone();
+        let c = indexies.len();
+        count -= c;
+        for i in indexies {
+            for &k in &K[i] {
+                food_indexies[k].remove(&i);
+            }
+        }
+    }
+    println!("{}", ans.into_iter().join("\n"));
 }
 
 fn main() {
