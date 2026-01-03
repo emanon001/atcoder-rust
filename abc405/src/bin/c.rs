d4dd796fd7d6a8d386dd35b2f822e5a49697c3ce
emanon001 <emanon001@gmail.com>
2026-01-03 23:06:57 +0900

    abc405/c

--- abc405/src/bin/c.rs
+++ abc405/src/bin/c.rs
@@ -11,5 +11,17 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize,
+        A: [u64; N],
     };
+
+    let mut cusum = vec![0; N + 1];
+    for i in 0..N {
+        cusum[i + 1] = cusum[i] + A[i]
+    }
+    let mut ans = 0_u64;
+    for i in 0..N {
+        ans += A[i] * (cusum[N] - cusum[i + 1])
+    }
+    println!("{}", ans);
 }
