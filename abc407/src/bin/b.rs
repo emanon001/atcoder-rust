d465a16668fc173c7914a5e10d959c179c9d4426
emanon001 <emanon001@gmail.com>
2026-01-05 00:06:00 +0900

    abc407/b

--- abc407/src/bin/b.rs
+++ abc407/src/bin/b.rs
@@ -11,5 +11,17 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        X: i64, Y: i64,
     };
+
+    let mut count = 0;
+    for a in 1..=6 {
+        for b in 1..=6 {
+            if a + b >= X || (a - b).abs() >= Y {
+                count += 1;
+            }
+        }
+    }
+    let ans = count as f64 / 36.0;
+    println!("{}", ans);
 }
