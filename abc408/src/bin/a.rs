3ac9b5cb827290ff3aad6c2b1e9c286de49c09d8
emanon001 <emanon001@gmail.com>
2026-01-06 23:48:00 +0900

    abc408/a

--- abc408/src/bin/a.rs
+++ abc408/src/bin/a.rs
@@ -11,5 +11,15 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize, S: usize,
+        T: [usize; N]
     };
+
+    let is_ok = vec![0]
+        .into_iter()
+        .chain(T.into_iter())
+        .tuple_windows()
+        .all(|(a, b)| b - a <= S);
+    let ans = if is_ok { "Yes" } else { "No" };
+    println!("{}", ans);
 }
