8cb3d5246a372699e3bc8b813ebedd0a7a0da365
emanon001 <emanon001@gmail.com>
2026-01-06 23:53:51 +0900

    abc408/c

--- abc408/src/bin/c.rs
+++ abc408/src/bin/c.rs
@@ -11,5 +11,18 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize, M: usize,
+        LR: [(Usize1, Usize1); M],
     };
+
+    let mut imos = vec![0; N + 1];
+    for (l, r) in LR {
+        imos[l] += 1;
+        imos[r + 1] -= 1;
+    }
+    for i in 0..N {
+        imos[i + 1] += imos[i];
+    }
+    let ans = imos.iter().dropping_back(1).min().unwrap();
+    println!("{}", ans);
 }
