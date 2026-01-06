c52e89ba573462031947dfe615dd07d22c7b2628
emanon001 <emanon001@gmail.com>
2026-01-07 07:27:50 +0900

    abc407/c

--- abc407/src/bin/c.rs
+++ abc407/src/bin/c.rs
@@ -11,5 +11,26 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        S: Chars
     };
+
+    let s = S
+        .into_iter()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+    let mut last_d = 0;
+    let mut ans = 0;
+    for d in s.into_iter().rev() {
+        // push button A
+        ans += 1;
+        // push button B
+        let c = if d < last_d {
+            d + 10 - last_d
+        } else {
+            d - last_d
+        };
+        ans += c;
+        last_d = (last_d + c) % 10;
+    }
+    println!("{}", ans);
 }
