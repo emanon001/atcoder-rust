2e7c373f7ad887df1b2a72b7b372a0b77c8fe551
emanon001 <emanon001@gmail.com>
2026-01-12 23:51:32 +0900

    abc409/b

--- abc409/src/bin/b.rs
+++ abc409/src/bin/b.rs
@@ -8,8 +8,38 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[macro_export]
+macro_rules! chmax {
+    ($ max : expr , $ v : expr ) => {{
+        let v = $v;
+        if $max < v {
+            $max = v;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize,
+        A: [usize; N],
     };
+
+    let mut ans = 0;
+    let counts = A.into_iter().counts().into_iter().sorted();
+    let mut count_sum = 0;
+    for (a, c) in counts {
+        let rest = N - count_sum;
+        if a <= rest {
+            chmax!(ans, a);
+        }
+        if a >= rest {
+            chmax!(ans, rest);
+        }
+        count_sum += c;
+    }
+    println!("{}", ans);
 }
