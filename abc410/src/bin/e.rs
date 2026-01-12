120fa6990276c1b96775619d65002399687dd2fd
emanon001 <emanon001@gmail.com>
2026-01-12 23:56:16 +0900

    bin/old abc410

---
+++ abc410/src/bin/e.rs
@@ -0,0 +1,15 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::*;
+use proconio::input_interactive;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+#[allow(non_snake_case)]
+fn main() {
+    input_interactive! {
+    };
+}
