95f06a3553cb1a4bd6f5e28c5eca81aa3e100412
emanon001 <emanon001@gmail.com>
2026-01-12 23:24:06 +0900

    abc409/a

--- abc409/src/bin/a.rs
+++ abc409/src/bin/a.rs
@@ -11,5 +11,15 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize,
+        T: Chars,
+        A: Chars,
     };
+
+    let is_ok = T
+        .into_iter()
+        .zip(A.into_iter())
+        .any(|(t, a)| t == 'o' && a == 'o');
+    let ans = if is_ok { "Yes" } else { "No" };
+    println!("{}", ans);
 }
