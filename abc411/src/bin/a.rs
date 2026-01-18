98c54a97fe2ccb4edae41a9caf43913cef15f094
emanon001 <emanon001@gmail.com>
2026-01-18 23:36:28 +0900

    abc411/a

--- abc411/src/bin/a.rs
+++ abc411/src/bin/a.rs
@@ -11,5 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        P: Chars,
+        L: usize,
     };
+
+    let is_ok = P.len() >= L;
+    let ans = if is_ok { "Yes" } else { "No" };
+    println!("{}", ans);
 }
