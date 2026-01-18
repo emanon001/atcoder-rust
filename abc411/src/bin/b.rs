a84e9fc4cf8d538286ba15dcf5c52af72c0dba95
emanon001 <emanon001@gmail.com>
2026-01-18 23:43:37 +0900

    abc411/b

--- abc411/src/bin/b.rs
+++ abc411/src/bin/b.rs
@@ -11,5 +11,19 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize,
+        D: [usize; N - 1],
     };
+
+    let mut ans = vec![];
+    for i in 0..N - 1 {
+        let mut v = vec![];
+        let mut d = 0;
+        for j in i..N - 1 {
+            d += D[j];
+            v.push(d);
+        }
+        ans.push(v.into_iter().join(" "));
+    }
+    println!("{}", ans.into_iter().join("\n"));
 }
