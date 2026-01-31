4a4f79ab985b01680ab98803a94dd08c5d62ad54
emanon001 <emanon001@gmail.com>
2026-01-31 23:49:31 +0900

    abc412/b

--- abc412/src/bin/b.rs
+++ abc412/src/bin/b.rs
@@ -11,5 +11,20 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        S: Chars,
+        T: Chars,
     };
+
+    let t_set = T.into_iter().collect::<HashSet<_>>();
+
+    let mut chars = vec![];
+    for i in 0..S.len() - 1 {
+        if S[i + 1].is_uppercase() {
+            chars.push(S[i]);
+        }
+    }
+
+    let is_ok = chars.iter().all(|ch| t_set.contains(ch));
+    let ans = if is_ok { "Yes" } else { "No" };
+    println!("{}", ans);
 }
