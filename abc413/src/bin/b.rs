ab3324e4866449769067948207a66ba5fed7abce
emanon001 <emanon001@gmail.com>
2026-02-01 23:40:35 +0900

    abc413/b

--- abc413/src/bin/b.rs
+++ abc413/src/bin/b.rs
@@ -11,5 +11,19 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize,
+        S: [String; N],
     };
+
+    let mut set = HashSet::new();
+    for i in 0..N {
+        for j in i + 1..N {
+            let s1 = format!("{}{}", S[i], S[j]);
+            set.insert(s1);
+            let s2 = format!("{}{}", S[j], S[i]);
+            set.insert(s2);
+        }
+    }
+    let ans = set.len();
+    println!("{}", ans);
 }
