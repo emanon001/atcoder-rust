2b3e35f459a54077ebee6cc7bbd314c8be83dbeb
emanon001 <emanon001@gmail.com>
2026-02-01 23:38:13 +0900

    abc413/a

--- abc413/src/bin/a.rs
+++ abc413/src/bin/a.rs
@@ -11,5 +11,11 @@ use std::collections::*;
 #[allow(non_snake_case)]
 fn main() {
     input_interactive! {
+        N: usize, M: usize,
+        A: [usize; N]
     };
+
+    let is_ok = A.into_iter().sum::<usize>() <= M;
+    let ans = if is_ok { "Yes" } else { "No" };
+    println!("{}", ans);
 }
